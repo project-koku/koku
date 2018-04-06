e465ea2cbee37f7c7d404803397964859e4589d0
Brett Lentz <blentz@redhat.com>
2018-04-06 14:21:57 -0400

    initial django skeleton

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # SaltCellar
+
+### Getting Started
+
+To deploy a new app on OpenShift Container Platform:
+
+```
+git clone https://github.com/SaltCellar/SaltCellar
+cd SaltCellar
+oc new-app openshift/templates/django-postgresql-persistent.json --code=https://github.com/SaltCellar/SaltCellar
+```
+
+### Contributing
+
+See [CONTRIBUTING.md](CONTRIBUTING.md)
---
+++ saltcellar/database.py
@@ -0,0 +1,29 @@
+import os
+
+from django.conf import settings
+
+
+engines = {
+    'sqlite': 'django.db.backends.sqlite3',
+    'postgresql': 'django.db.backends.postgresql_psycopg2',
+    'mysql': 'django.db.backends.mysql',
+}
+
+
+def config():
+    service_name = os.getenv('DATABASE_SERVICE_NAME', '').upper().replace('-', '_')
+    if service_name:
+        engine = engines.get(os.getenv('DATABASE_ENGINE'), engines['sqlite'])
+    else:
+        engine = engines['sqlite']
+    name = os.getenv('DATABASE_NAME')
+    if not name and engine == engines['sqlite']:
+        name = os.path.join(settings.BASE_DIR, 'db.sqlite3')
+    return {
+        'ENGINE': engine,
+        'NAME': name,
+        'USER': os.getenv('DATABASE_USER'),
+        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+        'HOST': os.getenv('{}_SERVICE_HOST'.format(service_name)),
+        'PORT': os.getenv('{}_SERVICE_PORT'.format(service_name)),
+    }
