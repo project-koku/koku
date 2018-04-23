c28bd9ee92564b99dab330da76a260c989598a16
Brett Lentz <blentz@redhat.com>
2018-04-23 10:36:58 -0400

    add openshift support for dev/test

--- Makefile
+++ Makefile
@@ -3,6 +3,17 @@ PYTHON	= $(shell which python)
 TOPDIR  = $(shell pwd)
 PYDIR	= saltcellar
 
+OC_SOURCE	= registry.access.redhat.com/openshift3/ose
+OC_VERSION	= v3.7
+OC_DATA_DIR	= ${HOME}/.oc/openshift.local.data
+
+OS := $(shell uname)
+ifeq ($(OS),Darwin)
+	PREFIX	=
+else
+	PREFIX	= sudo
+endif
+
 help:
 	@echo "Please use \`make <target>' where <target> is one of:"
 	@echo "  clean                    to clean the project directory of any scratch files, bytecode, logs, etc."
@@ -45,9 +56,23 @@ stop-compose:
 	docker-compose down
 
 unittest:
-	$(PYTHON) $(PYDIR)/manage.py test
+	$(PYTHON) $(PYDIR)/manage.py test $(PYDIR)
 
 user:
 	$(PYTHON) $(PYDIR)/manage.py createsuperuser
+
+oc-up:
+	oc cluster up \
+		--image=$(OC_SOURCE) \
+		--version=$(OC_VERSION) \
+		--host-data-dir=$(OC_DATA_DIR) \
+		--use-existing-config=true
+	./init-app.sh
+
+oc-down:
+	oc cluster down
+
+oc-clean: oc-down
+	$(PREFIX) rm -rf $(OC_DATA_DIR)
 
 .PHONY: docs
---
+++ saltcellar/saltcellar/env.py
@@ -0,0 +1,35 @@
+#
+# Copyright 2018 Red Hat, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+"""Obtain project environment."""
+
+import environ
+
+ROOT_DIR = environ.Path(__file__) - 2
+
+ENVIRONMENT = environ.Env()
+
+# .env file, should load only in development environment
+READ_DOT_ENV_FILE = ENVIRONMENT.bool('DJANGO_READ_DOT_ENV_FILE', default=False)
+
+if READ_DOT_ENV_FILE:
+    # Operating System Environment variables have precedence over variables
+    # defined in the .env file, that is to say variables from the .env files
+    # will only be used if not defined as environment variables.
+    ENV_FILE = str(ROOT_DIR.path('.env'))
+    print('Loading : {}'.format(ENV_FILE))
+    ENVIRONMENT.read_env(ENV_FILE)
+    print('The .env file has been loaded.')
