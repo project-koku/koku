f8ec4b9e99a28587bf0675c776106bdd5bbe45d2
Chris Hambridge <chambrid@redhat.com>
2018-04-23 11:41:01 -0400

    Create initial Open API swagger YAML. Closes #40. (#57)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -53,6 +53,7 @@ extensions = [
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
+    'sphinxcontrib.openapi',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
