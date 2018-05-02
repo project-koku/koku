bfabc7d0049a290aa19ccd1684e0c3d43fe2903a
Andrew Berglund <aberglun@redhat.com>
2018-05-02 13:13:14 -0400

    Var name tweak

--- koku/api/status/model.py
+++ koku/api/status/model.py
@@ -95,7 +95,7 @@ class Status(models.Model):
         logger.info('Python: %s', self.python_version)
         module_list = []
         for mod, version in self.modules.items():
-            module_list.append('{m} - {v}'.format(m=mod, v=version))
+            module_list.append('{mod} - {ver}'.format(mod=mod, ver=version))
         if module_list:
             logger.info('Modules: %s', ', '.join(module_list))
         else:
