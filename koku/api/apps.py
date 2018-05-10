9e2a660902a65a8e1e3fedd0b4b0febfec44d3fa
Chris Hambridge <chambrid@redhat.com>
2018-05-10 11:10:42 -0400

    Create service admin on application startup. Closes #121.

--- koku/api/apps.py
+++ koku/api/apps.py
@@ -14,8 +14,65 @@
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
+"""API application configuration module"""
+
+import logging
+
 from django.apps import AppConfig
+
+from koku.env import ENVIRONMENT
+
+
+logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
 
 
 class ApiConfig(AppConfig):
+    """API application configuration"""
+
     name = 'api'
+
+    def ready(self):
+        """Action on application startup."""
+        self.startup_status()
+        self.check_and_create_service_admin()
+
+    def startup_status(self):  # pylint: disable=R0201
+        """Log the status of the server at startup."""
+        # noqa: E402 pylint: disable=C0413
+        from api.status.model import Status
+        status_info = None
+        status_count = Status.objects.count()
+        if status_count == 0:
+            status_info = Status.objects.create()
+            status_info.save()
+        else:
+            status_info = Status.objects.get(pk=1)
+
+        status_info.startup()
+
+    def create_service_admin(self, service_email):  # pylint: disable=R0201
+        """Create the Service Admin."""
+        # noqa: E402 pylint: disable=C0413
+        from django.contrib.auth.models import User
+        service_user = ENVIRONMENT.get_value('SERVICE_ADMIN_USER',
+                                             default='admin')
+        service_pass = ENVIRONMENT.get_value('SERVICE_ADMIN_PASSWORD',
+                                             default='pass')
+
+        User.objects.create_superuser(service_user,
+                                      service_email,
+                                      service_pass)
+        logger.info('Created Service Admin: %s.', service_email)
+
+    def check_and_create_service_admin(self):  # pylint: disable=R0201
+        """Check for the service admin and create it if necessary."""
+        # noqa: E402 pylint: disable=C0413
+        from django.contrib.auth.models import User
+        service_email = ENVIRONMENT.get_value('SERVICE_ADMIN_EMAIL',
+                                              default='admin@example.com')
+        admin_not_present = User.objects.filter(
+            email=service_email).count() == 0
+        if admin_not_present:
+            self.create_service_admin(service_email)
+        else:
+            logger.info('Service Admin: %s.', service_email)
