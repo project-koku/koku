f8abcf681e17dff6af088377ead3288f6e1b1fbc
Chris Hambridge <chambrid@redhat.com>
2018-05-21 15:59:05 -0400

    Add model and serializers for creating a provider. Closes #163.

---
+++ koku/api/provider/test/tests_serializers.py
@@ -0,0 +1,105 @@
+#
+# Copyright 2018 Red Hat, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+"""Test the Provider serializers."""
+import uuid
+from unittest.mock import Mock
+
+from rest_framework import serializers
+
+from api.iam.serializers import CustomerSerializer
+from api.iam.test.iam_test_case import IamTestCase
+from api.provider.serializers import ProviderSerializer
+
+
+class ProviderSerializerTest(IamTestCase):
+    """Tests for the customer serializer."""
+
+    def test_create_provider_fails_user(self):
+        """Test creating a provider fails with no user."""
+        provider = {'name': 'test_provider',
+                    'authentication': {
+                        'provider_resource_name': 'arn:aws:s3:::my_s3_bucket'
+                    },
+                    'billing_source': {
+                        'bucket': 'my_s3_bucket'
+                    }}
+        serializer = ProviderSerializer(data=provider)
+        if serializer.is_valid(raise_exception=True):
+            with self.assertRaises(serializers.ValidationError):
+                serializer.save()
+
+    def test_create_provider_fails_group(self):  # pylint: disable=C0103
+        """Test creating a provider where group is not found for user."""
+        provider = {'name': 'test_provider',
+                    'authentication': {
+                        'provider_resource_name': 'arn:aws:s3:::my_s3_bucket'
+                    },
+                    'billing_source': {
+                        'bucket': 'my_s3_bucket'
+                    }}
+        request = Mock()
+        request.user = Mock()
+        request.user.groups.count.return_value = 0
+        context = {'request': request}
+        serializer = ProviderSerializer(data=provider, context=context)
+        if serializer.is_valid(raise_exception=True):
+            with self.assertRaises(serializers.ValidationError):
+                serializer.save()
+
+    def test_create_provider_fails_customer(self):  # pylint: disable=C0103
+        """Test creating a provider where customer is not found for user."""
+        provider = {'name': 'test_provider',
+                    'authentication': {
+                        'provider_resource_name': 'arn:aws:s3:::my_s3_bucket'
+                    },
+                    'billing_source': {
+                        'bucket': 'my_s3_bucket'
+                    }}
+        request = Mock()
+        request.user = Mock()
+        group = Mock()
+        group.id = 1
+        request.user.groups.count.return_value = 1
+        request.user.groups.first.return_value = group
+        context = {'request': request}
+        serializer = ProviderSerializer(data=provider, context=context)
+        if serializer.is_valid(raise_exception=True):
+            with self.assertRaises(serializers.ValidationError):
+                serializer.save()
+
+    def test_create_provider(self):
+        """Test creating a provider."""
+        provider = {'name': 'test_provider',
+                    'authentication': {
+                        'provider_resource_name': 'arn:aws:s3:::my_s3_bucket'
+                    },
+                    'billing_source': {
+                        'bucket': 'my_s3_bucket'
+                    }}
+        new_cust = None
+        serializer = CustomerSerializer(data=self.customer_data[0])
+        if serializer.is_valid(raise_exception=True):
+            new_cust = serializer.save()
+        request = Mock()
+        request.user = new_cust.owner
+        context = {'request': request}
+        instance = None
+        serializer = ProviderSerializer(data=provider, context=context)
+        if serializer.is_valid(raise_exception=True):
+            instance = serializer.save()
+
+        self.assertIsInstance(instance.uuid, uuid.UUID)
