f8d480a7350727014572ea0027499fc1ad040536
Andrew Berglund <aberglun@redhat.com>
2018-06-12 12:59:20 -0400

    Initial commit for script to create test db for masu

---
+++ scripts/create_test_customer.py
@@ -0,0 +1,137 @@
+import argparse
+import os
+
+import psycopg2
+import requests
+
+class TestCustomer:
+
+    def __init__(self):
+        self.customer_name = 'Test Customer'
+        self.user_name = 'test_customer'
+        self.email = 'test@example.com'
+        self.password = 'str0ng!P@ss'
+
+
+class KokuCustomerOnboarder:
+    """Uses the Koku API to create an onboarded customer."""
+
+    def __init__(self, conn, host, port):
+        self.conn = conn
+        self.host = host
+        self.port = port
+        self.customer = TestCustomer()
+        self.endpoint_base = f'http://{self.host}:{self.port}'
+        self.token = self.get_token('admin', 'pass')
+        self.headers = {'Authorization': f'Token {self.token}'}
+
+    def onboard(self):
+        self.created_customer = self.create_customer()
+        # self.token = self.get_token(
+        #     self.customer.user_name,
+        #     self.customer.password
+        # )
+        # self.headers = {'Authorization': f'Token {self.token}'}
+        self.provider = self.create_provider()
+
+    def get_token(self, user_name, password):
+        endpoint = self.endpoint_base + '/api/v1/token-auth/'
+        data = {'username': user_name , 'password': password}
+        token = requests.post(endpoint, data=data).json().get('token')
+        print(f'Acquired token {token}')
+        return token
+
+    def create_customer(self):
+        endpoint = self.endpoint_base + '/api/v1/customers/'
+        data = {
+            'name': self.customer.customer_name,
+            'owner': {
+                    'username': self.customer.user_name,
+                    'email': self.customer.email,
+                    'password': self.customer.password
+            }
+        }
+
+        response = requests.post(
+            endpoint,
+            headers=self.headers,
+            json=data
+        )
+        print(response.text)
+        return response
+
+    def create_provider(self):
+        cursor = self.conn.cursor()
+        auth_sql = """
+            INSERT INTO api_providerauthentication (uuid, provider_resource_name)
+                VALUES ('7e4ec31b-7ced-4a17-9f7e-f77e9efa8fd6', 'arn:aws:iam::111111111111:role/CostManagement')
+            ;
+        """
+
+        cursor.execute(auth_sql)
+        print('Created provider authentication')
+
+        # cursor.execute("SELECT id FROM api_providerauthentication WHERE provider_resource_name='arn:aws:iam::111111111111:role/CostManagement'")
+
+        # result = cursor.fetchone()
+
+        billing_sql = """
+            INSERT INTO api_providerbillingsource (uuid, bucket)
+                VALUES ('75b17096-319a-45ec-92c1-18dbd5e78f94', 'test-bucket')
+            ;
+        """
+
+        cursor.execute(billing_sql)
+        print('Created provider billing source')
+
+        provider_sql = """
+            INSERT INTO api_provider (uuid, name, type, authentication_id, billing_source_id, created_by_id, customer_id)
+                VALUES('6e212746-484a-40cd-bba0-09a19d132d64', 'Test Provider', 'AWS', 1, 1, 2, 1)
+            ;
+        """
+
+        cursor.execute(provider_sql)
+        print('Created provider')
+
+        self.conn.commit()
+
+        # endpoint = self.endpoint_base + '/api/v1/providers/'
+        # data = {
+        #     "name": "Test Provider",
+        #     "type": "AWS",
+        #     "uuid": "6e212746-484a-40cd-bba0-09a19d132d64",
+        #     "authentication": {
+        #             "uuid": "7e4ec31b-7ced-4a17-9f7e-f77e9efa8fd6",
+        #             "provider_resource_name": "arn:aws:iam::589173575777:role/CostManagement"
+        #     },
+        #     "billing_source": {
+        #             "uuid": "75b17096-319a-45ec-92c1-18dbd5e78f94",
+        #             "bucket": "test-bucket"
+        #     }
+        # }
+
+        # response = requests.post(
+        #     endpoint,
+        #     headers=self.headers,
+        #     json=data
+        # )
+        # print(response.text)
+        # return response
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('host')
+    parser.add_argument('port')
+    args = vars(parser.parse_args())
+
+    db_name = os.getenv('DATABASE_NAME')
+    db_host = os.getenv('POSTGRES_SQL_SERVICE_HOST')
+    db_port = os.getenv('POSTGRES_SQL_SERVICE_PORT')
+    db_user = os.getenv('DATABASE_USER')
+    db_password = os.getenv('DATABASE_PASSWORD')
+    conn = psycopg2.connect(database=db_name, user=db_user,
+                            password=db_password, port=db_port, host=db_host)
+    onboarder = KokuCustomerOnboarder(conn, **args)
+    onboarder.onboard()
+    conn.close()
