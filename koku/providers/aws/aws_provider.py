2eb7f992dcd6043de4bed0e7348c722d7f8aada9
Douglas Curtis <docurtis@redhat.com>
2018-06-14 09:36:21 -0400

    Create a Provider Interface for Koku (#218)

---
+++ koku/providers/aws/aws_provider.py
@@ -0,0 +1,132 @@
+#
+# Copyright 2018 Red Hat, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+"""Amazon Web services provider implementation to be used by Koku."""
+import logging
+
+import boto3
+from botocore.exceptions import ClientError, NoCredentialsError, ParamValidationError
+from django.utils.translation import ugettext as _
+from requests.exceptions import ConnectionError as BotoConnectionError
+from rest_framework import serializers   # meh
+
+from ..provider_interface import ProviderInterface
+
+LOG = logging.getLogger(__name__)
+
+
+def error_obj(key, message):
+    """Create an error object."""
+    error = {
+        key: [_(message)]
+    }
+    return error
+
+
+def _get_sts_access(provider_resource_name):
+    """Get for sts access."""
+    access_key_id = None
+    secret_access_key = None
+    session_token = None
+    # create an STS client
+    sts_client = boto3.client('sts')
+
+    try:
+        # Call the assume_role method of the STSConnection object and pass the role
+        # ARN and a role session name.
+        assumed_role = sts_client.assume_role(
+            RoleArn=provider_resource_name,
+            RoleSessionName='AccountCreationSession'
+        )
+        credentials = assumed_role.get('Credentials')
+        if credentials:
+            access_key_id = credentials.get('AccessKeyId')
+            secret_access_key = credentials.get('SecretAccessKey')
+            session_token = credentials.get('SessionToken')
+    except (ClientError, BotoConnectionError, NoCredentialsError, ParamValidationError) as boto_error:
+        LOG.exception(boto_error)
+
+    return (access_key_id, secret_access_key, session_token)
+
+
+def _check_s3_access(access_key_id, secret_access_key,
+                     session_token, bucket):
+    """Check for access to s3 bucket."""
+    s3_exists = True
+    s3_resource = boto3.resource(
+        's3',
+        aws_access_key_id=access_key_id,
+        aws_secret_access_key=secret_access_key,
+        aws_session_token=session_token,
+    )
+    try:
+        s3_resource.meta.client.head_bucket(Bucket=bucket)
+    except (ClientError, BotoConnectionError) as boto_error:
+        LOG.exception(boto_error)
+        s3_exists = False
+    return s3_exists
+
+
+def _check_org_access(access_key_id, secret_access_key, session_token):
+    """Check for provider organization access."""
+    access_ok = True
+    org_client = boto3.client(
+        'organizations',
+        aws_access_key_id=access_key_id,
+        aws_secret_access_key=secret_access_key,
+        aws_session_token=session_token,
+    )
+    try:
+        org_client.describe_organization()
+    except (ClientError, BotoConnectionError) as boto_error:
+        LOG.exception(boto_error)
+        access_ok = False
+    return access_ok
+
+
+class AWSProvider(ProviderInterface):
+    """Provider interface defnition."""
+
+    def name(self):
+        """Return name of the provider."""
+        return 'AWS'
+
+    def cost_usage_source_is_reachable(self, credential_name, storage_resource_name):
+        """Verify that the S3 bucket exists and is reachable."""
+        access_key_id, secret_access_key, session_token = _get_sts_access(
+            credential_name)
+        if (access_key_id is None or access_key_id is None or
+                session_token is None):
+            key = 'provider_resource_name'
+            message = 'Unable to obtain credentials with using {}.'.format(
+                credential_name)
+            raise serializers.ValidationError(error_obj(key, message))
+
+        s3_exists = _check_s3_access(access_key_id, secret_access_key,
+                                     session_token, storage_resource_name)
+        if not s3_exists:
+            key = 'bucket'
+            message = 'Bucket {} could not be found with {}.'.format(
+                storage_resource_name, credential_name)
+            raise serializers.ValidationError(error_obj(key, message))
+
+        org_access = _check_org_access(access_key_id, secret_access_key,
+                                       session_token)
+        if not org_access:
+            key = 'provider_resource_name'
+            message = 'Unable to obtain organization data with {}.'.format(
+                credential_name)
+            LOG.info(message)
---
+++ koku/providers/test/aws/tests_aws_provider.py
@@ -0,0 +1,164 @@
+#
+# Copyright 2018 Red Hat, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+"""Tests the AWSProvider implementation for the Koku interface."""
+
+from unittest.mock import Mock, patch
+
+import boto3
+from botocore.auth import SigV4Auth
+from botocore.exceptions import NoCredentialsError
+from django.test import TestCase
+from moto import mock_s3, mock_sts
+from providers.aws.aws_provider import AWSProvider, _check_org_access, _get_sts_access
+from rest_framework.exceptions import ValidationError
+
+
+class AWSProviderTestCase(TestCase):
+    """Parent Class for AWSProvider test cases."""
+
+    def setup(self):
+        """Create test case objects."""
+        pass
+
+    def tearDown(self):
+        """Tear down test case objects."""
+        pass
+
+    def test_get_name(self):
+        """Get name of provider."""
+        provider = AWSProvider()
+        self.assertEqual(provider.name(), 'AWS')
+
+    @mock_sts
+    @mock_s3
+    @patch('providers.aws.aws_provider._check_org_access')
+    def test_cost_usage_source_is_reachable(self, check_org_access):
+        """Verify that the cost usage source is authenticated and created."""
+        check_org_access.return_value = True
+
+        iam_arn = 'arn:aws:s3:::my_s3_bucket'
+        bucket_name = 'my_s3_bucket'
+        access_key_id, secret_access_key, session_token = _get_sts_access(
+            iam_arn)
+        s3_resource = boto3.resource(
+            's3',
+            aws_access_key_id=access_key_id,
+            aws_secret_access_key=secret_access_key,
+            aws_session_token=session_token,
+        )
+        s3_resource.create_bucket(Bucket=bucket_name)
+
+        provider_interface = AWSProvider()
+
+        try:
+            provider_interface.cost_usage_source_is_reachable(iam_arn, bucket_name)
+        except Exception:
+            self.fail('Unexpected Error')
+
+    @patch('providers.aws.aws_provider._get_sts_access')
+    def test_provider_sts_fail(self, get_sts_access):
+        """Test creating a provider with AWS STS failure."""
+        get_sts_access.return_value = (None, None, None)
+        iam_arn = 'arn:aws:s3:::my_s3_bucket'
+        bucket_name = 'my_s3_bucket'
+
+        provider_interface = AWSProvider()
+        with self.assertRaises(ValidationError):
+            provider_interface.cost_usage_source_is_reachable(iam_arn, bucket_name)
+
+    @mock_sts
+    @patch('providers.aws.aws_provider._check_s3_access')
+    def test_provider_s3_fail(self, check_s3_access):
+        """Test creating a provider with AWS s3 bucket doesn't exist."""
+        check_s3_access.return_value = False
+        iam_arn = 'arn:aws:s3:::my_s3_bucket'
+        bucket_name = 'my_s3_bucket'
+
+        provider_interface = AWSProvider()
+        with self.assertRaises(ValidationError):
+            provider_interface.cost_usage_source_is_reachable(iam_arn, bucket_name)
+
+    @mock_sts
+    @mock_s3
+    @patch('providers.aws.aws_provider._check_org_access')
+    def test_provider_org_fail(self, check_org_access):
+        """Test creating a provider with AWS org access failure."""
+        check_org_access.return_value = False
+        iam_arn = 'arn:aws:s3:::my_s3_bucket'
+        bucket_name = 'my_s3_bucket'
+
+        provider_interface = AWSProvider()
+        with self.assertRaises(ValidationError):
+            provider_interface.cost_usage_source_is_reachable(iam_arn, bucket_name)
+
+        access_key_id, secret_access_key, session_token = _get_sts_access(
+            iam_arn)
+        s3_resource = boto3.resource(
+            's3',
+            aws_access_key_id=access_key_id,
+            aws_secret_access_key=secret_access_key,
+            aws_session_token=session_token,
+        )
+        s3_resource.create_bucket(Bucket=bucket_name)
+
+        provider_interface = AWSProvider()
+        try:
+            provider_interface.cost_usage_source_is_reachable(iam_arn, bucket_name)
+        except Exception:
+            self.fail('Unexpected error thrown')
+
+    def test_no_credential_error(self):
+        """Test attempting STS access where no credentials are found."""
+        with patch.object(SigV4Auth, 'add_auth', side_effect=NoCredentialsError):
+            iam_arn = 'arn:aws:s3:::my_s3_bucket'
+            bucket_name = 'my_s3_bucket'
+
+            provider_interface = AWSProvider()
+            with self.assertRaises(ValidationError):
+                provider_interface.cost_usage_source_is_reachable(iam_arn, bucket_name)
+
+    @patch('boto3.client')
+    def test_get_sts_access_no_cred(self, mock_boto3):
+        """Test _get_sts_access with no credentials."""
+        client = Mock()
+        client.assume_role.return_value = {}
+        mock_boto3.return_value = client
+        iam_arn = 'arn:aws:s3:::my_s3_bucket'
+        access_key_id, secret_access_key, session_token = _get_sts_access(
+            iam_arn)
+        self.assertIsNone(access_key_id)
+        self.assertIsNone(secret_access_key)
+        self.assertIsNone(session_token)
+
+    def test_invalid_roleARN(self):
+        """Test _get_sts_access with an invalid RoleARN."""
+        iam_arn = 'toosmall'
+        bucket_name = 'my_s3_bucket'
+
+        provider_interface = AWSProvider()
+        with self.assertRaises(ValidationError):
+            provider_interface.cost_usage_source_is_reachable(iam_arn, bucket_name)
+
+    @mock_sts
+    def test_check_org_access_fail(self):
+        """Test _check_org_access with boto exception."""
+        iam_arn = 'arn:aws:s3:::my_s3_bucket'
+        access_key_id, secret_access_key, session_token = _get_sts_access(
+            iam_arn)
+        access_exists = _check_org_access(access_key_id, secret_access_key,
+                                          session_token)
+        self.assertFalse(access_exists)
