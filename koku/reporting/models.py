85cf302df94e1305379a197daa6cff795ac2b8e3
Andrew Berglund <aberglun@redhat.com>
2018-06-15 13:26:30 -0400

    Updates to the reporting models to support report processing

--- koku/reporting/models.py
+++ koku/reporting/models.py
@@ -63,75 +63,75 @@ class AWSCostEntryLineItem(models.Model):
     cost_entry_bill = models.ForeignKey('AWSCostEntryBill',
                                         on_delete=models.PROTECT)
     cost_entry_product = models.ForeignKey('AWSCostEntryProduct',
-                                           on_delete=models.PROTECT)
+                                           on_delete=models.PROTECT, null=True)
     cost_entry_pricing = models.ForeignKey('AWSCostEntryPricing',
-                                           on_delete=models.PROTECT)
+                                           on_delete=models.PROTECT, null=True)
     cost_entry_reservation = models.ForeignKey('AWSCostEntryReservation',
-                                               on_delete=models.PROTECT)
+                                               on_delete=models.PROTECT,
+                                               null=True)
 
     # There is a many-to-many relationship between line-items and tags.
     # Want to try JSON to avoid having to check if tags exist in the database
     # for every line item we add.
-    tags = JSONField()
+    tags = JSONField(null=True)
 
     # Invoice ID is null until the bill is finalized
-    invoice_id = models.CharField(max_length=63)
+    invoice_id = models.CharField(max_length=63, null=True)
     line_item_type = models.CharField(max_length=50, null=False)
     usage_account_id = models.CharField(max_length=50, null=False)
     usage_start = models.DateTimeField(null=False)
     usage_end = models.DateTimeField(null=False)
     product_code = models.CharField(max_length=50, null=False)
-    usage_type = models.CharField(max_length=50, null=False)
-    operation = models.CharField(max_length=50)
-    availability_zone = models.CharField(max_length=50)
-    resource_id = models.CharField(max_length=50)
-    usage_amount = models.PositiveIntegerField()
-    normalization_factor = models.PositiveIntegerField()
-    normalized_usage_amount = models.PositiveIntegerField()
+    usage_type = models.CharField(max_length=50, null=True)
+    operation = models.CharField(max_length=50, null=True)
+    availability_zone = models.CharField(max_length=50, null=True)
+    resource_id = models.CharField(max_length=256, null=True)
+    usage_amount = models.PositiveIntegerField(null=True)
+    normalization_factor = models.PositiveIntegerField(null=True)
+    normalized_usage_amount = models.PositiveIntegerField(null=True)
     currency_code = models.CharField(max_length=10)
-    unblended_rate = models.FloatField()
-    unblended_cost = models.FloatField()
-    blended_rate = models.FloatField()
-    blended_cost = models.FloatField()
-    tax_type = models.TextField()
+    unblended_rate = models.FloatField(null=True)
+    unblended_cost = models.FloatField(null=True)
+    blended_rate = models.FloatField(null=True)
+    blended_cost = models.FloatField(null=True)
+    tax_type = models.TextField(null=True)
 
 
 class AWSCostEntryPricing(models.Model):
     """Pricing information for a cost entry line item."""
 
-    line_item = models.ForeignKey('AWSCostEntryLineItem',
-                                  on_delete=models.PROTECT)
     public_on_demand_cost = models.FloatField()
     public_on_demand_rate = models.FloatField()
-    term = models.CharField(max_length=50)
-    unit = models.CharField(max_length=24)
+    term = models.CharField(max_length=63, null=True)
+    unit = models.CharField(max_length=63, null=True)
 
 
 class AWSCostEntryProduct(models.Model):
     """The AWS product identified in a cost entry line item."""
 
     # AWS unique identifier for the product
-    sku = models.CharField(max_length=128, null=False, unique=True)
-    product_name = models.CharField(max_length=63, null=False)
-    product_family = models.CharField(max_length=150)
-    service_code = models.CharField(max_length=50)
-    region = models.CharField(max_length=50)
+    sku = models.CharField(max_length=128, null=True, unique=True)
+    product_name = models.CharField(max_length=63, null=True)
+    product_family = models.CharField(max_length=150, null=True)
+    service_code = models.CharField(max_length=50, null=True)
+    region = models.CharField(max_length=50, null=True)
     # The following fields are useful for EC2 instances
-    instance_type = models.CharField(max_length=50)
-    memory = models.PositiveIntegerField()
-    vcpu = models.PositiveIntegerField()
+    instance_type = models.CharField(max_length=50, null=True)
+    memory = models.PositiveIntegerField(null=True)
+    memory_unit = models.CharField(max_length=24, null=True)
+    vcpu = models.PositiveIntegerField(null=True)
 
 
 class AWSCostEntryReservation(models.Model):
     """Information on a particular reservation in the AWS account."""
 
     reservation_arn = models.TextField(unique=True)
-    availability_zone = models.CharField(max_length=50)
-    number_of_reservations = models.PositiveIntegerField()
-    units_per_reservation = models.PositiveIntegerField()
-    amortized_upfront_fee = models.FloatField()
-    amortized_upfront_cost_for_usage = models.FloatField()
-    recurring_fee_for_usage = models.FloatField()
+    availability_zone = models.CharField(max_length=50, null=True)
+    number_of_reservations = models.PositiveIntegerField(null=True)
+    units_per_reservation = models.PositiveIntegerField(null=True)
+    amortized_upfront_fee = models.FloatField(null=True)
+    amortized_upfront_cost_for_usage = models.FloatField(null=True)
+    recurring_fee_for_usage = models.FloatField(null=True)
     # Unused fields more useful for later predictions.
-    unused_quantity = models.PositiveIntegerField()
-    unused_recurring_fee = models.FloatField()
+    unused_quantity = models.PositiveIntegerField(null=True)
+    unused_recurring_fee = models.FloatField(null=True)
