6e0d2ddca665c8d9da4899a45bd782520c910929
Chris Hambridge <chambrid@redhat.com>
2018-06-25 20:49:13 -0400

    Update policy and check ability to obtain AWS cost usage report definitions. Closes #239. (#243)

--- koku/providers/test/aws/tests_aws_provider.py
+++ koku/providers/test/aws/tests_aws_provider.py
@@ -20,11 +20,19 @@ from unittest.mock import Mock, patch
 
 import boto3
 from botocore.auth import SigV4Auth
-from botocore.exceptions import NoCredentialsError
+from botocore.exceptions import ClientError, NoCredentialsError
 from django.test import TestCase
 from moto import mock_s3, mock_sts
-from providers.aws.aws_provider import AWSProvider, _check_org_access, _get_sts_access
+from providers.aws.aws_provider import (AWSProvider,
+                                        _check_cost_report_access,
+                                        _check_org_access,
+                                        _get_sts_access)
 from rest_framework.exceptions import ValidationError
+
+
+def _mock_boto3_exception():
+    """Raise boto3 exception for testing."""
+    raise ClientError(operation_name='', error_response={})
 
 
 class AWSProviderTestCase(TestCase):
@@ -46,9 +54,11 @@ class AWSProviderTestCase(TestCase):
     @mock_sts
     @mock_s3
     @patch('providers.aws.aws_provider._check_org_access')
-    def test_cost_usage_source_is_reachable(self, check_org_access):
+    @patch('providers.aws.aws_provider._check_cost_report_access')
+    def test_cost_usage_source_is_reachable(self, check_org_access, check_cost_report_access):
         """Verify that the cost usage source is authenticated and created."""
         check_org_access.return_value = True
+        check_cost_report_access.return_value = True
 
         iam_arn = 'arn:aws:s3:::my_s3_bucket'
         bucket_name = 'my_s3_bucket'
@@ -94,10 +104,10 @@ class AWSProviderTestCase(TestCase):
 
     @mock_sts
     @mock_s3
-    @patch('providers.aws.aws_provider._check_org_access')
-    def test_provider_org_fail(self, check_org_access):
-        """Test creating a provider with AWS org access failure."""
-        check_org_access.return_value = False
+    @patch('providers.aws.aws_provider._check_cost_report_access')
+    def test_provider_cur_fail(self, check_cost_report_access):
+        """Test creating a provider with AWS cost report access failure."""
+        check_cost_report_access.return_value = False
         iam_arn = 'arn:aws:s3:::my_s3_bucket'
         bucket_name = 'my_s3_bucket'
 
@@ -105,6 +115,29 @@ class AWSProviderTestCase(TestCase):
         with self.assertRaises(ValidationError):
             provider_interface.cost_usage_source_is_reachable(iam_arn, bucket_name)
 
+        access_key_id, secret_access_key, session_token = _get_sts_access(
+            iam_arn)
+        s3_resource = boto3.resource(
+            's3',
+            aws_access_key_id=access_key_id,
+            aws_secret_access_key=secret_access_key,
+            aws_session_token=session_token,
+        )
+        s3_resource.create_bucket(Bucket=bucket_name)
+
+        provider_interface = AWSProvider()
+        with self.assertRaises(ValidationError):
+            provider_interface.cost_usage_source_is_reachable(iam_arn, bucket_name)
+
+    @mock_sts
+    @mock_s3
+    @patch('providers.aws.aws_provider._check_org_access', return_value=False)
+    @patch('providers.aws.aws_provider._check_cost_report_access', return_value=True)
+    @patch('providers.aws.aws_provider._check_s3_access', return_value=True)
+    def test_provider_org_fail(self, check_org_access, check_cost_report_access, check_s3_access):
+        """Test creating a provider with AWS org access failure."""
+        iam_arn = 'arn:aws:s3:::my_s3_bucket'
+        bucket_name = 'my_s3_bucket'
         access_key_id, secret_access_key, session_token = _get_sts_access(
             iam_arn)
         s3_resource = boto3.resource(
@@ -161,4 +194,18 @@ class AWSProviderTestCase(TestCase):
             iam_arn)
         access_exists = _check_org_access(access_key_id, secret_access_key,
                                           session_token)
+        self.assertFalse(access_exists)
+
+    @patch('boto3.client')
+    def test_check_cost_report_access_fail(self, mock_boto3):
+        """Test _check_cost_report_access with boto exception."""
+        access_key_id = 'access_key_id'
+        secret_access_key = 'secret_access_key'
+        session_token = 'session_token'
+
+        client = Mock()
+        client.describe_report_definitions.side_effect = _mock_boto3_exception
+        mock_boto3.return_value = client
+        access_exists = _check_cost_report_access(access_key_id, secret_access_key,
+                                                  session_token)
         self.assertFalse(access_exists)
