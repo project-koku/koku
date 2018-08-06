64335abdbc778c0bfe8340eed13cf80090467617
Douglas Curtis <docurtis@redhat.com>
2018-08-06 16:24:37 -0400

    Enable local provider (#126)
    This change will enable masu to download and process AWS reports located in a local directory. The local directory will have the same structure as an S3 bucket and reports can be generated with Nise (project-koku/nise#16). In Koku the local file directory path is to be used as the "S3 bucket name" when using the DEBUG Local provider (project-koku/koku#274).

--- external/downloader/local/local_report_downloader.py
+++ external/downloader/local/local_report_downloader.py
@@ -14,23 +14,21 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
-"""AWS Report Downloader."""
+"""Local Report Downloader."""
 
-# pylint: disable=fixme
-# disabled until we get travis to not fail on warnings, or the fixme is
-# resolved.
+# pylint: skip-file
+# Having trouble disabling the lint warning for duplicate-code (AWSReportDownloader..)
+# Disabling pylint on this file since LocalReportDownloader is a DEBUG feature.
 
+import hashlib
 import json
 import logging
 import os
-
-import boto3
-from botocore.exceptions import ClientError
+import shutil
+from datetime import datetime
 
 from masu.config import Config
 from masu.database.report_stats_db_accessor import ReportStatsDBAccessor
-from masu.exceptions import MasuProviderError
-from masu.external.date_accessor import DateAccessor
 from masu.external.downloader.aws import utils
 from masu.external.downloader.downloader_interface import DownloaderInterface
 from masu.external.downloader.report_downloader_base import ReportDownloaderBase
@@ -39,23 +37,12 @@ DATA_DIR = Config.TMP_DIR
 LOG = logging.getLogger(__name__)
 
 
-class AWSReportDownloaderError(Exception):
-    """AWS Report Downloader error."""
-
-    pass
-
-
-class AWSReportDownloader(ReportDownloaderBase, DownloaderInterface):
-    """
-    AWS Cost and Usage Report Downloader.
-
-    For configuration of AWS, see
-    https://docs.aws.amazon.com/awsaccountbilling/latest/aboutv2/billing-reports-costusage.html
-    """
+class LocalReportDownloader(ReportDownloaderBase, DownloaderInterface):
+    """Local Cost and Usage Report Downloader."""
 
     def __init__(self, customer_name, auth_credential, bucket, report_name=None, **kwargs):
         """
-        Constructor.
+        Initializer.
 
         Args:
             customer_name    (String) Name of the customer
@@ -68,34 +55,14 @@ class AWSReportDownloader(ReportDownloaderBase, DownloaderInterface):
 
         self.customer_name = customer_name.replace(' ', '_')
 
-        LOG.debug('Connecting to AWS...')
-        session = utils.get_assume_role_session(utils.AwsArn(auth_credential),
-                                                'MasuDownloaderSession')
-        self.cur = session.client('cur')
+        LOG.debug('Connecting to local service provider...')
 
-        # fetch details about the report from the cloud provider
-        defs = self.cur.describe_report_definitions()
-        if not report_name:
-            report_names = []
-            for report in defs.get('ReportDefinitions', []):
-                if bucket == report.get('S3Bucket'):
-                    report_names.append(report['ReportName'])
-
-            # FIXME: Get the first report in the bucket until Koku can specify
-            # which report the user wants
-            if report_names:
-                report_name = report_names[0]
-        self.report_name = report_name
-
-        report_defs = defs.get('ReportDefinitions', [])
-        report = [rep for rep in report_defs
-                  if rep['ReportName'] == self.report_name]
-
-        if not report:
-            raise MasuProviderError('Cost and Usage Report definition not found.')
-
-        self.report = report.pop()
-        self.s3_client = session.client('s3')
+        if report_name:
+            self.report_name = report_name
+        else:
+            self.report_name = os.listdir(bucket)[0]
+        self.bucket = bucket
+        self.credential = auth_credential
 
     def _get_manifest(self, date_time):
         """
@@ -110,7 +77,6 @@ class AWSReportDownloader(ReportDownloaderBase, DownloaderInterface):
         """
         manifest = '{}/{}-Manifest.json'.format(self._get_report_path(date_time),
                                                 self.report_name)
-        LOG.info('Will attempt to download manifest: %s', manifest)
 
         manifest_file, _ = self.download_file(manifest)
 
@@ -133,25 +99,9 @@ class AWSReportDownloader(ReportDownloaderBase, DownloaderInterface):
 
         """
         report_date_range = utils.month_date_range(date_time)
-        return '{}/{}/{}'.format(self.report.get('S3Prefix'),
+        return '{}/{}/{}'.format(self.bucket,
                                  self.report_name,
                                  report_date_range)
-
-    def download_bucket(self):
-        """
-        Bulk Download all files in an s3 bucket.
-
-        Returns:
-            (List) List of filenames downloaded.
-
-        """
-        s3_resource = boto3.resource('s3')
-        bucket = s3_resource.Bucket(self.report.get('S3Bucket'))
-        files = []
-        for s3obj in bucket.objects.all():
-            file_name, _ = self.download_file(s3obj.key)
-            files.append(file_name)
-        return files
 
     def download_current_report(self):
         """
@@ -161,7 +111,7 @@ class AWSReportDownloader(ReportDownloaderBase, DownloaderInterface):
             (List) List of filenames downloaded.
 
         """
-        return self.download_report(DateAccessor().today())
+        return self.download_report(datetime.today())
 
     def download_file(self, key, stored_etag=None):
         """
@@ -174,32 +124,20 @@ class AWSReportDownloader(ReportDownloaderBase, DownloaderInterface):
             (String): The path and file name of the saved file
 
         """
-        s3_filename = key.split('/')[-1]
-        directory_path = f'{DATA_DIR}/{self.customer_name}/aws'
-
         local_s3_filename = utils.get_local_file_name(key)
-        LOG.info('Local S3 filename: %s', local_s3_filename)
+
+        directory_path = f'{DATA_DIR}/{self.customer_name}/local'
         full_file_path = f'{directory_path}/{local_s3_filename}'
 
         # Make sure the data directory exists
         os.makedirs(directory_path, exist_ok=True)
-        s3_etag = None
-        try:
-            s3_file = self.s3_client.get_object(Bucket=self.report.get('S3Bucket'), Key=key)
-            s3_etag = s3_file.get('ETag')
-        except ClientError as ex:
-            if ex.response['Error']['Code'] == 'NoSuchKey':
-                log_msg = 'Unable to find {} in S3 Bucket: {}'.format(s3_filename,
-                                                                      self.report.get('S3Bucket'))
-                LOG.error(log_msg)
-                raise AWSReportDownloaderError(log_msg)
-            else:
-                LOG.error('Error downloading file: Error: %s', str(ex))
-                raise AWSReportDownloaderError(str(ex))
+        s3_etag_hasher = hashlib.new('ripemd160')
+        s3_etag_hasher.update(bytes(local_s3_filename, 'utf-8'))
+        s3_etag = s3_etag_hasher.hexdigest()
 
         if s3_etag != stored_etag or not os.path.isfile(full_file_path):
             LOG.info('Downloading %s to %s', key, full_file_path)
-            self.s3_client.download_file(self.report.get('S3Bucket'), key, full_file_path)
+            shutil.copy2(key, full_file_path)
         return full_file_path, s3_etag
 
     def download_report(self, date_time):
@@ -213,7 +151,6 @@ class AWSReportDownloader(ReportDownloaderBase, DownloaderInterface):
             ([{}]) List of dictionaries containing file path and compression.
 
         """
-        LOG.info('Current date is %s.  Attempting to get manifest...', str(date_time))
         manifest = self._get_manifest(date_time)
         reports = manifest.get('reportKeys')
 
@@ -223,13 +160,14 @@ class AWSReportDownloader(ReportDownloaderBase, DownloaderInterface):
             local_s3_filename = utils.get_local_file_name(report)
             stats_recorder = ReportStatsDBAccessor(local_s3_filename)
             stored_etag = stats_recorder.get_etag()
-            file_name, etag = self.download_file(report, stored_etag)
+            report_path = self.bucket + report
+            LOG.info('Downloading %s with credential %s', report_path, self.credential)
+            file_name, etag = self.download_file(report_path, stored_etag)
             stats_recorder.update(etag=etag)
             stats_recorder.commit()
-            stats_recorder.close_session()
 
             report_dictionary['file'] = file_name
-            report_dictionary['compression'] = self.report.get('Compression')
+            report_dictionary['compression'] = 'GZIP'
 
             cur_reports.append(report_dictionary)
         return cur_reports
