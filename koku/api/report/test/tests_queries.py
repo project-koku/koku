43d79a7495df8b1bfbd47b0c4e7c93f6f2dc7726
Andrew Berglund <aberglun@redhat.com>
2018-08-13 13:46:38 -0400

    Fix to not create duplicate pricing rows

--- koku/api/report/test/tests_queries.py
+++ koku/api/report/test/tests_queries.py
@@ -198,9 +198,10 @@ class ReportQueryTest(IamTestCase):
                                              memory=7.5,
                                              vcpu=4)
             ce_product.save()
-            ce_pricing = AWSCostEntryPricing(term='OnDemand',
-                                             unit='Hrs')
-            ce_pricing.save()
+            ce_pricing, _ = AWSCostEntryPricing.objects.get_or_create(
+                term='OnDemand',
+                unit='Hrs'
+            )
             current = yesterday
             while current < this_hour:
                 if current.month == this_month:
