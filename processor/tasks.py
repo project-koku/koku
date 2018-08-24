0cbdc50a7293cb7d9f84bd2e50ce9847c6e67597
Douglas Curtis <docurtis@redhat.com>
2018-08-24 10:59:32 -0400

    Avoid re-processing of CUR files (#162)

--- processor/tasks.py
+++ processor/tasks.py
@@ -22,11 +22,13 @@
 
 
 import os
+from datetime import timedelta
 
 from celery.utils.log import get_task_logger
 
 from masu.celery import celery
 from masu.database.report_stats_db_accessor import ReportStatsDBAccessor
+from masu.external.date_accessor import DateAccessor
 from masu.processor._tasks.download import _get_report_files
 from masu.processor._tasks.process import _process_report_file
 from masu.processor._tasks.remove_expired import _remove_expired_data
@@ -46,6 +48,10 @@ def get_report_files(customer_name,
 
     Note that report_name will be not optional once Koku can specify
     what report we should download.
+
+    FIXME: A 2 hour timeout is arbitrarily set for in progress processing requests.
+    Once we know a realistic processing time for the largest CUR file in production
+    this value can be adjusted or made configurable.
 
     Args:
         customer_name     (String): Name of the customer owning the cost usage report.
@@ -71,23 +77,33 @@ def get_report_files(customer_name,
     # initiate chained async task
     LOG.info('Reports to be processed: %s', str(reports))
     for report_dict in reports:
-        stats = ReportStatsDBAccessor(os.path.basename(report_dict.get('file')))
-        last_start = stats.get_last_started_datetime()
-        last_end = stats.get_last_completed_datetime()
+        file_name = os.path.basename(report_dict.get('file'))
+        stats = ReportStatsDBAccessor(file_name)
+        started_date = stats.get_last_started_datetime()
+        completed_date = stats.get_last_completed_datetime()
         stats.close_session()
 
-        # only queue up a processing job if there's none currently in-progress.
-        if not last_start or not last_end or last_start < last_end:
-            request = {'schema_name': schema_name,
-                       'report_path': report_dict.get('file'),
-                       'compression': report_dict.get('compression')}
-            result = process_report_file.delay(**request)
-            LOG.info('Processing task queued - File: %s, Task ID: %s',
-                     report_dict.get('file'),
-                     str(result))
-        else:
-            LOG.info('Processing task NOT queued. Start time is later than end time! Detail: %s',
-                     str(report_dict))
+        # Skip processing if already in progress.
+        if started_date and not completed_date:
+            expired_start_date = started_date + timedelta(hours=2)
+            if DateAccessor().today() < expired_start_date:
+                LOG.info('Skipping processing task for %s since it was started at: %s.',
+                         file_name, str(started_date))
+                continue
+
+        # Skip processing if complete.
+        if started_date and completed_date:
+            LOG.info('Skipping processing task for %s. Started on: %s and completed on: %s.',
+                     file_name, str(started_date), str(completed_date))
+            continue
+
+        request = {'schema_name': schema_name,
+                   'report_path': report_dict.get('file'),
+                   'compression': report_dict.get('compression')}
+        result = process_report_file.delay(**request)
+        LOG.info('Processing task queued - File: %s, Task ID: %s',
+                 report_dict.get('file'),
+                 str(result))
 
 
 @celery.task(name='masu.processor.tasks.process_report_file', queue_name='process')
