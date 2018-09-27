cf79df8e066937819f71f9f68156a8f7550ced2e
Andrew Berglund <aberglun@redhat.com>
2018-09-27 14:59:28 -0400

    Change migration to sync with squashed migrations

--- koku/reporting/migrations/0002_auto_20180926_1818.py
+++ koku/reporting/migrations/0002_auto_20180926_1818.py
@@ -7,7 +7,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('reporting', '0010_awscostentrylineitemdailysummary_account_alias'),
+        ('reporting', '0001_initial'),
     ]
 
     operations = [
