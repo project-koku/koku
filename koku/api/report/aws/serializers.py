3d0d88df28ddcc1d3fb02439f3cf4222fe0cf876
Douglas Curtis <docurtis@redhat.com>
2018-10-26 13:47:11 -0400

    Breaking up AWS API logic to prepare for code reuse with OCP (#436)

---
+++ koku/api/report/aws/serializers.py
@@ -0,0 +1,239 @@
+#
+# Copyright 2018 Red Hat, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+"""Report Serializers."""
+from pint.errors import UndefinedUnitError
+from rest_framework import serializers
+
+from api.report.serializers import (StringOrListField, handle_invalid_fields, validate_field)
+from api.utils import UnitConverter
+
+
+class GroupBySerializer(serializers.Serializer):
+    """Serializer for handling query parameter group_by."""
+
+    account = StringOrListField(child=serializers.CharField(),
+                                required=False)
+    avail_zone = StringOrListField(child=serializers.CharField(),
+                                   required=False)
+    instance_type = StringOrListField(child=serializers.CharField(),
+                                      required=False)
+    region = StringOrListField(child=serializers.CharField(),
+                               required=False)
+    service = StringOrListField(child=serializers.CharField(),
+                                required=False)
+    storage_type = StringOrListField(child=serializers.CharField(),
+                                     required=False)
+
+    def validate(self, data):
+        """Validate incoming data.
+
+        Args:
+            data    (Dict): data to be validated
+        Returns:
+            (Dict): Validated data
+        Raises:
+            (ValidationError): if field inputs are invalid
+        """
+        handle_invalid_fields(self, data)
+        return data
+
+
+class OrderBySerializer(serializers.Serializer):
+    """Serializer for handling query parameter order_by."""
+
+    ORDER_CHOICES = (('asc', 'asc'), ('desc', 'desc'))
+    total = serializers.ChoiceField(choices=ORDER_CHOICES,
+                                    required=False)
+    delta = serializers.ChoiceField(choices=ORDER_CHOICES,
+                                    required=False)
+    inventory = serializers.ChoiceField(choices=ORDER_CHOICES,
+                                        required=False)
+    account_alias = serializers.ChoiceField(choices=ORDER_CHOICES,
+                                            required=False)
+    region = serializers.ChoiceField(choices=ORDER_CHOICES,
+                                     required=False)
+    service = serializers.ChoiceField(choices=ORDER_CHOICES,
+                                      required=False)
+
+    def validate(self, data):
+        """Validate incoming data."""
+        handle_invalid_fields(self, data)
+        return data
+
+
+class FilterSerializer(serializers.Serializer):
+    """Serializer for handling query parameter filter."""
+
+    RESOLUTION_CHOICES = (
+        ('daily', 'daily'),
+        ('monthly', 'monthly'),
+    )
+    TIME_CHOICES = (
+        ('-10', '-10'),
+        ('-30', '-30'),
+        ('-1', '1'),
+        ('-2', '-2'),
+    )
+    TIME_UNIT_CHOICES = (
+        ('day', 'day'),
+        ('month', 'month'),
+    )
+
+    resolution = serializers.ChoiceField(choices=RESOLUTION_CHOICES,
+                                         required=False)
+    time_scope_value = serializers.ChoiceField(choices=TIME_CHOICES,
+                                               required=False)
+    time_scope_units = serializers.ChoiceField(choices=TIME_UNIT_CHOICES,
+                                               required=False)
+    resource_scope = StringOrListField(child=serializers.CharField(),
+                                       required=False)
+    limit = serializers.IntegerField(required=False, min_value=1)
+    account = StringOrListField(child=serializers.CharField(),
+                                required=False)
+    service = StringOrListField(child=serializers.CharField(),
+                                required=False)
+    region = StringOrListField(child=serializers.CharField(),
+                               required=False)
+    avail_zone = StringOrListField(child=serializers.CharField(),
+                                   required=False)
+
+    def validate(self, data):
+        """Validate incoming data.
+
+        Args:
+            data    (Dict): data to be validated
+        Returns:
+            (Dict): Validated data
+        Raises:
+            (ValidationError): if filter inputs are invalid
+        """
+        handle_invalid_fields(self, data)
+
+        resolution = data.get('resolution')
+        time_scope_value = data.get('time_scope_value')
+        time_scope_units = data.get('time_scope_units')
+
+        if time_scope_units and time_scope_value:
+            msg = 'Valid values are {} when time_scope_units is {}'
+            if (time_scope_units == 'day' and  # noqa: W504
+                    (time_scope_value == '-1' or time_scope_value == '-2')):
+                valid_values = ['-10', '-30']
+                valid_vals = ', '.join(valid_values)
+                error = {'time_scope_value': msg.format(valid_vals, 'day')}
+                raise serializers.ValidationError(error)
+            if (time_scope_units == 'day' and resolution == 'monthly'):
+                valid_values = ['daily']
+                valid_vals = ', '.join(valid_values)
+                error = {'resolution': msg.format(valid_vals, 'day')}
+                raise serializers.ValidationError(error)
+            if (time_scope_units == 'month' and  # noqa: W504
+                    (time_scope_value == '-10' or time_scope_value == '-30')):
+                valid_values = ['-1', '-2']
+                valid_vals = ', '.join(valid_values)
+                error = {'time_scope_value': msg.format(valid_vals, 'month')}
+                raise serializers.ValidationError(error)
+        return data
+
+
+class QueryParamSerializer(serializers.Serializer):
+    """Serializer for handling query parameters."""
+
+    # Tuples are (key, display_name)
+    OPERATION_CHOICES = (
+        ('sum', 'sum'),
+        ('none', 'none'),
+    )
+
+    delta = serializers.BooleanField(required=False)
+    group_by = GroupBySerializer(required=False)
+    order_by = OrderBySerializer(required=False)
+    filter = FilterSerializer(required=False)
+    units = serializers.CharField(required=False)
+    operation = serializers.ChoiceField(choices=OPERATION_CHOICES,
+                                        required=False)
+
+    def validate(self, data):
+        """Validate incoming data.
+
+        Args:
+            data    (Dict): data to be validated
+        Returns:
+            (Dict): Validated data
+        Raises:
+            (ValidationError): if field inputs are invalid
+        """
+        handle_invalid_fields(self, data)
+        return data
+
+    def validate_group_by(self, value):
+        """Validate incoming group_by data.
+
+        Args:
+            data    (Dict): data to be validated
+        Returns:
+            (Dict): Validated data
+        Raises:
+            (ValidationError): if group_by field inputs are invalid
+        """
+        validate_field(self, 'group_by', GroupBySerializer, value)
+        return value
+
+    def validate_order_by(self, value):
+        """Validate incoming order_by data.
+
+        Args:
+            data    (Dict): data to be validated
+        Returns:
+            (Dict): Validated data
+        Raises:
+            (ValidationError): if order_by field inputs are invalid
+        """
+        validate_field(self, 'order_by', OrderBySerializer, value)
+        return value
+
+    def validate_filter(self, value):
+        """Validate incoming filter data.
+
+        Args:
+            data    (Dict): data to be validated
+        Returns:
+            (Dict): Validated data
+        Raises:
+            (ValidationError): if filter field inputs are invalid
+        """
+        validate_field(self, 'filter', FilterSerializer, value)
+        return value
+
+    def validate_units(self, value):
+        """Validate incoming units data.
+
+        Args:
+            data    (Dict): data to be validated
+        Returns:
+            (Dict): Validated data
+        Raises:
+            (ValidationError): if units field inputs are invalid
+
+        """
+        unit_converter = UnitConverter()
+        try:
+            unit_converter.validate_unit(value)
+        except (AttributeError, UndefinedUnitError):
+            error = {'units': f'{value} is not a supported unit'}
+            raise serializers.ValidationError(error)
+
+        return value
---
+++ koku/api/report/query_filter.py
@@ -0,0 +1,270 @@
+#
+# Copyright 2018 Red Hat, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+"""QueryFilterfor Reports."""
+from collections import UserDict, defaultdict
+
+from django.db.models import Q
+
+
+class QueryFilter(UserDict):
+    """Dict-like object representing a single query filter."""
+
+    SEP = '__'    # separator
+    table = None
+    field = None
+    operation = None
+    parameter = None
+
+    def __init__(self, table=None, field=None, operation=None, parameter=None):
+        """Constructor.
+
+        Args:
+            table (str) - The name of a DB table
+            field (str) - The name of a DB field
+            operation (str) - The name of a DB operation, e.g. 'in' or 'gte'
+            parameter (object) - A valid query target, e.g. a list or datetime
+        """
+        super().__init__(table=table, field=field, operation=operation,
+                         parameter=parameter)
+        self.table = table
+        self.field = field
+        self.operation = operation
+        self.parameter = parameter
+
+    def composed_query_string(self):
+        """Return compiled query string."""
+        fields = [entry for entry in [self.table, self.field, self.operation]
+                  if entry is not None]
+        return self.SEP.join(fields)
+
+    def composed_Q(self):
+        """Return a Q object formatted for Django's ORM."""
+        query_dict = {self.composed_query_string(): self.parameter}
+        return Q(**query_dict)
+
+    def from_string(self, query_string):
+        """Parse a string representing a filter.
+
+        Returns:
+            QueryFilter instance.
+
+        Args:
+            query_string (str) A string representing a query filter.
+
+        Example:
+            QueryFilter().from_string('mytable__myfield__contains')
+
+        """
+        parts = query_string.split(self.SEP)
+        if len(parts) == 3:
+            self.table, self.field, self.operation = parts
+        elif len(parts) == 2:
+            self.table, self.operation = parts
+        else:
+            message = 'Incorrect number of parts in query string. ' + \
+                'Need at least two of [table, field, operation].'
+            raise TypeError(message)
+        return self
+
+    def __eq__(self, other):
+        """Exact comparison."""
+        return self.data == other.data
+
+    def __repr__(self):
+        """Return string representation."""
+        return str(self.composed_Q())
+
+
+class QueryFilterCollection(object):
+    """Object representing a set of filters for a query.
+
+    This object behaves in list-like ways.
+    """
+
+    def __init__(self, filters=None):
+        """Constructor.
+
+        Args:
+            filters (list) a list of QueryFilter instances.
+        """
+        if filters is None:
+            self._filters = list()    # a list of QueryFilter objects
+        else:
+            if not isinstance(filters, list):
+                raise TypeError('filters must be a list')
+
+            if not all(isinstance(item, QueryFilter) for item in filters):
+                raise TypeError('Filters list must contain QueryFilters.')
+
+            self._filters = filters
+
+    def add(self, query_filter=None, table=None, field=None,
+            operation=None, parameter=None):
+        """Add a query filter to the collection.
+
+        QueryFilterCollection does try to maintain filter uniqueness. A new
+        object will not be added to the collection if it already exists.
+
+        Args:
+            query_filter (QueryFilter) a QueryFilter object
+
+            - or -
+
+            table (str)  db table name
+            field (str)  db field/row name
+            operation (str) db operation
+            parameter (object) query object
+
+        """
+        error_message = 'query_filter can not be defined with other parameters'
+        if query_filter and (table or field or operation or parameter):
+            raise AttributeError(error_message)
+
+        if query_filter and query_filter not in self:
+            self._filters.append(query_filter)
+
+        if (table or field or operation or parameter):
+            qf = QueryFilter(table=table, field=field, operation=operation,
+                             parameter=parameter)
+            if qf not in self:
+                self._filters.append(qf)
+
+    def compose(self):
+        """Compose filters into a dict for submitting to Django's ORM."""
+        composed_query = None
+        compose_dict = defaultdict(list)
+        for filt in self._filters:
+            filt_key = filt.composed_query_string()
+            compose_dict[filt_key].append(filt)
+
+        for filter_list in compose_dict.values():
+            or_filter = None
+            for filter_item in filter_list:
+                if or_filter is None:
+                    or_filter = filter_item.composed_Q()
+                else:
+                    or_filter = or_filter | filter_item.composed_Q()
+            if composed_query is None:
+                composed_query = or_filter
+            else:
+                composed_query = composed_query & or_filter
+        return composed_query
+
+    def __contains__(self, item):
+        """Return a boolean about whether `item` is in the collection.
+
+        This will do both an exact and a fuzzy match. Exact matches are
+        preferred over fuzzy matches. However, if there is no exact match,
+        __contains__ will return `True` if there is an object that contains all
+        of the same properties as `item`, even if additional properties are
+        set on the object in our collection. (See: `QueryFilterCollection.get()`)
+
+        Args:
+            item (QueryFilter or dict) object to search for.
+
+        Returns:
+            (bool) Whether a matching object was found.
+
+        """
+        if isinstance(item, QueryFilter):
+            return item in self._filters
+        if isinstance(item, dict) and self.get(item):
+            return True
+        return False
+
+    def __iter__(self):
+        """Return an iterable of the collection."""
+        return self._filters.__iter__()
+
+    def __getitem__(self, key):
+        """Return object identified by key.
+
+        Args:
+            key (int) the index of the QueryFilter to return.
+        """
+        return self._filters[key]
+
+    def __repr__(self):
+        """Return string representation."""
+        out = f'{self.__class__}: '
+        for filt in self._filters:
+            out += filt.__repr__() + ', '
+        return out
+
+    def delete(self, query_filter=None, table=None, field=None,
+               operation=None, parameter=None):
+        """Delete a query filter from the collection.
+
+        Args:
+            query_filter (QueryFilter) a QueryFilter object
+
+            - or -
+
+            table (str)  db table name
+            field (str)  db field/row name
+            operation (str) db operation
+            parameter (object) query object
+
+        """
+        error_message = 'query_filter can not be defined with other parameters'
+        if query_filter and (table or field or operation or parameter):
+            raise AttributeError(error_message)
+
+        if query_filter and query_filter in self:
+            self._filters.remove(query_filter)
+
+        if (table or field or operation or parameter):
+            qf = QueryFilter(table=table, field=field, operation=operation,
+                             parameter=parameter)
+            if qf in self:
+                self._filters.remove(qf)
+
+    def get(self, search):
+        """Retrieve the first matching filter in the collection.
+
+        This is a "fuzzy" search. This method looks for an object that matches
+        all key-value pairs of `search` with an object in the collection. The
+        matched object may have additional properties set to a non-None value.
+
+        Args:
+            search (dict) A dictionary of filter parameters to search for.
+
+        Example:
+
+            These examples are in order of least-specific (most likely to have
+            more than one result) to most-specific (most likely to have exactly
+            one result).
+
+            QueryFilterCollection.get({'operation': 'contains'})
+
+            QueryFilterCollection.get({'table': 'mytable', 'operation': 'gte'})
+
+            QueryFilterCollection.get({'table': 'mytable',
+                                       'field': 'myfield',
+                                       'operation': 'in'})
+
+        """
+        for idx, filt in enumerate(self._filters):
+            filter_values = [filt.get(key) for key in search.keys()
+                             if filt.get(key)]
+            search_values = [search.get(key) for key in search.keys()
+                             if search.get(key)]
+            filter_values.sort()
+            search_values.sort()
+            if search_values == filter_values:
+                return (idx, filt)
+        return None
