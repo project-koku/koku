9ef1ee5c7fa001f0d358f9f5cb9f995a4b9dcd3a
Chris Hambridge <chambrid@redhat.com>
2018-11-12 15:39:14 -0500

    Handle others with matching rank. (#458)
    * Handle others with matching rank.
    
    * Use RowNumber instead of DenseRank.

--- koku/api/report/ocp/ocp_query_handler.py
+++ koku/api/report/ocp/ocp_query_handler.py
@@ -23,7 +23,7 @@ from django.db.models import (F,
                               Value,
                               Window)
 from django.db.models.functions import Concat
-from django.db.models.functions import DenseRank
+from django.db.models.functions import RowNumber
 from tenant_schemas.utils import tenant_context
 
 from api.report.queries import ReportQueryHandler
@@ -151,12 +151,12 @@ class OCPReportQueryHandler(ReportQueryHandler):
 
             if self._limit and group_by_value:
                 rank_order = getattr(F(group_by_value.pop()), self.order_direction)()
-                dense_rank_by_total = Window(
-                    expression=DenseRank(),
+                rank_by_total = Window(
+                    expression=RowNumber(),
                     partition_by=F('date'),
                     order_by=rank_order
                 )
-                query_data = query_data.annotate(rank=dense_rank_by_total)
+                query_data = query_data.annotate(rank=rank_by_total)
                 query_order_by = query_order_by + ('rank',)
 
             if self.order_field != 'delta':
