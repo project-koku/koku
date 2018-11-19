6e6797ce31ea8af214ca0b950e8305c3cfd36c66
Chris Hambridge <chambrid@redhat.com>
2018-11-19 18:35:16 -0500

    Add initial setup for tiered rates serializer. (#477)
    * Add initial setup for tiered rates serializer.
    
    * Add test cases.

--- koku/rates/serializers.py
+++ koku/rates/serializers.py
@@ -60,14 +60,61 @@ class FixedRateSerializer(serializers.Serializer):
         return str(value)
 
 
+class TieredRateSerializer(serializers.Serializer):
+    """Serializer for Tiered Rate."""
+
+    value = serializers.DecimalField(required=False, max_digits=19, decimal_places=10)
+    usage_start = serializers.DecimalField(required=False, max_digits=19, decimal_places=10)
+    usage_end = serializers.DecimalField(required=False, max_digits=19, decimal_places=10)
+    unit = serializers.ChoiceField(choices=CURRENCY_CHOICES)
+
+    def validate_value(self, value):
+        """Check that value is a positive value."""
+        if value <= 0:
+            raise serializers.ValidationError('A tiered rate value must be positive.')
+        return str(value)
+
+    def validate_usage_start(self, usage_start):
+        """Check that usage_start is a positive value."""
+        if usage_start < 0:
+            raise serializers.ValidationError('A tiered rate usage_start must be positive.')
+        return str(usage_start)
+
+    def validate_usage_end(self, usage_end):
+        """Check that usage_end is a positive value."""
+        if usage_end <= 0:
+            raise serializers.ValidationError('A tiered rate usage_end must be positive.')
+        return str(usage_end)
+
+    def validate(self, data):
+        """Validate that usage_end is greater than usage_start."""
+        usage_start = data.get('usage_start', '0.0')
+        usage_end = data.get('usage_end', '0.0')
+        if Decimal(usage_start) >= Decimal(usage_end):
+            raise serializers.ValidationError('A tiered rate usage_start must be less than usage_end.')
+        else:
+            return data
+
+
 class RateSerializer(serializers.ModelSerializer):
     """Rate Serializer."""
+
+    DECIMALS = ('value', 'usage_start', 'usage_end')
 
     uuid = serializers.UUIDField(read_only=True)
     provider_uuid = UUIDKeyRelatedField(queryset=Provider.objects.all(), pk_field='uuid')
     metric = serializers.ChoiceField(choices=Rate.METRIC_CHOICES,
                                      required=True)
-    fixed_rate = FixedRateSerializer()
+    fixed_rate = FixedRateSerializer(required=False)
+    tiered_rate = TieredRateSerializer(required=False, many=True)
+
+    @staticmethod
+    def _convert_to_decimal(rate):
+        for decimal_key in RateSerializer.DECIMALS:
+            if decimal_key in rate:
+                decimal_value = Decimal(rate.get(decimal_key))
+                rate[decimal_key] = decimal_value
+        return rate
 
     def validate_provider_uuid(self, provider_uuid):
         """Check that provider_uuid is a valid identifier."""
@@ -75,6 +122,14 @@ class RateSerializer(serializers.ModelSerializer):
             return provider_uuid
         else:
             raise serializers.ValidationError('Provider object does not exist with given uuid.')
+
+    def validate(self, data):
+        """Validate that a rate must be defined."""
+        rate_keys = ('fixed_rate', 'tiered_rate')
+        if any(data.get(rate_key) is not None for rate_key in rate_keys):
+            return data
+        else:
+            raise serializers.ValidationError('A rated must be provided (e.g. fixed_rate, tiered_rate).')
 
     def to_representation(self, rate):
         """Create external representation of a rate."""
@@ -85,9 +140,11 @@ class RateSerializer(serializers.ModelSerializer):
             'metric': rate.metric
         }
         for rate_type in rates.values():
-            if 'value' in rate_type:
-                value = Decimal(rate_type.get('value'))
-                rate_type['value'] = value
+            if isinstance(rate_type, list):
+                for rate_item in rate_type:
+                    RateSerializer._convert_to_decimal(rate_item)
+            else:
+                RateSerializer._convert_to_decimal(rate_type)
         out.update(rates)
         return out
 
@@ -111,4 +168,4 @@ class RateSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Rate
-        fields = ('uuid', 'provider_uuid', 'metric', 'fixed_rate')
+        fields = ('uuid', 'provider_uuid', 'metric', 'fixed_rate', 'tiered_rate')
