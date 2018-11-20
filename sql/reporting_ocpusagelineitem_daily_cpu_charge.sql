4f1b87a415814d6b6caf09c27bd5ecfcdd19eab5
Douglas Curtis <docurtis@redhat.com>
2018-11-20 11:14:17 -0500

    OCP tier charge updates (#267)

--- sql/reporting_ocpusagelineitem_daily_cpu_charge.sql
+++ sql/reporting_ocpusagelineitem_daily_cpu_charge.sql
@@ -1,2 +1,2 @@
 -- Calculate and update the OCP CPU usage charge
-UPDATE reporting_ocpusagelineitem_daily_summary SET pod_charge_cpu_cores=GREATEST(pod_usage_cpu_core_hours*'{cpu_rate}', pod_request_cpu_core_hours*'{cpu_rate}');
+UPDATE reporting_ocpusagelineitem_daily_summary SET pod_charge_cpu_cores = '{cpu_charge}' WHERE id = '{line_id}';
