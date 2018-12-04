28b97ec218919806ff1d0db83cab851a9e5afd6c
Andrew Berglund <aberglun@redhat.com>
2018-12-04 11:41:21 -0500

    Ocp node capacity (#277)
    * Add node capacity to summary SQL
    
    * Update OCP columns to use -hours on charge and memory
    
    * Update test.sql

--- database/sql/reporting_ocpusagelineitem_daily_cpu_charge.sql
+++ database/sql/reporting_ocpusagelineitem_daily_cpu_charge.sql
@@ -1,2 +1,2 @@
 -- Calculate and update the OCP CPU usage charge
-UPDATE reporting_ocpusagelineitem_daily_summary SET pod_charge_cpu_cores = '{cpu_charge}' WHERE id = '{line_id}';
+UPDATE reporting_ocpusagelineitem_daily_summary SET pod_charge_cpu_core_hours = '{cpu_charge}' WHERE id = '{line_id}';
