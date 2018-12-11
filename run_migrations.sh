cae615c86ad7c88fc7a7647ef3c423919773b524
Douglas Curtis <docurtis@redhat.com>
2018-12-11 12:54:56 -0500

    migrations and server running

--- run_migrations.sh
+++ run_migrations.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 sleep 15
-python koku/manage.py migrate_schemas --database $DATABASE_HOST:$DATABASE_PORT
+python koku/manage.py migrate_schemas
+DJANGO_READ_DOT_ENV_FILE=True python koku/manage.py runserver
