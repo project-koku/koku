a6745af7d5a28e898242161803eac17aaeb86102
Chris Hambridge <chambrid@redhat.com>
2018-12-20 14:01:37 -0500

    Use HTTP_REFERER header to rewrite the previous and next links in pagination.

--- koku/api/common/pagination.py
+++ koku/api/common/pagination.py
@@ -16,8 +16,9 @@
 #
 
 """Common pagination class."""
-
 from rest_framework.pagination import PageNumberPagination
+
+HTTP_REFERER = 'HTTP_REFERER'
 
 
 class StandardResultsSetPagination(PageNumberPagination):
@@ -26,3 +27,29 @@ class StandardResultsSetPagination(PageNumberPagination):
     page_size = 10
     page_size_query_param = 'page_size'
     max_page_size = 1000
+
+    def get_next_link(self):
+        """Create next link with referer rewrite."""
+        next_link = super().get_next_link()
+        if next_link is None:
+            return next_link
+        api_index = next_link.index('api')
+        if HTTP_REFERER in self.request.META:
+            http_referer = self.request.META.get(HTTP_REFERER)
+            insights_str = '{}{}'
+            insights_url = insights_str.format(http_referer, next_link[api_index:])
+            return insights_url
+        return next_link
+
+    def get_previous_link(self):
+        """Create previous link with referer rewrite."""
+        previous_link = super().get_previous_link()
+        if previous_link is None:
+            return previous_link
+        api_index = previous_link.index('api')
+        if HTTP_REFERER in self.request.META:
+            http_referer = self.request.META.get(HTTP_REFERER)
+            insights_str = '{}{}'
+            insights_url = insights_str.format(http_referer, previous_link[api_index:])
+            return insights_url
+        return previous_link
