e7001190a4baa7d1b62c698358da7de22727a53e
Andrew Berglund <aberglun@redhat.com>
2019-01-04 11:01:15 -0500

    Update tests for tag prefix

--- koku/api/report/queries.py
+++ koku/api/report/queries.py
@@ -371,9 +371,8 @@ class ReportQueryHandler(QueryHandler):
         tag_group_by = self.get_tag_group_by_keys()
         tag_filters.extend(tag_group_by)
         for tag in tag_filters:
-            tag = strip_tag_prefix(tag)
             # Update the filter to use the label column name
-            tag_db_name = tag_column + '__' + tag
+            tag_db_name = tag_column + '__' + strip_tag_prefix(tag)
             filt = {
                 'field': tag_db_name,
                 'operation': 'icontains'
@@ -390,7 +389,8 @@ class ReportQueryHandler(QueryHandler):
                     'field': tag_column,
                     'operation': 'has_key'
                 }
-                q_filter = QueryFilter(parameter=tag, **wild_card_filt)
+                q_filter = QueryFilter(parameter=strip_tag_prefix(tag),
+                                       **wild_card_filt)
                 filters.add(q_filter)
         return filters
 
@@ -426,8 +426,7 @@ class ReportQueryHandler(QueryHandler):
         tag_group_by = self.get_tag_group_by_keys()
         if tag_group_by:
             for tag in tag_group_by:
-                tag = strip_tag_prefix(tag)
-                tag_db_name = tag_column + '__' + tag
+                tag_db_name = tag_column + '__' + strip_tag_prefix(tag)
                 filt = {
                     'field': tag_db_name,
                     'operation': 'isnull',
@@ -464,8 +463,7 @@ class ReportQueryHandler(QueryHandler):
         tag_column = self._mapper._provider_map.get('tag_column')
         tag_groups = self.get_tag_group_by_keys()
         for tag in tag_groups:
-            tag = strip_tag_prefix(tag)
-            tag_db_name = tag_column + '__' + tag
+            tag_db_name = tag_column + '__' + strip_tag_prefix(tag)
             group_data = self.get_query_param_data('group_by', tag)
             if group_data:
                 tag = quote_plus(tag)
