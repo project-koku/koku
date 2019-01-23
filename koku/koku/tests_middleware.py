841d50366c319007b418b44e3c3c584618975cd4
Chris Hambridge <chambrid@redhat.com>
2019-01-23 15:08:19 -0500

    Add appropriate exception handling for potential race conditions. Closes #587.

--- koku/koku/tests_middleware.py
+++ koku/koku/tests_middleware.py
@@ -121,3 +121,26 @@ class IdentityHeaderMiddlewareTest(IamTestCase):
 
         with self.assertRaises(User.DoesNotExist):
             User.objects.get(username=self.user_data['username'])
+
+    def test_race_condition_customer(self):
+        """Test case where another request may create the customer in a race condition."""
+        customer = self._create_customer_data()
+        account_id = customer['account_id']
+        orig_cust = IdentityHeaderMiddleware._create_customer(account_id)  # pylint: disable=W0212
+        dup_cust = IdentityHeaderMiddleware._create_customer(account_id)  # pylint: disable=W0212
+        self.assertEqual(orig_cust, dup_cust)
+
+    def test_race_condition_user(self):
+        """Test case where another request may create the user in a race condition."""
+        mock_request = self.request
+        middleware = IdentityHeaderMiddleware()
+        middleware.process_request(mock_request)
+        self.assertTrue(hasattr(mock_request, 'user'))
+        customer = Customer.objects.get(account_id=self.customer['account_id'])
+        self.assertIsNotNone(customer)
+        user = User.objects.get(username=self.user_data['username'])
+        self.assertIsNotNone(user)
+        IdentityHeaderMiddleware._create_user(username=self.user_data['username'],  # pylint: disable=W0212
+                                              email=self.user_data['email'],
+                                              customer=customer,
+                                              request=mock_request)
