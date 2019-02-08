f733565eeac283f7b84b5b6858f6ba9e4e04e816
Andrew Berglund <aberglun@redhat.com>
2019-02-08 16:55:46 -0500

    OCP on AWS query handler starting point

--- koku/api/report/ocp_aws/ocp_aws_query_handler.py
+++ koku/api/report/ocp_aws/ocp_aws_query_handler.py
@@ -14,34 +14,94 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
-"""AWS Query Handling for Reports."""
+"""OCP Query Handling for Reports."""
 import copy
+from collections import defaultdict
+from decimal import Decimal, DivisionByZero, InvalidOperation
 
-from django.db.models import (F, Q, Value, Window)
+from django.db.models import F, Max, Sum, Value, Window
 from django.db.models.functions import (Coalesce, Concat, RowNumber)
 from tenant_schemas.utils import tenant_context
 
-from api.query_filter import QueryFilter, QueryFilterCollection
-from api.report.queries import ReportQueryHandler
-
-EXPORT_COLUMNS = ['cost_entry_id', 'cost_entry_bill_id',
-                  'cost_entry_product_id', 'cost_entry_pricing_id',
-                  'cost_entry_reservation_id', 'tags',
-                  'invoice_id', 'line_item_type', 'usage_account_id',
-                  'usage_start', 'usage_end', 'product_code',
-                  'usage_type', 'operation', 'availability_zone',
-                  'usage_amount', 'normalization_factor',
-                  'normalized_usage_amount', 'currency_code',
-                  'unblended_rate', 'unblended_cost', 'blended_rate',
-                  'blended_cost', 'tax_type']
+from api.report.aws.aws_query_handler import AWSReportQueryHandler
+from api.report.queries import ProviderMap
+from reporting.models import OCPAWSCostLineItemDailySummary
 
 
-class AWSReportQueryHandler(ReportQueryHandler):
-    """Handles report queries and responses for AWS."""
+
+class OCPAWSProviderMap(ProviderMap):
+    """OCP Provider Map."""
+
+    mapping = {
+        'provider': 'OCP_AWS',
+        'annotations': {'cluster': 'cluster_id',
+                        'project': 'namespace',
+                        'account': 'usage_account_id',
+                        'service': 'product_code',
+                        'az': 'availability_zone'},
+        'end_date': 'usage_end',
+        'filters': {
+            'project': {'field': 'namespace',
+                        'operation': 'icontains'},
+            'cluster': [{'field': 'cluster_alias',
+                        'operation': 'icontains',
+                        'composition_key': 'cluster_filter'},
+                        {'field': 'cluster_id',
+                        'operation': 'icontains',
+                        'composition_key': 'cluster_filter'}],
+            'node': {'field': 'node',
+                        'operation': 'icontains'},
+            'account': [{'field': 'account_alias__account_alias',
+                        'operation': 'icontains',
+                        'composition_key': 'account_filter'},
+                        {'field': 'usage_account_id',
+                        'operation': 'icontains',
+                        'composition_key': 'account_filter'}],
+            'service': {'field': 'product_code',
+                        'operation': 'icontains'},
+            'az': {'field': 'availability_zone',
+                            'operation': 'icontains'},
+            'region': {'field': 'region',
+                        'operation': 'icontains'}
+        },
+        'group_by_options': ['account', 'service', 'region', 'cluster', 'project', 'node'],
+        'tag_column': 'tags',
+        'report_type': {
+            'storage': {
+                'aggregate': {
+                    'value': Sum('usage_amount'),
+                    'cost': Sum('unblended_cost')
+                },
+                'aggregate_key': 'usage_amount',
+                'annotations': {'cost': Sum('unblended_cost'),
+                                'total': Sum('usage_amount'),
+                                'units': Coalesce(Max('unit'),
+                                Value('GB-Mo'))},
+                'count': None,
+                'delta_key': {'total': Sum('usage_amount')},
+                'filter': {
+                    'field': 'product_family',
+                    'operation': 'contains',
+                    'parameter': 'Storage'
+                },
+                'units_key': 'unit',
+                'units_fallback': 'GB-Mo',
+                'sum_columns': ['total', 'cost'],
+                'default_ordering': {'total': 'desc'},
+            },
+        },
+        'start_date': 'usage_start',
+        'tables': {'previous_query': OCPAWSCostLineItemDailySummary,
+                    'query': OCPAWSCostLineItemDailySummary,
+                    'total': OCPAWSCostLineItemDailySummary},
+    }
+
+class OCPAWSReportQueryHandler(AWSReportQueryHandler):
+    """Handles report queries and responses for OCP on AWS."""
 
     def __init__(self, query_parameters, url_data,
                  tenant, **kwargs):
-        """Establish AWS report query handler.
+        """Establish OCP report query handler.
 
         Args:
             query_parameters    (Dict): parameters for query
@@ -49,48 +109,9 @@ class AWSReportQueryHandler(ReportQueryHandler):
             tenant    (String): the tenant to use to access CUR data
             kwargs    (Dict): A dictionary for internal query alteration based on path
         """
-        if not kwargs.get('provider'):
-            kwargs['provider'] = 'AWS'
-        kwargs['no_tag_query'] = QueryFilter(operation='tags__iexact', parameter='{}')
+        kwargs['provider'] = 'OCP_AWS'
         super().__init__(query_parameters, url_data,
                          tenant, **kwargs)
-
-    @property
-    def annotations(self):
-        """Create dictionary for query annotations.
-
-        Returns:
-            (Dict): query annotations dictionary
-
-        """
-        units_fallback = self._mapper._report_type_map.get('units_fallback')
-        annotations = {
-            'date': self.date_trunc('usage_start'),
-            'units': Coalesce(self._mapper.units_key, Value(units_fallback))
-        }
-
-        # { query_param: database_field_name }
-        fields = self._mapper._provider_map.get('annotations')
-        for q_param, db_field in fields.items():
-            annotations[q_param] = Concat(db_field, Value(''))
-
-        return annotations
-
-    def _format_query_response(self):
-        """Format the query response with data.
-
-        Returns:
-            (Dict): Dictionary response of query params, data, and total
-
-        """
-        output = copy.deepcopy(self.query_parameters)
-        output['data'] = self.query_data
-        output['total'] = self.query_sum
-
-        if self._delta:
-            output['delta'] = self.query_delta
-
-        return output
 
     def execute_sum_query(self):
         """Execute query and return provided data when self.is_sum == True.
@@ -135,7 +156,11 @@ class AWSReportQueryHandler(ReportQueryHandler):
                 }
                 sum_query = query.annotate(**sum_annotations)
                 units_value = sum_query.values('units').first().get('units')
-                query_sum = self.calculate_total(units_value)
+
+                aggregates = self._mapper._report_type_map.get('aggregates')
+                metric_sum = query.aggregate(**aggregates)
+                query_sum = {key: metric_sum.get(key) for key in aggregates}
+
             if self._delta:
                 query_data = self.add_deltas(query_data, query_sum)
 
@@ -166,43 +191,3 @@ class AWSReportQueryHandler(ReportQueryHandler):
         self.query_sum = ordered_total
         self.query_data = data
         return self._format_query_response()
-
-    def execute_query(self):
-        """Execute query and return provided data.
-
-        Returns:
-            (Dict): Dictionary response of query params, data, and total
-
-        """
-        return self.execute_sum_query()
-
-    def calculate_total(self, units_value):
-        """Calculate aggregated totals for the query.
-
-        Args:
-            units_value (str): The unit of the reported total
-
-        Returns:
-            (dict) The aggregated totals for the query
-
-        """
-        filt_collection = QueryFilterCollection()
-        total_filter = self._get_search_filter(filt_collection)
-
-        time_scope_value = self.get_query_param_data('filter',
-                                                     'time_scope_value',
-                                                     -10)
-        time_and_report_filter = Q(time_scope_value=time_scope_value) & \
-            Q(report_type=self._report_type)
-
-        if total_filter is None:
-            total_filter = time_and_report_filter
-        else:
-            total_filter = total_filter & time_and_report_filter
-
-        q_table = self._mapper._provider_map.get('tables').get('total')
-        aggregates = self._mapper._report_type_map.get('aggregate')
-        total_query = q_table.objects.filter(total_filter).aggregate(**aggregates)
-        total_query['units'] = units_value
-
-        return total_query
