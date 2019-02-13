eb41b4f0b8a274e0b4cab737a5b45472b3e989f1
Chris Hambridge <chambrid@redhat.com>
2019-02-13 14:40:22 -0500

    Enable graceful_timeout setting.

--- koku/gunicorn.py
+++ koku/gunicorn.py
@@ -6,3 +6,4 @@ bind = 'unix:/var/run/koku/gunicorn.sock'
 workers = multiprocessing.cpu_count() * 2 + 1
 timeout = int(os.environ.get('TIMEOUT', '90'))
 loglevel = os.environ.get('LOG_LEVEL', 'DEBUG')
+graceful_timeout = int(os.environ.get('GRACEFUL_TIMEOUT', '180'))
