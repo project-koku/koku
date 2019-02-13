9fc52384451cce01f5bbac2ee84bcb63c7359a35
Chris Hambridge <chambrid@redhat.com>
2019-02-13 14:23:21 -0500

    Add log level setting for debug.

--- koku/gunicorn.py
+++ koku/gunicorn.py
@@ -5,3 +5,4 @@ import os
 bind = 'unix:/var/run/koku/gunicorn.sock'
 workers = multiprocessing.cpu_count() * 2 + 1
 timeout = int(os.environ.get('TIMEOUT', '90'))
+loglevel = os.environ.get('LOG_LEVEL', 'DEBUG')
