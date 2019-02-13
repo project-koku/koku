090f822bad326bbddca9ac77e4a7e479640e5b4a
Chris Hambridge <chambrid@redhat.com>
2019-02-13 15:17:05 -0500

    Set default log level to INFO.

--- koku/gunicorn.py
+++ koku/gunicorn.py
@@ -5,5 +5,5 @@ import os
 bind = 'unix:/var/run/koku/gunicorn.sock'
 workers = multiprocessing.cpu_count() * 2 + 1
 timeout = int(os.environ.get('TIMEOUT', '90'))
-loglevel = os.environ.get('LOG_LEVEL', 'DEBUG')
+loglevel = os.environ.get('LOG_LEVEL', 'INFO')
 graceful_timeout = int(os.environ.get('GRACEFUL_TIMEOUT', '180'))
