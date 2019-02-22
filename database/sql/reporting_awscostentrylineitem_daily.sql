dfc833dcdf69d68a898c251c8f181fa48b9acf82
Andrew Berglund <aberglun@redhat.com>
2019-02-22 14:42:10 -0500

    Summary sql tag left joins (#382)
    * Update AWS summary SQL so tags are left joined not unioned
    
    * LEFT JOIN pod labels in OCP summary to not drop nulls

--- database/sql/reporting_awscostentrylineitem_daily.sql
+++ database/sql/reporting_awscostentrylineitem_daily.sql
@@ -63,98 +63,100 @@ CREATE TEMPORARY TABLE aws_tag_summary_{uuid} AS (
 
 -- Place our query in a temporary table
 CREATE TEMPORARY TABLE reporting_awscostentrylineitem_daily_{uuid} AS (
-    SELECT date(ce.interval_start) as usage_start,
-        date(ce.interval_start) as usage_end,
-        li.cost_entry_product_id,
-        li.cost_entry_pricing_id,
-        li.cost_entry_reservation_id,
-        li.line_item_type,
-        li.usage_account_id,
-        li.usage_type,
-        li.operation,
-        li.availability_zone,
-        li.resource_id,
-        li.tax_type,
-        li.product_code,
-        '{{}}'::jsonb as tags,
-        sum(li.usage_amount) as usage_amount,
-        max(li.normalization_factor) as normalization_factor,
-        sum(li.normalized_usage_amount) as normalized_usage_amount,
-        max(li.currency_code) as currency_code,
-        max(li.unblended_rate) as unblended_rate,
-        sum(li.unblended_cost) as unblended_cost,
-        max(li.blended_rate) as blended_rate,
-        sum(li.blended_cost) as blended_cost,
-        sum(li.public_on_demand_cost) as public_on_demand_cost,
-        max(li.public_on_demand_rate) as public_on_demand_rate
-    FROM reporting_awscostentrylineitem AS li
-    JOIN reporting_awscostentry AS ce
-        ON li.cost_entry_id = ce.id
-    WHERE date(ce.interval_start) >= '{start_date}'
-        AND date(ce.interval_start) <= '{end_date}'
-    GROUP BY date(ce.interval_start),
-        li.cost_entry_product_id,
-        li.cost_entry_pricing_id,
-        li.cost_entry_reservation_id,
-        li.resource_id,
-        li.line_item_type,
-        li.usage_account_id,
-        li.usage_type,
-        li.operation,
-        li.availability_zone,
-        li.tax_type,
-        li.product_code
+    SELECT li.*,
+        CASE WHEN ats.tags IS NOT NULL
+            THEN ats.tags
+            ELSE '{{}}'::jsonb
+            END as tags
+    FROM (
+        SELECT date(ce.interval_start) as usage_start,
+            date(ce.interval_start) as usage_end,
+            li.cost_entry_product_id,
+            li.cost_entry_pricing_id,
+            li.cost_entry_reservation_id,
+            li.line_item_type,
+            li.usage_account_id,
+            li.usage_type,
+            li.operation,
+            li.availability_zone,
+            li.resource_id,
+            li.tax_type,
+            li.product_code,
+            sum(li.usage_amount) as usage_amount,
+            max(li.normalization_factor) as normalization_factor,
+            sum(li.normalized_usage_amount) as normalized_usage_amount,
+            max(li.currency_code) as currency_code,
+            max(li.unblended_rate) as unblended_rate,
+            sum(li.unblended_cost) as unblended_cost,
+            max(li.blended_rate) as blended_rate,
+            sum(li.blended_cost) as blended_cost,
+            sum(li.public_on_demand_cost) as public_on_demand_cost,
+            max(li.public_on_demand_rate) as public_on_demand_rate
+        FROM reporting_awscostentrylineitem AS li
+        JOIN reporting_awscostentry AS ce
+            ON li.cost_entry_id = ce.id
+        WHERE date(ce.interval_start) >= '{start_date}'
+            AND date(ce.interval_start) <= '{end_date}'
+        GROUP BY date(ce.interval_start),
+            li.cost_entry_product_id,
+            li.cost_entry_pricing_id,
+            li.cost_entry_reservation_id,
+            li.resource_id,
+            li.line_item_type,
+            li.usage_account_id,
+            li.usage_type,
+            li.operation,
+            li.availability_zone,
+            li.tax_type,
+            li.product_code
+    ) AS li
+    LEFT JOIN aws_tag_summary_{uuid} AS ats
+        ON  li.usage_start = ats.usage_start
+                AND (
+                    li.cost_entry_product_id = ats.cost_entry_product_id
+                    OR (li.cost_entry_product_id IS NULL AND ats.cost_entry_product_id IS NULL)
+                )
+                AND (
+                    li.cost_entry_pricing_id = ats.cost_entry_pricing_id
+                    OR (li.cost_entry_pricing_id IS NULL AND ats.cost_entry_pricing_id IS NULL)
+                )
+                AND (
+                    li.cost_entry_reservation_id = ats.cost_entry_reservation_id
+                    OR (li.cost_entry_reservation_id IS NULL AND ats.cost_entry_reservation_id IS NULL)
+                )
+                AND (
+                    li.line_item_type = ats.line_item_type
+                    OR (li.line_item_type IS NULL AND ats.line_item_type IS NULL)
+                )
+                AND (
+                    li.usage_account_id = ats.usage_account_id
+                    OR (li.usage_account_id IS NULL AND ats.usage_account_id IS NULL)
+                )
+                AND (
+                    li.usage_type = ats.usage_type
+                    OR (li.usage_type IS NULL AND ats.usage_type IS NULL)
+                )
+                AND (
+                    li.operation = ats.operation
+                    OR (li.operation IS NULL AND ats.operation IS NULL)
+                )
+                AND (
+                    li.availability_zone = ats.availability_zone
+                    OR (li.availability_zone IS NULL AND ats.availability_zone IS NULL)
+                )
+                AND (
+                    li.resource_id = ats.resource_id
+                    OR (li.resource_id IS NULL AND ats.resource_id IS NULL)
+                )
+                AND (
+                    li.tax_type = ats.tax_type
+                    OR (li.tax_type IS NULL AND ats.tax_type IS NULL)
+                )
+                AND (
+                    li.product_code = ats.product_code
+                    OR (li.product_code IS NULL AND ats.product_code IS NULL)
+                )
 )
-;
-
-UPDATE reporting_awscostentrylineitem_daily_{uuid} d
-SET tags = t.tags
-FROM aws_tag_summary_{uuid} t
-WHERE d.usage_start::date = t.usage_start::date
-    AND (
-        d.cost_entry_product_id = t.cost_entry_product_id
-        OR (d.cost_entry_product_id IS NULL AND t.cost_entry_product_id IS NULL)
-    )
-    AND (
-        d.cost_entry_pricing_id = t.cost_entry_pricing_id
-        OR (d.cost_entry_pricing_id IS NULL AND t.cost_entry_pricing_id IS NULL)
-    )
-    AND (
-        d.cost_entry_reservation_id = t.cost_entry_reservation_id
-        OR (d.cost_entry_reservation_id IS NULL AND t.cost_entry_reservation_id IS NULL)
-    )
-    AND (
-        d.line_item_type = t.line_item_type
-        OR (d.line_item_type IS NULL AND t.line_item_type IS NULL)
-    )
-    AND (
-        d.usage_account_id = t.usage_account_id
-        OR (d.usage_account_id IS NULL AND t.usage_account_id IS NULL)
-    )
-    AND (
-        d.usage_type = t.usage_type
-        OR (d.usage_type IS NULL AND t.usage_type IS NULL)
-    )
-    AND (
-        d.operation = t.operation
-        OR (d.operation IS NULL AND t.operation IS NULL)
-    )
-    AND (
-        d.availability_zone = t.availability_zone
-        OR (d.availability_zone IS NULL AND t.availability_zone IS NULL)
-    )
-    AND (
-        d.resource_id = t.resource_id
-        OR (d.resource_id IS NULL AND t.resource_id IS NULL)
-    )
-    AND (
-        d.tax_type = t.tax_type
-        OR (d.tax_type IS NULL AND t.tax_type IS NULL)
-    )
-    AND (
-        d.product_code = t.product_code
-        OR (d.product_code IS NULL AND t.product_code IS NULL)
-    )
 ;
 
 -- Clear out old entries first
--- database/sql/reporting_awscostentrylineitem_daily_summary.sql
+++ database/sql/reporting_awscostentrylineitem_daily_summary.sql
@@ -1,68 +1,75 @@
+-- Aggregate tags by summary grouping
+CREATE TEMPORARY TABLE aws_tag_summary_{uuid} AS (
+    SELECT t.usage_start,
+        t.product_code,
+        t.product_family,
+        t.usage_account_id,
+        t.availability_zone,
+        t.region,
+        t.instance_type,
+        t.unit,
+        jsonb_object_agg(key, value) as tags
+    FROM (
+        SELECT li.usage_start,
+            li.usage_end,
+            li.product_code,
+            p.product_family,
+            li.usage_account_id,
+            li.availability_zone,
+            p.region,
+            p.instance_type,
+            pr.unit,
+            key,
+            value
+        FROM (
+            SELECT usage_start,
+                usage_end,
+                product_code,
+                usage_account_id,
+                availability_zone,
+                cost_entry_product_id,
+                cost_entry_pricing_id,
+                key,
+                value
+            FROM reporting_awscostentrylineitem_daily AS li,
+                jsonb_each_text(li.tags) tags
+        ) li
+        JOIN reporting_awscostentryproduct AS p
+            ON li.cost_entry_product_id = p.id
+        LEFT JOIN reporting_awscostentrypricing as pr
+            ON li.cost_entry_pricing_id = pr.id
+        WHERE date(li.usage_start) >= '{start_date}'
+            AND date(li.usage_start) <= '{end_date}'
+        GROUP BY li.usage_start,
+            li.usage_end,
+            li.product_code,
+            p.product_family,
+            li.usage_account_id,
+            li.availability_zone,
+            p.region,
+            p.instance_type,
+            pr.unit,
+            key,
+            value
+    ) t
+    GROUP BY t.usage_start,
+        t.product_code,
+        t.product_family,
+        t.usage_account_id,
+        t.availability_zone,
+        t.region,
+        t.instance_type,
+        t.unit
+)
+;
+
 -- Place our query for data with no tags in a temporary table
 CREATE TEMPORARY TABLE reporting_awscostentrylineitem_daily_summary_{uuid} AS (
-    SELECT li.usage_start,
-        li.usage_end,
-        li.product_code,
-        p.product_family,
-        li.usage_account_id,
-        max(aa.id) as account_alias_id,
-        li.availability_zone,
-        p.region,
-        p.instance_type,
-        pr.unit,
-        '{{}}'::jsonb as tags,
-        sum(li.usage_amount) as usage_amount,
-        max(li.normalization_factor) as normalization_factor,
-        sum(li.normalized_usage_amount) as normalized_usage_amount,
-        max(li.currency_code) as currency_code,
-        max(li.unblended_rate) as unblended_rate,
-        sum(li.unblended_cost) as unblended_cost,
-        max(li.blended_rate) as blended_rate,
-        sum(li.blended_cost) as blended_cost,
-        sum(li.public_on_demand_cost) as public_on_demand_cost,
-        max(li.public_on_demand_rate) as public_on_demand_rate,
-        count(DISTINCT li.resource_id) as resource_count
-    FROM reporting_awscostentrylineitem_daily AS li
-    JOIN reporting_awscostentryproduct AS p
-        ON li.cost_entry_product_id = p.id
-    LEFT JOIN reporting_awscostentrypricing as pr
-        ON li.cost_entry_pricing_id = pr.id
-    LEFT JOIN reporting_awsaccountalias AS aa
-        ON li.usage_account_id = aa.account_id
-    WHERE date(li.usage_start) >= '{start_date}'
-        AND date(li.usage_start) <= '{end_date}'
-    GROUP BY li.usage_start,
-        li.usage_end,
-        li.product_code,
-        p.product_family,
-        li.usage_account_id,
-        li.availability_zone,
-        p.region,
-        p.instance_type,
-        pr.unit
-    UNION
-    SELECT usage_start,
-        usage_end,
-        product_code,
-        product_family,
-        usage_account_id,
-        account_alias_id,
-        availability_zone,
-        region,
-        instance_type,
-        unit,
-        cast(concat('{{"', key, '": "', value, '"}}') as jsonb) as tags,
-        usage_amount,
-        normalization_factor,
-        normalized_usage_amount,
-        currency_code,
-        unblended_rate,
-        unblended_cost,
-        blended_rate,
-        blended_cost,
-        public_on_demand_cost,
-        public_on_demand_rate,
-        resource_count
+    SELECT li.*,
+        CASE WHEN ats.tags IS NOT NULL
+            THEN ats.tags
+            ELSE '{{}}'::jsonb
+            END as tags
     FROM (
         SELECT li.usage_start,
             li.usage_end,
@@ -74,8 +81,6 @@ CREATE TEMPORARY TABLE reporting_awscostentrylineitem_daily_summary_{uuid} AS (
             p.region,
             p.instance_type,
             pr.unit,
-            li.key,
-            li.value,
             sum(li.usage_amount) as usage_amount,
             max(li.normalization_factor) as normalization_factor,
             sum(li.normalized_usage_amount) as normalized_usage_amount,
@@ -87,30 +92,7 @@ CREATE TEMPORARY TABLE reporting_awscostentrylineitem_daily_summary_{uuid} AS (
             sum(li.public_on_demand_cost) as public_on_demand_cost,
             max(li.public_on_demand_rate) as public_on_demand_rate,
             count(DISTINCT li.resource_id) as resource_count
-        FROM (
-            SELECT usage_start,
-                usage_end,
-                product_code,
-                usage_account_id,
-                availability_zone,
-                usage_amount,
-                normalization_factor,
-                normalized_usage_amount,
-                currency_code,
-                unblended_rate,
-                unblended_cost,
-                blended_rate,
-                blended_cost,
-                public_on_demand_cost,
-                public_on_demand_rate,
-                resource_id,
-                cost_entry_product_id,
-                cost_entry_pricing_id,
-                key,
-                value
-            FROM reporting_awscostentrylineitem_daily AS li,
-                jsonb_each_text(li.tags) tags
-        ) li
+        FROM reporting_awscostentrylineitem_daily AS li
         JOIN reporting_awscostentryproduct AS p
             ON li.cost_entry_product_id = p.id
         LEFT JOIN reporting_awscostentrypricing as pr
@@ -127,10 +109,38 @@ CREATE TEMPORARY TABLE reporting_awscostentrylineitem_daily_summary_{uuid} AS (
             li.availability_zone,
             p.region,
             p.instance_type,
-            pr.unit,
-            li.key,
-            li.value
-    ) t
+            pr.unit
+    ) AS li
+    LEFT JOIN aws_tag_summary_{uuid} as ats
+        ON li.usage_start = ats.usage_start
+            AND (
+                li.product_code = ats.product_code
+                OR (li.product_code IS NULL AND ats.product_code IS NULL)
+            )
+            AND (
+                li.product_family = ats.product_family
+                OR (li.product_family IS NULL AND ats.product_family IS NULL)
+            )
+            AND (
+                li.usage_account_id = ats.usage_account_id
+                OR (li.usage_account_id IS NULL AND ats.usage_account_id IS NULL)
+            )
+            AND (
+                li.availability_zone = ats.availability_zone
+                OR (li.availability_zone IS NULL AND ats.availability_zone IS NULL)
+            )
+            AND (
+                li.region = ats.region
+                OR (li.region IS NULL AND ats.region IS NULL)
+            )
+            AND (
+                li.instance_type = ats.instance_type
+                OR (li.instance_type IS NULL AND ats.instance_type IS NULL)
+            )
+            AND (
+                li.unit = ats.unit
+                OR (li.unit IS NULL AND ats.unit IS NULL)
+            )
 )
 ;
 
