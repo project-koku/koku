caaa94ff28e9b481809faf11636857e6a659b385
Andrew Berglund <aberglun@redhat.com>
2019-02-26 15:18:25 -0500

    Loading up the provider map with capacity options

--- koku/api/report/queries.py
+++ koku/api/report/queries.py
@@ -238,8 +238,10 @@ class ProviderMap(object):
                         'usage': Sum('pod_usage_cpu_core_hours'),
                         'request': Sum('pod_request_cpu_core_hours'),
                         'limit': Sum('pod_limit_cpu_core_hours'),
-                        # Capacity will be set in the query handler
-                        'capacity': Value(0, output_field=DecimalField()),
+                        'capacity': {
+                            'total': Max('total_capacity_cpu_core_hours'),
+                            'cluster': Max('cluster_capacity_cpu_core_hours'),
+                        },
                         'charge': Sum('pod_charge_cpu_core_hours'),
                         'units': Value('Core-Hours', output_field=CharField())
                     },
@@ -267,8 +269,10 @@ class ProviderMap(object):
                         'usage': Sum('pod_usage_memory_gigabyte_hours'),
                         'request': Sum('pod_request_memory_gigabyte_hours'),
                         'limit': Sum('pod_limit_memory_gigabyte_hours'),
-                        # Capacity will be set in the query handler
-                        'capacity': Value(0, output_field=DecimalField()),
+                        'capacity': {
+                            'total': Max('total_capacity_memory_gigabyte_hours'),
+                            'cluster': Max('cluster_capacity_memory_gigabyte_hours'),
+                        },
                         'charge': Sum('pod_charge_memory_gigabyte_hours'),
                         'units': Value('GB-Hours', output_field=CharField())
                     },
@@ -298,8 +302,10 @@ class ProviderMap(object):
                     'annotations': {
                         'usage': Sum('persistentvolumeclaim_usage_gigabyte_months'),
                         'request': Sum('volume_request_storage_gigabyte_months'),
-                        # Capacity will be set in the query handler
-                        'capacity': Value(0, output_field=DecimalField()),
+                        'capacity': {
+                            'total': Sum('persistentvolumeclaim_capacity_gigabyte_months'),
+                            'cluster': Sum('persistentvolumeclaim_capacity_gigabyte_months'),
+                        },
                         'charge': Sum('persistentvolumeclaim_charge_gb_month'),
                         'units': Value('GB-Mo', output_field=CharField()),
                     },
