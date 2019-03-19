0cb338556b956c05c9de3ccc6b8db0f0884c3514
Brett Lentz <blentz@redhat.com>
2019-03-19 15:23:19 -0400

    remove logging

--- koku/api/query_handler.py
+++ koku/api/query_handler.py
@@ -111,7 +111,6 @@ class QueryHandler(object):
         The default is 'total'
         """
         order_by = self.query_parameters.get('order_by', self.default_ordering)
-        LOG.info('XXX: %s', list(order_by.keys()))
         return list(order_by.keys()).pop()
 
     @property
@@ -123,7 +122,6 @@ class QueryHandler(object):
 
         """
         order_by = self.query_parameters.get('order_by', self.default_ordering)
-        LOG.info('XXX: %s', list(order_by.values()))
         return list(order_by.values()).pop()
 
     @property
