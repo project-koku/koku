b42c9e9a82ed56965450ddd9dbbf6d542657f082
Brett Lentz <blentz@users.noreply.github.com>
2019-03-25 09:08:40 -0400

    add optional rdsexporter container (#767)

---
+++ openshift/rdsexporter/deploy_config.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+#
+# Copyright 2018 Red Hat, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+"""RDS Exporter config file deployer."""
+
+import json
+import os
+import sys
+import yaml
+
+APP_ROOT = os.environ.get('APP_ROOT')
+CONFIG = os.environ.get('RDS_EXPORTER_CONFIG')
+
+if APP_ROOT and CONFIG:
+    FILENAME = '{}/etc/config.yml'.format(APP_ROOT)
+    with open(FILENAME, 'w') as fh:
+        yaml.safe_dump(json.loads(CONFIG), fh, default_flow_style=False)
+else:
+    print('Environment vars APP_ROOT and RDS_EXPORTER_CONFIG are required.')
+    sys.exit(-1)
