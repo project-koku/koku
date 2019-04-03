c6bcd6fc040173ceb62597f43548057787fbc0fc
Douglas Curtis <docurtis@redhat.com>
2019-04-03 13:25:27 -0400

    hanlding project special case for ocp-on-aws

--- koku/api/report/ocp_aws/ocp_aws_query_handler.py
+++ koku/api/report/ocp_aws/ocp_aws_query_handler.py
@@ -47,7 +47,8 @@ class OCPAWSReportQueryHandler(AWSReportQueryHandler):
 
         # Update which field is used to calculate cost by group by param.
         group_by = self._get_group_by()
-        if group_by and group_by[0] == 'project':
+        if (group_by and group_by[0] == 'project') or \
+            'project' in self.query_parameters.get('filter', {}).keys():
             self._report_type = self._report_type + '_by_project'
             self._mapper = ProviderMap(provider=provider,
                                        report_type=self._report_type)
