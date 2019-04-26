f04e96ccea529b4e4abc52ecb2d26aec4c6d080d
Douglas Curtis <docurtis@redhat.com>
2019-04-26 11:29:05 -0400

    Bugfix ocp cost summary double count (#424)

--- database/sql/reporting_ocpcosts_summary.sql
+++ database/sql/reporting_ocpcosts_summary.sql
@@ -1,4 +1,81 @@
 CREATE TEMPORARY TABLE reporting_ocpcosts_summary_{uuid} AS (
+    WITH cte_storage_tag_matched AS (
+        SELECT aws_id
+        FROM (
+            SELECT id as aws_id,
+                usage_start,
+                pds.resource_id,
+                LOWER(key) as key,
+                LOWER(value) as value
+            FROM reporting_ocpawscostlineitem_project_daily_summary AS pds,
+                jsonb_each_text(pds.tags) tags
+            WHERE date(pds.usage_start) >= '{start_date}'
+                AND date(pds.usage_start) <= '{end_date}'
+                AND pds.cluster_id = '{cluster_id}'
+
+        ) AS aws
+        JOIN (
+            SELECT lids.id as ocp_id,
+                lids.usage_start,
+                lids.cluster_alias,
+                lids.node,
+                lids.namespace,
+                LOWER(key) as key,
+                LOWER(value) as value
+            FROM reporting_ocpstoragelineitem_daily_summary AS lids,
+                jsonb_each_text(lids.volume_labels) labels
+            WHERE date(lids.usage_start) >= '{start_date}'
+                AND date(lids.usage_start) <= '{end_date}'
+                AND lids.cluster_id = '{cluster_id}'
+        ) AS ocp
+        ON aws.usage_start::date = ocp.usage_start::date
+            AND (
+                (aws.key = ocp.key AND aws.value = ocp.value)
+                OR (aws.key = 'openshift_cluster' AND aws.value = ocp.cluster_alias)
+                OR (aws.key = 'openshift_node' AND aws.value = ocp.node)
+                OR (aws.key = 'openshift_project' AND aws.value = ocp.namespace)
+            )
+        GROUP BY aws_id
+    ),
+    cte_pod_matched AS (
+        SELECT aws_id
+        FROM (
+            SELECT pds.id as aws_id,
+                pds.usage_start,
+                pds.resource_id,
+                LOWER(key) as key,
+                LOWER(value) as value
+            FROM reporting_ocpawscostlineitem_project_daily_summary AS pds,
+                jsonb_each_text(pds.tags) tags
+            WHERE date(pds.usage_start) >= '{start_date}'
+                AND date(pds.usage_start) <= '{end_date}'
+                AND pds.cluster_id = '{cluster_id}'
+        ) AS aws
+        JOIN (
+            SELECT lids.id as ocp_id,
+                lids.usage_start,
+                lids.resource_id,
+                lids.cluster_alias,
+                lids.node,
+                lids.namespace,
+                LOWER(key) as key,
+                LOWER(value) as value
+            FROM reporting_ocpusagelineitem_daily_summary AS lids,
+                jsonb_each_text(lids.pod_labels) labels
+            WHERE date(lids.usage_start) >= '{start_date}'
+                AND date(lids.usage_start) <= '{end_date}'
+                AND lids.cluster_id = '{cluster_id}'
+        ) AS ocp
+        ON aws.usage_start::date = ocp.usage_start::date
+            AND (
+                (aws.key = ocp.key AND aws.value = ocp.value)
+                OR (aws.key = 'openshift_cluster' AND aws.value = ocp.cluster_alias)
+                OR (aws.key = 'openshift_node' AND aws.value = ocp.node)
+                OR (aws.key = 'openshift_project' AND aws.value = ocp.namespace)
+                OR (aws.resource_id = ocp.resource_id)
+            )
+        GROUP BY aws_id
+    )
     SELECT usageli.usage_start,
         usageli.usage_end,
         usageli.cluster_id,
@@ -21,6 +98,8 @@ CREATE TEMPORARY TABLE reporting_ocpcosts_summary_{uuid} AS (
             sum(ocp_aws.unblended_cost / ocp_aws.shared_projects) as infra_cost,
             sum(ocp_aws.project_cost) as project_infra_cost
         FROM reporting_ocpawscostlineitem_project_daily_summary AS ocp_aws
+        JOIN cte_storage_tag_matched AS stm
+            ON ocp_aws.id = stm.aws_id
         GROUP BY cluster_id,
                 usage_start,
                 namespace,
@@ -30,8 +109,8 @@ CREATE TEMPORARY TABLE reporting_ocpcosts_summary_{uuid} AS (
             AND usageli.cluster_id = ocp_aws.cluster_id
             AND usageli.namespace = ocp_aws.namespace
             AND usageli.node = ocp_aws.node
-    WHERE usageli.usage_start >= '{start_date}'
-        AND usageli.usage_start <= '{end_date}'
+    WHERE date(usageli.usage_start) >= '{start_date}'
+        AND date(usageli.usage_start) <= '{end_date}'
         AND usageli.cluster_id = '{cluster_id}'
 
     UNION
@@ -58,6 +137,8 @@ CREATE TEMPORARY TABLE reporting_ocpcosts_summary_{uuid} AS (
             sum(ocp_aws.unblended_cost / ocp_aws.shared_projects) as infra_cost,
             sum(ocp_aws.project_cost) as project_infra_cost
         FROM reporting_ocpawscostlineitem_project_daily_summary AS ocp_aws
+        JOIN cte_pod_matched AS pm
+            ON ocp_aws.id = pm.aws_id
         GROUP BY cluster_id,
                 usage_start,
                 namespace,
@@ -67,16 +148,16 @@ CREATE TEMPORARY TABLE reporting_ocpcosts_summary_{uuid} AS (
             AND storageli.cluster_id = ocp_aws.cluster_id
             AND storageli.namespace = ocp_aws.namespace
             AND storageli.node = ocp_aws.node
-    WHERE storageli.usage_start >= '{start_date}'
-        AND storageli.usage_start <= '{end_date}'
+    WHERE date(storageli.usage_start) >= '{start_date}'
+        AND date(storageli.usage_start) <= '{end_date}'
         AND storageli.cluster_id = '{cluster_id}'
 )
 ;
 
 -- Clear out old entries first
 DELETE FROM reporting_ocpcosts_summary
-WHERE usage_start >= '{start_date}'
-    AND usage_start <= '{end_date}'
+WHERE date(usage_start) >= '{start_date}'
+    AND date(usage_start) <= '{end_date}'
     AND cluster_id = '{cluster_id}'
 ;
 
