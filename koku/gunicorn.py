afc618f654ae481af10e77e88f70164c0e9e042e
Andrew Berglund <aberglun@redhat.com>
2019-08-02 09:52:17 -0400

    Switch to using thread worker for gunicorn (#969)

--- koku/gunicorn.py
+++ koku/gunicorn.py
@@ -5,6 +5,7 @@ import os
 bind = 'unix:/var/run/koku/gunicorn.sock'
 cpu_resources = int(os.environ.get('POD_CPU_LIMIT', multiprocessing.cpu_count()))
 workers = cpu_resources * 2
+threads = 10
 timeout = int(os.environ.get('TIMEOUT', '90'))
 loglevel = os.environ.get('LOG_LEVEL', 'INFO')
 graceful_timeout = int(os.environ.get('GRACEFUL_TIMEOUT', '180'))
