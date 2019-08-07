9a2f7bb0213be081549cdd400259d552aaad2428
Brad Smith <brasmith@redhat.com>
2019-08-07 14:23:14 -0400

    clean up testing helper scripts
    - prevent prompting for password
    - use PG variables to simplify code
    - use purpose-specific postgres commands (e.g. `dropuser`)
    - echo informational messages when optional commands fail
    - exit non-zero when required commands fail
    - drop redundant shell arguments since environment variables are already set
    - add missing shebangs

--- scripts/create_test_db_user.sh
+++ scripts/create_test_db_user.sh
@@ -1,8 +1,27 @@
-DB_USER=$1
-DB_ADMIN=$2
+#!/usr/bin/env bash
+# Recreate the Koku database and user.
+#
+# Assumes environment variables have been set:
+#   - POSTGRES_SQL_SERVICE_PORT
+#   - POSTGRES_SQL_SERVICE_HOST
+#   - DATABASE_ADMIN (postgres admin user)
+#   - DATABASE_PASSWORD (postgres admin user's password)
+#   - DATABASE_USER (postgres user to be recreated)
+#   - DATABASE_NAME (postgres database to be recreated)
 
-psql ${DATABASE_NAME} -p ${POSTGRES_SQL_SERVICE_PORT} -h ${POSTGRES_SQL_SERVICE_HOST} -U ${DB_ADMIN} -c "DROP DATABASE IF EXISTS test_${DATABASE_NAME};" || true
+export PGPASSWORD="${DATABASE_PASSWORD}"
+export PGPORT="${POSTGRES_SQL_SERVICE_PORT}"
+export PGHOST="${POSTGRES_SQL_SERVICE_HOST}"
+export PGUSER="${DATABASE_ADMIN}"
 
-psql ${DATABASE_NAME} -p ${POSTGRES_SQL_SERVICE_PORT} -h ${POSTGRES_SQL_SERVICE_HOST} -U ${DB_ADMIN} -c "DROP USER ${DB_USER};" || true
+dropuser -w --if-exists "${DATABASE_USER}" || \
+    echo "NOTICE: Could not drop user '${DATABASE_USER}'"
+dropdb -w --if-exists "${DATABASE_NAME}" || \
+    echo "NOTICE: Could not drop database '${DATABASE_NAME}'"
 
-psql ${DATABASE_NAME} -p ${POSTGRES_SQL_SERVICE_PORT} -h ${POSTGRES_SQL_SERVICE_HOST} -U ${DB_ADMIN} -c "CREATE USER ${DB_USER} WITH SUPERUSER LOGIN PASSWORD '${DATABASE_PASSWORD}';"
+# Chain the following commands because they must *all* succeed,
+# or else this script must exit with non-zero code.
+createdb -w "${DATABASE_NAME}" && \
+createuser -w -s "${DATABASE_USER}" && \
+psql -w --quiet -d "${DATABASE_NAME}" \
+    -c "ALTER USER ${DATABASE_USER} WITH PASSWORD '${DATABASE_PASSWORD}'"
--- scripts/drop_test_db_user.sh
+++ scripts/drop_test_db_user.sh
@@ -1,4 +1,17 @@
-DB_USER=$1
-DB_ADMIN=$2
+#!/usr/bin/env bash
+# Drop the Koku database user.
+#
+# Assumes environment variables have been set:
+#   - POSTGRES_SQL_SERVICE_PORT
+#   - POSTGRES_SQL_SERVICE_HOST
+#   - DATABASE_ADMIN (postgres admin user)
+#   - DATABASE_PASSWORD (postgres admin user's password)
+#   - DATABASE_USER (postgres user to be dropped)
 
-psql ${DATABASE_NAME} -p ${POSTGRES_SQL_SERVICE_PORT} -h ${POSTGRES_SQL_SERVICE_HOST} -U $DB_ADMIN -c "DROP USER ${DB_USER};" || true
+export PGPASSWORD="${DATABASE_PASSWORD}"
+export PGPORT="${POSTGRES_SQL_SERVICE_PORT}"
+export PGHOST="${POSTGRES_SQL_SERVICE_HOST}"
+export PGUSER="${DATABASE_ADMIN}"
+
+dropuser -w --if-exists "${DATABASE_USER}" || \
+    echo "WARNING: Could not drop user '${DATABASE_USER}'"
