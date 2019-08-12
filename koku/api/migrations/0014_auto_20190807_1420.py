5c2748ddded395b5700c9242ccaecb2b71c7ab4d
Andrew Berglund <aberglun@redhat.com>
2019-08-12 16:35:19 -0400

    Rename migration to resolve conflict

--- koku/api/migrations/0014_auto_20190807_1420.py
+++ koku/api/migrations/0014_auto_20190807_1420.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('api', '0012_auto_20190723_1655'),
+        ('api', '0013_auto_20190812_1815'),
     ]
 
     operations = [
