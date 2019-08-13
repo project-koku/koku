a6b2dea0d28e13beda30c09afd371b4b9319e892
Chris Hambridge <chambrid@redhat.com>
2019-08-13 12:01:30 -0400

    Update to call with correct port.

--- scripts/run_migrations.sh
+++ scripts/run_migrations.sh
@@ -2,7 +2,8 @@
 
 APP_NAME_DEFAULT="koku"
 APP_NAME="${1:-$APP_NAME_DEFAULT}"
-TARGET_DEFAULT="http://${APP_NAME}.${APP_NAMESPACE}.svc.cluster.local"
+APP_PORT="${$KOKU_SERVICE_PORT:-'8080'}"
+TARGET_DEFAULT="http://${APP_NAME}.${APP_NAMESPACE}.svc.cluster.local:${APP_PORT}"
 TARGET="${2:-$TARGET_DEFAULT}"
 COMMIT=`curl  -X GET ${TARGET}${API_PATH_PREFIX}/v1/status/ | sed 's/{.*commit":"*\([0-9a-zA-Z]*\)"*,*.*}/\1/'`
 
