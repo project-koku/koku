77e4b87a1ddb4b9b4b872e3b2e050ff16c260f82
Chris Hambridge <chambrid@redhat.com>
2019-08-13 12:24:43 -0400

    Fix bash substitution.

--- scripts/run_migrations.sh
+++ scripts/run_migrations.sh
@@ -2,7 +2,7 @@
 
 APP_NAME_DEFAULT="koku"
 APP_NAME="${1:-$APP_NAME_DEFAULT}"
-APP_PORT="${$KOKU_SERVICE_PORT:-'8080'}"
+APP_PORT="${KOKU_SERVICE_PORT:-'8080'}"
 TARGET_DEFAULT="http://${APP_NAME}.${APP_NAMESPACE}.svc.cluster.local:${APP_PORT}"
 TARGET="${2:-$TARGET_DEFAULT}"
 COMMIT=`curl  -X GET ${TARGET}${API_PATH_PREFIX}/v1/status/ | sed 's/{.*commit":"*\([0-9a-zA-Z]*\)"*,*.*}/\1/'`
