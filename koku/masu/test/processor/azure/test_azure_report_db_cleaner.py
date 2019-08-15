4b0180089e48c639ea85e411ac51d1443fda2883
Douglas Curtis <docurtis@redhat.com>
2019-08-15 14:27:59 -0400

    processor stub tests

---
+++ koku/masu/test/processor/azure/test_azure_report_db_cleaner.py
@@ -0,0 +1,36 @@
+#
+# Copyright 2019 Red Hat, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+"""Test the AzureReportDBCleaner object."""
+from masu.database.provider_db_accessor import ProviderDBAccessor
+from masu.processor.azure.azure_report_db_cleaner import AzureReportDBCleaner
+from masu.test import MasuTestCase
+
+
+class AzureReportDBCleanerTest(MasuTestCase):
+    """Test Cases for the AzureReportChargeUpdater object."""
+
+    def setUp(self):
+        """Set up each test."""
+        super().setUp()
+
+        self.cleaner = AzureReportDBCleaner(
+            schema=self.schema)
+
+    def test_purge_expired_report_data(self):
+        """Test to verify that expired Azure report data is cleared."""
+        self.cleaner.purge_expired_report_data()
---
+++ koku/masu/test/processor/azure/test_azure_report_summary_updater.py
@@ -0,0 +1,43 @@
+#
+# Copyright 2019 Red Hat, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+"""Test the AzureReportSummaryUpdater object."""
+from masu.database.provider_db_accessor import ProviderDBAccessor
+from masu.processor.azure.azure_report_summary_updater import AzureReportSummaryUpdater
+from masu.external import GZIP_COMPRESSED, UNCOMPRESSED
+
+from masu.test import MasuTestCase
+
+
+class AzureReportSummaryUpdaterTest(MasuTestCase):
+    """Test Cases for the AzureReportSummaryUpdater object."""
+
+    def setUp(self):
+        """Set up each test."""
+        super().setUp()
+        self.updater = AzureReportSummaryUpdater(
+           self.schema, self.azure_provider, None
+        )
+
+
+    def test_update_daily_tables(self):
+        """Test process method."""
+        self.updater.update_daily_tables(None, None)
+
+    def test_update_summary_tables(self):
+        """Test verify temporary files are removed."""
+        self.updater.update_summary_tables(None, None)
