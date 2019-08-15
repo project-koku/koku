a2eb62e1098bcdd658e4d4328b1f7f3237a9fc7d
Brad Smith <brasmith@redhat.com>
2019-08-15 11:36:08 -0400

    do not allow creating duplicate pending data export requests

--- koku/api/dataexport/test/tests_views.py
+++ koku/api/dataexport/test/tests_views.py
@@ -8,6 +8,37 @@ from api.iam.test.iam_test_case import IamTestCase
 class DataExportRequestViewSetTest(IamTestCase):
     """DataExportRequestViewSet test case."""
 
+    def test_second_request_with_same_dates_fails(self):
+        """
+        Test saving a request and then the same request again.
+
+        We expect the first request to succeed and the second to fail.
+        This prevents the customer from overwhelming the export process with
+        unnecessary requests.
+        """
+        start_date = '2019-01-01'
+        end_date = '2019-02-01'
+        post_data = {'start_date': start_date, 'end_date': end_date}
+
+        url = reverse('dataexportrequests-list')
+        client = APIClient()
+        # First request should succeed.
+        response = client.post(url, data=post_data, **self.headers)
+        self.assertEqual(response.status_code, 201)
+        response_json = response.json()
+        self.assertEqual(response_json['start_date'], start_date)
+        self.assertEqual(response_json['end_date'], end_date)
+        self.assertEqual(response_json['status'], 'pending')
+
+        # Second request should fail.
+        response = client.post(url, data=post_data, **self.headers)
+        self.assertEqual(response.status_code, 400)
+        response_json = response.json()
+        self.assertEqual(len(response_json['errors']), 1)
+        error = response_json['errors'][0]
+        self.assertEqual(error['status'], 400)
+        self.assertEqual(error['source'], 'non_field_errors')
+
     def test_end_date_before_start_date_fails(self):
         """Test refusing a request with end date before start date."""
         start_date = '2019-02-01'
