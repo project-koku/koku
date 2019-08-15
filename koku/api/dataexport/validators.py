812f25e7cafeb2e0b6df5c572b21d77e314f6c55
Brad Smith <brasmith@redhat.com>
2019-08-15 11:36:08 -0400

    do not allow creating duplicate pending data export requests

--- koku/api/dataexport/validators.py
+++ koku/api/dataexport/validators.py
@@ -3,9 +3,30 @@
 from django.utils.translation import gettext_lazy as _
 from rest_framework.exceptions import ValidationError
 
+from api.dataexport.models import DataExportRequest
+
 
 class DataExportRequestValidator:
-    """Validator that ensures date fields are appropriately ordered."""
+    """Validator that ensures date fields are appropriately defined for a new request."""
+
+    def set_context(self, serializer):
+        """
+        Set extra data from the serializer so we can do extra lookup validation.
+
+        This hook is called by the serializer instance prior to the validation call being made.
+        """
+        self.queryset = serializer.context['view'].get_queryset()
+        self.instance = getattr(serializer, 'instance', None)
+
+    def pending_instance_exists(self, start_date, end_date):
+        """Check for a pending instance that matches the requested dates."""
+        if self.instance is not None:
+            # This is an update and does not need to check for existence.
+            return
+        queryset = self.queryset.filter(
+            status=DataExportRequest.PENDING, start_date=start_date, end_date=end_date
+        )
+        return queryset.exists()
 
     def __call__(self, attrs):
         """Enforce validation of all relevant fields."""
@@ -17,3 +38,8 @@ class DataExportRequestValidator:
                 'end_date': _('"end_date" must not be older than "start_date".'),
             }
             raise ValidationError(bad_items, code='bad_request')
+        if self.pending_instance_exists(start_date, end_date):
+            exists_message = _(
+                'A pending data export already exists with the given "start_date" and "end_date".'
+            )
+            raise ValidationError(exists_message, code='bad_request')
