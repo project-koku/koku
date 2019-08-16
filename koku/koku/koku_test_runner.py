187d87d952cccb42d58c7d09d00fd2b53af8b26c
Andrew Berglund <aberglun@redhat.com>
2019-08-15 21:08:38 -0400

    Use Django setup_databases with a tenant

--- koku/koku/koku_test_runner.py
+++ koku/koku/koku_test_runner.py
@@ -18,8 +18,9 @@
 """Koku Test Runner."""
 import logging
 
+from django.db import connections
 from django.test.runner import DiscoverRunner
-from django.test.utils import setup_databases
+from django.test.utils import get_unique_databases_and_mirrors
 
 from api.models import Tenant
 
@@ -39,7 +40,49 @@ class KokuTestRunner(DiscoverRunner):
             self.parallel, **kwargs
         )
 
-        tenant = Tenant.objects.get_or_create(schema_name=KokuTestRunner.schema)[0]
-        tenant.save()
-
         return main_db
+
+def setup_databases(verbosity, interactive, keepdb=False, debug_sql=False, parallel=0, aliases=None, **kwargs):
+    """Create the test databases."""
+    test_databases, mirrored_aliases = get_unique_databases_and_mirrors(aliases)
+
+    old_names = []
+
+    for db_name, aliases in test_databases.values():
+        first_alias = None
+        for alias in aliases:
+            connection = connections[alias]
+            old_names.append((connection, db_name, first_alias is None))
+
+            # Actually create the database for the first connection
+            if first_alias is None:
+                first_alias = alias
+                connection.creation.create_test_db(
+                    verbosity=verbosity,
+                    autoclobber=not interactive,
+                    keepdb=keepdb,
+                    serialize=connection.settings_dict.get('TEST', {}).get('SERIALIZE', True),
+                )
+                tenant = Tenant.objects.get_or_create(schema_name=KokuTestRunner.schema)[0]
+                tenant.save()
+                if parallel > 1:
+                    for index in range(parallel):
+                        connection.creation.clone_test_db(
+                            suffix=str(index + 1),
+                            verbosity=verbosity,
+                            keepdb=keepdb,
+                        )
+            # Configure all other connections as mirrors of the first one
+            else:
+                connections[alias].creation.set_as_test_mirror(connections[first_alias].settings_dict)
+
+    # Configure the test mirrors.
+    for alias, mirror_alias in mirrored_aliases.items():
+        connections[alias].creation.set_as_test_mirror(
+            connections[mirror_alias].settings_dict)
+
+    if debug_sql:
+        for alias in connections:
+            connections[alias].force_debug_cursor = True
+
+    return old_names
