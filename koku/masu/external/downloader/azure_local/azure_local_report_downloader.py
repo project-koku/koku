6cddbafc93827e55f02455406738715384b1120f
Douglas Curtis <docurtis@redhat.com>
2019-08-27 15:17:16 -0400

    working on downloader

--- koku/masu/external/downloader/azure_local/azure_local_report_downloader.py
+++ koku/masu/external/downloader/azure_local/azure_local_report_downloader.py
@@ -1,5 +1,5 @@
 #
-# Copyright 2018 Red Hat, Inc.
+# Copyright 2019 Red Hat, Inc.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
@@ -14,105 +14,96 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
-"""Azure Local Report Downloader."""
+"""Azure-Local Report Downloader."""
 
-# pylint: skip-file
-# Having trouble disabling the lint warning for duplicate-code (AWSReportDownloader..)
-# Disabling pylint on this file since AWSLocalReportDownloader is a DEBUG feature.
-
+# pylint: disable=fixme
+# disabled until we get travis to not fail on warnings, or the fixme is
+# resolved.
 import datetime
-import hashlib
 import json
 import logging
 import os
-import re
-import shutil
 
 from masu.config import Config
+from masu.external import UNCOMPRESSED
+from masu.external.downloader.azure.azure_service import AzureCostReportNotFound, AzureService
 from masu.external.downloader.downloader_interface import DownloaderInterface
 from masu.external.downloader.report_downloader_base import ReportDownloaderBase
-from masu.util.aws import common as utils
+from masu.util.azure import common as utils
+from masu.util.common import extract_uuids_from_string
 
 DATA_DIR = Config.TMP_DIR
 LOG = logging.getLogger(__name__)
 
 
+class AzureReportDownloaderError(Exception):
+    """Azure Report Downloader error."""
+
+
 class AzureReportDownloaderNoFileError(Exception):
-    """AWS Report Downloader error for missing file."""
-
-    pass
+    """Azure Report Downloader error for missing file."""
 
 
-class AzureReportDownloader(ReportDownloaderBase, DownloaderInterface):
-    """Local Cost and Usage Report Downloader."""
+class AzureLocalReportDownloader(ReportDownloaderBase, DownloaderInterface):
+    """Azure Cost and Usage Report Downloader."""
 
-    empty_manifest = {'reportKeys': []}
-
-    def __init__(self, customer_name, auth_credential, bucket, report_name=None, **kwargs):
+    def __init__(self, customer_name, auth_credential, billing_source, report_name=None, **kwargs):
         """
-        Initializer.
+        Constructor.
 
         Args:
             customer_name    (String) Name of the customer
-            auth_credential  (String) Authentication credential for S3 bucket (RoleARN)
+            auth_credential  (Dict) Dictionary containing Azure authentication details.
             report_name      (String) Name of the Cost Usage Report to download (optional)
-            bucket           (String) Name of the S3 bucket containing the CUR
+            billing_source   (Dict) Dictionary containing Azure Storage blob details.
 
         """
         super().__init__(**kwargs)
 
+        self._provider_id = kwargs.get('provider_id')
         self.customer_name = customer_name.replace(' ', '_')
+        self._azure_client = self._get_azure_client(auth_credential, billing_source)
+        export_reports = self._azure_client.describe_cost_management_exports()
+        export_report = export_reports[0] if export_reports else {}
 
-        LOG.debug('Connecting to local service provider...')
-        prefix, name = self._extract_names(bucket)
+        self.export_name = export_report.get('name')
+        self.container_name = export_report.get('container')
+        self.directory = export_report.get('directory')
 
-        if report_name:
-            self.report_name = report_name
-        else:
-            self.report_name = name
-        self.report_prefix = prefix
+    @staticmethod
+    def _get_azure_client(credentials, billing_source):
+        subscription_id = credentials.get('subscription_id')
+        tenant_id = credentials.get('tenant_id')
+        client_id = credentials.get('client_id')
+        client_secret = credentials.get('client_secret')
+        resource_group_name = billing_source.get('resource_group')
+        storage_account_name = billing_source.get('storage_account')
 
-        LOG.info('Found report name: %s, report prefix: %s', self.report_name, self.report_prefix)
-        if self.report_prefix:
-            self.base_path = '{}/{}/'.format(bucket, self.report_prefix)
-        else:
-            self.base_path = bucket
-        self.bucket_path = bucket
-        self.bucket = bucket.replace('/', '_')
-        self.credential = auth_credential
+        service = AzureService(subscription_id, tenant_id, client_id, client_secret,
+                               resource_group_name, storage_account_name)
+        return service
 
-    @property
-    def manifest_date_format(self):
-        """Set the AWS manifest date format."""
-        return '%Y%m%dT000000.000Z'
+    def _get_exports_data_directory(self):
+        """Return the path of the exports temporary data directory."""
+        directory_path = f'{DATA_DIR}/{self.customer_name}/azure/{self.container_name}'
+        os.makedirs(directory_path, exist_ok=True)
+        return directory_path
 
-    def _extract_names(self, bucket):
+    def _get_report_path(self, date_time):
         """
-        Find the report name and prefix given the bucket path.
+        Return path of report files.
 
         Args:
-            bucket (String): Path to the local file
+            date_time (DateTime): The starting datetime object
 
         Returns:
-            (String, String) report_prefix, report_name
+            (String): "/blob_dir/export_name/YYYYMMDD-YYYYMMDD",
+                    example: "/cost/costreport/20190801-20190831"
 
         """
-        daterange = '\d{8}-\d{8}'  # noqa: W605
-        full_path = ''
-        for item in os.walk(bucket, followlinks=True):
-            if not item[2]:
-                if any(re.findall(daterange, date) for date in item[1]):
-                    full_path = item[0]
-                    break
-        directories = full_path[len(bucket):]
-
-        report_prefix = None
-        report_name = None
-        if directories:
-            parts = directories.strip('/').split('/')
-            report_name = parts.pop()
-            report_prefix = parts.pop() if parts else None
-        return report_prefix, report_name
+        report_date_range = utils.month_date_range(date_time)
+        return '{}/{}/{}'.format(self.directory, self.export_name,
+                                 report_date_range)
 
     def _get_manifest(self, date_time):
         """
@@ -125,68 +116,32 @@ class AzureReportDownloader(ReportDownloaderBase, DownloaderInterface):
             (Dict): A dict-like object serialized from JSON data.
 
         """
-        manifest = '{}/{}-Manifest.json'.format(self._get_report_path(date_time),
-                                                self.report_name)
+        report_path = self._get_report_path(date_time)
+        manifest = {}
+        try:
+            blob = self._azure_client.get_latest_cost_export_for_path(report_path, self.container_name)
+        except AzureCostReportNotFound as ex:
+            LOG.error('Unable to find manifest. Error: %s', str(ex))
+            return manifest
+        report_name = blob.name
 
         try:
-            manifest_file, _ = self.download_file(manifest)
-        except AWSReportDownloaderNoFileError as err:
-            LOG.error('Unable to get report manifest. Reason: %s', str(err))
-            return self.empty_manifest
+            manifest['assemblyId'] = extract_uuids_from_string(report_name).pop()
+        except IndexError:
+            message = 'Unable to extract assemblyID from %s'.format(report_name)
+            raise AzureReportDownloaderError(message)
 
-        manifest_json = None
-        with open(manifest_file, 'r') as manifest_file_handle:
-            manifest_json = json.load(manifest_file_handle)
+        billing_period = {'start': (report_path.split('/')[-1]).split('-')[0],
+                          'end': (report_path.split('/')[-1]).split('-')[1]}
+        manifest['billingPeriod'] = billing_period
+        manifest['reportKeys'] = [report_name]
+        manifest['Compression'] = UNCOMPRESSED
 
-        return manifest_json
+        manifest_file = '{}/{}'.format(self._get_exports_data_directory(), 'Manifest.json')
+        with open(manifest_file, 'w') as manifest_hdl:
+            manifest_hdl.write(json.dumps(manifest))
 
-    def _get_report_path(self, date_time):
-        """
-        Return path of report files.
-
-        Args:
-            date_time (DateTime): The starting datetime object
-
-        Returns:
-            (String): "/prefix/report_name/YYYYMMDD-YYYYMMDD",
-                    example: "/my-prefix/my-report/19701101-19701201"
-
-        """
-        report_date_range = utils.month_date_range(date_time)
-        return '{}/{}/{}'.format(self.base_path,
-                                 self.report_name,
-                                 report_date_range)
-
-    def download_file(self, key, stored_etag=None):
-        """
-        Download an S3 object to file.
-
-        Args:
-            key (str): The S3 object key identified.
-
-        Returns:
-            (String): The path and file name of the saved file
-
-        """
-        local_s3_filename = utils.get_local_file_name(key)
-
-        directory_path = f'{DATA_DIR}/{self.customer_name}/aws-local/{self.bucket}'
-        full_file_path = f'{directory_path}/{local_s3_filename}'
-
-        if not os.path.isfile(key):
-            log_msg = 'Unable to locate {} in {}'.format(key, self.bucket_path)
-            raise AWSReportDownloaderNoFileError(log_msg)
-
-        # Make sure the data directory exists
-        os.makedirs(directory_path, exist_ok=True)
-        s3_etag_hasher = hashlib.new('ripemd160')
-        s3_etag_hasher.update(bytes(local_s3_filename, 'utf-8'))
-        s3_etag = s3_etag_hasher.hexdigest()
-
-        if s3_etag != stored_etag or not os.path.isfile(full_file_path):
-            LOG.info('Downloading %s to %s', key, full_file_path)
-            shutil.copy2(key, full_file_path)
-        return full_file_path, s3_etag
+        return manifest
 
     def get_report_context_for_date(self, date_time):
         """
@@ -203,24 +158,46 @@ class AzureReportDownloader(ReportDownloaderBase, DownloaderInterface):
                 files       - ([]): List of report files.
 
         """
+        should_download = True
+        manifest_dict = {}
         report_dict = {}
         manifest = self._get_manifest(date_time)
-        manifest_id = None
-        if manifest != self.empty_manifest:
-            manifest_id = self._prepare_db_manifest_record(manifest)
 
-        report_dict['manifest_id'] = manifest_id
-        report_dict['assembly_id'] = manifest.get('assemblyId')
-        report_dict['compression'] = 'GZIP'
-        report_dict['files'] = []
-        for report in manifest.get('reportKeys'):
-            report_path = self.bucket_path + '/' + report
-            report_dict['files'].append(report_path)
+        if manifest != {}:
+            manifest_dict = self._prepare_db_manifest_record(manifest)
+            should_download = self.check_if_manifest_should_be_downloaded(
+                manifest_dict.get('assembly_id')
+            )
+
+        if not should_download:
+            manifest_id = self._get_existing_manifest_db_id(manifest_dict.get('assembly_id'))
+            stmt = ('This manifest has already been downloaded and processed:\n'
+                    ' customer: {},\n'
+                    ' provider_id: {},\n'
+                    ' manifest_id: {}')
+            stmt = stmt.format(self.customer_name,
+                               self._provider_id,
+                               manifest_id)
+            LOG.info(stmt)
+            return report_dict
+
+        if manifest_dict:
+            manifest_id = self._process_manifest_db_record(
+                manifest_dict.get('assembly_id'),
+                manifest_dict.get('billing_start'),
+                manifest_dict.get('num_of_files')
+            )
+
+            report_dict['manifest_id'] = manifest_id
+            report_dict['assembly_id'] = manifest.get('assemblyId')
+            report_dict['compression'] = manifest.get('Compression')
+            report_dict['files'] = manifest.get('reportKeys')
         return report_dict
 
-    def get_local_file_for_report(self, report):
-        """Get full path for local report file."""
-        return utils.get_local_file_name(report)
+    @property
+    def manifest_date_format(self):
+        """Set the Azure manifest date format."""
+        return '%Y%m%d'
 
     def _prepare_db_manifest_record(self, manifest):
         """Prepare to insert or update the manifest DB record."""
@@ -231,4 +208,41 @@ class AzureReportDownloader(ReportDownloaderBase, DownloaderInterface):
             self.manifest_date_format
         )
         num_of_files = len(manifest.get('reportKeys', []))
-        return self._process_manifest_db_record(assembly_id, billing_start, num_of_files)
+        return {
+            'assembly_id': assembly_id,
+            'billing_start': billing_start,
+            'num_of_files': num_of_files
+        }
+
+    @staticmethod
+    def get_local_file_for_report(report):
+        """Get full path for local report file."""
+        return utils.get_local_file_name(report)
+
+    def download_file(self, key, stored_etag=None):
+        """
+        Download a file from Azure bucket.
+
+        Args:
+            key (str): The object key identified.
+
+        Returns:
+            (String): The path and file name of the saved file
+
+        """
+        local_filename = utils.get_local_file_name(key)
+        full_file_path = f'{self._get_exports_data_directory()}/{local_filename}'
+
+        try:
+            blob = self._azure_client.get_cost_export_for_key(key, self.container_name)
+            etag = blob.properties.etag
+        except AzureCostReportNotFound as ex:
+            log_msg = 'Error when downloading Azure report for key: %s. Error %s'.format(key, str(ex))
+            LOG.error(log_msg)
+            raise AzureReportDownloaderError(log_msg)
+
+        if etag != stored_etag:
+            LOG.info('Downloading %s to %s', key, full_file_path)
+            blob = self._azure_client.download_cost_export(key, self.container_name, destination=full_file_path)
+        LOG.info('Returning full_file_path: %s, etag: %s', full_file_path, etag)
+        return full_file_path, etag
