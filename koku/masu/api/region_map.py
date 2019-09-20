74bd7db4cc2f9f5f396bb077e4a038bd188293b5
Brett Lentz <blentz@users.noreply.github.com>
2019-09-20 17:05:48 -0400

    configure cache-control headers for all views. (#1145)
    * configure cache-control headers for all views.
    * vary on RH_IDENTITY header
    * never_cache some views

--- koku/masu/api/region_map.py
+++ koku/masu/api/region_map.py
@@ -17,15 +17,16 @@
 
 """View endpoint for updating region mapping."""
 
+from django.views.decorators.cache import never_cache
 from rest_framework.decorators import api_view, permission_classes, renderer_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.settings import api_settings
 
-
 from masu.util.aws.region_map import update_region_mapping
 
 
+@never_cache
 @api_view(http_method_names=['GET'])
 @permission_classes((AllowAny,))
 @renderer_classes(tuple(api_settings.DEFAULT_RENDERER_CLASSES))
