954a18ef44284d6bb026d5b5b6bf9a6cd18d89d7
Douglas Curtis <docurtis@redhat.com>
2019-10-09 14:27:00 -0400

    empty dir and fixes

--- testing/run_tests.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-#mkdir /test
-#mkdir /test/aws_local && mkdir /test/aws_local_0 && mkdir /test/aws_local_1 && mkdir /test/aws_local_2 && mkdir /test/aws_local_3 && mkdir /test/aws_local_4 && mkdir /test/insights_local
-while :; do echo 'Hit CTRL+C'; sleep 1; done
-#iqe tests plugin hccm -k $TESTS
