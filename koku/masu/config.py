45d712a7815ae1c4ed73c1ea6493f40c7cf8b887
Brett Lentz <blentz@redhat.com>
2019-10-16 10:33:28 -0400

    add minimal auth to rabbit config

--- koku/masu/config.py
+++ koku/masu/config.py
@@ -93,7 +93,7 @@ class Config:
     TMP_DIR = f'{PVC_DIR}/processing'
 
     # Celery settings
-    CELERY_BROKER_URL = f'amqp://{RABBITMQ_HOST}:{RABBITMQ_PORT}'
+    CELERY_BROKER_URL = f'amqp://koku:koku@{RABBITMQ_HOST}:{RABBITMQ_PORT}'
     # CELERY_RESULT_BACKEND = f'amqp://{RABBITMQ_HOST}:{RABBITMQ_PORT}'
 
     REPORT_PROCESSING_BATCH_SIZE = 100000
