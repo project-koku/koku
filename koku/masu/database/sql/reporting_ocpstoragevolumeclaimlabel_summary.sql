47f72f1c1078cc620204245482fa4f9873a5bcc9
nbon12 <nbon12@users.noreply.github.com>
2019-10-16 16:32:04 -0400

    populate_pod_label_summary and populate_volume_claim_label_summary --> safer sql

--- koku/masu/database/sql/reporting_ocpstoragevolumeclaimlabel_summary.sql
+++ koku/masu/database/sql/reporting_ocpstoragevolumeclaimlabel_summary.sql
@@ -1,10 +1,10 @@
-INSERT INTO {schema}.reporting_ocpstoragevolumeclaimlabel_summary
+INSERT INTO {{schema | sqlsafe}}.reporting_ocpstoragevolumeclaimlabel_summary
 SELECT l.key,
     array_agg(DISTINCT l.value) as values
 FROM (
     SELECT key,
         value
-    FROM {schema}.reporting_ocpstoragelineitem_daily AS li,
+    FROM {{schema | sqlsafe}}.reporting_ocpstoragelineitem_daily AS li,
         jsonb_each_text(li.persistentvolumeclaim_labels) labels
 ) l
 GROUP BY l.key
