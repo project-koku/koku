3161db1c7299cbfcfd097e155e33990d5e4d7037
bizhang <bizhang@redhat.com>
2019-10-18 14:54:14 -0400

    Add GCPAccessor
    Clean up GCP report processors, fix and add tests
    
    Update GCP processor logic, move date parser from azure.common to common

--- koku/masu/test/processor/gcp/test_gcp_report_processor.py
+++ koku/masu/test/processor/gcp/test_gcp_report_processor.py
@@ -1,46 +1,53 @@
 """Test GCPReportProcessor."""
 import uuid
+from datetime import datetime
 
-from django.test import TestCase
+import pytz
+from dateutil import parser
 from faker import Faker
 from tenant_schemas.utils import schema_context
 
-from api.models import CostModelMetricsMap, Customer, Tenant
 from api.provider.models import Provider, ProviderAuthentication, ProviderBillingSource
+from masu.database.report_manifest_db_accessor import ReportManifestDBAccessor
+from masu.database.reporting_common_db_accessor import ReportingCommonDBAccessor
+from masu.database.gcp_report_db_accessor import GCPReportDBAccessor
 from masu.external import UNCOMPRESSED
-from masu.processor.gcp.gcp_report_processor import GCPReportProcessor
-from reporting.provider.gcp.models import GCPProject, GCPCostEntryBill, GCPCostEntryLineItemDaily
 from masu.external.date_accessor import DateAccessor
+from masu.processor.gcp.gcp_report_processor import GCPReportProcessor
+from masu.test import MasuTestCase
+from masu.util import common as utils
+from reporting.provider.gcp.models import GCPProject, GCPCostEntryBill, GCPCostEntryLineItemDaily
 
 
 fake = Faker()
 
 
-class GCPReportProcessorTest(TestCase):
+class GCPReportProcessorTest(MasuTestCase):
     """Test Cases for the GCPReportProcessor object."""
 
+    @classmethod
+    def setUpClass(cls):
+        """Set up the test class with required objects."""
+        super().setUpClass()
+        cls.test_report = './koku/masu/test/data/gcp/evidence-2019-06-03.csv'
+
+        cls.date_accessor = DateAccessor()
+        cls.manifest_accessor = ReportManifestDBAccessor()
+
+        with ReportingCommonDBAccessor() as report_common_db:
+            cls.column_map = report_common_db.column_map
+
     def setUp(self):
-        """Set up each test."""
-        self.schema = 'acct10001'
-        self.acct = '10001'
-        self.test_report = './koku/masu/test/data/gcp/evidence-2019-06-03.csv'
-
-        self.date_accessor = DateAccessor()
-
-        self.customer = Customer.objects.create(
-            account_id=self.acct, schema_name=self.schema
-        )
-
+        """Set up GCP tests."""
+        super().setUp()
         gcp_auth = ProviderAuthentication.objects.create(
             credentials={"project-id": fake.word()}
         )
-
         gcp_billing_source = ProviderBillingSource.objects.create(
             data_source={
                 "bucket": fake.word()
             }
         )
-
         self.gcp_provider = Provider.objects.create(
             uuid=uuid.uuid4(),
             name='Test Provider',
@@ -51,20 +58,142 @@ class GCPReportProcessorTest(TestCase):
             setup_complete=True,
         )
 
+        start_time = '2019-09-17T00:00:00-07:00'
+        report_date_range = utils.month_date_range(parser.parse(start_time))
+        start_date, end_date = report_date_range.split('-')
+
+        self.start_date_utc = parser.parse(start_date).replace(hour=0, minute=0, tzinfo=pytz.UTC)
+        self.end_date_utc = parser.parse(end_date).replace(hour=0, minute=0, tzinfo=pytz.UTC)
+
         self.assembly_id = '1234'
+        self.manifest_dict = {
+            'assembly_id': self.assembly_id,
+            'billing_period_start_datetime': self.start_date_utc,
+            'num_total_files': 1,
+            'provider_id': self.gcp_provider.id,
+        }
+        manifest_accessor = ReportManifestDBAccessor()
+        self.manifest = manifest_accessor.add(**self.manifest_dict)
+
         self.processor = GCPReportProcessor(
             schema_name=self.schema,
             report_path=self.test_report,
             compression=UNCOMPRESSED,
             provider_id=self.gcp_provider.id,
+            manifest_id=self.manifest.id
         )
+        self.accessor = GCPReportDBAccessor(self.schema, self.column_map)
 
     def test_gcp_process(self):
-        """Test the processing of an uncompressed GCP file."""
+        """Test the processing of an GCP file writes objects to the database."""
+        self.processor.process()
         with schema_context(self.schema):
-
-            self.assertFalse(GCPCostEntryLineItemDaily.objects.all().exists())
-            self.processor.process()
             self.assertTrue(len(GCPCostEntryLineItemDaily.objects.all()) > 0)
             self.assertTrue(len(GCPProject.objects.all()) > 0)
             self.assertEquals(1, len(GCPCostEntryBill.objects.all()))
+
+    def test_create_gcp_cost_entry_bill(self):
+        """Test calling _get_or_create_cost_entry_bill on an entry bill that doesn't exist creates it."""
+        bill_row = {
+            'Start Time': '2019-09-17T00:00:00-07:00'
+        }
+        entry_bill_id = \
+            self.processor._get_or_create_cost_entry_bill(bill_row, self.accessor)
+
+        with schema_context(self.schema):
+            self.assertTrue(GCPCostEntryBill.objects.filter(id=entry_bill_id).exists())
+
+    def test_get_gcp_cost_entry_bill(self):
+        """Test calling _get_or_create_cost_entry_bill on an entry bill that exists fetches its id."""
+        start_time = '2019-09-17T00:00:00-07:00'
+        report_date_range = utils.month_date_range(parser.parse(start_time))
+        start_date, end_date = report_date_range.split('-')
+
+        start_date_utc = parser.parse(start_date).replace(hour=0, minute=0, tzinfo=pytz.UTC)
+        end_date_utc = parser.parse(end_date).replace(hour=0, minute=0, tzinfo=pytz.UTC)
+
+        with schema_context(self.schema):
+            entry_bill = GCPCostEntryBill.objects.create(
+                provider_id=self.gcp_provider.id,
+                billing_period_start=start_date_utc,
+                billing_period_end=end_date_utc
+            )
+        entry_bill_id = \
+            self.processor._get_or_create_cost_entry_bill(
+                {'Start Time': datetime.strftime(start_date_utc, '%Y-%m-%d %H:%M%z')},
+                self.accessor)
+        self.assertEquals(entry_bill.id, entry_bill_id)
+
+    def test_create_gcp_project(self):
+        """Test calling _get_or_create_gcp_project on a project id that doesn't exist creates it."""
+
+        project_data = {
+            'Project ID': fake.word(),
+            'Account ID': fake.word(),
+            'Project Number': fake.pyint(),
+            'Project Name': fake.word()
+        }
+        project_id = self.processor._get_or_create_gcp_project(
+            project_data, self.accessor
+        )
+        with schema_context(self.schema):
+            self.assertTrue(GCPProject.objects.filter(id=project_id).exists())
+
+    def test_get_gcp_project(self):
+        """Test calling _get_or_create_gcp_project on a project id that exists gets it."""
+
+        project_id = fake.word()
+        account_id = fake.word()
+        with schema_context(self.schema):
+            project = GCPProject.objects.create(
+                project_id=project_id,
+                account_id=account_id,
+                project_number=fake.pyint(),
+                project_name=fake.word()
+            )
+        fetched_project_id = self.processor._get_or_create_gcp_project(
+            {
+                'Project ID': project_id,
+                'Account ID': fake.word(),
+                'Project Number': fake.pyint(),
+                'Project Name': fake.word()
+            }, self.accessor
+        )
+        self.assertEquals(fetched_project_id, project.id)
+        with schema_context(self.schema):
+            # Even if _get_or_create_gcp_project is called with a different
+            # account_id, but the same project_id, we expect account_id to remain the same
+            gcp_project = GCPProject.objects.get(id=project.id)
+            self.assertEquals(gcp_project.account_id, account_id)
+
+    def test_gcp_process_twice(self):
+        """Test the processing of an GCP file again, results in the same amount of objects."""
+        self.processor.process()
+        with schema_context(self.schema):
+            num_line_items = len(GCPCostEntryLineItemDaily.objects.all())
+            num_projects = len(GCPProject.objects.all())
+            num_bills = len(GCPCostEntryBill.objects.all())
+
+        # Why another processor instance? because calling process() with the same processor instance fails
+        # django.db.utils.InternalError: no such savepoint.
+        processor = GCPReportProcessor(
+            schema_name=self.schema,
+            report_path=self.test_report,
+            compression=UNCOMPRESSED,
+            provider_id=self.gcp_provider.id,
+            manifest_id=self.manifest.id
+        )
+        processor.process()
+        with schema_context(self.schema):
+            self.assertEquals(
+                num_line_items,
+                len(GCPCostEntryLineItemDaily.objects.all())
+            )
+            self.assertEquals(
+                num_projects,
+                len(GCPProject.objects.all())
+            )
+            self.assertEquals(
+                num_bills,
+                len(GCPCostEntryBill.objects.all())
+            )
