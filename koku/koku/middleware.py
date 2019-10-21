8ed6fa089d994f1420ebde6be95751afb246ceac
Andrew Berglund <aberglun@redhat.com>
2019-10-21 14:13:59 -0400

    Allow for hybrid and cost_management in middleware

--- koku/koku/middleware.py
+++ koku/koku/middleware.py
@@ -189,7 +189,9 @@ class IdentityHeaderMiddleware(MiddlewareMixin):  # pylint: disable=R0903
             is_admin = json_rh_auth.get('identity', {}).get('user', {}).get('is_org_admin')
             is_cost_management = json_rh_auth.get(
                 'entitlements', {}).get('cost_management', {}).get('is_entitled', False)
-            if not is_cost_management:
+            is_hybrid_cloud = json_rh_auth.get(
+                'entitlements', {}).get('hybrid_cloud', {}).get('is_entitled', False)
+            if not is_hybrid_cloud and not is_cost_management:
                 raise PermissionDenied()
         except (KeyError, JSONDecodeError):
             logger.warning('Could not obtain identity on request.')
