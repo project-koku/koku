b34db5fbf0a3d27605fd3d735d91c3e473bcd1b8
nbon12 <nbon12@users.noreply.github.com>
2019-10-22 14:03:50 -0400

    add updated_timestamp to attributes

--- koku/attributes/api_views.py
+++ koku/attributes/api_views.py
@@ -1,11 +1,10 @@
 from rest_framework import viewsets
 from .models import Attribute
 from .serializers import AttributeSerializer
-
 from rest_framework.permissions import AllowAny
 
 
-class AttributeViewSet(viewsets.ModelViewSet):
+class AttributeViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Attribute.objects.all()
     serializer_class = AttributeSerializer
     permission_classes = (AllowAny,)
--- koku/attributes/models.py
+++ koku/attributes/models.py
@@ -4,3 +4,5 @@ class Attribute(models.Model):
     name = models.CharField(max_length=255, help_text='The name of the attribute')
     value = models.TextField(null=True)
     description = models.TextField(null=True)
+    updated_timestamp = models.DateTimeField(auto_now=True, blank=True, null=True)
+    created_timestamp = models.DateTimeField(auto_now_add=True, blank=True, null=True)
