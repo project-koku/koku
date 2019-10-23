d6f5690901fedc2f39f35f8b2d82524e2384c209
nbon12 <nbon12@users.noreply.github.com>
2019-10-23 11:04:45 -0400

    lint fixes

--- koku/attribute/serializers.py
+++ koku/attribute/serializers.py
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
-"""Attribute Model Serializer."""
+"""Serializer for Attribute"""
 from rest_framework import serializers
 from .models import Attribute
 
