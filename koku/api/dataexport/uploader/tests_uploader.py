4a78aa3f18df3224984ffb67cb516ca668aa6da9
Brad Smith <brasmith@redhat.com>
2019-11-13 14:53:15 -0500

    add logging around boto3 s3.upload_file to diagnose issues in ci

--- koku/api/dataexport/uploader/tests_uploader.py
+++ koku/api/dataexport/uploader/tests_uploader.py
@@ -10,11 +10,15 @@ from api.dataexport.uploader import AwsS3Uploader
 fake = faker.Faker()
 
 
+class DummyException(Exception):
+    """Dummy exception for testing."""
+
+
 class AwsS3UploaderTest(TestCase):
     """AwsS3Uploader test case."""
 
     @patch('api.dataexport.uploader.boto3')
-    def test_upload_file(self, mock_boto3):
+    def test_upload_file_success(self, mock_boto3):
         """Test uploading a file to AWS S3."""
         bucket_name = fake.slug()
         local_path = fake.file_path()
@@ -26,4 +30,27 @@ class AwsS3UploaderTest(TestCase):
 
         mock_boto3.client.assert_called_with('s3', settings.S3_REGION)
         mock_client = mock_boto3.client.return_value
+        mock_client.upload_file.assert_called_with(local_path, bucket_name, remote_path)
+
+    @patch('api.dataexport.uploader.boto3')
+    def test_upload_file_exception(self, mock_boto3):
+        """Test uploading a file to AWS S3 when boto3 raises an exception."""
+        bucket_name = fake.slug()
+        local_path = fake.file_path()
+        remote_path = fake.file_path()
+        self.assertNotEqual(local_path, remote_path)
+
+        mock_client = mock_boto3.client.return_value
+        exception_message = 'something broke'
+        mock_client.upload_file.side_effect = DummyException(exception_message)
+
+        uploader = AwsS3Uploader(bucket_name)
+        with self.assertRaises(DummyException) as the_exception, self.assertLogs(
+            'api.dataexport.uploader', 'ERROR'
+        ) as capture_logs:
+            uploader.upload_file(local_path, remote_path)
+        self.assertEqual(str(the_exception.exception), exception_message)
+        self.assertIn('Failed to upload', capture_logs.output[0])
+
+        mock_boto3.client.assert_called_with('s3', settings.S3_REGION)
         mock_client.upload_file.assert_called_with(local_path, bucket_name, remote_path)
