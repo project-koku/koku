b68beec77e78c80cfb3b32d797b8a3c8799efe12
Brett Lentz <blentz@redhat.com>
2019-11-15 14:48:52 -0500

    lint!

--- koku/koku/tests_metrics.py
+++ koku/koku/tests_metrics.py
@@ -30,6 +30,8 @@ from koku.metrics import DatabaseStatus
 FAKE = Faker()
 
 
+# noqa: W0212,E1101
+# pylint: disable=no-member,protected-access
 class DatabaseStatusTest(IamTestCase):
     """Test DatabaseStatus object."""
 
