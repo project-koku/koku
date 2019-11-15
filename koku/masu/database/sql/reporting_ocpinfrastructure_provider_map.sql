5c4fd526c735dda9f681ad841eebe393a25cb952
Andrew Berglund <aberglun@redhat.com>
2019-11-15 15:22:26 -0500

    Speed up infra map SQL

--- koku/masu/database/sql/reporting_ocpinfrastructure_provider_map.sql
+++ koku/masu/database/sql/reporting_ocpinfrastructure_provider_map.sql
@@ -1,60 +1,45 @@
-CREATE TEMPORARY TABLE aws_daily_tags_temp AS (
-    SELECT aws.usage_start,
-        aws.cost_entry_bill_id,
-        aws.resource_id,
-        LOWER(key) as key,
-        value
-    FROM {{schema | sqlsafe}}.reporting_awscostentrylineitem_daily as aws,
-        jsonb_each_text(aws.tags) labels
-)
-;
 
 CREATE TEMPORARY TABLE ocp_infrastructure_temp AS (
     SELECT aws.cost_entry_bill_id,
-        ocp.cluster_id
-    FROM aws_daily_tags_temp as aws
+        ocp.report_period_id
+    FROM {{schema | sqlsafe}}.reporting_awscostentrylineitem_daily as aws
     JOIN {{schema | sqlsafe}}.reporting_ocpusagelineitem_daily as ocp
-        ON aws.usage_start::date = ocp.usage_start::date
-            AND (aws.resource_id = ocp.resource_id
-                OR (aws.key = 'openshift_project' AND aws.value = ocp.namespace)
-                OR (aws.key = 'openshift_node' AND aws.value = ocp.node)
-                OR (aws.key = 'openshift_cluster' AND aws.value = ocp.cluster_id)
-                OR (aws.key = 'openshift_cluster' AND aws.value = ocp.cluster_alias))
+        ON date(aws.usage_start) = date(ocp.usage_start)
+            AND aws.resource_id = ocp.resource_id
     WHERE date(aws.usage_start) >= {{start_date}}
         AND date(aws.usage_start) <= {{end_date}}
-    GROUP BY aws.cost_entry_bill_id, ocp.cluster_id
+        AND date(ocp.usage_start) >= {{start_date}}
+        AND date(ocp.usage_start) <= {{end_date}}
+    GROUP BY aws.cost_entry_bill_id, ocp.report_period_id
 )
 ;
 
-CREATE TEMPORARY TABLE aws_infrastructure_uuid_temp AS (
+WITH cte_aws_infrastructure_uuid_temp AS (
     SELECT provider.uuid as aws_uuid,
+        provider.type,
         ocp_infra_temp.cost_entry_bill_id,
-        ocp_infra_temp.cluster_id
+        ocp_infra_temp.report_period_id
     FROM {{schema | sqlsafe}}.reporting_awscostentrybill as awsbill
     JOIN ocp_infrastructure_temp as ocp_infra_temp
         ON awsbill.id = ocp_infra_temp.cost_entry_bill_id
     JOIN public.api_provider as provider
         ON provider.uuid = awsbill.provider_id
-)
-;
-
-CREATE TEMPORARY TABLE ocp_uuid_temp AS (
+),
+cte_ocp_uuid_temp AS (
     SELECT provider.uuid as ocp_uuid,
         ocp_infra_temp.cost_entry_bill_id,
-        ocp_infra_temp.cluster_id
-    FROM public.api_providerauthentication as authentication
-    JOIN public.api_provider as provider
-        ON provider.authentication_id = authentication.id
+        ocp_infra_temp.report_period_id
+    FROM {{schema | sqlsafe}}.reporting_ocpusagereportperiod as ocprp
     JOIN ocp_infrastructure_temp as ocp_infra_temp
-        ON authentication.provider_resource_name = ocp_infra_temp.cluster_id
+        ON ocprp.id = ocp_infra_temp.report_period_id
+    JOIN public.api_provider as provider
+        ON provider.uuid = ocprp.provider_id
 )
-;
-
-SELECT aws.aws_uuid as aws_uuid,
-    ocp.ocp_uuid as ocp_uuid,
-    ocp.cluster_id
-FROM aws_infrastructure_uuid_temp as aws
-JOIN ocp_uuid_temp as ocp
+SELECT ocp.ocp_uuid,
+    aws.aws_uuid,
+    aws.type
+FROM cte_aws_infrastructure_uuid_temp as aws
+JOIN cte_ocp_uuid_temp as ocp
     ON aws.cost_entry_bill_id = ocp.cost_entry_bill_id
-        AND aws.cluster_id = ocp.cluster_id
+        AND aws.report_period_id = ocp.report_period_id
 ;
