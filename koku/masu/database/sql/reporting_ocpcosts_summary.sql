005809241248c55ea90905c9eace7e4ed8a3e209
Andrew Berglund <aberglun@redhat.com>
2019-12-02 14:26:24 -0500

    1230 openshift on azure (#1427)
    * Models for OpenShift on Azure
    
    * SQL for OCP on Azure
    
    * Don't delete files when running locally
    
    * Add report_period to OCP on Azure
    
    * Add to AZURE_REPORT_TABLE_MAP
    
    * Add populate_ocp_on_azure_cost_daily_summary
    
    * Add Azure to infra map
    
    * Add report_period_id to ocp on azure
    
    * Add OCP on Azure handling for infra cost
    
    * Add specific handling for Azure in cloud updater
    
    * Genericize update markup cost method
    
    * Tests for OCP on Azure
    
    * SQL tweak to set nulls to 0
    
    * Update koku/masu/processor/ocp/ocp_cloud_summary_updater.py

--- koku/masu/database/sql/reporting_ocpcosts_summary.sql
+++ koku/masu/database/sql/reporting_ocpcosts_summary.sql
@@ -23,6 +23,33 @@ CREATE TEMPORARY TABLE reporting_ocp_infrastructure_cost AS (
         ocp_aws.pod,
         ocp_aws.node,
         ocp_aws.pod_labels
+
+    UNION
+
+    SELECT ocp_azure.report_period_id,
+        ocp_azure.usage_start,
+        ocp_azure.cluster_id,
+        ocp_azure.cluster_alias,
+        ocp_azure.namespace,
+        ocp_azure.data_source,
+        ocp_azure.pod,
+        ocp_azure.node,
+        ocp_azure.pod_labels,
+        sum(ocp_azure.pretax_cost) AS infra_cost,
+        sum(ocp_azure.pod_cost) AS project_infra_cost
+    FROM {{schema | sqlsafe}}.reporting_ocpazurecostlineitem_project_daily_summary AS ocp_azure
+    WHERE date(ocp_azure.usage_start) >= {{start_date}}
+        AND date(ocp_azure.usage_start) <= {{end_date}}
+        AND ocp_azure.cluster_id = {{cluster_id}}
+    GROUP BY ocp_azure.report_period_id,
+        ocp_azure.usage_start,
+        ocp_azure.cluster_id,
+        ocp_azure.cluster_alias,
+        ocp_azure.namespace,
+        ocp_azure.data_source,
+        ocp_azure.pod,
+        ocp_azure.node,
+        ocp_azure.pod_labels
 )
 ;
 
@@ -32,7 +59,7 @@ UPDATE reporting_ocpusagelineitem_daily_summary ods
     FROM reporting_ocp_infrastructure_cost AS ic
     WHERE ic.data_source = 'Pod'
         AND ods.report_period_id = ic.report_period_id
-        AND ods.usage_start = ic.usage_start
+        AND date(ods.usage_start) = date(ic.usage_start)
         AND ods.cluster_id = ic.cluster_id
         AND ods.cluster_alias = ic.cluster_alias
         AND ods.namespace = ic.namespace
@@ -48,7 +75,7 @@ UPDATE reporting_ocpusagelineitem_daily_summary ods
     FROM reporting_ocp_infrastructure_cost AS ic
     WHERE ic.data_source = 'Storage'
         AND ods.report_period_id = ic.report_period_id
-        AND ods.usage_start = ic.usage_start
+        AND date(ods.usage_start) = date(ic.usage_start)
         AND ods.cluster_id = ic.cluster_id
         AND ods.cluster_alias = ic.cluster_alias
         AND ods.namespace = ic.namespace
@@ -56,4 +83,14 @@ UPDATE reporting_ocpusagelineitem_daily_summary ods
         AND ods.pod = ic.pod
         AND ods.node = ic.node
         AND ods.volume_labels = ic.pod_labels
+;
+
+UPDATE reporting_ocpusagelineitem_daily_summary ods
+    SET infra_cost = 0::decimal
+    WHERE infra_cost IS NULL
+;
+
+UPDATE reporting_ocpusagelineitem_daily_summary ods
+    SET project_infra_cost = 0::decimal
+    WHERE project_infra_cost IS NULL
 ;
--- koku/masu/database/sql/reporting_ocpinfrastructure_provider_map.sql
+++ koku/masu/database/sql/reporting_ocpinfrastructure_provider_map.sql
@@ -21,11 +21,37 @@ CREATE TEMPORARY TABLE ocp_infrastructure_temp AS (
         AND rp.provider_id = {{ocp_provider_uuid}}
         {% endif %}
     GROUP BY aws.cost_entry_bill_id, ocp.report_period_id
+
+    UNION
+
+    SELECT azure.cost_entry_bill_id,
+        ocp.report_period_id
+    FROM {{schema | sqlsafe}}.reporting_azurecostentrylineitem_daily as azure
+    JOIN {{schema | sqlsafe}}.reporting_azurecostentryproductservice as aps
+            ON azure.cost_entry_product_id = aps.id
+    JOIN {{schema | sqlsafe}}.reporting_azurecostentrybill as bill
+        ON azure.cost_entry_bill_id = bill.id
+    JOIN {{schema | sqlsafe}}.reporting_ocpusagelineitem_daily as ocp
+        ON split_part(aps.instance_id, '/', 9) = ocp.node
+            AND date(azure.usage_date_time) = date(ocp.usage_start)
+    JOIN {{schema | sqlsafe}}.reporting_ocpusagereportperiod as rp
+        ON ocp.report_period_id = rp.id
+    WHERE date(azure.usage_date_time) >= {{start_date}}
+        AND date(azure.usage_date_time) <= {{end_date}}
+        AND date(ocp.usage_start) >= {{start_date}}
+        AND date(ocp.usage_start) <= {{end_date}}
+        {% if azure_provider_uuid %}
+        AND bill.provider_id = {{azure_provider_uuid}}
+        {% endif %}
+        {% if ocp_provider_uuid %}
+        AND rp.provider_id = {{ocp_provider_uuid}}
+        {% endif %}
+    GROUP BY azure.cost_entry_bill_id, ocp.report_period_id
 )
 ;
 
-WITH cte_aws_infrastructure_uuid_temp AS (
-    SELECT provider.uuid as aws_uuid,
+WITH cte_infrastructure_uuid_temp AS (
+    SELECT provider.uuid as infra_uuid,
         provider.type,
         ocp_infra_temp.cost_entry_bill_id,
         ocp_infra_temp.report_period_id
@@ -34,6 +60,18 @@ WITH cte_aws_infrastructure_uuid_temp AS (
         ON awsbill.id = ocp_infra_temp.cost_entry_bill_id
     JOIN public.api_provider as provider
         ON provider.uuid = awsbill.provider_id
+
+    UNION
+
+    SELECT provider.uuid as infra_uuid,
+        provider.type,
+        ocp_infra_temp.cost_entry_bill_id,
+        ocp_infra_temp.report_period_id
+    FROM {{schema | sqlsafe}}.reporting_azurecostentrybill as azurebill
+    JOIN ocp_infrastructure_temp as ocp_infra_temp
+        ON azurebill.id = ocp_infra_temp.cost_entry_bill_id
+    JOIN public.api_provider as provider
+        ON provider.uuid = azurebill.provider_id
 ),
 cte_ocp_uuid_temp AS (
     SELECT provider.uuid as ocp_uuid,
@@ -46,10 +84,10 @@ cte_ocp_uuid_temp AS (
         ON provider.uuid = ocprp.provider_id
 )
 SELECT ocp.ocp_uuid,
-    aws.aws_uuid,
-    aws.type
-FROM cte_aws_infrastructure_uuid_temp as aws
+    infra.infra_uuid,
+    infra.type
+FROM cte_infrastructure_uuid_temp as infra
 JOIN cte_ocp_uuid_temp as ocp
-    ON aws.cost_entry_bill_id = ocp.cost_entry_bill_id
-        AND aws.report_period_id = ocp.report_period_id
+    ON infra.cost_entry_bill_id = ocp.cost_entry_bill_id
+        AND infra.report_period_id = ocp.report_period_id
 ;
