8120c654dacd3229cc772b4a67846183c80aa38c
Andrew Berglund <aberglun@redhat.com>
2019-12-02 14:26:24 -0500

    1230 openshift on azure (#1427)
    * Models for OpenShift on Azure
    
    * SQL for OCP on Azure
    
    * Don't delete files when running locally
    
    * Add report_period to OCP on Azure
    
    * Add to AZURE_REPORT_TABLE_MAP
    
    * Add populate_ocp_on_azure_cost_daily_summary
    
    * Add Azure to infra map
    
    * Add report_period_id to ocp on azure
    
    * Add OCP on Azure handling for infra cost
    
    * Add specific handling for Azure in cloud updater
    
    * Genericize update markup cost method
    
    * Tests for OCP on Azure
    
    * SQL tweak to set nulls to 0
    
    * Update koku/masu/processor/ocp/ocp_cloud_summary_updater.py

--- koku/masu/processor/ocp/ocp_cloud_updater_base.py
+++ koku/masu/processor/ocp/ocp_cloud_updater_base.py
@@ -95,6 +95,11 @@ class OCPCloudUpdaterBase:
                 infra_map = accessor.get_ocp_infrastructure_map(
                     start_date, end_date, aws_provider_uuid=self._provider_uuid
                 )
+        elif self._provider.type in (Provider.PROVIDER_AZURE, Provider.PROVIDER_AZURE_LOCAL):
+            with OCPReportDBAccessor(self._schema, self._column_map) as accessor:
+                infra_map = accessor.get_ocp_infrastructure_map(
+                    start_date, end_date, azure_provider_uuid=self._provider_uuid
+                )
 
         # Save to DB
         self.set_provider_infra_map(infra_map)
