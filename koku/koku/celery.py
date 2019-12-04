408da834de3989c3d86a7dec16300e142d63242a
Michael Skarbek <mskarbek@redhat.com>
2019-12-04 15:30:41 -0500

    Celery tracebacks (#1480)
    * add celery exception/traceback logging
    
    * exclude LogErrorsTask from coverage

--- koku/koku/celery.py
+++ koku/koku/celery.py
@@ -3,7 +3,7 @@ import datetime
 import logging
 import os
 
-from celery import Celery
+from celery import Celery, Task
 from celery.schedules import crontab
 from django.conf import settings
 
@@ -16,6 +16,29 @@ from .env import ENVIRONMENT
 
 LOGGER = logging.getLogger(__name__)
 
+
+# pylint: disable=abstract-method
+class LogErrorsTask(Task):  # pragma: no cover
+    """Log Celery task exceptions."""
+
+    def on_failure(self, exc, task_id, args, kwargs, einfo):  # pylint: disable=too-many-arguments
+        """Log exceptions when a celery task fails."""
+        LOGGER.exception('Task failed: %s', exc, exc_info=exc)
+        super().on_failure(exc, task_id, args, kwargs, einfo)
+
+
+class LoggingCelery(Celery):
+    """Log Celery task exceptions."""
+
+    def task(self, *args, **kwargs):
+        """Set the default base logger for the celery app.
+
+        Let's us avoid typing `base=LogErrorsTask` for every app.task.
+        """
+        kwargs.setdefault('base', LogErrorsTask)
+        return super().task(*args, **kwargs)
+
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'koku.settings')
 
 LOGGER.info('Starting celery.')
@@ -26,7 +49,7 @@ LOGGER.info('Database configured.')
 # 'app' is the recommended convention from celery docs
 # following this for ease of comparison to reference implementation
 # pylint: disable=invalid-name
-app = Celery('koku', broker=settings.CELERY_BROKER_URL)
+app = LoggingCelery('koku', broker=settings.CELERY_BROKER_URL)
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 LOGGER.info('Celery autodiscover tasks.')
