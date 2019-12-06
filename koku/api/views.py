61775720494a0dbf7913519a8df840d54afc5793
Douglas Curtis <docurtis@redhat.com>
2019-12-06 14:43:55 -0500

    Consolidate Sources APIs and enable PATCH (#1481)

--- koku/api/views.py
+++ koku/api/views.py
@@ -47,4 +47,5 @@ from api.tags.ocp.view import OCPTagView
 from api.tags.ocp_aws.view import OCPAWSTagView
 from api.sources.billing_source import billing_source
 from api.sources.authentication import authentication
+from api.sources.view import SourcesProxyViewSet
 from api.cloud_accounts.views import CloudAccountViewSet
