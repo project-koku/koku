360ca669c0751cf7d916024f9da58103c8347b6f
Douglas Curtis <docurtis@redhat.com>
2019-12-12 13:17:45 -0500

    lint

--- koku/sources/api/view.py
+++ koku/sources/api/view.py
@@ -27,13 +27,10 @@ from django.views.decorators.cache import never_cache
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import mixins, viewsets
 from rest_framework import status
-from rest_framework.decorators import action
 from rest_framework.exceptions import APIException
 from rest_framework.exceptions import ParseError
 from rest_framework.permissions import AllowAny
-from rest_framework.response import Response
 from sources.api.serializers import SourcesSerializer
-from sources.api.source_status import SourceStatus
 from sources.storage import SourcesStorageError
 
 from api.provider.models import Sources
