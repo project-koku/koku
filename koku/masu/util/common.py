80dcf4c78496bc239851f9130c67d06f0828cc28
Andrew Berglund <aberglun@redhat.com>
2020-01-03 10:01:36 -0500

    Add date range utility functions

--- koku/masu/util/common.py
+++ koku/masu/util/common.py
@@ -25,6 +25,9 @@ from os import remove
 from tempfile import gettempdir
 from uuid import uuid4
 
+from dateutil import parser
+from dateutil.rrule import rrule, DAILY
+
 from masu.external import (AMAZON_WEB_SERVICES,
                            AWS_LOCAL_SERVICE_PROVIDER,
                            AZURE,
@@ -160,3 +163,49 @@ def dictify_table_export_settings(table_export_settings):
         'iterate_daily': table_export_settings.iterate_daily,
         'sql': table_export_settings.sql
     }
+
+
+def date_range(start_date, end_date, step=5):
+    """Create a range generator for dates.
+
+    Given a start date and end date make an generator that returns the next date
+    in the range with the given interval.
+
+    """
+    if isinstance(start_date, str):
+        start_date = parser.parse(start_date)
+    if isinstance(end_date, str):
+        end_date = parser.parse(end_date)
+    dates = list(
+        rrule(freq=DAILY, dtstart=start_date, until=end_date, interval=step)
+    )
+    for date in dates:
+        yield date
+    if end_date not in dates:
+        yield end_date
+
+
+def date_range_pair(start_date, end_date, step=5):
+    """Create a range generator for dates.
+
+    Given a start date and end date make an generator that returns a start
+    and end date over the interval.
+
+    """
+    if isinstance(start_date, str):
+        start_date = parser.parse(start_date)
+    if isinstance(end_date, str):
+        end_date = parser.parse(end_date)
+    dates = list(
+        rrule(freq=DAILY, dtstart=start_date, until=end_date, interval=step)
+    )
+    # Special case with only 1 period
+    if len(dates) == 1:
+        yield start_date, end_date
+    for date in dates:
+        if date == start_date:
+            continue
+        yield start_date, date
+        start_date = date + timedelta(days=1)
+    if len(dates) != 1 and end_date not in dates:
+        yield start_date, end_date
