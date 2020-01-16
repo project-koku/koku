0c966c07d6992bedf471602fca2419bcf3f503ce
HAP <hproctor@redhat.com>
2020-01-16 17:18:47 -0500

    Revert "WIP: 1549 -- A learning experience to not bite off more than can be chewed. No matter how straightforward it looks."
    This reverts commit 2e22a86a153b6510ae61f93aae1a24ab15dff195.

--- koku/reporting/migrations/0051_auto_20190315_1458.py
+++ koku/reporting/migrations/0051_auto_20190315_1458.py
@@ -6,7 +6,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        #('reporting', '0050_auto_20190315_1339'),
+        ('reporting', '0050_auto_20190315_1339'),
     ]
 
     operations = [
