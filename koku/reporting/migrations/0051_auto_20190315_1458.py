a813c0ec491cdc57ae77c85a329af0d1280e818b
HAP <hproctor@redhat.com>
2020-01-17 08:54:10 -0500

    WIP: 1549 manually squashing/optimizing reporting migrations

--- koku/reporting/migrations/0051_auto_20190315_1458.py
+++ koku/reporting/migrations/0051_auto_20190315_1458.py
@@ -6,7 +6,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('reporting', '0050_auto_20190315_1339'),
+        ('reporting', '0001_initial_squashed_0050_auto_20190315_1339'),
     ]
 
     operations = [
