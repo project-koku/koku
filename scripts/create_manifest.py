2cabaf3cd4bcc13cd5e95956b73521e50f7ba41e
Chris Hambridge <chambrid@redhat.com>
2020-02-10 12:47:38 -0500

    Update product manifest to include base container name. (#1725)
    * Update output script to include base container name
    * Update resulting manifest

--- scripts/create_manifest.py
+++ scripts/create_manifest.py
@@ -19,11 +19,13 @@ import json
 
 lockfile = {}
 
+CONTAINER_NAME = "registry.access.redhat.com/ubi7/python-36:latest"
 
 with open("Pipfile.lock") as json_file:
     lockfile = json.load(json_file)
 
 with open("koku-manifest", "w") as manifest:
+    manifest.write(f"mgmt_services/cost-mgmt:koku/{CONTAINER_NAME}\n")
     for name, value in sorted(lockfile["default"].items()):
         if "version" in value:
             version = value["version"].replace("=", "")
