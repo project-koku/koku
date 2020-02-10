fd14f90b148312fd5f43cd9272504562322517ef
Michael Skarbek <mskarbek@redhat.com>
2020-02-10 15:03:56 -0500

    update sql

--- koku/masu/database/sql/reporting_azuretags_summary.sql
+++ koku/masu/database/sql/reporting_azuretags_summary.sql
@@ -1,13 +1,19 @@
-INSERT INTO {{schema | sqlsafe }}.reporting_azuretags_summary
+INSERT INTO {{schema | sqlsafe }}.reporting_azuretags_summary (
+    key,
+    values,
+    cost_entry_bill_id
+)
 SELECT l.key,
-    array_agg(DISTINCT l.value) as values
+    array_agg(DISTINCT l.value) as values,
+    l.cost_entry_bill_id
 FROM (
     SELECT key,
-        value
+        value,
+        li.cost_entry_bill_id
     FROM {{schema | sqlsafe}}.reporting_azurecostentrylineitem_daily AS li,
         jsonb_each_text(li.tags) labels
 ) l
-GROUP BY l.key
-ON CONFLICT (key) DO UPDATE
+GROUP BY l.key, l.cost_entry_bill_id
+ON CONFLICT (key, cost_entry_bill_id) DO UPDATE
 SET values = EXCLUDED.values
 ;
