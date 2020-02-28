6194c8e87422c4344fa756c24659a90e445c3bee
Andrew Berglund <aberglun@redhat.com>
2020-02-28 16:23:31 -0500

    Typo fixes

--- koku/masu/processor/azure/azure_cost_model_cost_updater.py
+++ koku/masu/processor/azure/azure_cost_model_cost_updater.py
@@ -52,7 +52,7 @@ class AzureCostModelCostUpdater:
         """Store markup costs."""
         try:
             bills = get_bills_from_provider(self._provider.uuid, self._schema, start_date, end_date)
-            with CostModelDBAccessor(self._schema, self._provider_uuid) as cost_model_accessor:
+            with CostModelDBAccessor(self._schema, self._provider.uuid) as cost_model_accessor:
                 markup = cost_model_accessor.markup
                 markup_value = float(markup.get("value", 0)) / 100
 
