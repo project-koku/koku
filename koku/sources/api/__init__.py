394bda69ceee7fecf1d2555dbe576b92c0225bbf
Chris Hambridge <chambrid@redhat.com>
2020-02-28 12:19:08 -0500

    Create development only POST & DELETE for Sources. (#1823)
    * Create development only POST & DELETE for Sources.
    
    * Remove Sources Proxy and just use SourcesViewSet
    * Add support for AWS-local and Azure-local in development mode with Sources
    * Add filter to Sources API for query by type or name
    * Updated OpenAPI spec with filter params
    * Added dev-only Admin mixin and serializer

--- koku/sources/api/__init__.py
+++ koku/sources/api/__init__.py
@@ -1,1 +1,28 @@
 # noqa
+import binascii
+import logging
+from base64 import b64decode
+from json import loads as json_loads
+from json.decoder import JSONDecodeError
+
+LOG = logging.getLogger(__name__)
+HEADER_X_RH_IDENTITY = "X-Rh-Identity"
+
+
+def get_auth_header(request):
+    """Get the auth header from the request."""
+    return request.headers.get(HEADER_X_RH_IDENTITY)
+
+
+def get_account_from_header(request):
+    """ Get account from header."""
+    account_id = None
+    auth_header = get_auth_header(request)
+    if auth_header:
+        try:
+            decoded_rh_auth = b64decode(auth_header)
+            json_rh_auth = json_loads(decoded_rh_auth)
+            account_id = json_rh_auth.get("identity", {}).get("account_number")
+        except (binascii.Error, JSONDecodeError) as error:
+            LOG.error(f"Error decoding authentication header: {str(error)}")
+    return account_id
