74f329c8eb61e2b35d8f8802d49491e6f3aa6cf1
Chris Hambridge <chambrid@redhat.com>
2020-03-10 16:18:01 -0400

    Retire providers API in favor of sources API. (#1830)
    Retire providers API in favor of sources API.
    
    * Switch primary wording in docs and setup (Makefile and scripts) to use sources
    * Remove providers viewset
    * Add stats endpoint to sources api for testing
    * Remove providers from openapi spec
    
    Co-authored-by: Brett Lentz <blentz@users.noreply.github.com>
    Co-authored-by: Michael Skarbek <mskarbek@redhat.com>

--- koku/api/provider/test/test_provider_manager.py
+++ koku/api/provider/test/test_provider_manager.py
@@ -25,6 +25,7 @@ from rest_framework.request import Request
 from tenant_schemas.utils import tenant_context
 
 from api.iam.models import Customer
+from api.iam.models import User
 from api.iam.serializers import UserSerializer
 from api.iam.test.iam_test_case import IamTestCase
 from api.metrics.models import CostModelMetricsMap
@@ -61,9 +62,7 @@ class ProviderManagerTest(IamTestCase):
         """Set up the provider manager tests."""
         super().setUp()
         self.customer = Customer.objects.get(account_id=self.customer_data["account_id"])
-        serializer = UserSerializer(data=self.user_data, context=self.request_context)
-        if serializer.is_valid(raise_exception=True):
-            self.user = serializer.save()
+        self.user = User.objects.get(username=self.user_data["username"])
 
     @staticmethod
     def _create_delete_request(user, headers={}):
@@ -130,7 +129,7 @@ class ProviderManagerTest(IamTestCase):
         provider = Provider.objects.create(name="providername", created_by=self.user, customer=self.customer)
         provider_uuid = provider.uuid
         user_data = self._create_user_data()
-        request_context = self._create_request_context(self.create_mock_customer_data(), user_data)
+        request_context = self._create_request_context(self.create_mock_customer_data(), user_data, create_user=False)
         new_user = None
         serializer = UserSerializer(data=user_data, context=request_context)
         if serializer.is_valid(raise_exception=True):
@@ -165,7 +164,7 @@ class ProviderManagerTest(IamTestCase):
         provider_uuid = provider.uuid
 
         new_user_dict = self._create_user_data()
-        request_context = self._create_request_context(self.customer_data, new_user_dict, False)
+        request_context = self._create_request_context(self.customer_data, new_user_dict, False, create_user=False)
         user_serializer = UserSerializer(data=new_user_dict, context=request_context)
         other_user = None
         if user_serializer.is_valid(raise_exception=True):
@@ -210,7 +209,7 @@ class ProviderManagerTest(IamTestCase):
 
         self.assertNotEqual(provider.uuid, provider2.uuid)
         new_user_dict = self._create_user_data()
-        request_context = self._create_request_context(self.customer_data, new_user_dict, False)
+        request_context = self._create_request_context(self.customer_data, new_user_dict, False, create_user=False)
         user_serializer = UserSerializer(data=new_user_dict, context=request_context)
         other_user = None
         if user_serializer.is_valid(raise_exception=True):
@@ -240,7 +239,7 @@ class ProviderManagerTest(IamTestCase):
         provider_uuid = provider.uuid
 
         new_user_dict = self._create_user_data()
-        request_context = self._create_request_context(self.customer_data, new_user_dict, False)
+        request_context = self._create_request_context(self.customer_data, new_user_dict, False, create_user=False)
         user_serializer = UserSerializer(data=new_user_dict, context=request_context)
         other_user = None
         if user_serializer.is_valid(raise_exception=True):
@@ -551,7 +550,7 @@ class ProviderManagerTest(IamTestCase):
         provider_uuid = provider.uuid
 
         new_user_dict = self._create_user_data()
-        request_context = self._create_request_context(self.customer_data, new_user_dict, False)
+        request_context = self._create_request_context(self.customer_data, new_user_dict, False, create_user=False)
         user_serializer = UserSerializer(data=new_user_dict, context=request_context)
         other_user = None
         if user_serializer.is_valid(raise_exception=True):
--- koku/api/report/test/ocp/test_ocp_query_handler.py
+++ koku/api/report/test/ocp/test_ocp_query_handler.py
@@ -63,7 +63,7 @@ class OCPReportQueryHandlerTest(IamTestCase):
         super().setUp()
         self.dh = DateHelper()
 
-        _, self.provider = create_generic_provider(Provider.PROVIDER_OCP, self.headers)
+        _, self.provider = create_generic_provider(Provider.PROVIDER_OCP, self.request_context)
 
         self.this_month_filter = {"usage_start__gte": self.dh.this_month_start}
         self.ten_day_filter = {"usage_start__gte": self.dh.n_days_ago(self.dh.today, 9)}
