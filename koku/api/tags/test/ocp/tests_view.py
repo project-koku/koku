ad9e91e8f3fc354dc061392b56d9c4fc74f0feff
Chris Hambridge <chambrid@redhat.com>
2020-03-16 09:29:29 -0400

    add unit test for enabled

--- koku/api/tags/test/ocp/tests_view.py
+++ koku/api/tags/test/ocp/tests_view.py
@@ -69,10 +69,14 @@ class OCPTagsViewTest(IamTestCase):
     def test_execute_ocp_tags_queries_keys_only(self):
         """Test that tag key data is for the correct time queries."""
         test_cases = [
-            {"value": "-1", "unit": "month", "resolution": "monthly"},
-            {"value": "-2", "unit": "month", "resolution": "monthly"},
-            {"value": "-10", "unit": "day", "resolution": "daily"},
-            {"value": "-30", "unit": "day", "resolution": "daily"},
+            {"value": "-1", "unit": "month", "resolution": "monthly", "enabled": False},
+            {"value": "-2", "unit": "month", "resolution": "monthly", "enabled": False},
+            {"value": "-10", "unit": "day", "resolution": "daily", "enabled": False},
+            {"value": "-30", "unit": "day", "resolution": "daily", "enabled": False},
+            {"value": "-1", "unit": "month", "resolution": "monthly", "enabled": True},
+            {"value": "-2", "unit": "month", "resolution": "monthly", "enabled": True},
+            {"value": "-10", "unit": "day", "resolution": "daily", "enabled": True},
+            {"value": "-30", "unit": "day", "resolution": "daily", "enabled": True},
         ]
 
         for case in test_cases:
@@ -83,7 +87,7 @@ class OCPTagsViewTest(IamTestCase):
                 "filter[time_scope_value]": case.get("value"),
                 "filter[time_scope_units]": case.get("unit"),
                 "key_only": True,
-                "filter[enabled]": False,
+                "filter[enabled]": case.get("enabled"),
             }
             url = url + "?" + urlencode(params, quote_via=quote_plus)
             response = client.get(url, **self.headers)
@@ -91,7 +95,8 @@ class OCPTagsViewTest(IamTestCase):
             self.assertEqual(response.status_code, status.HTTP_200_OK)
             data = response.json()
 
-            self.assertTrue(data.get("data"))
+            if not case.get("enabled"):
+                self.assertTrue(data.get("data"))
             self.assertTrue(isinstance(data.get("data"), list))
 
     def test_execute_ocp_tags_queries(self):
