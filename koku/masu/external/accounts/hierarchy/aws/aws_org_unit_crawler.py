990acd3c3334a844a381e64536f86025603194b2
Kevan Holdaway <kholdawa@redhat.com>
2020-03-31 17:07:35 -0400

    Add error logging

--- koku/masu/external/accounts/hierarchy/aws/aws_org_unit_crawler.py
+++ koku/masu/external/accounts/hierarchy/aws/aws_org_unit_crawler.py
@@ -44,7 +44,7 @@ class AWSOrgUnitCrawler(AccountCrawler):
         """
         super().__init__(account)
         self._auth_cred = self.account.get("authentication")
-        self.client = self.get_session()
+        self.client = None
 
     def get_session(self):
         """
@@ -96,17 +96,12 @@ class AWSOrgUnitCrawler(AccountCrawler):
         for act_info in child_accounts:
             self._save_aws_org_method(ou.get("Name", ou.get("Id")), ou.get("Id"), prefix, act_info.get("Id"))
 
-    def get_root_ou(self):
-        """
-        Gets the root org unit and preps it for the recursive crawl.
-        """
-        root_ou = self.client.list_roots()["Roots"][0]
-        LOG.info("Obtained the root identifier: %s" % (root_ou["Id"]))
-        return root_ou
-
     @transaction.atomic
     def crawl_account_hierarchy(self):
-        root_ou = self.get_root_ou()
+        self.client = self.get_session()
+        root_ou = self.client.list_roots()["Roots"][0]
+        LOG.info("Obtained the root identifier: %s" % (root_ou["Id"]))
+
         self._crawl_org_for_acts(root_ou, root_ou.get("Id"))
 
     def _crawl_org_for_acts(self, ou, prefix):
@@ -119,16 +114,20 @@ class AWSOrgUnitCrawler(AccountCrawler):
 
         # Save entry for current OU
         self._save_aws_org_method(ou.get("Name", ou.get("Id")), ou.get("Id"), prefix, None)
+        try:
 
-        # process accounts for this org unit
-        self.get_accounts_per_id(ou, prefix)
+            # process accounts for this org unit
+            self.get_accounts_per_id(ou, prefix)
 
-        # recurse and look for sub org units
-        ou_pager = self.client.get_paginator("list_organizational_units_for_parent")
-        for sub_ou in ou_pager.paginate(ParentId=ou.get("Id")).build_full_result().get("OrganizationalUnits"):
-            new_prefix = prefix + ("&%s" % sub_ou.get("Id"))
-            LOG.info("Organizational unit found during crawl: %s" % (sub_ou.get("Id")))
-            self._crawl_org_for_acts(sub_ou, new_prefix)
+            # recurse and look for sub org units
+            ou_pager = self.client.get_paginator("list_organizational_units_for_parent")
+            for sub_ou in ou_pager.paginate(ParentId=ou.get("Id")).build_full_result().get("OrganizationalUnits"):
+                new_prefix = prefix + ("&%s" % sub_ou.get("Id"))
+                LOG.info("Organizational unit found during crawl: %s" % (sub_ou.get("Id")))
+                self._crawl_org_for_acts(sub_ou, new_prefix)
+        except Exception:
+            LOG.exception('Failure processing org unit.  Account schema %s and org_unit_id %s' %
+                          (self.schema, ou.get("Id")))
 
     def _save_aws_org_method(self, unit_name, unit_id, unit_path, account_id=None):
         """
