0e2c3c84e5332227ba4f532914a9f1c1ba8666d0
Andrew Berglund <aberglun@redhat.com>
2020-04-02 16:17:27 -0400

    Add masu enabled_tags

---
+++ koku/masu/api/enabled_tags.py
@@ -0,0 +1,88 @@
+#
+# Copyright 2020 Red Hat, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+"""View for enable_tags masu admin endpoint."""
+# pylint: disable=inconsistent-return-statements,too-many-return-statements,too-many-branches,too-many-statements
+import logging
+
+from django.views.decorators.cache import never_cache
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.decorators import permission_classes
+from rest_framework.decorators import renderer_classes
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.settings import api_settings
+from tenant_schemas.utils import schema_context
+
+from reporting.models import OCPEnabledTagKeys
+
+
+LOG = logging.getLogger(__name__)
+RESPONSE_KEY = "tag_keys"
+
+
+@never_cache
+@api_view(http_method_names=["GET", "POST"])
+@permission_classes((AllowAny,))
+@renderer_classes(tuple(api_settings.DEFAULT_RENDERER_CLASSES))
+def enabled_tags(request):
+    """Set enabled OCP tags in the DB for testing."""
+    if request.method == "GET":
+        params = request.query_params
+        schema_name = params.get("schema")
+
+        if schema_name is None:
+            errmsg = "schema is a required parameter."
+            return Response({"Error": errmsg}, status=status.HTTP_400_BAD_REQUEST)
+
+        with schema_context(schema_name):
+            enabled_tags = OCPEnabledTagKeys.objects.all()
+            tag_keys = [tag.key for tag in enabled_tags]
+
+        msg = f"Retreived enabled tags {tag_keys} for schema: {schema_name}."
+        LOG.info(msg)
+
+        return Response({RESPONSE_KEY: tag_keys})
+
+    if request.method == "POST":
+        data = request.data
+
+        schema_name = data.get("schema")
+        if schema_name is None:
+            errmsg = "schema is required."
+            return Response({"Error": errmsg}, status=status.HTTP_400_BAD_REQUEST)
+
+        action = data.get("action")
+        if action is None:
+            errmsg = "action is required."
+            return Response({"Error": errmsg}, status=status.HTTP_400_BAD_REQUEST)
+
+        tag_keys = request.POST.getlist("tag_keys")
+
+        with schema_context(schema_name):
+            if action.lower() == "create":
+                for key in tag_keys:
+                    OCPEnabledTagKeys.objects.get_or_create(key=key)
+                msg = f"Inserted enabled tags for schema: {schema_name}."
+                LOG.info(msg)
+            if action.lower() == "delete":
+                for key in tag_keys:
+                    OCPEnabledTagKeys.objects.filter(key=key).delete()
+                msg = f"Deleted enabled tags for schema: {schema_name}."
+                LOG.info(msg)
+
+        return Response({RESPONSE_KEY: tag_keys})
---
+++ koku/masu/test/api/test_enabled_tags.py
@@ -0,0 +1,106 @@
+#
+# Copyright 2020 Red Hat, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+"""Test the enabled_tags endpoint view."""
+from unittest.mock import patch
+
+from django.test.utils import override_settings
+from django.urls import reverse
+from tenant_schemas.utils import schema_context
+
+from masu.test import MasuTestCase
+from reporting.models import OCPEnabledTagKeys
+
+# from django.test import TestCase
+
+
+@override_settings(ROOT_URLCONF="masu.urls")
+class EnabledTagsTest(MasuTestCase):
+    """Test Cases for the enabled_tags endpoint."""
+
+    @patch("koku.middleware.MASU", return_value=True)
+    def test_get_enabled_tags(self, _):
+        """Test the GET enabled_tags endpoint."""
+        with schema_context(self.schema):
+            expected_keys = OCPEnabledTagKeys.objects.values_list("key")
+            expected_keys = [key[0] for key in expected_keys]
+
+        response = self.client.get(reverse("enabled_tags") + f"?schema={self.schema}")
+        body = response.json()
+
+        self.assertEqual(response.status_code, 200)
+        for key in expected_keys:
+            self.assertIn(key, body.get("tag_keys"))
+
+    @patch("koku.middleware.MASU", return_value=True)
+    def test_get_enabled_tags_no_schema(self, _):
+        """Test the GET enabled_tags endpoint."""
+        response = self.client.get(reverse("enabled_tags"))
+        self.assertEqual(response.status_code, 400)
+
+    @patch("koku.middleware.MASU", return_value=True)
+    def test_post_enabled_tags_create(self, _):
+        """Test the GET enabled_tags endpoint."""
+        with schema_context(self.schema):
+            OCPEnabledTagKeys.objects.all().delete()
+
+        post_data = {"schema": "acct10001", "action": "create", "tag_keys": ["tag1", "tag2"]}
+        response = self.client.post(reverse("enabled_tags"), post_data)
+        body = response.json()
+
+        self.assertEqual(response.status_code, 200)
+        for key in post_data.get("tag_keys", []):
+            self.assertIn(key, body.get("tag_keys"))
+
+    @patch("koku.middleware.MASU", return_value=True)
+    def test_post_enabled_tags_delete(self, _):
+        """Test the GET enabled_tags endpoint."""
+        with schema_context(self.schema):
+            keys = OCPEnabledTagKeys.objects.values_list("key")
+            keys = [key[0] for key in keys]
+            print(keys)
+
+        post_data = {"schema": "acct10001", "action": "delete", "tag_keys": keys}
+
+        response = self.client.post(reverse("enabled_tags"), post_data)
+        body = response.json()
+
+        self.assertEqual(response.status_code, 200)
+        for key in post_data.get("tag_keys", []):
+            self.assertIn(key, body.get("tag_keys"))
+
+        with schema_context(self.schema):
+            self.assertEqual(OCPEnabledTagKeys.objects.count(), 0)
+
+    @patch("koku.middleware.MASU", return_value=True)
+    def test_post_enabled_tags_no_schema(self, _):
+        """Test the GET enabled_tags endpoint."""
+        with schema_context(self.schema):
+            OCPEnabledTagKeys.objects.all().delete()
+
+        post_data = {"action": "create", "tag_keys": ["tag1", "tag2"]}
+        response = self.client.post(reverse("enabled_tags"), post_data)
+        self.assertEqual(response.status_code, 400)
+
+    @patch("koku.middleware.MASU", return_value=True)
+    def test_post_enabled_tags_no_action(self, _):
+        """Test the GET enabled_tags endpoint."""
+        with schema_context(self.schema):
+            OCPEnabledTagKeys.objects.all().delete()
+
+        post_data = {"schema": "acct10001", "tag_keys": ["tag1", "tag2"]}
+        response = self.client.post(reverse("enabled_tags"), post_data)
+        self.assertEqual(response.status_code, 400)
