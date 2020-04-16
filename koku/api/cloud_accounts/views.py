2bf02373c0def5a3fe322d012e807aa19df990a5
nbon12 <nbon12@users.noreply.github.com>
2020-04-16 16:14:56 -0400

    convert Cloud Accounts from View to ViewSet

--- koku/api/cloud_accounts/views.py
+++ koku/api/cloud_accounts/views.py
@@ -19,40 +19,85 @@ import json
 import logging
 import os
 
-from rest_framework import viewsets
-from rest_framework.permissions import AllowAny
+from rest_framework import permissions
+from rest_framework.decorators import api_view
+from rest_framework.decorators import permission_classes
+from rest_framework.decorators import renderer_classes
 from rest_framework.renderers import JSONRenderer
 from rest_framework.settings import api_settings
 
-from api.cloud_accounts.serializers import CloudAccountQueryParamsSerializer
-from api.cloud_accounts.serializers import CloudAccountSerializer
+from api.common.pagination import StandardResultsSetPagination
+from api.metrics.serializers import QueryParamsSerializer
 from koku.settings import BASE_DIR
+
 
 LOG = logging.getLogger(__name__)
 CLOUD_ACCOUNTS_FILE_NAME = os.path.join(BASE_DIR, "api/cloud_accounts/data/cloud_accounts.json")
 """View for Cloud Accounts."""
 
 
-class CloudAccountViewSet(viewsets.ReadOnlyModelViewSet):
-    """View for Cloud Accounts."""
+def _get_int_query_param(request, key, default):
+    """Get query param integer value safely."""
+    result = default
+    try:
+        result = int(request.query_params.get(key, default))
+    except ValueError:
+        pass
+    return result
 
-    serializer_class = CloudAccountSerializer
-    permission_classes = (AllowAny,)
-    renderer_classes = [JSONRenderer] + api_settings.DEFAULT_RENDERER_CLASSES
 
-    def get_queryset(self):
-        """ViewSet get_queryset method."""
-        serializer = CloudAccountQueryParamsSerializer(data=self.request.query_params)
-        serializer.is_valid(raise_exception=True)
-        data = self.get_json(CLOUD_ACCOUNTS_FILE_NAME)
-        return data
+def get_paginator(request, count):
+    """Get Paginator."""
+    paginator = StandardResultsSetPagination()
+    paginator.count = count
+    paginator.request = request
+    paginator.limit = _get_int_query_param(request, "limit", 10)
+    paginator.offset = _get_int_query_param(request, "offset", 0)
+    return paginator
 
-    def get_json(self, path):
-        """Obtain API JSON data from file path."""
-        json_data = None
-        with open(path) as json_file:
-            try:
-                json_data = json.load(json_file)
-            except (IOError, json.JSONDecodeError) as exc:
-                LOG.exception(exc)
-        return json_data
+
+@api_view(["GET"])  # noqa: C901
+@permission_classes((permissions.AllowAny,))
+@renderer_classes([JSONRenderer] + api_settings.DEFAULT_RENDERER_CLASSES)
+def cloud_accounts(request):
+    """View for cloud accounts."""
+    serializer = QueryParamsSerializer(data=request.query_params)
+    serializer.is_valid(raise_exception=True)
+    data = get_json(CLOUD_ACCOUNTS_FILE_NAME)
+    paginator = get_paginator(request, len(data))
+    limit = _get_int_query_param(request, "limit", 10)
+    offset = _get_int_query_param(request, "offset", 0)
+    if limit > len(data):
+        limit = len(data)
+    try:
+        data = data[offset : offset + limit]  # noqa E203
+    except IndexError:
+        data = []
+    page_obj = paginator.get_paginated_response(data)
+    return page_obj
+
+
+def get_json(path):
+    """Obtain API JSON data from file path."""
+    json_data = None
+    with open(path) as json_file:
+        try:
+            json_data = json.load(json_file)
+        except (IOError, json.JSONDecodeError) as exc:
+            LOG.exception(exc)
+    return json_data
+
+
+# class CloudAccountViewSet(viewsets.ReadOnlyModelViewSet):
+#     """View for Cloud Accounts."""
+
+#     serializer_class = CloudAccountSerializer
+#     permission_classes = (AllowAny,)
+#     renderer_classes = [JSONRenderer] + api_settings.DEFAULT_RENDERER_CLASSES
+
+#     def get_queryset(self):
+#         """ViewSet get_queryset method."""
+#         serializer = CloudAccountQueryParamsSerializer(data=self.request.query_params)
+#         serializer.is_valid(raise_exception=True)
+#         data = self.get_json(CLOUD_ACCOUNTS_FILE_NAME)
+#         return data
