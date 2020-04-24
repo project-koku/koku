133c45300a38eec9a19ecf5ebb00dd2e04fc6d78
HAP <hproctor@redhat.com>
2020-04-24 15:19:16 -0400

    Fix syntax error

--- koku/koku/celery.py
+++ koku/koku/celery.py
@@ -100,11 +100,11 @@ VACUUM_HOUR, VACUUM_MINUTE = VACUUM_DATA_UTC_TIME.split(":")
 if VACUUM_DATA_DAY_OF_WEEK:
     schedule = crontab(day_of_week=VACUUM_DATA_DAY_OF_WEEK, hour=int(VACUUM_HOUR), minute=int(VACUUM_MINUTE))
     autovacuum_schedule = crontab(
-        day_of_week=VACUUM_DATA_DAY_OF_WEEK, hour=int(VACUUM_HOUR - 1), minute=int(VACUUM_MINUTE)
+        day_of_week=VACUUM_DATA_DAY_OF_WEEK, hour=int(VACUUM_HOUR) - 1, minute=int(VACUUM_MINUTE)
     )
 else:
     schedule = crontab(hour=int(VACUUM_HOUR), minute=int(VACUUM_MINUTE))
-    autovacuum_schedule = crontab(hour=int(VACUUM_HOUR - 1), minute=int(VACUUM_MINUTE))
+    autovacuum_schedule = crontab(hour=int(VACUUM_HOUR) - 1, minute=int(VACUUM_MINUTE))
 
 app.conf.beat_schedule["vacuum-schemas"] = {
     "task": "masu.celery.tasks.vacuum_schemas",
