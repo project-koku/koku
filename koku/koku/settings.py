aae5f5f2be8be4ec0e9931d5a9d127ad1d2bb1dc
Michael Skarbek <mskarbek@redhat.com>
2020-05-05 13:45:07 -0400

    create the worker cache db backend

--- koku/koku/settings.py
+++ koku/koku/settings.py
@@ -185,6 +185,7 @@ if "test" in sys.argv:
     CACHES = {
         "default": {"BACKEND": "django.core.cache.backends.locmem.LocMemCache", "LOCATION": "unique-snowflake"},
         "rbac": {"BACKEND": "django.core.cache.backends.locmem.LocMemCache", "LOCATION": "unique-snowflake"},
+        "worker": {"BACKEND": "django.core.cache.backends.locmem.LocMemCache", "LOCATION": "unique-snowflake"},
     }
 else:
     CACHES = {
@@ -206,6 +207,7 @@ else:
                 "MAX_ENTRIES": 1000,
             },
         },
+        "worker": {"BACKEND": "django.core.cache.backends.db.DatabaseCache", "LOCATION": "worker_cache_table"},
     }
 
 DATABASES = {"default": database.config()}
