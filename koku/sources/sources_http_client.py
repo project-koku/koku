b8a53692d249b0caf69ffae4ee2df49a57d25b0a
Douglas Curtis <docurtis@redhat.com>
2020-05-07 13:37:20 -0400

    generating status header for internal status push (#2126)

--- koku/sources/sources_http_client.py
+++ koku/sources/sources_http_client.py
@@ -15,11 +15,21 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
 """Sources HTTP Client."""
+import binascii
+import json
+import logging
+from base64 import b64decode
+from base64 import b64encode
+from json import dumps as json_dumps
+
 import requests
 from requests.exceptions import RequestException
 
 from sources.config import Config
 from sources.sources_error_message import SourcesErrorMessage
+
+
+LOG = logging.getLogger(__name__)
 
 
 class SourcesHTTPClientError(Exception):
@@ -243,15 +253,40 @@ class SourcesHTTPClient:
         json_data = {"availability_status": status, "availability_status_error": user_facing_string}
         return json_data
 
+    def build_status_header(self):
+        """Build org-admin header for internal status delivery."""
+        try:
+            encoded_auth_header = self._identity_header.get("x-rh-identity")
+            identity = json.loads(b64decode(encoded_auth_header))
+            account = identity["identity"]["account_number"]
+
+            identity_header = {
+                "identity": {
+                    "account_number": account,
+                    "type": "User",
+                    "user": {"username": "cost-mgmt", "email": "cost-mgmt@redhat.com", "is_org_admin": True},
+                }
+            }
+            json_identity = json_dumps(identity_header)
+            cost_internal_header = b64encode(json_identity.encode("utf-8"))
+
+            return {"x-rh-identity": cost_internal_header}
+        except (binascii.Error, json.JSONDecodeError, TypeError, KeyError) as error:
+            LOG.error(f"Unable to build internal status header. Error: {str(error)}")
+
     def set_source_status(self, error_msg, cost_management_type_id=None):
         """Set the source status with error message."""
+        status_header = self.build_status_header()
+        if not status_header:
+            return False
+
         if not cost_management_type_id:
             cost_management_type_id = self.get_cost_management_application_type_id()
 
         application_query_url = "{}/applications?filter[application_type_id]={}&filter[source_id]={}".format(
             self._base_url, cost_management_type_id, str(self._source_id)
         )
-        application_query_response = requests.get(application_query_url, headers=self._identity_header)
+        application_query_response = requests.get(application_query_url, headers=status_header)
         response_data = application_query_response.json().get("data")
         if response_data:
             application_id = response_data[0].get("id")
@@ -259,7 +294,7 @@ class SourcesHTTPClient:
 
             json_data = self.build_source_status(error_msg)
 
-            application_response = requests.patch(application_url, json=json_data, headers=self._identity_header)
+            application_response = requests.patch(application_url, json=json_data, headers=status_header)
             if application_response.status_code != 204:
                 raise SourcesHTTPClientError(
                     f"Unable to set status for Source {self._source_id}. Reason: "
