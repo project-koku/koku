124066a0f1d6256eeede9ec40dad30a2c34eafde
Michael Skarbek <mskarbek@redhat.com>
2020-05-12 12:18:24 -0400

    github-action workflow/pre-commit config updates (#2141)
    * bump setup-python version. specify py36.
    
    * specify py36 in pre-commit-config
    
    * fix f541 errors

--- koku/api/tags/view.py
+++ koku/api/tags/view.py
@@ -18,7 +18,6 @@
 import logging
 
 from django.http import Http404
-from django.utils.translation import ugettext as _
 from django.views.decorators.vary import vary_on_headers
 from rest_framework import status
 from rest_framework.response import Response
@@ -63,7 +62,7 @@ class TagView(ReportView):
 
         if key and params.get("key_only"):
             LOG.debug("Invalid query parameter 'key_only'.")
-            error = {"details": _(f"Invalid query parameter 'key_only'.")}
+            error = {"details": "Invalid query parameter 'key_only'."}
             raise ValidationError(error)
 
         handler = self.query_handler(params)
