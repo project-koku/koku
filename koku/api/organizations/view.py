726061bbe09b0d2e3adb5f8acd0307eb50849840
Ashley Aiken <aaiken@redhat.com>
2020-05-20 17:49:31 -0400

    Apply suggestions from code review
    Co-authored-by: Douglas Curtis <docurtis@redhat.com>

--- koku/api/organizations/view.py
+++ koku/api/organizations/view.py
@@ -14,11 +14,11 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
-"""View for organizatins."""
+"""View for organizations."""
 from api.report.view import ReportView
 
 
 class OrganizationView(ReportView):
-    """Base Tag View."""
+    """Base Organizations View."""
 
     report = "organizations"
