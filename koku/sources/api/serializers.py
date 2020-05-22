91dfc3b7b96380f636853e5eab89dfac6ef794c7
Chris Hambridge <chambrid@redhat.com>
2020-05-22 11:39:46 -0400

    Handle invalid source type flows for provider accessor. (#2160)
    * Handle invalid source type flows for provider accessor.
    
    * Create a check to raise error if source type was invalid on provider accessor creation
    * Refactor error_msg function to common location.
    
    * Update import for common error_obj method.

--- koku/sources/api/serializers.py
+++ koku/sources/api/serializers.py
@@ -20,10 +20,10 @@ import logging
 from uuid import uuid4
 
 from django.db import transaction
-from django.utils.translation import ugettext as _
 from kombu.exceptions import OperationalError
 from rest_framework import serializers
 
+from api.common import error_obj
 from api.provider.models import Provider
 from api.provider.models import Sources
 from api.provider.serializers import LCASE_PROVIDER_CHOICE_LIST
@@ -47,12 +47,6 @@ ALLOWED_AUTHENTICATION_PROVIDERS = (Provider.PROVIDER_AZURE, Provider.PROVIDER_A
 
 class SourcesDependencyError(Exception):
     """General Exception for sources dependency errors."""
-
-
-def error_obj(key, message):
-    """Create an error object."""
-    error = {key: [_(message)]}
-    return error
 
 
 def validate_field(data, valid_fields, key):
