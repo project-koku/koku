f0f4d63ad1e347e210a5d4ca8f5b3dccf036bd3e
Andrew Berglund <aberglun@redhat.com>
2020-05-28 13:30:10 -0400

    Remove extra header log

--- koku/api/report/view.py
+++ koku/api/report/view.py
@@ -150,7 +150,7 @@ class ReportView(APIView):
 
         """
         LOG.debug(f"API: {request.path} USER: {request.user.username}")
-        LOG.info(request.headers)
+
         try:
             params = QueryParameters(request=request, caller=self, **kwargs)
         except ValidationError as exc:
