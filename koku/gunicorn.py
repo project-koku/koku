7a585b659b8fe89b49dccbba53d03d95b369edbe
Douglas Curtis <docurtis@redhat.com>
2020-06-10 12:59:20 -0400

    bumping worker count to match gunicorn docs

--- koku/gunicorn.py
+++ koku/gunicorn.py
@@ -10,7 +10,7 @@ SOURCES = ENVIRONMENT.bool("SOURCES", default=False)
 
 bind = "unix:/var/run/koku/gunicorn.sock"
 cpu_resources = int(os.environ.get("POD_CPU_LIMIT", multiprocessing.cpu_count()))
-workers = 1 if SOURCES else cpu_resources * 2
+workers = 1 if SOURCES else cpu_resources * 2 + 1
 
 timeout = int(os.environ.get("TIMEOUT", "90"))
 loglevel = os.environ.get("LOG_LEVEL", "INFO")
