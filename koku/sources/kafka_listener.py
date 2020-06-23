3b2e68b950c79cbe421dd9767d144a28df013808
Michael Skarbek <mskarbek@redhat.com>
2020-06-22 22:49:29 -0400

    undo kafka change;

--- koku/sources/kafka_listener.py
+++ koku/sources/kafka_listener.py
@@ -25,7 +25,6 @@ import threading
 import time
 
 from confluent_kafka import Consumer
-from confluent_kafka import KafkaError
 from confluent_kafka import TopicPartition
 from django.db import connection
 from django.db import InterfaceError
@@ -33,6 +32,7 @@ from django.db import OperationalError
 from django.db import transaction
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from kafka.errors import KafkaError
 from kombu.exceptions import OperationalError as RabbitOperationalError
 from rest_framework.exceptions import ValidationError
 
