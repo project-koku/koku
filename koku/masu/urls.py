a4974f7f9fa1596bfe0fd2982302fe5e75d43e30
Michael Skarbek <mskarbek@redhat.com>
2020-06-26 15:38:45 -0400

    Replace url() with path() (#2217)

--- koku/masu/urls.py
+++ koku/masu/urls.py
@@ -21,7 +21,6 @@ The `urlpatterns` list routes URLs to views. For more information please see:
 """
 from django.conf import settings
 from django.conf.urls import include
-from django.conf.urls import url
 from django.urls import path
 
 API_PATH_PREFIX = settings.API_PATH_PREFIX
@@ -31,4 +30,4 @@ if API_PATH_PREFIX != "":
     if not API_PATH_PREFIX.endswith("/"):
         API_PATH_PREFIX = API_PATH_PREFIX + "/"
 
-urlpatterns = [url(fr"^{API_PATH_PREFIX}v1/", include("masu.api.urls")), path("", include("django_prometheus.urls"))]
+urlpatterns = [path(f"{API_PATH_PREFIX}v1/", include("masu.api.urls")), path("", include("django_prometheus.urls"))]
