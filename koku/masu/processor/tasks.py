01ff28f7f33c04ca08f19ece28edeeb81d0bf560
Corey Goodfred <cgoodfre@redhat.com>
2020-07-13 11:07:03 -0400

    removed refresh_materialized_views call since it is in update_cost_model_costs

--- koku/masu/processor/tasks.py
+++ koku/masu/processor/tasks.py
@@ -279,14 +279,10 @@ def update_summary_tables(schema_name, provider, provider_uuid, start_date, end_
             line_items_only = True
             chain(
                 update_cost_model_costs.s(schema_name, provider_uuid, start_date, end_date),
-                refresh_materialized_views.si(schema_name, provider, manifest_id),
                 remove_expired_data.si(schema_name, provider, simulate, provider_uuid, line_items_only),
             ).apply_async()
         else:
-            chain(
-                update_cost_model_costs.s(schema_name, provider_uuid, start_date, end_date),
-                refresh_materialized_views.si(schema_name, provider, manifest_id),
-            ).apply_async()
+            update_cost_model_costs.delay(schema_name, provider_uuid, start_date, end_date)
     else:
         refresh_materialized_views.delay(schema_name, provider, manifest_id)
 
