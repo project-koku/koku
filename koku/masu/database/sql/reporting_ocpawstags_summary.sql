5dbf077ac6307b3c95be0d020dd69a9766585a64
Andrew Berglund <aberglun@redhat.com>
2020-07-18 18:35:03 -0400

    ocpaws tag summary to only have enabled tags

--- koku/masu/database/sql/reporting_ocpawstags_summary.sql
+++ koku/masu/database/sql/reporting_ocpawstags_summary.sql
@@ -2,6 +2,7 @@ INSERT INTO {{schema | sqlsafe}}.reporting_ocpawstags_summary (
     key,
     values,
     cost_entry_bill_id,
+    report_period_id,
     accounts,
     namespace
 )
@@ -13,10 +14,10 @@ WITH cte_unnested_aws_tags AS (
             value,
             cost_entry_bill_id,
             accounts
-        FROM reporting_awstags_summary AS ts,
+        FROM {{schema | sqlsafe}}.reporting_awstags_summary AS ts,
             unnest(ts.values) AS values(value)
     ) AS tags
-    JOIN reporting_awscostentrybill AS b
+    JOIN {{schema | sqlsafe}}.reporting_awscostentrybill AS b
         ON tags.cost_entry_bill_id = b.id
 ),
 cte_unnested_ocp_pod_tags AS (
@@ -27,12 +28,15 @@ cte_unnested_ocp_pod_tags AS (
             value,
             report_period_id,
             project
-        FROM reporting_ocpusagepodlabel_summary AS ts,
+        FROM {{schema | sqlsafe}}.reporting_ocpusagepodlabel_summary AS ts,
             unnest(ts.values) AS values(value),
             unnest(ts.namespace) AS namespaces(project)
     ) AS tags
-    JOIN reporting_ocpusagereportperiod AS rp
+    JOIN {{schema | sqlsafe}}.reporting_ocpusagereportperiod AS rp
         ON tags.report_period_id = rp.id
+    -- Filter out tags that aren't enabled
+    JOIN {{schema | sqlsafe}}.reporting_ocpenabledtagkeys as enabled_tags
+        ON lower(enabled_tags.key) = lower(tags.key)
 ),
 cte_unnested_ocp_volume_tags AS (
     SELECT tags.*,
@@ -42,16 +46,20 @@ cte_unnested_ocp_volume_tags AS (
             value,
             report_period_id,
             project
-        FROM reporting_ocpstoragevolumelabel_summary AS ts,
+        FROM {{schema | sqlsafe}}.reporting_ocpstoragevolumelabel_summary AS ts,
             unnest(ts.values) AS values(value),
             unnest(ts.namespace) AS namespaces(project)
     ) AS tags
-    JOIN reporting_ocpusagereportperiod AS rp
+    JOIN {{schema | sqlsafe}}.reporting_ocpusagereportperiod AS rp
         ON tags.report_period_id = rp.id
+    -- Filter out tags that aren't enabled
+    JOIN {{schema | sqlsafe}}.reporting_ocpenabledtagkeys as enabled_tags
+        ON lower(enabled_tags.key) = lower(tags.key)
 )
 SELECT key,
     array_agg(DISTINCT value) as values,
     cost_entry_bill_id,
+    report_period_id,
     accounts,
     array_agg(DISTINCT project) as namespace
 FROM (
@@ -59,11 +67,12 @@ FROM (
         aws.value,
         aws.accounts,
         ocp.project,
-        aws.cost_entry_bill_id
+        aws.cost_entry_bill_id,
+        ocp.report_period_id
     FROM cte_unnested_aws_tags AS aws
     JOIN cte_unnested_ocp_pod_tags AS ocp
-        ON aws.key = ocp.key
-            AND aws.value = ocp.value
+        ON lower(aws.key) = lower(ocp.key)
+            AND lower(aws.value) = lower(ocp.value)
             AND aws.billing_period_start = ocp.report_period_start
 
     UNION
@@ -72,16 +81,18 @@ FROM (
         aws.value,
         aws.accounts,
         ocp.project,
-        aws.cost_entry_bill_id
+        aws.cost_entry_bill_id,
+        ocp.report_period_id
     FROM cte_unnested_aws_tags AS aws
     JOIN cte_unnested_ocp_volume_tags AS ocp
-        ON aws.key = ocp.key
-            AND aws.value = ocp.value
+        ON lower(aws.key) = lower(ocp.key)
+            AND lower(aws.value) = lower(ocp.value)
             AND aws.billing_period_start = ocp.report_period_start
 ) AS matches
 GROUP BY key,
     accounts,
-    cost_entry_bill_id
+    cost_entry_bill_id,
+    report_period_id
 ON CONFLICT (key, cost_entry_bill_id) DO UPDATE
 SET values = EXCLUDED.values
 ;
