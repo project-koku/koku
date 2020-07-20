f78e6e82d47a56a7a0fd2e125534b230d4a6c39a
HAP <hproctor@redhat.com>
2020-07-20 10:26:25 -0400

    Add call in sql processing to create partitions on-the-fly

--- koku/masu/database/sql/reporting_awscostentrylineitem_daily_summary.sql
+++ koku/masu/database/sql/reporting_awscostentrylineitem_daily_summary.sql
@@ -76,6 +76,16 @@ WHERE li.usage_start >= {{start_date}}
     {% endif %}
 ;
 
+-- This procedure will scan the temp table for distinct start-of-month usage_start dates
+-- and create any missing table partitions
+CALL public.create_date_partitions(
+        'reporting_awscostentrylineitem_daily_summary_{{uuid | sqlsafe}}',
+        'usage_start',
+        '{{schema | sqlsafe}}',
+        'reporting_awscostentrylineitem_daily_summary',
+        'usage_start'
+    );
+
 -- Populate the daily aggregate line item data
 INSERT INTO {{schema | sqlsafe}}.reporting_awscostentrylineitem_daily_summary (
     cost_entry_bill_id,
