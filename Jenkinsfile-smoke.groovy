6f3427cec26f2c270f27636cd9093fe99c0c325e
Michael Skarbek <mskarbek@redhat.com>
2020-07-24 13:57:26 -0400

    add appConfigs

--- Jenkinsfile-smoke.groovy
+++ Jenkinsfile-smoke.groovy
@@ -4,15 +4,20 @@
 
 @Library("github.com/RedHatInsights/insights-pipeline-lib@v3") _
 
+// This smoke job will run upload tests.
+def appConfigs = [
+    cost_management: [plugins: ["cost_management"]]
+]
 
 if (env.CHANGE_ID) {
     execSmokeTest (
         ocDeployerBuilderPath: "hccm/koku",
         ocDeployerComponentPath: "hccm",
         ocDeployerServiceSets: "ingress,platform-mq,sources,hccm",
-        iqePlugins: ["iqe-cost_management-plugin"],
+        // iqePlugins: ["iqe-cost-management-plugin"],
         pytestMarker: "cost_smoke",
         // local settings file
         configFileCredentialsId: "hccm_smoke_settings_local_yaml",
+        appConfigs: appConfigs,
     )
 }
