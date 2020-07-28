8e0277abe03b0e9dd5be90b6c6a0dff8aedc711a
Corey Goodfred <cgoodfre@redhat.com>
2020-07-28 17:15:09 -0400

    changed _process_report_file to no longer take in a provider uuid since it is in the report_dict

--- koku/masu/processor/_tasks/process.py
+++ koku/masu/processor/_tasks/process.py
@@ -28,7 +28,7 @@ from masu.processor.report_processor import ReportProcessor
 LOG = get_task_logger(__name__)
 
 
-def _process_report_file(schema_name, provider, provider_uuid, report_dict):
+def _process_report_file(schema_name, provider, report_dict):
     """
     Task to process a Report.
 
