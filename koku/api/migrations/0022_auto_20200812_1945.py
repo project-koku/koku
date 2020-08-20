11b9148627f25a269ea36b904742de6b0bfc7f14
Michael Skarbek <mskarbek@redhat.com>
2020-08-20 16:29:49 -0400

    update data migration

--- koku/api/migrations/0022_auto_20200812_1945.py
+++ koku/api/migrations/0022_auto_20200812_1945.py
@@ -10,8 +10,8 @@ class Migration(migrations.Migration):
     operations = [
         migrations.RunSQL(
             """
-            UPDATE api_provider SET billing_source_id = (SELECT id FROM api_providerbillingsource WHERE data_source = '{"bucket": ""}'::jsonb) WHERE billing_source_id = (SELECT id FROM api_providerbillingsource WHERE bucket = '' and data_source = '{}'::jsonb);
-            DELETE FROM api_providerbillingsource WHERE data_source = '{"bucket": ""}'::jsonb;
+            UPDATE api_provider SET billing_source_id = (SELECT id FROM api_providerbillingsource WHERE bucket = '' and data_source = '{}'::jsonb) WHERE billing_source_id = (SELECT id FROM api_providerbillingsource WHERE data_source = '{"bucket": ""}'::jsonb);
+            DELETE FROM api_providerbillingsource WHERE data_source = '{"bucket": ""}'::jsonb AND bucket != '';
             UPDATE api_providerbillingsource SET data_source = jsonb_build_object('bucket', bucket) WHERE data_source = '{}'::jsonb;
             UPDATE api_providerauthentication SET credentials = jsonb_build_object('provider_resource_name', provider_resource_name) WHERE credentials = '{}'::jsonb;
             """
