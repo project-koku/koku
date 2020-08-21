44e91231a7decdbe4abb1046e1948b3e867d8f0f
HAP <hproctor@redhat.com>
2020-08-21 10:45:48 -0400

    Change to update SOURCE_REPOSITORY_REF on multiple files

--- scripts/update_e2e_srr.py
+++ scripts/update_e2e_srr.py
@@ -19,9 +19,15 @@
 Search for the koku SOURCE_REPOSITORY_REF setting in the
 e2e-deploy buildfactory dir koku.yaml file.
 """
+import logging
 import os
 import re
 import sys
+
+
+logging.basicConfig(level=logging.INFO, stream=sys.stderr)
+LOG = logging.getLogger("update_e2e_srr")
+
 
 # This regex will search the yaml text for the following pattern:
 #
@@ -32,26 +38,32 @@ import sys
 # To be able to replace XXXX with the correct ref
 # reading the yaml and dumping the updated yaml was not used
 # so that the whole file would not be reordered
-regex = "^(.*?name: SOURCE_REPOSITORY_REF.*?" + os.linesep + ".*?required: false.*?" + os.linesep + ".*?value: )(.+?)$"
-
-SRR = re.compile(regex, flags=re.MULTILINE)
-
-TARGET_FILE = os.path.join(os.environ["E2E_REPO"], "buildfactory", "hccm", "koku.yaml")
-
-try:
-    GIT_REF = sys.argv[1]
-except Exception:
-    raise Exception(f"Usage: {os.path.basename(sys.argv[0])} <git-ref>")
+REGEX = "^(.*?name: SOURCE_REPOSITORY_REF.*?" + os.linesep + ".*?required: false.*?" + os.linesep + ".*?value: )(.+?)$"
+SRR = re.compile(REGEX, flags=re.MULTILINE)
 
 
-# This is to ensure that the replacement has no issues since part of the grouping is multiline
-def repl(match):
-    return "{}{}".format(match.group(1), GIT_REF)
+def set_srr(yaml_file_name, git_ref):
+    # This is to ensure that the replacement has no issues since part of the grouping is multiline
+    def repl(match):
+        return "{}{}".format(match.group(1), git_ref)
+
+    target_file = os.path.join(os.environ["E2E_REPO"], "buildfactory", yaml_file_name)
+    LOG.info(f'Processing "{target_file}"')
+    with open(target_file, "rt+") as e2e_yaml:
+        buff = e2e_yaml.read()
+        e2e_yaml.seek(0)
+        e2e_yaml.write(SRR.sub(repl, buff))
+        e2e_yaml.flush()
+        e2e_yaml.truncate()
 
 
-with open(TARGET_FILE, "rt+") as koku_yaml:
-    buff = koku_yaml.read()
-    koku_yaml.seek(0)
-    koku_yaml.write(SRR.sub(repl, buff))
-    koku_yaml.flush()
-    koku_yaml.truncate()
+if __name__ == "__main__":
+    try:
+        git_ref = sys.argv[1]
+    except Exception:
+        raise Exception(f"Usage: {os.path.basename(sys.argv[0])} <git-ref>")
+
+    YAML_FILES = [os.path.join("hccm", "koku.yaml"), os.path.join("hccm-optional", "celery-flower.yaml")]
+
+    for yml_file in YAML_FILES:
+        set_srr(yml_file, git_ref)
