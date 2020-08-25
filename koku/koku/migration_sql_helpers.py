128bfd42f9dc0b8487fac39cb3ab06425206af66
HAP <59652772+Red-HAP@users.noreply.github.com>
2020-08-25 13:43:52 -0400

    Cost 156 partitioned table prototype -- TESTED (#2256)
    * Added partitioning script and some make changes
    
    * Now with indexing and constraints
    
    * Working on fixing views, materialized views w/indexes
    
    * Matviews and sequences are working now so source table can be dropped
    
    * Set the sequences to the last known value
    
    * Added some functionality and generation of PK from record parts
    
    * Edit masu SQL to set the UUID pk on the partitioned table; edit model to change the PK type to UUIDField; added cascade to drop table statement in the converter script
    
    * Partitioning as migration
    
    * Update README with partition information
    
    * Making optimizations to migrations
    
    * Reworked text migrations into something programmatic and easier to deal with
    
    * Fixed matview getter SQL to be much more precise
    
    * Added tests
    
    * Added some migration helpers to help with application of sql files
    
    * Merged COST-351, COST-352 into this branch and coalesced the migrations into one file
    
    Co-authored-by: Andrew Berglund <aberglun@redhat.com>

---
+++ koku/koku/migration_sql_helpers.py
@@ -0,0 +1,59 @@
+#
+# Copyright 2020 Red Hat, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+import os
+
+from django.db.backends.base.schema import BaseDatabaseSchemaEditor
+
+
+def find_db_functions_dir(db_func_dir_name="db_functions"):
+    """
+    Find the directory in the project path that contains the database functions.
+    Params:
+        db_func_dir_name (str) : name of the directory containing the SQL function files
+    Returns:
+        (str) : Directory path including the function dir name
+    """
+    path = os.path.dirname(os.path.abspath(__file__))
+    while True:
+        if db_func_dir_name not in os.listdir(path):
+            if path == os.path.sep:
+                raise FileNotFoundError("Could not find the db_functions dir")
+            path = os.path.dirname(path)
+        else:
+            path = os.path.join(path, db_func_dir_name)
+            break
+
+    return path
+
+
+def apply_sql_file(conn, path):
+    """
+    Reads the contents of the given SQL file and passes the buffer to the executor
+    for application to the database.
+    Params:
+        conn (BaseDatabaseSchemaEditor) : DB executor
+        path (str) : Path to the SQL file to apply
+    Returns:
+        True
+    """
+    sqlbuff = open(path, "rt").read()
+    if isinstance(conn, BaseDatabaseSchemaEditor):
+        conn.execute(sqlbuff)
+    else:
+        raise TypeError(f'Cannot apply SQL file "{path}" with instance of "{conn.__class__.__name__}"')
+
+    return True
