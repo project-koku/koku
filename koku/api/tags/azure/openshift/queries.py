fc6ceb9764e59b2c776e5e87807f2126837dd18e
Corey Goodfred <53098963+cgoodfred@users.noreply.github.com>
2020-08-27 17:13:26 -0400

    [COST-272] Filter on tag key/value (#2339)
    * AWSTagsValues model and migration
    
    * update sql for many-to-many relationship
    
    * got ?filter[key]= working like before database change
    
    * moved changes to aws tag query handler since they will likely be specific to each provider
    
    * got base ?filter[value] working
    
    * filter by value works and didnt break other endpoints this time
    
    * did not mean to remove that... oops
    
    * azure model and migration for tag filtering
    
    * azure tags summary sql update
    
    * added arrayfield back and adjusted sql/migrations accordingly
    
    * added some basic tests in aws and azure tag handlers
    
    * Model changes for many to many, _get_key_filter for each tag provider, tag values query works and first set of tests... def shoulda split this commit
    
    * bypass enabled check for values search and ocp tests
    
    * grouping by namespace in ocp sql
    
    * some SQL formatting changes
    
    * _get_key_filter not in every provider, field defined in each provider class
    
    * remove some excess code, OCP on All working, can't filter on value at wrong endpoint now
    
    * put fields in a list and loop through fields in case there is more than one for each table
    
    * sort based on starts with first, then contains
    
    Co-authored-by: Michael Skarbek <mskarbek@redhat.com>

--- koku/api/tags/azure/openshift/queries.py
+++ koku/api/tags/azure/openshift/queries.py
@@ -15,12 +15,14 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
 """OCP-on-Azure Tag Query Handling."""
+from copy import deepcopy
+
 from api.models import Provider
 from api.report.azure.openshift.provider_map import OCPAzureProviderMap
 from api.tags.azure.queries import AzureTagQueryHandler
 from api.tags.ocp.queries import OCPTagQueryHandler
-from api.utils import merge_dicts
 from reporting.models import OCPAzureTagsSummary
+from reporting.provider.azure.openshift.models import OCPAzureTagsValues
 
 
 class OCPAzureTagQueryHandler(AzureTagQueryHandler, OCPTagQueryHandler):
@@ -28,8 +30,10 @@ class OCPAzureTagQueryHandler(AzureTagQueryHandler, OCPTagQueryHandler):
 
     provider = Provider.OCP_AZURE
     data_sources = [{"db_table": OCPAzureTagsSummary, "db_column_period": "cost_entry_bill__billing_period"}]
+    TAGS_VALUES_SOURCE = [{"db_table": OCPAzureTagsValues, "fields": ["ocpazuretagssummary__key"]}]
     SUPPORTED_FILTERS = AzureTagQueryHandler.SUPPORTED_FILTERS + OCPTagQueryHandler.SUPPORTED_FILTERS
-    FILTER_MAP = merge_dicts(AzureTagQueryHandler.FILTER_MAP, OCPTagQueryHandler.FILTER_MAP)
+    FILTER_MAP = deepcopy(AzureTagQueryHandler.FILTER_MAP)
+    FILTER_MAP.update(OCPTagQueryHandler.FILTER_MAP)
     # override cluster since we are getting it from a different table and field(s)
     FILTER_MAP["cluster"] = [
         {"field": "cluster_alias", "operation": "icontains", "composition_key": "cluster_filter"},
--- koku/masu/database/sql/reporting_ocpstoragevolumelabel_summary.sql
+++ koku/masu/database/sql/reporting_ocpstoragevolumelabel_summary.sql
@@ -1,29 +1,47 @@
-INSERT INTO {{schema | sqlsafe}}.reporting_ocpstoragevolumelabel_summary (
-    key,
-    values,
-    report_period_id,
-    namespace
-)
-SELECT l.key,
-    array_agg(DISTINCT l.value) as values,
-    l.report_period_id,
-    array_agg(DISTINCT l.namespace)
-FROM (
-    SELECT key,
-        value,
-        li.report_period_id,
-        li.namespace
-    FROM {{schema | sqlsafe}}.reporting_ocpstoragelineitem_daily AS li,
-        jsonb_each_text(li.persistentvolume_labels || li.persistentvolumeclaim_labels) labels
-    {% if report_periods %}
-    WHERE li.report_period_id IN (
-        {%- for report_period_id in report_period_ids -%}
-        {{report_period_id}}{% if not loop.last %},{% endif %}
-        {%- endfor -%}
+WITH data(key, value, report_period_id, namespace) AS (
+    SELECT l.key,
+        l.value,
+        l.report_period_id,
+        array_agg(DISTINCT l.namespace)
+    FROM (
+        SELECT key,
+            value,
+            li.report_period_id,
+            li.namespace
+        FROM {{schema | sqlsafe}}.reporting_ocpstoragelineitem_daily AS li,
+            jsonb_each_text(li.persistentvolume_labels || li.persistentvolumeclaim_labels) labels
+        {% if report_periods %}
+        WHERE li.report_period_id IN (
+            {%- for report_period_id in report_period_ids -%}
+            {{report_period_id}}{% if not loop.last %},{% endif %}
+            {%- endfor -%}
+        )
+        {% endif %}
+    ) l
+    GROUP BY l.key, l.value, l.report_period_id, l.namespace
+),
+data2(key, values, namespace) AS (SELECT data.key, array_agg(DISTINCT data.value), namespace from data GROUP BY data.key, data.namespace)
+, ins1 AS (
+    INSERT INTO {{schema | sqlsafe}}.reporting_ocpstoragevolumelabel_summary (key, report_period_id, namespace, values)
+    SELECT DISTINCT data.key AS key,
+    data.report_period_id AS report_period_id,
+    data.namespace AS namespace,
+    data2.values AS values
+    FROM data INNER JOIN data2 ON data.key = data2.key AND data.namespace = data2.namespace
+    ON CONFLICT (key, report_period_id, namespace) DO UPDATE SET key = EXCLUDED.key
+    RETURNING key, id AS key_id
     )
-    {% endif %}
-) l
-GROUP BY l.key, l.report_period_id, l.namespace
-ON CONFLICT (key, report_period_id, namespace) DO UPDATE
-SET values = EXCLUDED.values
+, ins2 AS (
+   INSERT INTO {{schema | sqlsafe}}.reporting_ocptags_values (value)
+   SELECT DISTINCT d.value
+   FROM data d
+   ON CONFLICT (value) DO UPDATE SET value=EXCLUDED.value
+   RETURNING value, id AS values_id
+    )
+INSERT INTO {{schema | sqlsafe}}.reporting_ocpstoragevolumelabel_summary_values_mtm (ocpstoragevolumelabelsummary_id, ocptagsvalues_id)
+SELECT DISTINCT ins1.key_id, ins2.values_id
+FROM data d
+INNER JOIN ins1 ON d.key = ins1.key
+INNER JOIN ins2 ON d.value = ins2.value
+ON CONFLICT (ocpstoragevolumelabelsummary_id, ocptagsvalues_id) DO NOTHING
 ;
