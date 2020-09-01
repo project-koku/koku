40f0c6a61c3724f4aa3fd525f8d5e238abdeb8c7
HAP <hproctor@redhat.com>
2020-09-01 10:55:27 -0400

    Added logging and more error checking

--- scripts/status_probe.py
+++ scripts/status_probe.py
@@ -17,6 +17,7 @@
 #
 import argparse
 import json
+import logging
 import sys
 
 import requests
@@ -37,37 +38,91 @@ parser.add_argument(
 parser.add_argument(
     "--display-key", dest="display_key", action="store_true", default=False, help="Display key with value"
 )
-args = parser.parse_args()
+parser.add_argument(
+    "--no-convert-int-key",
+    dest="convert_int",
+    action="store_false",
+    default=True,
+    help="Do not convert digit keys to integers",
+)
+choices = ("INFO", "WARNING", "DEBUG", "ERROR")
+parser.add_argument(
+    "--log-level",
+    dest="level",
+    default="INFO",
+    choices=choices,
+    metavar="LEVEL",
+    help=f"Set log level {choices}. Default: INFO",
+)
 
-if args.status_url:
+args = parser.parse_args()
+logging.basicConfig(
+    format="%(asctime)s %(name)s [%(levelname)s] %(message)s", stream=sys.stderr, level=getattr(logging, args.level)
+)
+LOG = logging.getLogger("status_probe")
+
+if args.status_url:  # noqa: C901
+    LOG.debug(f"Reading status from {args.status_url}")
     resp = requests.get(args.status_url)
     if resp.status_code != 200:
-        raise Exception(f"Server did not responde with 200! ({resp.status_code})")
+        LOG.error(f"Server did not responde with 200! ({resp.status_code})")
+        sys.exit(-2)
 
     app_status = resp.json()
 else:
     if args.filename == "-":
+        fname = "stdin"
         jfile = sys.stdin
     else:
-        jfile = open(args.filename, "rt")
+        fname = args.filename
+        try:
+            jfile = open(args.filename, "rt")
+        except Exception as e:
+            LOG.error(f"{e.__class__.__name__}: Cannot open file {fname}: {e}")
+            sys.exit(-1)
 
-    content = jfile.read()
+    LOG.debug(f"Reading status from {fname}")
+    try:
+        content = jfile.read()
+    except Exception as e:
+        LOG.error(f"{e.__class__.__name__}: Cannot read file {fname}: {e}")
+        sys.exit(-1)
+
     if jfile != sys.stdin:
         jfile.close()
-    app_status = json.loads(content)
+
+    if content:
+        try:
+            app_status = json.loads(content)
+        except Exception as e:
+            LOG.error(f"{e.__class__.__name__}: Cannot parse json: {e}")
+            sys.exit(-1)
+    else:
+        LOG.warning("No content to parse!")
+        sys.exit(1)
 
 
 paths = args.path.split(";")
 
 for path in paths:
+    LOG.debug(f'Processing path "{path}"')
     value = app_status
-    if args.display_key:
-        print(f"{path}\t", end="")
+    err = False
     for key in path.split("."):
-        if key.isdigit():
+        if key.isdigit() and args.convert_int:
             key = int(key)
-        value = value[key]
-    print(value)
+
+        LOG.debug(f'Processing "{key}" ({type(key).__name__}) from path "{path}"')
+        try:
+            value = value[key]
+        except KeyError:
+            LOG.error(f'Key "{key}" ({type(key).__name__}) in path "{path}" does not exist.')
+            err = True
+
+    if not err:
+        if args.display_key:
+            print(f"{path}\t", end="")
+        print(value)
 
 sys.stdout.flush()
 
