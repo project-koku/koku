45b18664fc388ed8811b04290cbc50156cd31122
lrlevine6 <lrlevine@ncsu.edu>
2020-09-08 09:43:28 -0400

    removed comments

--- koku/api/resource_types/aws_accounts/view.py
+++ koku/api/resource_types/aws_accounts/view.py
@@ -17,12 +17,10 @@
 from django.utils.decorators import method_decorator
 from django.views.decorators.vary import vary_on_headers
 from rest_framework.views import APIView
-from tenant_schemas.utils import tenant_context
 
 from api.common import CACHE_RH_IDENTITY_HEADER
 from api.common.pagination import ListPaginator
 from api.common.permissions.resource_type_access import ResourceTypeAccessPermission
-from api.query_params import get_tenant
 from reporting.provider.aws.models import AWSCostSummaryByAccount
 
 
@@ -33,119 +31,6 @@ class AWSAccountView(APIView):
 
     @method_decorator(vary_on_headers(CACHE_RH_IDENTITY_HEADER))
     def get(self, request, **kwargs):
-
-        tenant = get_tenant(request.user)
-        with tenant_context(tenant):
-
-            # aws_account_count = AWSCostSummaryByAccount.objects.values("usage_account_id").distinct().count()
-            # aws_org_unit_count = (
-            #     AWSOrganizationalUnit.objects.filter(deleted_timestamp__isnull=True)
-            #     .values("org_unit_id")
-            #     .distinct()
-            #     .count()
-            # )
-            # azure_sub_guid_count = AzureCostSummaryByAccount.objects.values("subscription_guid").distinct().count()
-            # ocp_cluster_count = OCPCostSummary.objects.values("cluster_id").distinct().count()
-            # ocp_node_count = OCPCostSummaryByNode.objects.values("node").distinct().count()
-            # ocp_project_count = OCPCostSummaryByProject.objects.values("namespace").distinct().count()
-            # cost_model_count = CostModel.objects.count()
-
-            # aws_account_dict = {
-            #     "value": "aws.account",
-            #     "path": "/api/cost-management/v1/resource-types/aws-accounts/",
-            #     "count": aws_account_count,
-            # }
-            # aws_org_unit_dict = {
-            #     "value": "aws.organizational_unit",
-            #     "path": "/api/cost-management/v1/resource-types/aws-organizational-units/",
-            #     "count": aws_org_unit_count,
-            # }
-            # azure_sub_guid_dict = {
-            #     "value": "azure.subscription_guid",
-            #     "path": "/api/cost-management/v1/resource-types/azure-subscription-guids/",
-            #     "count": azure_sub_guid_count,
-            # }
-            # ocp_cluster_dict = {
-            #     "value": "openshift.cluster",
-            #     "path": "/api/cost-management/v1/resource-types/openshift-clusters/",
-            #     "count": ocp_cluster_count,
-            # }
-            # ocp_node_dict = {
-            #     "value": "openshift.node",
-            #     "path": "/api/cost-management/v1/resource-types/openshift-nodes/",
-            #     "count": ocp_node_count,
-            # }
-            # ocp_project_dict = {
-            #     "value": "openshift.project",
-            #     "path": "/api/cost-management/v1/resource-types/openshift-projects/",
-            #     "count": ocp_project_count,
-            # }
-            # cost_model_dict = {
-            #     "value": "rate",
-            #     "path": "/api/cost-management/v1/resource-types/rates/",
-            #     "count": cost_model_count,
-            # }
-            # data = [
-            #     aws_account_dict,
-            #     aws_org_unit_dict,
-            #     azure_sub_guid_dict,
-            #     ocp_cluster_dict,
-            #     ocp_node_dict,
-            #     ocp_project_dict,
-            #     cost_model_dict,
-            # ]
-            data = AWSCostSummaryByAccount.objects.values("usage_account_id").distinct()
-            paginator = ListPaginator(data, request)
-            # return Response(data)
-            return paginator.get_paginated_response(data)
-
-        """
-        {
-            "meta": {
-                "count": 30
-            },
-            "links": {
-                "first": "/api/cost-management/v1/resource-types/?offset=0",
-                "previous": "/api/cost-management/v1/resource-types/?offset=20",
-                "next": "/api/cost-management/v1/resource-types/?offset=40",
-                "last": "/api/cost-management/v1/resource-types/?offset=100"
-            },
-            "data": [
-                {
-                "value": "aws.account",
-                "path": "/api/cost-management/v1/resource-types/aws-accounts/",
-                "count": 10
-                },
-                {
-                "value": "aws.organizational_unit",
-                "path": "/api/cost-management/v1/resource-types/aws-organizational-units/",
-                "count": 2
-                },
-                {
-                "value": "azure.subscription_guid",
-                "path": "/api/cost-management/v1/resource-types/azure-subscription-guids/",
-                "count": 0
-                },
-                {
-                "value": "openshift.cluster",
-                "path": "/api/cost-management/v1/resource-types/openshift-clusters/",
-                "count": 3
-                },
-                {
-                "value": "openshift.node",
-                "path": "/api/cost-management/v1/resource-types/openshift-nodes/",
-                "count": 12
-                },
-                {
-                "value": "openshift.project",
-                "path": "/api/cost-management/v1/resource-types/openshift-projects/",
-                "count": 47
-                },
-                {
-                "value": "rate",
-                "path": "/api/cost-management/v1/resource-types/rates/",
-                "count": 4
-                }
-            ]
-            }
-            """
+        data = AWSCostSummaryByAccount.objects.values("usage_account_id").distinct()
+        paginator = ListPaginator(data, request)
+        return paginator.get_paginated_response(data)
