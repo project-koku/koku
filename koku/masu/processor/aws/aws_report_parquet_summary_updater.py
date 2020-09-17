88a78b3bf375e85c23d237e9bcdbebae34d02085
Andrew Berglund <aberglun@redhat.com>
2020-09-17 16:15:01 -0400

    Sneak in markup calculation for AWS

--- koku/masu/processor/aws/aws_report_parquet_summary_updater.py
+++ koku/masu/processor/aws/aws_report_parquet_summary_updater.py
@@ -1,6 +1,7 @@
 import logging
 
 from masu.database.aws_report_db_accessor import AWSReportDBAccessor
+from masu.database.cost_model_db_accessor import CostModelDBAccessor
 from masu.util.common import date_range_pair
 
 LOG = logging.getLogger(__name__)
@@ -49,6 +50,10 @@ class AWSReportParquetSummaryUpdater:
         """
         start_date, end_date = self._get_sql_inputs(start_date, end_date)
 
+        with CostModelDBAccessor(self._schema, self._provider.uuid) as cost_model_accessor:
+            markup = cost_model_accessor.markup
+            markup_value = float(markup.get("value", 0)) / 100
+
         with AWSReportDBAccessor(self._schema) as accessor:
             # Need these bills on the session to update dates after processing
             for start, end in date_range_pair(start_date, end_date):
@@ -61,7 +66,7 @@ class AWSReportParquetSummaryUpdater:
                     end,
                 )
                 accessor.populate_line_item_daily_summary_table_from_parquet(
-                    start, end, self._provider.uuid, self._manifest.id
+                    start, end, self._provider.uuid, self._manifest.id, markup_value
                 )
 
         return start_date, end_date
