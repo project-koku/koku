dd71f507d9f67b10ed965cb604a765da52d8625c
Andrew Berglund <aberglun@redhat.com>
2020-09-21 15:35:56 -0400

    Add ocp on x infra to migration

--- koku/reporting/migrations/0138_auto_20200918_1724.py
+++ koku/reporting/migrations/0138_auto_20200918_1724.py
@@ -22,6 +22,12 @@ class Migration(migrations.Migration):
 
             TRUNCATE TABLE reporting_azuretags_summary CASCADE;
             TRUNCATE TABLE reporting_azuretags_values CASCADE;
+
+            TRUNCATE TABLE reporting_ocpawstags_summary CASCADE;
+            TRUNCATE TABLE reporting_ocpawstags_values CASCADE;
+
+            TRUNCATE TABLE reporting_ocpazuretags_summary CASCADE;
+            TRUNCATE TABLE reporting_ocpazuretags_values CASCADE;
             """
         ),
         migrations.RemoveField(model_name="awstagssummary", name="id"),
@@ -35,6 +41,16 @@ class Migration(migrations.Migration):
         migrations.RemoveField(model_name="awstagsvalues", name="id"),
         migrations.RemoveField(model_name="azuretagsvalues", name="id"),
         migrations.RemoveField(model_name="ocptagsvalues", name="id"),
+        migrations.RemoveField(model_name="ocpazuretagsvalues", name="id"),
+        migrations.RemoveField(model_name="ocpawstagssummary", name="id"),
+        migrations.RemoveField(model_name="ocpawstagsvalues", name="id"),
+        migrations.RemoveField(model_name="ocpazuretagssummary", name="id"),
+        migrations.RemoveField(model_name="ocpawstagssummary", name="cluster_alias"),
+        migrations.RemoveField(model_name="ocpawstagssummary", name="cluster_id"),
+        migrations.RemoveField(model_name="ocpazuretagssummary", name="cluster_alias"),
+        migrations.RemoveField(model_name="ocpazuretagssummary", name="cluster_id"),
+        migrations.RemoveField(model_name="ocpazuretagssummary", name="values_mtm"),
+        migrations.RemoveField(model_name="ocpawstagssummary", name="values_mtm"),
         migrations.AddField(
             model_name="awstagssummary",
             name="uuid",
@@ -163,5 +179,143 @@ class Migration(migrations.Migration):
         ),
         migrations.AddIndex(
             model_name="ocptagsvalues", index=models.Index(fields=["key"], name="openshift_tags_value_key_idx")
+        ),
+        migrations.AddField(model_name="ocpawstagssummary", name="node", field=models.TextField(null=True)),
+        migrations.AddField(
+            model_name="ocpawstagssummary",
+            name="report_period",
+            field=models.ForeignKey(
+                default="", on_delete=django.db.models.deletion.CASCADE, to="reporting.OCPUsageReportPeriod"
+            ),
+            preserve_default=False,
+        ),
+        migrations.AddField(
+            model_name="ocpawstagssummary",
+            name="uuid",
+            field=models.UUIDField(default=uuid.uuid4, primary_key=True, serialize=False),
+        ),
+        migrations.AddField(
+            model_name="ocpawstagsvalues", name="key", field=models.TextField(default=""), preserve_default=False
+        ),
+        migrations.AddField(
+            model_name="ocpawstagsvalues",
+            name="account_aliases",
+            field=django.contrib.postgres.fields.ArrayField(base_field=models.TextField(), default=[], size=None),
+            preserve_default=False,
+        ),
+        migrations.AddField(
+            model_name="ocpawstagsvalues",
+            name="cluster_aliases",
+            field=django.contrib.postgres.fields.ArrayField(base_field=models.TextField(), default=[], size=None),
+            preserve_default=False,
+        ),
+        migrations.AddField(
+            model_name="ocpawstagsvalues",
+            name="cluster_ids",
+            field=django.contrib.postgres.fields.ArrayField(base_field=models.TextField(), default=[], size=None),
+            preserve_default=False,
+        ),
+        migrations.AddField(
+            model_name="ocpawstagsvalues",
+            name="namespaces",
+            field=django.contrib.postgres.fields.ArrayField(base_field=models.TextField(), default=[], size=None),
+            preserve_default=False,
+        ),
+        migrations.AddField(
+            model_name="ocpawstagsvalues",
+            name="nodes",
+            field=django.contrib.postgres.fields.ArrayField(base_field=models.TextField(), null=True, size=None),
+        ),
+        migrations.AddField(
+            model_name="ocpawstagsvalues",
+            name="usage_account_ids",
+            field=django.contrib.postgres.fields.ArrayField(base_field=models.TextField(), default=[], size=None),
+            preserve_default=False,
+        ),
+        migrations.AddField(
+            model_name="ocpawstagsvalues",
+            name="uuid",
+            field=models.UUIDField(default=uuid.uuid4, primary_key=True, serialize=False),
+        ),
+        migrations.AddField(model_name="ocpazuretagssummary", name="node", field=models.TextField(null=True)),
+        migrations.AddField(
+            model_name="ocpazuretagssummary",
+            name="report_period",
+            field=models.ForeignKey(
+                default="", on_delete=django.db.models.deletion.CASCADE, to="reporting.OCPUsageReportPeriod"
+            ),
+            preserve_default=False,
+        ),
+        migrations.AddField(
+            model_name="ocpazuretagssummary",
+            name="uuid",
+            field=models.UUIDField(default=uuid.uuid4, primary_key=True, serialize=False),
+        ),
+        migrations.AddField(
+            model_name="ocpazuretagsvalues", name="key", field=models.TextField(default=""), preserve_default=False
+        ),
+        migrations.AddField(
+            model_name="ocpazuretagsvalues",
+            name="cluster_aliases",
+            field=django.contrib.postgres.fields.ArrayField(base_field=models.TextField(), default=[], size=None),
+            preserve_default=False,
+        ),
+        migrations.AddField(
+            model_name="ocpazuretagsvalues",
+            name="cluster_ids",
+            field=django.contrib.postgres.fields.ArrayField(base_field=models.TextField(), default=[], size=None),
+            preserve_default=False,
+        ),
+        migrations.AddField(
+            model_name="ocpazuretagsvalues",
+            name="namespaces",
+            field=django.contrib.postgres.fields.ArrayField(base_field=models.TextField(), default=[], size=None),
+            preserve_default=False,
+        ),
+        migrations.AddField(
+            model_name="ocpazuretagsvalues",
+            name="nodes",
+            field=django.contrib.postgres.fields.ArrayField(base_field=models.TextField(), null=True, size=None),
+        ),
+        migrations.AddField(
+            model_name="ocpazuretagsvalues",
+            name="subscription_guids",
+            field=django.contrib.postgres.fields.ArrayField(base_field=models.TextField(), default=[], size=None),
+            preserve_default=False,
+        ),
+        migrations.AddField(
+            model_name="ocpazuretagsvalues",
+            name="uuid",
+            field=models.UUIDField(default=uuid.uuid4, primary_key=True, serialize=False),
+        ),
+        migrations.AlterField(
+            model_name="ocpawstagssummary",
+            name="namespace",
+            field=models.TextField(default=""),
+            preserve_default=False,
+        ),
+        migrations.AlterField(model_name="ocpawstagsvalues", name="value", field=models.TextField()),
+        migrations.AlterField(
+            model_name="ocpazuretagssummary",
+            name="namespace",
+            field=models.TextField(default=""),
+            preserve_default=False,
+        ),
+        migrations.AlterField(model_name="ocpazuretagsvalues", name="value", field=models.TextField()),
+        migrations.AlterUniqueTogether(
+            name="ocpawstagssummary",
+            unique_together={("key", "cost_entry_bill", "report_period", "usage_account_id", "namespace")},
+        ),
+        migrations.AlterUniqueTogether(name="ocpawstagsvalues", unique_together={("key", "value")}),
+        migrations.AlterUniqueTogether(
+            name="ocpazuretagssummary",
+            unique_together={("key", "cost_entry_bill", "report_period", "subscription_guid", "namespace")},
+        ),
+        migrations.AddIndex(
+            model_name="ocpawstagsvalues", index=models.Index(fields=["key"], name="ocp_aws_tags_value_key_idx")
+        ),
+        migrations.AlterUniqueTogether(name="ocpazuretagsvalues", unique_together={("key", "value")}),
+        migrations.AddIndex(
+            model_name="ocpazuretagsvalues", index=models.Index(fields=["key"], name="ocp_azure_tags_value_key_idx")
         ),
     ]
