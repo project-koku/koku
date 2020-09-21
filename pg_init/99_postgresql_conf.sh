a462e83dad7a6caf1f666dcfe846bba1b450470a
Andrew Berglund <aberglun@redhat.com>
2020-09-21 13:59:13 -0400

    Add uuid extention

--- pg_init/99_postgresql_conf.sh
+++ pg_init/99_postgresql_conf.sh
@@ -3,5 +3,9 @@
 echo "Creating pg_stat_statements extension..."
 psql -d $POSTGRES_DB -U $POSTGRES_USER -c "create extension if not exists pg_stat_statements;" 2>/dev/null
 
+echo "Creating uuid-ossp extension..."
+psql -d $POSTGRES_DB -U $POSTGRES_USER -c 'create extension if not exists "uuid-ossp";' 2>/dev/null
+
+
 echo "Creating dbmonitor role..."
 psql -d $POSTGRES_DB -U $POSTGRES_USER -c "create role dbmonitor with inherit login encrypted password 'dbmonitor' superuser in role $POSTGRES_USER;"
