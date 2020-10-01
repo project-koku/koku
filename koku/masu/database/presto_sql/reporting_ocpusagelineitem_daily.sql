813b46c031f9285d1310d73085239336cde9555e
HAP <hproctor@redhat.com>
2020-10-01 10:13:34 -0400

    Handle fallout from left join

--- koku/masu/database/presto_sql/reporting_ocpusagelineitem_daily.sql
+++ koku/masu/database/presto_sql/reporting_ocpusagelineitem_daily.sql
@@ -1,5 +1,15 @@
 
+-- ============================================================================================
+-- NOTE TO ANY PREVIEWERS:: I'm using "uuid_eek" as a placeholder so I don't forget to change
+--                          that back to jinja templating (along with the hardcoded values)
+--                          for use in the app.
+-- ============================================================================================
+
+-- Using the convention of a double-underscore prefix to denote a temp table.
+
 -- node label line items by day presto sql
+-- still using a "temp" table here because there is no guarantee how big this might get
+DROP TABLE IF EXISTS hive.acct10001.__ocp_node_label_line_item_daily_uuid_eek;
 CREATE TABLE hive.acct10001.__ocp_node_label_line_item_daily_uuid_eek AS (
     SELECT cast(p_src.authentication as MAP(varchar, MAP(varchar, varchar)))['credentials']['cluster_id'] as "cluster_id",
            date(nli.interval_start) as "usage_start",
@@ -12,7 +22,10 @@ CREATE TABLE hive.acct10001.__ocp_node_label_line_item_daily_uuid_eek AS (
       FROM hive.acct10001.openshift_node_labels_line_items as "nli"
       JOIN postgres.public.api_sources p_src
         ON cast(p_src.source_uuid as varchar) = nli.source
-     WHERE date(nli.interval_start) >= DATE '2020-09-01'
+     WHERE nli.source = '511aab54-5b4f-4c69-a985-325ff9aa1329'
+       AND nli.year = '2020'
+       AND nli.month = '09'
+       AND date(nli.interval_start) >= DATE '2020-09-01'
        AND date(nli.interval_start) <= DATE '2020-09-08'
      GROUP
         BY 1, 2, 4
@@ -20,6 +33,8 @@ CREATE TABLE hive.acct10001.__ocp_node_label_line_item_daily_uuid_eek AS (
 ;
 
 -- cluster daily cappacity presto sql
+-- still using a "temp" table here because there is no guarantee how big this might get
+DROP TABLE IF EXISTS hive.acct10001.__ocp_cluster_capacity_uuid_eek;
 CREATE TABLE hive.acct10001.__ocp_cluster_capacity_uuid_eek as (
     SELECT cc.cluster_id,
            coalesce(cc.cluster_alias, cc.cluster_id) as "cluster_alias",
@@ -41,7 +56,10 @@ CREATE TABLE hive.acct10001.__ocp_cluster_capacity_uuid_eek as (
                  FROM hive.acct10001.openshift_pod_usage_line_items AS li
                  JOIN postgres.public.api_sources p_src
                    ON cast(p_src.source_uuid as varchar) = li.source
-                WHERE date(li.interval_start) >= DATE '2020-09-01'
+                WHERE li.source = '511aab54-5b4f-4c69-a985-325ff9aa1329'
+                  AND li.year = '2020'
+                  AND li.month = '09'
+                  AND date(li.interval_start) >= DATE '2020-09-01'
                   AND date(li.interval_start) <= DATE '2020-09-08'
                 GROUP
                    BY 1, 2, 3
@@ -51,7 +69,11 @@ CREATE TABLE hive.acct10001.__ocp_cluster_capacity_uuid_eek as (
 )
 ;
 
--- daily summarization??
+-- This is the target summarization sql
+-- It combines the prior daily summarization query with the final summarization query
+-- by use of MAP_FILTER to filter the combined node line item labels as well as
+-- the line-item pod labels against the postgres enabled keys in the same query
+DROP TABLE IF EXISTS hive.acct10001.__reporting_ocpusagelineitem_daily_uuid_eek;
 CREATE TABLE hive.acct10001.__reporting_ocpusagelineitem_daily_uuid_eek AS (
     SELECT li.report_period_start,
            cc.cluster_id,
@@ -62,7 +84,7 @@ CREATE TABLE hive.acct10001.__reporting_ocpusagelineitem_daily_uuid_eek AS (
            li.pod,
            li.node,
            max(li.resource_id) as "resource_id",
-           map_filter(map_concat(cast(json_parse(nli.node_labels) as map(varchar, varchar)),
+           map_filter(map_concat(cast(json_parse(coalesce(nli.node_labels, '{}')) as map(varchar, varchar)),
                                  cast(json_parse(li.pod_labels) as map(varchar, varchar))),
                       (k, v) -> contains(ek.enabled_keys, k)) as "pod_labels",
            sum(li.pod_usage_cpu_core_seconds) as "pod_usage_cpu_core_seconds",
@@ -79,10 +101,12 @@ CREATE TABLE hive.acct10001.__reporting_ocpusagelineitem_daily_uuid_eek AS (
            max(cc.cluster_capacity_memory_byte_seconds) as "cluster_capacity_memory_byte_seconds",
            count(li.interval_start) * 3600 as "total_seconds"
       FROM hive.acct10001.openshift_pod_usage_line_items as "li"
+      LEFT
       JOIN hive.acct10001.__ocp_node_label_line_item_daily_uuid_eek as "nli"
         ON nli.node = li.node
        AND nli.usage_start = date(li.interval_start)
        AND nli.source = li.source
+      LEFT
       JOIN hive.acct10001.__ocp_cluster_capacity_uuid_eek as "cc"
         ON cc.source = li.source
        AND cc.usage_start = date(li.interval_start)
@@ -91,12 +115,20 @@ CREATE TABLE hive.acct10001.__reporting_ocpusagelineitem_daily_uuid_eek AS (
                SELECT array_agg(distinct key) as enabled_keys
                  FROM postgres.acct10001.reporting_ocpenabledtagkeys
            ) as "ek"
-     WHERE date(li.interval_start) >= DATE '2020-09-01'
+     WHERE li.source = '511aab54-5b4f-4c69-a985-325ff9aa1329'
+       AND li.year = '2020'
+       AND li.month = '09'
+       AND date(li.interval_start) >= DATE '2020-09-01'
        AND date(li.interval_start) <= DATE '2020-09-08'
      GROUP
         BY 1, 2, 3, 4, 5, 6, 7, 8, 10
 )
 ;
+
+-- Drop temps
+DROP TABLE hive.acct10001.__ocp_node_label_line_item_daily_uuid_eek;
+DROP TABLE hive.acct10001.__ocp_cluster_capacity_uuid_eek;
+
 
 /*
 -- Calculate cluster capacity at daily level
@@ -179,9 +211,6 @@ CREATE TABLE hive.acct10001.__reporting_ocpusagelineitem_daily_uuid_eek AS (
 ;
 
 */
-
-DROP TABLE hive.acct10001.__ocp_node_label_line_item_daily_uuid_eek;
-DROP TABLE hive.acct10001.__ocp_cluster_capacity_uuid_eek;
 
 /*
 -- Clear out old entries first
