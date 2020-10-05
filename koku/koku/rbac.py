d0b5c6b3c1c4fa2b80c452fff5ac073700c42803
Andrew Berglund <aberglun@redhat.com>
2020-10-05 16:43:37 -0400

    Support existing rbac with rate

--- koku/koku/rbac.py
+++ koku/koku/rbac.py
@@ -75,6 +75,9 @@ def _process_acls(acls):
     access = {}
     for acl in acls:
         permission = acl.get("permission")
+        # Support the legacy term "rate" for cost_model permissions
+        if "cost-management:rate" in permission:
+            permission = permission.replace("rate", "cost_model")
         resource_definitions = acl.get("resourceDefinitions", [])
         try:
             res_typ, operation = _extract_permission_data(permission)
