6f261e7443b95beb5f09a8d022c3fef34f4cf435
Andrew Berglund <aberglun@redhat.com>
2020-10-07 14:15:50 -0400

    Migration uptick

--- koku/reporting/migrations/0145_auto_20200917_1448.py
+++ koku/reporting/migrations/0145_auto_20200917_1448.py
@@ -18,7 +18,7 @@ def add_views(apps, schema_editor):
 
 class Migration(migrations.Migration):
 
-    dependencies = [("reporting", "0143_awsorganizationalunit_provider")]
+    dependencies = [("reporting", "0144_auto_20201007_1441")]
 
     operations = [
         migrations.RunSQL(
