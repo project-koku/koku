a68e012122949e52362679cd4740fc13dd120b52
boaz0 <boaz.shuster.github@gmail.com>
2020-10-07 20:20:24 +0300

    Fix Django 3 deprecation & settings warnings and sources server start (#2440)
    Signed-off-by: Boaz Shuster <boaz.shuster.github@gmail.com>
    Co-authored-by: Douglas Curtis <docurtis@redhat.com>

--- koku/reporting/partition/models.py
+++ koku/reporting/partition/models.py
@@ -1,7 +1,7 @@
-from django.contrib.postgres.fields.jsonb import JSONField
 from django.contrib.postgres.indexes import GinIndex
 from django.core.exceptions import ValidationError
 from django.db import models
+from django.db.models import JSONField
 
 
 def validate_not_empty(value):
