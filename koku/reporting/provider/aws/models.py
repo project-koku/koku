aa8e802c32c1f5b32dd0ff1f8fc59c0fc102a23f
Brett Lentz <blentz@users.noreply.github.com>
2020-10-12 12:41:50 -0400

    COST-592 - squash migrations (#2436)
    * squash migrations
    
    Co-authored-by: Douglas Curtis <docurtis@redhat.com>

--- koku/reporting/provider/aws/models.py
+++ koku/reporting/provider/aws/models.py
@@ -22,6 +22,23 @@ from django.contrib.postgres.indexes import GinIndex
 from django.db import models
 from django.db.models import JSONField
 
+VIEWS = (
+    "reporting_aws_compute_summary",
+    "reporting_aws_compute_summary_by_account",
+    "reporting_aws_compute_summary_by_region",
+    "reporting_aws_compute_summary_by_service",
+    "reporting_aws_cost_summary",
+    "reporting_aws_cost_summary_by_account",
+    "reporting_aws_cost_summary_by_region",
+    "reporting_aws_cost_summary_by_service",
+    "reporting_aws_storage_summary",
+    "reporting_aws_storage_summary_by_account",
+    "reporting_aws_storage_summary_by_region",
+    "reporting_aws_storage_summary_by_service",
+    "reporting_aws_database_summary",
+    "reporting_aws_network_summary",
+)
+
 
 class AWSCostEntryBill(models.Model):
     """The billing information for a Cost Usage Report.
