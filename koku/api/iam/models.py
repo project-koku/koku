bdcb3575c9a49b021fa0bdcc6fa50313bfb21a01
HAP <hproctor@redhat.com>
2020-10-14 17:04:13 -0400

    PoC for schema cloning for new tenant creation

--- koku/api/iam/models.py
+++ koku/api/iam/models.py
@@ -15,10 +15,22 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
 """Models for identity and access management."""
+import logging
+import os
 from uuid import uuid4
 
 from django.db import models
+from django.db import transaction
 from tenant_schemas.models import TenantMixin
+from tenant_schemas.postgresql_backend.base import _check_schema_name
+from tenant_schemas.utils import schema_exists
+
+
+LOG = logging.getLogger(__name__)
+
+
+class CloneSchemaError(Exception):
+    pass
 
 
 class Customer(models.Model):
@@ -62,8 +74,84 @@ class User(models.Model):
 class Tenant(TenantMixin):
     """The model used to create a tenant schema."""
 
+    _TEMPLATE_SCHEMA = os.environ.get("TEMPLATE_SCHEMA", "template0")
+
     # Override the mixin domain url to make it nullable, non-unique
     domain_url = None
 
     # Delete all schemas when a tenant is removed
     auto_drop_schema = True
+
+    def _check_clone_func(self):
+        func_sig = (
+            "public.clone_schema(source_schema text, dest_schema text, "
+            "copy_data boolean DEFAULT false, "
+            "_verbose boolean DEFAULT false)"
+        )
+        sql = """
+select pronamespace::regnamespace::text || '.' || proname || '(' || pg_get_function_arguments(oid) || ')'
+  from pg_proc
+ where pronamespace = 'public'::regnamespace
+   and proname = 'clone_schema';
+"""
+        conn = transaction.get_connection()
+        cur = conn.cursor()
+        cur.execute(sql)
+        result = cur.fetchone()
+        cur.close()
+        if (not result) or (result[0] != func_sig):
+            raise CloneSchemaError('Database Function "public.clone_schema" not found')
+
+    def _verify_template(self, verbosity=1):
+        LOG.info(f'Verify that template schema "{self._TEMPLATE_SCHEMA}" exists')
+        _schema = self.schema_name
+        self.schema_name = self._TEMPLATE_SCHEMA
+        ret = super().create_schema(check_if_exists=True, sync_schema=True, verbosity=verbosity)
+        self.schema_name = _schema
+        return ret
+
+    def create_schema(self, check_if_exists=True, sync_schema=True, verbosity=1):
+        if self.schema_name == "public":
+            LOG.info("Using superclass for public schema creation")
+            return super().create_schema(check_if_exists=True, sync_schema=sync_schema, verbosity=verbosity)
+
+        # Verify name structure
+        _check_schema_name(self.schema_name)
+
+        # Make sure all of our special pieces are in play
+        self._check_clone_func()
+        ret = self._verify_template(verbosity=verbosity)
+        if self.schema_name == self._TEMPLATE_SCHEMA:
+            return ret
+
+        # ignoring check_if_exists flag -- ALWAYS CHECK!
+        if schema_exists(self.schema_name):
+            return False
+
+        # Clone the schema
+        conn = transaction.get_connection()
+        with transaction.atomic():
+            cur = conn.cursor()
+
+            # This db func will clone the schema objects
+            # bypassing the time it takes to run migrations
+            sql = """
+select public.clone_schema(%s, %s, copy_data => true) as "clone_result";
+"""
+            LOG.info(f"Cloning template schema {self._TEMPLATE_SCHEMA} to {self.schema_name} with data")
+            cur.execute(sql, [self._TEMPLATE_SCHEMA, self.schema_name])
+            result = cur.fetchone()
+            cur.close()
+
+            result = result[0] if result else False
+            if not result:
+                # Error creating schema
+                conn.rollback()
+                LOG.error(f"Error cloning template schema {self._TEMPLATE_SCHEMA}")
+                raise CloneSchemaError(f'Schema "{self.schema_name} creation has failed! Check DB logs!')
+            else:
+                LOG.info(f"Successful clone of {self._TEMPLATE_SCHEMA} to {self.schema_name}")
+
+            conn.set_schema_to_public()
+
+        return result
