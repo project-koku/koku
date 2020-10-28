a9124dffcc98e199ffa6125ee3b93327eacab7c8
Andrew Berglund <aberglun@redhat.com>
2020-10-28 15:44:48 -0400

    Line up updater with AWS/Azure

--- koku/masu/processor/ocp/ocp_report_parquet_summary_updater.py
+++ koku/masu/processor/ocp/ocp_report_parquet_summary_updater.py
@@ -18,13 +18,12 @@
 import calendar
 import logging
 
+import ciso8601
 from tenant_schemas.utils import schema_context
 
-from masu.database.cost_model_db_accessor import CostModelDBAccessor
 from masu.database.ocp_report_db_accessor import OCPReportDBAccessor
-from masu.database.report_manifest_db_accessor import ReportManifestDBAccessor
 from masu.external.date_accessor import DateAccessor
-from masu.util.common import date_range_pair
+from masu.util.common import determine_if_full_summary_update_needed
 from masu.util.ocp.common import get_cluster_alias_from_cluster_id
 from masu.util.ocp.common import get_cluster_id_from_provider
 
@@ -47,6 +46,33 @@ class OCPReportParquetSummaryUpdater:
         self._cluster_id = get_cluster_id_from_provider(self._provider.uuid)
         self._cluster_alias = get_cluster_alias_from_cluster_id(self._cluster_id)
         self._date_accessor = DateAccessor()
+
+    def _get_sql_inputs(self, start_date, end_date):
+        """Get the required inputs for running summary SQL."""
+        with OCPReportDBAccessor(self._schema) as accessor:
+            # This is the normal processing route
+            if self._manifest:
+                # Override the bill date to correspond with the manifest
+                bill_date = self._manifest.billing_period_start_datetime.date()
+                report_periods = accessor.get_usage_period_query_by_provider(self._provider.uuid)
+                report_periods = report_periods.filter(report_period_start=bill_date).all()
+                first_period = report_periods.first()
+                do_month_update = False
+                with schema_context(self._schema):
+                    if first_period:
+                        do_month_update = determine_if_full_summary_update_needed(first_period)
+                if do_month_update:
+                    last_day_of_month = calendar.monthrange(bill_date.year, bill_date.month)[1]
+                    start_date = bill_date
+                    end_date = bill_date.replace(day=last_day_of_month)
+                    LOG.info("Overriding start and end date to process full month.")
+
+        if isinstance(start_date, str):
+            start_date = ciso8601.parse_datetime(start_date).date()
+        if isinstance(end_date, str):
+            end_date = ciso8601.parse_datetime(end_date).date()
+
+        return start_date, end_date
 
     def update_daily_tables(self, start_date, end_date):
         """Populate the daily tables for reporting.
@@ -77,50 +103,47 @@ class OCPReportParquetSummaryUpdater:
         """
         start_date, end_date = self._get_sql_inputs(start_date, end_date)
 
-        with CostModelDBAccessor(self._schema, self._provider.uuid) as cost_model_accessor:
-            markup = cost_model_accessor.markup
-            markup_value = float(markup.get("value", 0)) / 100
-
         report_periods = None
         with OCPReportDBAccessor(self._schema) as accessor:
-            report_periods = accessor.report_periods_for_provider_uuid(self._provider.uuid, start_date)
             with schema_context(self._schema):
-                report_period_ids = [report_period.id for report_period in report_periods]
-                report_date_map = {
-                    (report_period.report_period_start, report_period.report_period_end): report_period.id
-                    for report_period in report_periods
-                }
-            for start, end in date_range_pair(start_date, end_date):
-                LOG.info(
-                    "Updating OpenShift report summary tables for \n\tSchema: %s "
-                    "\n\tProvider: %s \n\tCluster: %s \n\tDates: %s - %s",
-                    self._schema,
-                    self._provider.uuid,
-                    self._cluster_id,
-                    start,
-                    end,
-                )
+                report_periods = accessor.report_periods_for_provider_uuid(self._provider.uuid, start_date)
+                current_report_period_id = report_periods.first().id if report_periods else None
+                # report_period_ids = [report_period.id for report_period in report_periods]
+                # report_date_map = {
+                #     (report_period.report_period_start, report_period.report_period_end): report_period.id
+                #     for report_period in report_periods
+                # }
+            # for start, end in date_range_pair(start_date, end_date):
+            LOG.info(
+                "Updating OpenShift report summary tables for \n\tSchema: %s "
+                "\n\tProvider: %s \n\tCluster: %s \n\tDates: %s - %s",
+                self._schema,
+                self._provider.uuid,
+                self._cluster_id,
+                start_date,
+                end_date,
+            )
 
-                # Find the associated report_period_id for the date range
-                #
-                report_period_id = None
-                for report_date_range, rp_id in report_date_map.items():
-                    rps, rpe = report_date_range
-                    if (rps <= end) and (rpe >= start):
-                        report_period_id = rp_id
-                if report_period_id is None:
-                    msg = f"Cannot determine report_period_id from date range {start} - {end}"
-                    LOG.error(msg)
-                    raise ValueError(msg)
+            # Find the associated report_period_id for the date range
+            #
+            # report_period_id = None
+            # for report_date_range, rp_id in report_date_map.items():
+            #     rps, rpe = report_date_range
+            #     if (rps <= end) and (rpe >= start):
+            #         report_period_id = rp_id
+            # if report_period_id is None:
+            #     msg = f"Cannot determine report_period_id from date range {start} - {end}"
+            #     LOG.error(msg)
+            #     raise ValueError(msg)
 
-                accessor.populate_line_item_daily_summary_table_presto(
-                    start, end, report_period_id, self._cluster_id, self._cluster_alias, markup_value
-                )
-                accessor.populate_storage_line_item_daily_summary_table_presto(
-                    start, end, report_period_id, self._cluster_id, self._cluster_alias, markup_value
-                )
-            accessor.populate_pod_label_summary_table_presto(report_period_ids)
-            accessor.populate_volume_label_summary_table_presto(report_period_ids)
+            accessor.populate_line_item_daily_summary_table_presto(
+                start_date, end_date, current_report_period_id, self._cluster_id, self._cluster_alias
+            )
+            accessor.populate_storage_line_item_daily_summary_table_presto(
+                start_date, end_date, current_report_period_id, self._cluster_id, self._cluster_alias
+            )
+            accessor.populate_pod_label_summary_table_presto(current_report_period_id)
+            accessor.populate_volume_label_summary_table_presto(current_report_period_id)
 
             for period in report_periods:
                 if period.summary_data_creation_datetime is None:
@@ -129,40 +152,3 @@ class OCPReportParquetSummaryUpdater:
                 period.save()
 
         return start_date, end_date
-
-    def _get_sql_inputs(self, start_date, end_date):
-        """Get the required inputs for running summary SQL."""
-        # Default to this month's bill
-        with OCPReportDBAccessor(self._schema) as accessor:
-            if self._manifest:
-                # Override the bill date to correspond with the manifest
-                bill_date = self._manifest.billing_period_start_datetime.date()
-                report_periods = accessor.get_usage_period_query_by_provider(self._provider.uuid)
-                report_periods = report_periods.filter(report_period_start=bill_date).all()
-                do_month_update = True
-                with schema_context(self._schema):
-                    if report_periods is not None and len(report_periods) > 0:
-                        do_month_update = self._determine_if_full_summary_update_needed(report_periods[0])
-                if do_month_update:
-                    last_day_of_month = calendar.monthrange(bill_date.year, bill_date.month)[1]
-                    start_date = bill_date.strftime("%Y-%m-%d")
-                    end_date = bill_date.replace(day=last_day_of_month)
-                    end_date = end_date.strftime("%Y-%m-%d")
-                    LOG.info("Overriding start and end date to process full month.")
-                LOG.info("Returning start: %s, end: %s", str(start_date), str(end_date))
-        return start_date, end_date
-
-    def _determine_if_full_summary_update_needed(self, report_period):
-        """Decide whether to update summary tables for full billing period."""
-        summary_creation = report_period.summary_data_creation_datetime
-        is_done_processing = False
-        with ReportManifestDBAccessor() as manifest_accesor:
-            is_done_processing = manifest_accesor.manifest_ready_for_summary(self._manifest.id)
-        is_new_period = summary_creation is None
-
-        # Run the full month if this is the first time we've seen this report
-        # period
-        if is_done_processing and is_new_period:
-            return True
-
-        return False
