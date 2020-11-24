5e43d1d27f1d3f5b79379eddff4b0cfe95041c1e
myersCody <codymyers63@gmail.com>
2020-11-24 13:10:07 -0500

    Add a masu endpoint to return a list of active tasks in masu for QE.

---
+++ koku/masu/api/running_celery_tasks.py
@@ -0,0 +1,45 @@
+#
+# Copyright 2018 Red Hat, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+"""View for update_cost_model_costs endpoint."""
+import logging
+
+from django.views.decorators.cache import never_cache
+from rest_framework.decorators import api_view
+from rest_framework.decorators import permission_classes
+from rest_framework.decorators import renderer_classes
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.settings import api_settings
+
+from koku.celery import CELERY_INSPECT
+
+logger = logging.getLogger(__name__)
+
+
+@never_cache
+@api_view(http_method_names=["GET"])
+@permission_classes((AllowAny,))
+@renderer_classes(tuple(api_settings.DEFAULT_RENDERER_CLASSES))
+def running_celery_tasks(request):
+    """Get the task ids of running cerlery tasks."""
+    active_dict = CELERY_INSPECT.active()
+    active_tasks = []
+    for task_list in active_dict.values():
+        active_tasks.extend(task_list)
+    if active_tasks:
+        active_tasks = [dikt.get("id", "") for dikt in active_tasks]
+    return Response({"active_tasks": active_tasks})
