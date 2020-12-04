21486e2208a59f97d69e0b1ba78487399cd51362
Douglas Curtis <docurtis@redhat.com>
2020-12-04 12:41:37 -0500

    handle celery inspect not being available in worker cache

--- koku/masu/test/processor/test_worker_cache.py
+++ koku/masu/test/processor/test_worker_cache.py
@@ -159,6 +159,13 @@ class WorkerCacheTest(MasuTestCase):
                 mock_inspect.reserved.return_value = mock_worker_list
                 _cache = WorkerCache()
                 self.assertEqual(_cache.active_workers, test.get("expected_workers"))
+    
+    @patch("masu.processor.worker_cache.CELERY_INSPECT")
+    def test_active_worker_property_instance_not_available(self, mock_inspect):
+        """Test the active_workers property when celery inspect is not available."""
+        mock_inspect.reserved.return_value = None
+        _cache = WorkerCache()
+        self.assertEqual(_cache.active_workers, [])
 
     @override_settings(HOSTNAME="kokuworker")
     @patch("masu.processor.worker_cache.CELERY_INSPECT")
