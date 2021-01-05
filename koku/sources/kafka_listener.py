d0140ec7bae51fd08b15707eb2c5ea56f21ffbfd
Douglas Curtis <docurtis@redhat.com>
2021-01-05 11:16:50 -0500

    lint

--- koku/sources/kafka_listener.py
+++ koku/sources/kafka_listener.py
@@ -223,7 +223,9 @@ def get_sources_msg_data(msg, app_type_id):
                     msg_data["partition"] = msg.partition()
                     msg_data["source_id"] = int(value.get("source_id"))
                     msg_data["auth_header"] = _extract_from_header(msg.headers(), KAFKA_HDR_RH_IDENTITY)
-                    LOG.info(f"Application Create/Destroy Message headers for Source ID: {value.get('source_id')}: {str(msg.headers())}")
+                    LOG.info(
+                        f"Application Create/Destroy Message headers for Source ID: {value.get('source_id')}: {str(msg.headers())}"
+                    )
             elif event_type in (KAFKA_AUTHENTICATION_CREATE, KAFKA_AUTHENTICATION_UPDATE):
                 LOG.debug("Authentication Message: %s", str(msg))
                 if value.get("resource_type") in ("Endpoint", "Application"):
@@ -233,7 +235,9 @@ def get_sources_msg_data(msg, app_type_id):
                     msg_data["resource_id"] = int(value.get("resource_id"))
                     msg_data["resource_type"] = value.get("resource_type")
                     msg_data["auth_header"] = _extract_from_header(msg.headers(), KAFKA_HDR_RH_IDENTITY)
-                    LOG.info(f"Authentication Create/Update Message headers for Source ID: {value.get('resource_id')}: {str(msg.headers())}")
+                    LOG.info(
+                        f"Authentication Create/Update Message headers for Source ID: {value.get('resource_id')}: {str(msg.headers())}"
+                    )
 
             elif event_type in (KAFKA_SOURCE_DESTROY, KAFKA_SOURCE_UPDATE):
                 LOG.debug("Source Message: %s", str(msg))
@@ -242,7 +246,9 @@ def get_sources_msg_data(msg, app_type_id):
                 msg_data["partition"] = msg.partition()
                 msg_data["source_id"] = int(value.get("id"))
                 msg_data["auth_header"] = _extract_from_header(msg.headers(), KAFKA_HDR_RH_IDENTITY)
-                LOG.info(f"Source Update/Destroy Message headers for Source ID: {value.get('source_id')}: {str(msg.headers())}")
+                LOG.info(
+                    f"Source Update/Destroy Message headers for Source ID: {value.get('source_id')}: {str(msg.headers())}"
+                )
             else:
                 LOG.debug("Other Message: %s", str(msg))
         except (AttributeError, ValueError, TypeError) as error:
