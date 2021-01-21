39627ce8ae3e0c748f48e357fc1d10d441dfd304
Douglas Curtis <docurtis@redhat.com>
2021-01-21 12:03:11 -0500

    COST-741: User Access API  (#2601)

---
+++ koku/api/user_access/test/test_view.py
@@ -0,0 +1,327 @@
+#
+# Copyright 2021 Red Hat, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+"""Test the UserAccess view."""
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient
+
+from api.iam.test.iam_test_case import IamTestCase
+from api.iam.test.iam_test_case import RbacPermissions
+
+
+class UserAccessViewTest(IamTestCase):
+    """Tests the resource types views."""
+
+    def setUp(self):
+        """Set up the UserAccess view tests."""
+        super().setUp()
+        self.client = APIClient()
+
+    @RbacPermissions({"aws.account": {"read": ["*"]}})
+    def test_aws_view_read(self):
+        """Test user-access view with aws read wildcard permission."""
+        url = reverse("user-access")
+        response = self.client.get(url, **self.headers)
+
+        self.assertEqual(len(response.data.get("data")), 5)
+        self.assertTrue({"type": "aws", "access": True} in response.data.get("data"))
+        self.assertTrue({"type": "ocp", "access": False} in response.data.get("data"))
+        self.assertTrue({"type": "gcp", "access": False} in response.data.get("data"))
+        self.assertTrue({"type": "azure", "access": False} in response.data.get("data"))
+        self.assertTrue({"type": "cost_model", "access": False} in response.data.get("data"))
+
+    @RbacPermissions({"aws.account": {"read": ["123"]}})
+    def test_aws_view_read_specific_account(self):
+        """Test user-access view with aws read specific account permission."""
+        url = reverse("user-access")
+        response = self.client.get(url, **self.headers)
+
+        self.assertEqual(len(response.data.get("data")), 5)
+        self.assertTrue({"type": "aws", "access": True} in response.data.get("data"))
+        self.assertTrue({"type": "ocp", "access": False} in response.data.get("data"))
+        self.assertTrue({"type": "gcp", "access": False} in response.data.get("data"))
+        self.assertTrue({"type": "azure", "access": False} in response.data.get("data"))
+        self.assertTrue({"type": "cost_model", "access": False} in response.data.get("data"))
+
+    @RbacPermissions({"aws.account": "*"})
+    def test_aws_view_wildcard(self):
+        """Test user-access view with aws wildcard permission."""
+        url = reverse("user-access")
+        response = self.client.get(url, **self.headers)
+
+        self.assertEqual(len(response.data.get("data")), 5)
+        self.assertTrue({"type": "aws", "access": True} in response.data.get("data"))
+        self.assertTrue({"type": "ocp", "access": False} in response.data.get("data"))
+        self.assertTrue({"type": "gcp", "access": False} in response.data.get("data"))
+        self.assertTrue({"type": "azure", "access": False} in response.data.get("data"))
+        self.assertTrue({"type": "cost_model", "access": False} in response.data.get("data"))
+
+    @RbacPermissions(
+        {
+            "openshift.cluster": {"read": ["*"]},
+            "openshift.project": {"read": ["myproject"]},
+            "openshift.node": {"read": ["mynode"]},
+        }
+    )
+    def test_ocp_view_cluster(self):
+        """Test user-access view with openshift cluster read wildcard permission."""
+        url = reverse("user-access")
+        response = self.client.get(url, **self.headers)
+
+        self.assertEqual(len(response.data.get("data")), 5)
+        self.assertTrue({"type": "aws", "access": False} in response.data.get("data"))
+        self.assertTrue({"type": "ocp", "access": True} in response.data.get("data"))
+        self.assertTrue({"type": "gcp", "access": False} in response.data.get("data"))
+        self.assertTrue({"type": "azure", "access": False} in response.data.get("data"))
+        self.assertTrue({"type": "cost_model", "access": False} in response.data.get("data"))
+
+    @RbacPermissions(
+        {
+            "openshift.cluster": {"read": ["mycluster"]},
+            "openshift.project": {"read": ["*"]},
+            "openshift.node": {"read": ["mynode"]},
+        }
+    )
+    def test_ocp_view_project(self):
+        """Test user-access view with openshift project read wildcard permission."""
+        url = reverse("user-access")
+        response = self.client.get(url, **self.headers)
+
+        self.assertEqual(len(response.data.get("data")), 5)
+        self.assertTrue({"type": "aws", "access": False} in response.data.get("data"))
+        self.assertTrue({"type": "ocp", "access": True} in response.data.get("data"))
+        self.assertTrue({"type": "gcp", "access": False} in response.data.get("data"))
+        self.assertTrue({"type": "azure", "access": False} in response.data.get("data"))
+        self.assertTrue({"type": "cost_model", "access": False} in response.data.get("data"))
+
+    @RbacPermissions(
+        {
+            "openshift.cluster": {"read": ["mycluster"]},
+            "openshift.project": {"read": ["myproject"]},
+            "openshift.node": {"read": ["*"]},
+        }
+    )
+    def test_ocp_view_node(self):
+        """Test user-access view with openshift node read wildcard permission."""
+        url = reverse("user-access")
+        response = self.client.get(url, **self.headers)
+
+        self.assertEqual(len(response.data.get("data")), 5)
+        self.assertTrue({"type": "aws", "access": False} in response.data.get("data"))
+        self.assertTrue({"type": "ocp", "access": True} in response.data.get("data"))
+        self.assertTrue({"type": "gcp", "access": False} in response.data.get("data"))
+        self.assertTrue({"type": "azure", "access": False} in response.data.get("data"))
+        self.assertTrue({"type": "cost_model", "access": False} in response.data.get("data"))
+
+    @RbacPermissions({"openshift.cluster": "*"})
+    def test_ocp_view_cluster_wildcard(self):
+        """Test user-access view with openshift cluster wildcard permission."""
+        url = reverse("user-access")
+
+        response = self.client.get(url, **self.headers)
+
+        self.assertEqual(len(response.data.get("data")), 5)
+        self.assertTrue({"type": "aws", "access": False} in response.data.get("data"))
+        self.assertTrue({"type": "ocp", "access": True} in response.data.get("data"))
+        self.assertTrue({"type": "gcp", "access": False} in response.data.get("data"))
+        self.assertTrue({"type": "azure", "access": False} in response.data.get("data"))
+        self.assertTrue({"type": "cost_model", "access": False} in response.data.get("data"))
+
+    @RbacPermissions({"openshift.project": "*"})
+    def test_ocp_view_project_wildcard(self):
+        """Test user-access view with openshift project wildcard permission."""
+        url = reverse("user-access")
+
+        response = self.client.get(url, **self.headers)
+
+        self.assertEqual(len(response.data.get("data")), 5)
+        self.assertTrue({"type": "aws", "access": False} in response.data.get("data"))
+        self.assertTrue({"type": "ocp", "access": True} in response.data.get("data"))
+        self.assertTrue({"type": "gcp", "access": False} in response.data.get("data"))
+        self.assertTrue({"type": "azure", "access": False} in response.data.get("data"))
+        self.assertTrue({"type": "cost_model", "access": False} in response.data.get("data"))
+
+    @RbacPermissions({"openshift.node": "*"})
+    def test_ocp_view_node_wildcard(self):
+        """Test user-access view with openshift node wildcard permission."""
+        url = reverse("user-access")
+
+        response = self.client.get(url, **self.headers)
+
+        self.assertEqual(len(response.data.get("data")), 5)
+        self.assertTrue({"type": "aws", "access": False} in response.data.get("data"))
+        self.assertTrue({"type": "ocp", "access": True} in response.data.get("data"))
+        self.assertTrue({"type": "gcp", "access": False} in response.data.get("data"))
+        self.assertTrue({"type": "azure", "access": False} in response.data.get("data"))
+        self.assertTrue({"type": "cost_model", "access": False} in response.data.get("data"))
+
+    @RbacPermissions({"gcp.account": {"read": ["*"]}, "gcp.project": {"read": ["myproject"]}})
+    def test_gcp_view_account(self):
+        """Test user-access view with gcp account read wildcard permission."""
+        url = reverse("user-access")
+        response = self.client.get(url, **self.headers)
+
+        self.assertEqual(len(response.data.get("data")), 5)
+        self.assertTrue({"type": "aws", "access": False} in response.data.get("data"))
+        self.assertTrue({"type": "ocp", "access": False} in response.data.get("data"))
+        self.assertTrue({"type": "gcp", "access": True} in response.data.get("data"))
+        self.assertTrue({"type": "azure", "access": False} in response.data.get("data"))
+        self.assertTrue({"type": "cost_model", "access": False} in response.data.get("data"))
+
+    @RbacPermissions({"gcp.account": {"read": ["myaccount"]}, "gcp.project": {"read": ["*"]}})
+    def test_gcp_view_project(self):
+        """Test user-access view with gcp project read wildcard permission."""
+        url = reverse("user-access")
+        response = self.client.get(url, **self.headers)
+
+        self.assertEqual(len(response.data.get("data")), 5)
+        self.assertTrue({"type": "aws", "access": False} in response.data.get("data"))
+        self.assertTrue({"type": "ocp", "access": False} in response.data.get("data"))
+        self.assertTrue({"type": "gcp", "access": True} in response.data.get("data"))
+        self.assertTrue({"type": "azure", "access": False} in response.data.get("data"))
+        self.assertTrue({"type": "cost_model", "access": False} in response.data.get("data"))
+
+    @RbacPermissions({"gcp.account": "*"})
+    def test_gcp_view_account_wildcard(self):
+        """Test user-access view with gcp account wildcard permission."""
+        url = reverse("user-access")
+
+        response = self.client.get(url, **self.headers)
+
+        self.assertEqual(len(response.data.get("data")), 5)
+        self.assertTrue({"type": "aws", "access": False} in response.data.get("data"))
+        self.assertTrue({"type": "ocp", "access": False} in response.data.get("data"))
+        self.assertTrue({"type": "gcp", "access": True} in response.data.get("data"))
+        self.assertTrue({"type": "azure", "access": False} in response.data.get("data"))
+        self.assertTrue({"type": "cost_model", "access": False} in response.data.get("data"))
+
+    @RbacPermissions({"gcp.project": "*"})
+    def test_gcp_view_project_wildcard(self):
+        """Test user-access view with gcp project wildcard permission."""
+        url = reverse("user-access")
+
+        response = self.client.get(url, **self.headers)
+
+        self.assertEqual(len(response.data.get("data")), 5)
+        self.assertTrue({"type": "aws", "access": False} in response.data.get("data"))
+        self.assertTrue({"type": "ocp", "access": False} in response.data.get("data"))
+        self.assertTrue({"type": "gcp", "access": True} in response.data.get("data"))
+        self.assertTrue({"type": "azure", "access": False} in response.data.get("data"))
+        self.assertTrue({"type": "cost_model", "access": False} in response.data.get("data"))
+
+    @RbacPermissions({"azure.subscription_guid": {"read": ["*"]}})
+    def test_azure_view_read(self):
+        """Test user-access view with azure subscription read wildcard permission."""
+        url = reverse("user-access")
+        response = self.client.get(url, **self.headers)
+
+        self.assertEqual(len(response.data.get("data")), 5)
+        self.assertTrue({"type": "aws", "access": False} in response.data.get("data"))
+        self.assertTrue({"type": "ocp", "access": False} in response.data.get("data"))
+        self.assertTrue({"type": "gcp", "access": False} in response.data.get("data"))
+        self.assertTrue({"type": "azure", "access": True} in response.data.get("data"))
+        self.assertTrue({"type": "cost_model", "access": False} in response.data.get("data"))
+
+    @RbacPermissions({"azure.subscription_guid": "*"})
+    def test_azure_view_wildcard(self):
+        """Test user-access view with azure subscription wildcard permission."""
+        url = reverse("user-access")
+        response = self.client.get(url, **self.headers)
+
+        self.assertEqual(len(response.data.get("data")), 5)
+        self.assertTrue({"type": "aws", "access": False} in response.data.get("data"))
+        self.assertTrue({"type": "ocp", "access": False} in response.data.get("data"))
+        self.assertTrue({"type": "gcp", "access": False} in response.data.get("data"))
+        self.assertTrue({"type": "azure", "access": True} in response.data.get("data"))
+        self.assertTrue({"type": "cost_model", "access": False} in response.data.get("data"))
+
+    def test_view_as_org_admin(self):
+        """Test user-access view as an org admin."""
+        url = reverse("user-access")
+        response = self.client.get(url, **self.headers)
+
+        self.assertEqual(len(response.data.get("data")), 5)
+        self.assertTrue({"type": "aws", "access": True} in response.data.get("data"))
+        self.assertTrue({"type": "ocp", "access": True} in response.data.get("data"))
+        self.assertTrue({"type": "gcp", "access": True} in response.data.get("data"))
+        self.assertTrue({"type": "azure", "access": True} in response.data.get("data"))
+        self.assertTrue({"type": "cost_model", "access": True} in response.data.get("data"))
+
+    @RbacPermissions({"*": "*"})
+    def test_view_as_cost_admin(self):
+        """Test user-access view as a cost admin."""
+        url = reverse("user-access")
+        response = self.client.get(url, **self.headers)
+
+        self.assertEqual(len(response.data.get("data")), 5)
+        self.assertTrue({"type": "aws", "access": True} in response.data.get("data"))
+        self.assertTrue({"type": "ocp", "access": True} in response.data.get("data"))
+        self.assertTrue({"type": "gcp", "access": True} in response.data.get("data"))
+        self.assertTrue({"type": "azure", "access": True} in response.data.get("data"))
+        self.assertTrue({"type": "cost_model", "access": True} in response.data.get("data"))
+
+    def test_aws_view_query_read_org_admin(self):
+        """Test user-access view query as an org admin."""
+        url = reverse("user-access")
+        query_url = f"{url}?source_type=aws"
+        response = self.client.get(query_url, **self.headers)
+
+        self.assertTrue(response.data.get("data"))
+
+    @RbacPermissions({"aws.account": "*"})
+    def test_aws_view_query_read(self):
+        """Test user-access view query for aws."""
+        url = reverse("user-access")
+        query_url = f"{url}?source_type=aws"
+        response = self.client.get(query_url, **self.headers)
+
+        self.assertTrue(response.data.get("data"))
+
+    @RbacPermissions({"openshift.cluster": "*"})
+    def test_openshift_view_query_read_for_aws(self):
+        """Test user-access view query for aws with openshift permissions."""
+        url = reverse("user-access")
+        query_url = f"{url}?type=aws"
+        response = self.client.get(query_url, **self.headers)
+
+        self.assertFalse(response.data.get("data"))
+
+    @RbacPermissions({"cost_model": "*"})
+    def test_cost_model_view_query_read_for_aws(self):
+        """Test user-access view query for cost_model."""
+        url = reverse("user-access")
+        query_url = f"{url}?type=cost_model"
+        response = self.client.get(query_url, **self.headers)
+
+        self.assertTrue(response.data.get("data"))
+
+    @RbacPermissions({"cost_model": {"write": ["*"]}})
+    def test_cost_model_view_query_write_for_aws(self):
+        """Test user-access view query for cost_model with write access."""
+        url = reverse("user-access")
+        query_url = f"{url}?type=cost_model"
+        response = self.client.get(query_url, **self.headers)
+
+        self.assertTrue(response.data.get("data"))
+
+    def test_view_query_invalid_source_type(self):
+        """Test user-access view query for invalid type."""
+        url = reverse("user-access")
+        query_url = f"{url}?type=bad"
+        response = self.client.get(query_url, **self.headers)
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
---
+++ koku/api/user_access/view.py
@@ -0,0 +1,155 @@
+#
+# Copyright 2021 Red Hat, Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+"""View for UserAccess."""
+import logging
+
+from django.utils.decorators import method_decorator
+from django.views.decorators.vary import vary_on_headers
+from rest_framework import status
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from api.common import CACHE_RH_IDENTITY_HEADER
+from api.common.pagination import ListPaginator
+
+LOGGER = logging.getLogger(__name__)
+
+
+class UserAccess:
+    def check_access(self, access_list):
+        if access_list:
+            return True
+        return False
+
+
+class AWSUserAccess(UserAccess):
+    def __init__(self, access):
+        self.account_access = access.get("aws.account")
+
+    @property
+    def access(self):
+        if self.check_access(self.account_access):
+            return True
+        return False
+
+
+class OCPUserAccess(UserAccess):
+    def __init__(self, access):
+        self.cluster_access = access.get("openshift.cluster")
+        self.node_access = access.get("openshift.node")
+        self.project_access = access.get("openshift.project")
+
+    @property
+    def access(self):
+        if (
+            self.check_access(self.cluster_access)
+            or self.check_access(self.node_access)
+            or self.check_access(self.project_access)
+        ):
+            return True
+        return False
+
+
+class AzureUserAccess(UserAccess):
+    def __init__(self, access):
+        self.subscription_access = access.get("azure.subscription_guid")
+
+    @property
+    def access(self):
+        if self.check_access(self.subscription_access):
+            return True
+        return False
+
+
+class GCPUserAccess(UserAccess):
+    def __init__(self, access):
+        self.account_access = access.get("gcp.account")
+        self.project_access = access.get("gcp.project")
+
+    @property
+    def access(self):
+        if self.check_access(self.account_access) or self.check_access(self.project_access):
+            return True
+        return False
+
+
+class CostModelUserAccess(UserAccess):
+    def __init__(self, access):
+        self.subscription_access = access.get("cost_model")
+
+    @property
+    def access(self):
+        if self.check_access(self.subscription_access):
+            return True
+        return False
+
+
+class CostManagementAllAccess(UserAccess):
+    def __init__(self, access):
+        self.all_access = access.get("*")
+
+    @property
+    def access(self):
+        if self.check_access(self.all_access):
+            return True
+        return False
+
+
+class UserAccessView(APIView):
+    """API GET view for User API."""
+
+    permission_classes = [AllowAny]
+
+    @method_decorator(vary_on_headers(CACHE_RH_IDENTITY_HEADER))
+    def get(self, request, **kwargs):
+        query_params = request.query_params
+        user_access = request.user.access
+        admin_user = request.user.admin or CostManagementAllAccess(user_access).access
+
+        source_types = [
+            {"type": "aws", "access_class": AWSUserAccess},
+            {"type": "ocp", "access_class": OCPUserAccess},
+            {"type": "gcp", "access_class": GCPUserAccess},
+            {"type": "azure", "access_class": AzureUserAccess},
+            {"type": "cost_model", "access_class": CostModelUserAccess},
+        ]
+
+        source_type = query_params.get("type")
+        if source_type:
+            source_accessor = next((item for item in source_types if item.get("type") == source_type.lower()), False)
+            if source_accessor:
+                access_class = source_accessor.get("access_class")
+                if admin_user:
+                    access_granted = True
+                else:
+                    access_granted = access_class(user_access).access
+                return Response({"data": access_granted})
+            else:
+                return Response({f"Unknown source type: {source_type}"}, status=status.HTTP_400_BAD_REQUEST)
+
+        data = []
+        for source_type in source_types:
+            access_granted = False
+            if admin_user:
+                access_granted = True
+            else:
+                access_granted = source_type.get("access_class")(user_access).access
+            data.append({"type": source_type.get("type"), "access": access_granted})
+
+        paginator = ListPaginator(data, request)
+
+        return paginator.get_paginated_response(data)
