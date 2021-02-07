e06da191959e2dbb4b73f190a3d794028ff1b7fc
Douglas Curtis <docurtis@redhat.com>
2021-02-07 18:32:35 -0500

    typo

--- koku/sources/storage.py
+++ koku/sources/storage.py
@@ -171,7 +171,7 @@ APP_SETTINGS_SCREEN_MAP = {
 def source_settings_complete(provider):
     """Determine if the source application settings are complete."""
     LOG.info(f"SOURCE WHEN CHECKING IF SETTINGS ARE COMPLETE: {str(provider)}")
-    is provider.koku_uuid:
+    if provider.koku_uuid:
         screen_fn = APP_SETTINGS_SCREEN_MAP.get(provider.source_type)
         return screen_fn(provider)
     return False
