74ced60074356d7f6392f59a893ac85d8604416b
Douglas Curtis <docurtis@redhat.com>
2021-02-07 14:56:28 -0500

    temp disabling source-status

--- koku/sources/storage.py
+++ koku/sources/storage.py
@@ -171,7 +171,8 @@ APP_SETTINGS_SCREEN_MAP = {
 def source_settings_complete(provider):
     """Determine if the source application settings are complete."""
     screen_fn = APP_SETTINGS_SCREEN_MAP.get(provider.source_type)
-    return screen_fn(provider)
+    return False
+    #return screen_fn(provider)
 
 
 def load_providers_to_create():
