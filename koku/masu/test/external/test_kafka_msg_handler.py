79ec62d490e76feab4d85ce172beac7c778de5cf
Michael Skarbek <mskarbek@redhat.com>
2021-03-18 15:45:49 -0400

    COST-1166 - add ocp related tasks to ocp queue (#2735)

--- koku/masu/test/external/test_kafka_msg_handler.py
+++ koku/masu/test/external/test_kafka_msg_handler.py
@@ -38,6 +38,7 @@ from masu.external.accounts_accessor import AccountsAccessorError
 from masu.external.downloader.ocp.ocp_report_downloader import OCPReportDownloader
 from masu.external.kafka_msg_handler import KafkaMsgHandlerError
 from masu.processor.report_processor import ReportProcessorError
+from masu.processor.tasks import OCP_QUEUE
 from masu.prometheus_stats import WORKER_REGISTRY
 from masu.test import MasuTestCase
 
@@ -428,7 +429,7 @@ class KafkaMsgHandlerTest(MasuTestCase):
 
         with patch("masu.external.kafka_msg_handler.ReportManifestDBAccessor") as mock_accessor:
             mock_accessor.return_value.__enter__.return_value = mock_manifest_accessor
-            with patch("masu.external.kafka_msg_handler.summarize_reports.delay") as mock_summarize_reports:
+            with patch("masu.external.kafka_msg_handler.summarize_reports.s") as mock_summarize_reports:
                 msg_handler.summarize_manifest(report_meta)
                 mock_summarize_reports.assert_called()
 
@@ -437,7 +438,7 @@ class KafkaMsgHandlerTest(MasuTestCase):
 
         with patch("masu.external.kafka_msg_handler.ReportManifestDBAccessor") as mock_accessor:
             mock_accessor.return_value.__enter__.return_value = mock_manifest_accessor
-            with patch("masu.external.kafka_msg_handler.summarize_reports.delay") as mock_summarize_reports:
+            with patch("masu.external.kafka_msg_handler.summarize_reports.s") as mock_summarize_reports:
                 msg_handler.summarize_manifest(report_meta)
                 mock_summarize_reports.assert_not_called()
 
@@ -478,16 +479,16 @@ class KafkaMsgHandlerTest(MasuTestCase):
 
         with patch("masu.external.kafka_msg_handler.ReportManifestDBAccessor") as mock_accessor:
             mock_accessor.return_value.__enter__.return_value = mock_manifest_accessor
-            with patch("masu.external.kafka_msg_handler.summarize_reports.delay") as mock_summarize_reports:
+            with patch("masu.external.kafka_msg_handler.summarize_reports.s") as mock_summarize_reports:
                 msg_handler.summarize_manifest(report_meta)
-                mock_summarize_reports.assert_called_with([expected_meta])
+                mock_summarize_reports.assert_called_with([expected_meta], OCP_QUEUE)
 
         # Check when manifest is not done
         mock_manifest_accessor = FakeManifest(num_processed_files=1, num_total_files=2)
 
         with patch("masu.external.kafka_msg_handler.ReportManifestDBAccessor") as mock_accessor:
             mock_accessor.return_value.__enter__.return_value = mock_manifest_accessor
-            with patch("masu.external.kafka_msg_handler.summarize_reports.delay") as mock_summarize_reports:
+            with patch("masu.external.kafka_msg_handler.summarize_reports.s") as mock_summarize_reports:
                 msg_handler.summarize_manifest(report_meta)
                 mock_summarize_reports.assert_not_called()
 
