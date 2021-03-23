075a515bb3361935854f2e61616c608d636eca07
Andrew Berglund <aberglun@redhat.com>
2021-03-23 14:01:24 -0400

    [COST-1192] Use chunks in pandas to process csv->parquet (#2740)
    * Use chunks in pandas to process csv->parquet
    * Update hive/postgres properties
    * Add resourceTags as required AWS column
    * Update AWS, Azure, GCP post processors
    * Add env vars
    * Run trino update sql over smaller date ranges

--- koku/masu/util/gcp/common.py
+++ koku/masu/util/gcp/common.py
@@ -111,9 +111,9 @@ def process_gcp_credits(credit_string):
 def gcp_post_processor(data_frame):
     """Guarantee column order for Azure parquet files"""
     columns = list(data_frame)
+    column_name_map = {}
     for column in columns:
-        if "." in column:
-            new_col_name = column.replace(".", "_")
-            data_frame[new_col_name] = data_frame[column]
-            data_frame = data_frame.drop(columns=[column])
+        new_col_name = column.replace("-", "_").replace("/", "_").replace(":", "_").replace(".", "_").lower()
+        column_name_map[column] = new_col_name
+    data_frame = data_frame.rename(columns=column_name_map)
     return data_frame
