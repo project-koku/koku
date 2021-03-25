da715dcc5d743c2f138bfe361802a3e13ebc196e
Andrew Berglund <aberglun@redhat.com>
2021-03-25 15:03:27 -0400

    Tail the check migration command (#2759)

--- scripts/run_migrations.sh
+++ scripts/run_migrations.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RESULT=$(scl enable rh-python38 "${APP_HOME}/manage.py check_migrations")
+RESULT=$(scl enable rh-python38 "${APP_HOME}/manage.py check_migrations | tail -1")
 
 if [ "$RESULT" = "True" ]; then
     echo "Migration already executed."
