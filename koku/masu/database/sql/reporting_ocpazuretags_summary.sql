578188edd49b589714e9e3efc0673735c92bde54
Andrew Berglund <aberglun@redhat.com>
2021-04-07 16:11:41 -0400

    Update AWS tag summary sql + ocp on cloud

--- koku/masu/database/sql/reporting_ocpazuretags_summary.sql
+++ koku/masu/database/sql/reporting_ocpazuretags_summary.sql
@@ -9,11 +9,20 @@ WITH cte_tag_value AS (
     FROM {{schema | sqlsafe}}.reporting_ocpazurecostlineitem_daily_summary AS li,
         jsonb_each_text(li.tags) labels,
         unnest(li.namespace) projects(project)
+    WHERE li.usage_start >= {{start_date}}
+        AND li.usage_start <= {{end_date}}
+    {% if bill_ids %}
+        AND li.cost_entry_bill_id IN (
+        {%- for bill_id in bill_ids -%}
+        {{bill_id}}{% if not loop.last %},{% endif %}
+        {%- endfor -%}
+    )
+    {% endif %}
     GROUP BY key, value, li.cost_entry_bill_id, li.subscription_guid, li.report_period_id, project, li.node
 ),
 cte_values_agg AS (
     SELECT key,
-        array_agg(DISTINCT value) as values,
+        array_agg(DISTINCT value) as "values",
         cost_entry_bill_id,
         report_period_id,
         subscription_guid,
@@ -21,19 +30,46 @@ cte_values_agg AS (
         node
     FROM cte_tag_value
     GROUP BY key, cost_entry_bill_id, report_period_id, subscription_guid, namespace, node
-)
-, ins1 AS (
+),
+cte_distinct_values_agg AS (
+    SELECT v.key,
+        array_agg(DISTINCT v."values") as "values",
+        v.cost_entry_bill_id,
+        v.report_period_id,
+        v.subscription_guid,
+        v.namespace,
+        v.node
+    FROM (
+        SELECT va.key,
+            unnest(va."values" || coalesce(ls."values", '{}'::text[])) as "values",
+            va.cost_entry_bill_id,
+            va.report_period_id,
+            va.subscription_guid,
+            va.namespace,
+            va.node
+        FROM cte_values_agg AS va
+        LEFT JOIN {{schema | sqlsafe}}.reporting_ocpawstags_summary AS ls
+            ON va.key = ls.key
+                AND va.cost_entry_bill_id = ls.cost_entry_bill_id
+                AND va.report_period_id = ls.report_period_id
+                AND va.subscription_guid = ls.subscription_guid
+                AND va.namespace = ls.namespace
+                AND va.node = ls.node
+    ) as v
+    GROUP BY key, cost_entry_bill_id, report_period_id, subscription_guid, namespace, node
+),
+ins1 AS (
     INSERT INTO {{schema | sqlsafe}}.reporting_ocpazuretags_summary (uuid, key, values, cost_entry_bill_id, report_period_id, subscription_guid, namespace, node)
     SELECT uuid_generate_v4() as uuid,
         key,
-        values,
+        "values",
         cost_entry_bill_id,
         report_period_id,
         subscription_guid,
         namespace,
         node
-    FROM cte_values_agg
-    ON CONFLICT (key, cost_entry_bill_id, report_period_id, subscription_guid, namespace, node) DO UPDATE SET values=EXCLUDED.values
+    FROM cte_distinct_values_agg
+    ON CONFLICT (key, cost_entry_bill_id, report_period_id, subscription_guid, namespace, node) DO UPDATE SET values=EXCLUDED."values"
     )
 INSERT INTO {{schema | sqlsafe}}.reporting_ocpazuretags_values (uuid, key, value, subscription_guids, cluster_ids, cluster_aliases, namespaces, nodes)
 SELECT uuid_generate_v4() as uuid,
