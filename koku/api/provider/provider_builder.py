9a16e3402bb77282139ea9539c26c9e19efcef25
Douglas Curtis <docurtis@redhat.com>
2021-04-08 11:06:17 -0400

    Sources tenant get or create (#2548)

--- koku/api/provider/provider_builder.py
+++ koku/api/provider/provider_builder.py
@@ -114,14 +114,19 @@ class ProviderBuilder:
         context = {"user": user, "customer": customer}
         return context, customer, user
 
+    def _tenant_for_schema(self, schema_name):
+        """Get or create tenant for schema."""
+        tenant, created = Tenant.objects.get_or_create(schema_name=schema_name)
+        if created:
+            msg = f"Created tenant {schema_name}"
+            LOG.info(msg)
+        return tenant
+
     def create_provider_from_source(self, source):
         """Call to create provider."""
         connection.set_schema_to_public()
         context, customer, _ = self._create_context()
-        tenant, created = Tenant.objects.get_or_create(schema_name=customer.schema_name)
-        if created:
-            msg = f"Created tenant {customer.schema_name}"
-            LOG.info(msg)
+        tenant = self._tenant_for_schema(customer.schema_name)
         provider_type = source.source_type
         json_data = {
             "name": source.name,
@@ -147,7 +152,7 @@ class ProviderBuilder:
         """Call to update provider."""
         connection.set_schema_to_public()
         context, customer, _ = self._create_context()
-        tenant = Tenant.objects.get(schema_name=customer.schema_name)
+        tenant = self._tenant_for_schema(customer.schema_name)
         provider_type = source.source_type
         json_data = {
             "name": source.name,
@@ -167,7 +172,7 @@ class ProviderBuilder:
         """Call to destroy provider."""
         connection.set_schema_to_public()
         _, customer, user = self._create_context()
-        tenant = Tenant.objects.get(schema_name=customer.schema_name)
+        tenant = self._tenant_for_schema(customer.schema_name)
         connection.set_tenant(tenant)
         try:
             manager = ProviderManager(provider_uuid)
