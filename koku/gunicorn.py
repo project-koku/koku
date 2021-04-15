a20d3b9cbfe6a95d97a0c65ae0a7e8ff37de6325
Michael Skarbek <mskarbek@redhat.com>
2021-04-15 14:14:34 -0400

    1 worker for sources

--- koku/gunicorn.py
+++ koku/gunicorn.py
@@ -9,7 +9,7 @@ SOURCES = ENVIRONMENT.bool("SOURCES", default=False)
 
 bind = "unix:/var/run/koku/gunicorn.sock"
 cpu_resources = ENVIRONMENT.int("POD_CPU_LIMIT", default=multiprocessing.cpu_count())
-workers = 2 if SOURCES else cpu_resources * 2 + 1
+workers = 1 if SOURCES else cpu_resources * 2 + 1
 timeout = ENVIRONMENT.int("TIMEOUT", default=120)
 loglevel = ENVIRONMENT.get_value("LOG_LEVEL", default="INFO")
 graceful_timeout = ENVIRONMENT.int("GRACEFUL_TIMEOUT", default=180)
