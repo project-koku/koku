ce70a1ba1aab0e49829c686d630f2011dba51977
Michael Skarbek <mskarbek@redhat.com>
2021-04-17 11:54:50 -0400

    move gunicorn changes to separate PR

--- koku/gunicorn.py
+++ koku/gunicorn.py
@@ -1,5 +1,6 @@
 """Gunicorn configuration file."""
 import multiprocessing
+import os
 
 import environ
 
@@ -8,8 +9,9 @@ ENVIRONMENT = environ.Env()
 SOURCES = ENVIRONMENT.bool("SOURCES", default=False)
 
 bind = "unix:/var/run/koku/gunicorn.sock"
-cpu_resources = ENVIRONMENT.int("POD_CPU_LIMIT", default=multiprocessing.cpu_count())
+cpu_resources = int(os.environ.get("POD_CPU_LIMIT", multiprocessing.cpu_count()))
 workers = 1 if SOURCES else cpu_resources * 2 + 1
-timeout = ENVIRONMENT.int("TIMEOUT", default=120)
-loglevel = ENVIRONMENT.get_value("LOG_LEVEL", default="INFO")
-graceful_timeout = ENVIRONMENT.int("GRACEFUL_TIMEOUT", default=180)
+
+timeout = int(os.environ.get("TIMEOUT", "90"))
+loglevel = os.environ.get("LOG_LEVEL", "INFO")
+graceful_timeout = int(os.environ.get("GRACEFUL_TIMEOUT", "180"))
