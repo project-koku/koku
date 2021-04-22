7cb1ef64f8071a3863f860d7a4ad11bfe5d04e53
Douglas Curtis <docurtis@redhat.com>
2021-04-21 21:57:02 -0400

    typo

--- koku/api/user_access/view.py
+++ koku/api/user_access/view.py
@@ -145,11 +145,11 @@ class UserAccessView(APIView):
         """
         query_params = request.query_params
         user_access = request.user.access
-        LOG.info(
-            f"User Access RBAC permissions: {str(user_access)}. Org Admin: {str(request.user.admin)}. Beta: {str(beta)}"
-        )
         admin_user = request.user.admin
         beta = request.user.beta
+        LOG.info(
+            f"User Access RBAC permissions: {str(user_access)}. Org Admin: {str(admin_user)}. Beta: {str(beta)}"
+        )
 
         # only show pre-release features in approved environments
         flag = query_params.get("beta", "False")  # query_params are strings, not bools.
