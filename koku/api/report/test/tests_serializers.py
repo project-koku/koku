112433752829cbf6e1d628eacecd024c2b06cec9
myersCody <codymyers63@gmail.com>
2021-04-26 13:21:24 -0400

    COST-1331: Disable start/end date being used with delta.

--- koku/api/report/test/tests_serializers.py
+++ koku/api/report/test/tests_serializers.py
@@ -594,6 +594,20 @@ class QueryParamSerializerTest(TestCase):
                     serializer = QueryParamSerializer(data=params)
                     serializer.is_valid(raise_exception=True)
 
+    def test_parse_filter_dates_invalid_delta(self):
+        """Test parse of a filter date-based param with monthly presolution should not succeed."""
+        dh = DateHelper()
+        scenarios = [
+            {"start_date": dh.last_month_end.date(), "end_date": dh.this_month_start.date(), "delta": "cost"},
+            {"start_date": materialized_view_month_start().date(), "end_date": dh.today.date(), "delta": "cost"},
+        ]
+
+        for params in scenarios:
+            with self.subTest(params=params):
+                with self.assertRaises(ValidationError):
+                    serializer = QueryParamSerializer(data=params)
+                    serializer.is_valid(raise_exception=True)
+
     def test_parse_filter_dates_invalid(self):
         """Test parse of invalid data for filter date-based param should not succeed."""
         dh = DateHelper()
