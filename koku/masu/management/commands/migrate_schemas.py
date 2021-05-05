06ad8118df014d27ca666c973651d46613eb8c79
Andrew Berglund <aberglun@redhat.com>
2021-05-05 10:37:22 -0400

    Filter tenant set in migrate_schemas command

--- koku/masu/management/commands/migrate_schemas.py
+++ koku/masu/management/commands/migrate_schemas.py
@@ -15,7 +15,15 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
 """command overrides."""
+import logging
+
 from tenant_schemas.management.commands import migrate_schemas
+from tenant_schemas.migration_executors import get_executor
+from tenant_schemas.utils import get_public_schema_name
+from tenant_schemas.utils import get_tenant_model
+from tenant_schemas.utils import schema_exists
+
+LOG = logging.getLogger(__name__)
 
 
 class Command(migrate_schemas.Command):
@@ -26,3 +34,30 @@ class Command(migrate_schemas.Command):
     """
 
     requires_system_checks = []
+
+    def handle(self, *args, **options):
+        super(migrate_schemas.Command, self).handle(*args, **options)
+        self.PUBLIC_SCHEMA_NAME = get_public_schema_name()
+
+        executor = get_executor(codename=self.executor)(self.args, self.options)
+
+        if self.sync_public and not self.schema_name:
+            self.schema_name = self.PUBLIC_SCHEMA_NAME
+
+        if self.sync_public:
+            executor.run_migrations(tenants=[self.schema_name])
+        if self.sync_tenant:
+            if self.schema_name and self.schema_name != self.PUBLIC_SCHEMA_NAME:
+                if not schema_exists(self.schema_name):
+                    msg = f"Schema {self.schema_name} does not exist, skipping."
+                    LOG.info(msg)
+                else:
+                    tenants = [self.schema_name]
+            else:
+                tenants = (
+                    get_tenant_model()
+                    .objects.exclude(schema_name=get_public_schema_name())
+                    .values_list("schema_name", flat=True)
+                )
+                tenants = [tenant for tenant in tenants if schema_exists(tenant[0])]
+            executor.run_migrations(tenants=tenants)
