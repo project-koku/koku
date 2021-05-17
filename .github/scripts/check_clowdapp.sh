6179cc0321901769f9e200130bd9cb01ecd06c9e
Michael Skarbek <mskarbek@redhat.com>
2021-05-17 16:45:03 -0400

    add clowdapp.yaml

--- .github/scripts/check_clowdapp.sh
+++ .github/scripts/check_clowdapp.sh
@@ -9,11 +9,13 @@ curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack
 changed=`git diff --name-only HEAD`
 
 if [[ $changed == *"clowdapp"* ]]; then
-  echo "clowdapp.yaml needs to be updated."
-  exit 1
+  echo "clowdapp.yaml needs to be updated through kustomize."
+  exit_code=1
 else
   echo "clowdapp.yaml is up to date."
-  exit 0
+  exit_code=0
 fi
 
 rm -rf ./kustomize
+
+exit $exit_code
