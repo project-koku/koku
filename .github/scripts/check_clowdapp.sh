02177c90322c6f0070c7745fc34738ef6cbb357b
Michael Skarbek <mskarbek@redhat.com>
2021-05-17 16:39:24 -0400

    improve check script

--- .github/scripts/check_clowdapp.sh
+++ .github/scripts/check_clowdapp.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
+set -e
+
 curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh" | bash
 
 ./kustomize build ./deploy/kustomize > ./deploy/clowdapp.yaml
 
 changed=`git diff --name-only HEAD`
 
-if [[ $changed == *"koku-manifest"* ]]; then
-  echo "Pipfile.lock changed without updating koku-manifest. Run 'make requirements' to update."
+if [[ $changed == *"clowdapp"* ]]; then
+  echo "clowdapp.yaml needs to be updated."
   exit 1
 else
-  echo "Manifest is up to date."
+  echo "clowdapp.yaml is up to date."
   exit 0
 fi
+
+rm -rf ./kustomize
