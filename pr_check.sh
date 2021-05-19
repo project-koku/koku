81b4244d88e3bb5140175a7749457dc4adf7b2ef
Michael Skarbek <mskarbek@redhat.com>
2021-05-19 15:15:51 -0400

    fix pr_check

--- pr_check.sh
+++ pr_check.sh
@@ -16,9 +16,8 @@ IQE_FILTER_EXPRESSION=""
 
 # Install bonfire repo/initialize
 CICD_URL=https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd
-curl -s $CICD_URL/bootstrap.sh -o bootstrap.sh
-source bootstrap.sh  # checks out bonfire and changes to "cicd" dir...
+curl -s $CICD_URL/bootstrap.sh > .cicd_bootstrap.sh && source .cicd_bootstrap.sh
 
-source build.sh
-source deploy_ephemeral_env.sh
-source smoke_test.sh
+source $CICD_ROOT/build.sh
+source $CICD_ROOT/deploy_ephemeral_env.sh
+source $CICD_ROOT/smoke_test.sh
