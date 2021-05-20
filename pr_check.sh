1fdbd458d211a9774a809a382e75daf910ee3b0d
Michael Skarbek <mskarbek@redhat.com>
2021-05-20 16:44:19 -0400

    turn on unit tests in pr_check

--- pr_check.sh
+++ pr_check.sh
@@ -19,7 +19,7 @@ CICD_URL=https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd
 curl -s $CICD_URL/bootstrap.sh > .cicd_bootstrap.sh && source .cicd_bootstrap.sh
 
 source $CICD_ROOT/build.sh
-# source $APP_ROOT/unit_test.sh
+source $APP_ROOT/unit_test.sh
 
 # source $CICD_ROOT/deploy_ephemeral_env.sh
 # source $CICD_ROOT/smoke_test.sh
