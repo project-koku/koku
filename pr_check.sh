4a32a9c9d32d7fbd2234a58a3d6d7ed6d7888d8a
Michael Skarbek <mskarbek@redhat.com>
2021-05-21 15:36:25 -0400

    temporary bootstrap.sh

--- pr_check.sh
+++ pr_check.sh
@@ -16,7 +16,9 @@ IQE_FILTER_EXPRESSION=""
 
 # Install bonfire repo/initialize
 CICD_URL=https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd
-curl -s $CICD_URL/bootstrap.sh > .cicd_bootstrap.sh && source .cicd_bootstrap.sh
+# curl -s $CICD_URL/bootstrap.sh > .cicd_bootstrap.sh && source .cicd_bootstrap.sh
+
+source $APP_ROOT/bootstrap.sh
 
 source $CICD_ROOT/build.sh
 source $APP_ROOT/unit_test.sh
