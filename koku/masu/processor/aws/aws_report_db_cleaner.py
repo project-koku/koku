922a3cc9b4521a9fd1208bbbbe93861df4ed81f1
HAP <hproctor@redhat.com>
2021-06-24 06:58:21 -0400

    begin work using cascade_delete

--- koku/masu/processor/aws/aws_report_db_cleaner.py
+++ koku/masu/processor/aws/aws_report_db_cleaner.py
@@ -9,6 +9,8 @@ from datetime import datetime
 
 from tenant_schemas.utils import schema_context
 
+from koku.database import cascade_delete
+from koku.database import execute_delete_sql
 from masu.database.aws_report_db_accessor import AWSReportDBAccessor
 from reporting.models import PartitionedTable
 
@@ -93,109 +95,81 @@ class AWSReportDBCleaner:
         """
         LOG.info("Calling purge_expired_report_data for aws")
 
+        removed_items = []
+        all_account_ids = []
+        all_period_start = set()
+
         with AWSReportDBAccessor(self._schema) as accessor:
             if (expired_date is None and provider_uuid is None) or (  # noqa: W504
                 expired_date is not None and provider_uuid is not None
             ):
                 err = "This method must be called with either expired_date or provider_uuid"
                 raise AWSReportDBCleanerError(err)
-            removed_items = []
 
             if expired_date is not None:
                 return self.purge_expired_report_data_by_date(expired_date, simulate=simulate)
-            else:
-                bill_objects = accessor.get_cost_entry_bills_query_by_provider(provider_uuid)
+
+            bill_objects = accessor.get_cost_entry_bills_query_by_provider(provider_uuid)
 
             with schema_context(self._schema):
-                for bill in bill_objects.all():
-                    bill_id = bill.id
-                    removed_payer_account_id = bill.payer_account_id
-                    removed_billing_period_start = bill.billing_period_start
-
-                    if not simulate:
-                        del_count = accessor.execute_delete_sql(accessor.get_ocp_aws_summary_query_for_billid(bill_id))
-                        LOG.info("Removing %s OCP-on-AWS summary items for bill id %s", del_count, bill_id)
-
-                        del_count = accessor.execute_delete_sql(
-                            accessor.get_ocp_aws_project_summary_query_for_billid(bill_id)
-                        )
-                        LOG.info("Removing %s OCP-on-AWS project summary items for bill id %s", del_count, bill_id)
-
-                        del_count = accessor.execute_delete_sql(accessor.get_lineitem_query_for_billid(bill_id))
-                        LOG.info("Removing %s cost entry line items for bill id %s", del_count, bill_id)
-
-                        del_count = accessor.execute_delete_sql(accessor.get_daily_query_for_billid(bill_id))
-                        LOG.info("Removing %s cost entry daily items for bill id %s", del_count, bill_id)
-
-                        del_count = accessor.execute_delete_sql(accessor.get_summary_query_for_billid(bill_id))
-                        LOG.info("Removing %s cost entry summary items for bill id %s", del_count, bill_id)
-
-                        del_count = accessor.execute_delete_sql(accessor.get_cost_entry_query_for_billid(bill_id))
-                        LOG.info("Removing %s cost entry items for bill id %s", del_count, bill_id)
-
-                    LOG.info(
-                        "Report data removed for Account Payer ID: %s with billing period: %s",
-                        removed_payer_account_id,
-                        removed_billing_period_start,
-                    )
+                for bill in bill_objects:
                     removed_items.append(
                         {
-                            "account_payer_id": removed_payer_account_id,
-                            "billing_period_start": str(removed_billing_period_start),
+                            "account_payer_id": bill.payer_account_id,
+                            "billing_period_start": str(bill.billing_period_start),
                         }
                     )
+                    all_account_ids.append(bill.payer_account_id)
+                    all_period_start.add(str(bill.billing_period_start))
 
+                LOG.info(
+                    f"Deleting data related to billing account ids {all_account_ids} "
+                    f"for billing periods starting {sorted(all_period_start)}"
+                )
                 if not simulate:
-                    bill_objects.delete()
+                    cascade_delete(bill_objects.query.model, bill_objects.query.model, bill_objects)
 
         return removed_items
 
     def purge_expired_report_data_by_date(self, expired_date, simulate=False):
         paritition_from = str(date(expired_date.year, expired_date.month, 1))
+        removed_items = []
+        all_account_ids = []
+        all_period_start = set()
+
         with AWSReportDBAccessor(self._schema) as accessor:
             all_bill_objects = accessor.get_bill_query_before_date(expired_date).all()
+            for bill in all_bill_objects:
+                removed_items.append(
+                    {"account_payer_id": bill.payer_account_id, "billing_period_start": str(bill.billing_period_start)}
+                )
+                all_account_ids.append(bill.payer_account_id)
+                all_period_start.add(str(bill.billing_period_start))
+
             table_names = [
                 accessor._table_map["ocp_on_aws_daily_summary"],
                 accessor._table_map["ocp_on_aws_project_daily_summary"],
                 accessor.line_item_daily_summary_table._meta.db_table,
             ]
-            base_lineitem_query = accessor._get_db_obj_query(accessor.line_item_table)
-            base_daily_query = accessor._get_db_obj_query(accessor.line_item_daily_table)
-            base_costentry_query = accessor._get_db_obj_query(accessor.cost_entry_table)
 
         with schema_context(self._schema):
-            removed_items = []
             if not simulate:
                 # Will call trigger to detach, truncate, and drop partitions
-                del_count = PartitionedTable.objects.filter(
-                    schema_name=self._schema,
-                    partition_of_table_name__in=table_names,
-                    partition_parameters__default=False,
-                    partition_parameters__from__lte=paritition_from,
-                ).delete()
+                del_count = execute_delete_sql(
+                    PartitionedTable.objects.filter(
+                        schema_name=self._schema,
+                        partition_of_table_name__in=table_names,
+                        partition_parameters__default=False,
+                        partition_parameters__from__lte=paritition_from,
+                    )
+                )
                 LOG.info(f"Deleted {del_count} table partitions total for the following tables: {table_names}")
 
-            # Iterate over the remainder as they could involve much larger amounts of data
-            for bill in all_bill_objects:
-                if not simulate:
-                    del_count = base_lineitem_query.filter(cost_entry_bill_id=bill.id).delete()
-                    LOG.info(f"Deleted {del_count} cost entry line items for bill_id {bill.id}")
+                cascade_delete(all_bill_objects.query.model, all_bill_objects.query.model, all_bill_objects)
 
-                    del_count = base_daily_query.filter(cost_entry_bill_id=bill.id).delete()
-                    LOG.info(f"Deleted {del_count} cost entry line items for bill_id {bill.id}")
-
-                    del_count = base_costentry_query.filter(bill_id=bill.id).delete()
-                    LOG.info(f"Deleted {del_count} cost entry line items for bill_id {bill.id}")
-
-                removed_items.append(
-                    {"account_payer_id": bill.payer_account_id, "billing_period_start": str(bill.billing_period_start)}
-                )
-                LOG.info(
-                    f"Report data deleted for account payer id {bill.payer_account_id} "
-                    f"and billing period {str(bill.billing_period_start)}"
-                )
-
-            if not simulate:
-                all_bill_objects.delete()
+            LOG.info(
+                f"Deleting data related to billing account ids {all_account_ids} "
+                f"for billing periods starting {sorted(all_period_start)}"
+            )
 
         return removed_items
