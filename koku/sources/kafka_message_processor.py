61852c173b1edc4db2ab0c78a30ed3f02f39ee5d
Michael Skarbek <mskarbek@redhat.com>
2021-07-08 11:14:11 -0400

    filter auth messages based on valid authtypes for cost-mgmt

--- koku/sources/kafka_message_processor.py
+++ koku/sources/kafka_message_processor.py
@@ -10,6 +10,7 @@ from rest_framework.exceptions import ValidationError
 from api.provider.models import Provider
 from sources import storage
 from sources.config import Config
+from sources.sources_http_client import AUTH_TYPES
 from sources.sources_http_client import SourcesHTTPClient
 from sources.sources_http_client import SourcesHTTPClientError
 
@@ -96,6 +97,10 @@ class KafkaMessageProcessor:
         if self.event_type in (KAFKA_APPLICATION_CREATE, KAFKA_APPLICATION_UPDATE, KAFKA_APPLICATION_DESTROY):
             return self.application_type_id == self.cost_mgmt_id
         if self.event_type in (KAFKA_AUTHENTICATION_CREATE, KAFKA_AUTHENTICATION_UPDATE):
+            if self.value.get("authtype") not in AUTH_TYPES.values():
+                # if authtype is not one of the valid auth types, then ignore the message
+                LOG.debug(f"[msg_for_cost_mgmt] AUTH TYPE: {self.value.get('authtype')}")
+                return False
             sources_network = self.get_sources_client()
             return sources_network.get_application_type_is_cost_management(self.cost_mgmt_id)
         return False
@@ -250,9 +255,6 @@ class AuthenticationMsgProcessor(KafkaMessageProcessor):
 
         if storage.is_known_source(self.source_id):
             if self.event_type in (KAFKA_AUTHENTICATION_CREATE):
-                if storage.get_source_type(self.source_id) is None:
-                    # this creates the source details if receiving Auth.create before App.create
-                    self.save_sources_details()
                 self.save_source_info(auth=True)
             if self.event_type in (KAFKA_AUTHENTICATION_UPDATE):
                 if storage.get_source_type(self.source_id) == Provider.PROVIDER_AZURE:
