381903cb820731b87002ee5d544bd958c419a938
Aaron <afosmire@redhat.com>
2021-07-08 14:06:22 -0400

    update aws accounts and azure endpoints

--- koku/api/resource_types/aws_accounts/view.py
+++ koku/api/resource_types/aws_accounts/view.py
@@ -4,15 +4,18 @@
 #
 """View for AWS accounts."""
 from django.db.models import F
+from django.db.models.functions import Coalesce
 from django.utils.decorators import method_decorator
 from django.views.decorators.vary import vary_on_headers
 from rest_framework import filters
 from rest_framework import generics
 
 from api.common import CACHE_RH_IDENTITY_HEADER
+from api.common.permissions.openshift_access import OpenShiftAccessPermission
 from api.common.permissions.resource_type_access import ResourceTypeAccessPermission
 from api.resource_types.serializers import ResourceTypeSerializer
 from reporting.provider.aws.models import AWSCostSummaryByAccount
+from reporting.provider.aws.openshift.models import OCPAWSCostSummaryByAccount
 
 
 class AWSAccountView(generics.ListAPIView):
@@ -20,17 +23,30 @@ class AWSAccountView(generics.ListAPIView):
 
     queryset = (
         AWSCostSummaryByAccount.objects.annotate(
-            **{"value": F("usage_account_id"), "alias": F("account_alias__account_alias")}
+            **({"value": F("usage_account_id"), "alias": F("account_alias__account_alias")})
         )
         .values("value", "alias")
         .distinct()
     )
     serializer_class = ResourceTypeSerializer
-    permission_classes = [ResourceTypeAccessPermission]
+    permission_classes = [ResourceTypeAccessPermission, OpenShiftAccessPermission]
     filter_backends = [filters.OrderingFilter, filters.SearchFilter]
     ordering = ["value", "alias"]
     search_fields = ["$value", "$alias"]
 
     @method_decorator(vary_on_headers(CACHE_RH_IDENTITY_HEADER))
     def list(self, request):
+        self.queryset.annotate(account_alias=Coalesce(F("alias"), "usage_account_id"))
+        openshift = self.request.query_params.get("openshift")
+        if openshift:
+            self.queryset = (
+                OCPAWSCostSummaryByAccount.objects.annotate(
+                    **{"value": F("usage_account_id"), "alias": F("account_alias__account_alias")}
+                )
+                .values("value", "alias")
+                .distinct()
+            )
         return super().list(request)
+
+
+# filter(usage_account_id__in=user_access)
