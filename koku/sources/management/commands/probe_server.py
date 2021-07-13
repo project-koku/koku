1b8756f1f4a399d5a328743b9589d58cc2795456
Michael Skarbek <mskarbek@redhat.com>
2021-07-12 20:36:23 -0400

    add probe server for sources

---
+++ koku/sources/management/commands/probe_server.py
@@ -0,0 +1,77 @@
+#
+# Copyright 2021 Red Hat Inc.
+# SPDX-License-Identifier: Apache-2.0
+#
+"""HTTP server for liveness/readiness probes."""
+import json
+import logging
+from http.server import BaseHTTPRequestHandler
+
+from sources.api.status import check_kafka_connection
+from sources.api.status import check_sources_connection
+
+
+LOG = logging.getLogger(__name__)
+
+
+class ProbeServer(BaseHTTPRequestHandler):
+    """HTTP server for liveness/readiness probes."""
+
+    ready = False
+
+    def _set_headers(self, status):
+        """Set the response headers."""
+        self.send_response(status)
+        self.send_header("Content-type", "application/json")
+        self.end_headers()
+
+    def _write_response(self, response):
+        """Write the response to the client."""
+        self._set_headers(response.status_code)
+        self.wfile.write(response.json.encode("utf-8"))
+
+    def do_GET(self):
+        """Handle GET requests."""
+        if self.path == "/livez":
+            self.liveness_check()
+        elif self.path == "/readyz":
+            self.readiness_check()
+        else:
+            self.default_response()
+
+    def default_response(self):
+        """Set the default response."""
+        self._write_response(Response(404, "not found"))
+
+    def liveness_check(self):
+        """Set the liveness check response."""
+        self._write_response(Response(200, "ok"))
+
+    def readiness_check(self):
+        """Set the readiness check response."""
+        status = 424
+        msg = "not ready"
+        if self.ready:
+            if not check_kafka_connection():
+                self._write_response(Response(status, "kafka not ready"))
+                return
+            if not check_sources_connection():
+                self._write_response(Response(status, "sources not ready"))
+                return
+            status = 200
+            msg = "ok"
+        self._write_response(Response(status, msg))
+
+    def log_message(self, format, *args):
+        msg, status, _ = args
+        log = {"timestamp": self.log_date_time_string(), "probe_server": {"request": msg, "status": status}}
+        LOG.info(json.dumps(log))
+
+
+class Response:
+    """Response object for the probe server."""
+
+    def __init__(self, status_code, msg):
+        """Initialize the response object."""
+        self.status_code = status_code
+        self.json = json.dumps({"status": status_code, "msg": msg})
--- koku/sources/management/commands/sources_listener.py
+++ koku/sources/management/commands/sources_listener.py
@@ -4,15 +4,42 @@
 #
 """Sources listener entry point."""
 import logging
+import threading
 import time
+from http.server import HTTPServer
 
 from django.core.management.base import BaseCommand
 
 from koku.database import check_migrations
+from koku.env import ENVIRONMENT
 from sources.kafka_listener import initialize_sources_integration
+from sources.management.commands.probe_server import ProbeServer
 
 
 LOG = logging.getLogger(__name__)
+CLOWDER_PORT = 8080
+if ENVIRONMENT.bool("CLOWDER_ENABLED", default=False):
+    from app_common_python import LoadedConfig
+
+    CLOWDER_PORT = LoadedConfig.publicPort
+
+
+def start_probe_server():
+    """Start the probe server."""
+    httpd = HTTPServer(("0.0.0.0", CLOWDER_PORT), ProbeServer)
+
+    def start_server():
+        """Start a simple webserver serving path on port"""
+        httpd.RequestHandlerClass.ready = False
+        httpd.serve_forever()
+
+    LOG.info("starting liveness/readiness probe server")
+    daemon = threading.Thread(name="probe_server", target=start_server)
+    daemon.setDaemon(True)  # Set as a daemon so it will be killed once the main thread is dead.
+    daemon.start()
+    LOG.info("liveness/readiness probe server started")
+
+    return httpd
 
 
 class Command(BaseCommand):
@@ -21,10 +48,14 @@ class Command(BaseCommand):
     help = "Starts koku-sources-kafka-listener"
 
     def handle(self, *args, **kwargs):
+        httpd = start_probe_server()
+
         timeout = 5
         while not check_migrations():
             LOG.warning(f"Migrations not done. Sleeping {timeout} seconds.")
             time.sleep(timeout)
+
+        httpd.RequestHandlerClass.ready = True  # Set `ready` to true to indicate migrations are done.
 
         LOG.info("Starting Sources Kafka Handler")
         initialize_sources_integration()
