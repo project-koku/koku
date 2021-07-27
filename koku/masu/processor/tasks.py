13cb375ecb49b685d0f18519a8742f3aff7e3346
Doug Curtis <docurtis@redhat.com>
2021-07-27 10:47:50 -0400

    aws logging work in progress

--- koku/masu/processor/tasks.py
+++ koku/masu/processor/tasks.py
@@ -178,10 +178,10 @@ def get_report_files(
         )
 
         stmt = (
-            f"Reports to be processed:\n"
-            f" schema_name: {customer_name}\n"
-            f" provider: {provider_type}\n"
-            f" provider_uuid: {provider_uuid}\n"
+            f"Reports to be processed: "
+            f" schema_name: {customer_name} "
+            f" provider: {provider_type} "
+            f" provider_uuid: {provider_uuid}"
         )
         if report_dict:
             stmt += f" file: {report_dict['file']}"
@@ -195,7 +195,7 @@ def get_report_files(
             "provider_type": provider_type,
             "provider_uuid": provider_uuid,
             "manifest_id": report_dict.get("manifest_id"),
-            "request_id": tracing_id,
+            "tracing_id": tracing_id,
         }
 
         try:
@@ -209,7 +209,7 @@ def get_report_files(
             LOG.info(log_json(tracing_id, stmt))
             worker_stats.PROCESS_REPORT_ATTEMPTS_COUNTER.labels(provider_type=provider_type).inc()
 
-            report_dict["request_id"] = tracing_id
+            report_dict["tracing_id"] = tracing_id
             report_dict["provider_type"] = provider_type
 
             _process_report_file(schema_name, provider_type, report_dict)
@@ -294,7 +294,7 @@ def summarize_reports(reports_to_summarize, queue_name=None):
                     start_date = start_date.strftime("%Y-%m-%d")
                     end_date = DateAccessor().today().strftime("%Y-%m-%d")
                 msg = f"report to summarize: {str(report)}"
-                tracing_id = report.get("request_id", report.get("manifest_uuid", "no-request-id"))
+                tracing_id = report.get("tracing_id", report.get("manifest_uuid", "no-tracing-id"))
                 LOG.info(log_json(tracing_id, msg))
                 update_summary_tables.s(
                     report.get("schema_name"),
@@ -416,8 +416,8 @@ def update_summary_tables(  # noqa: C901
         )
     else:
         stmt = (
-            f"\n update_cost_model_costs skipped.\n"
-            f" schema_name: {schema_name},\n"
+            f"update_cost_model_costs skipped. "
+            f" schema_name: {schema_name}, "
             f" provider_uuid: {provider_uuid}"
         )
         LOG.info(log_json(tracing_id, stmt))
