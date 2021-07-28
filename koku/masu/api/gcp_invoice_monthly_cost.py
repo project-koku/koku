565f38471bdaeb8b54129e0378404589821e1a01
myersCody <codymyers63@gmail.com>
2021-07-28 15:47:14 -0400

    Fix bug in internal api and add tests.

--- koku/masu/api/gcp_invoice_monthly_cost.py
+++ koku/masu/api/gcp_invoice_monthly_cost.py
@@ -16,7 +16,7 @@ from rest_framework.response import Response
 from rest_framework.settings import api_settings
 
 from api.utils import DateHelper
-from masu.external.accounts_accessor import AccountsAccessor
+from masu.database.provider_collector import ProviderCollector
 
 LOG = logging.getLogger(__name__)
 
@@ -35,10 +35,19 @@ def gcp_invoice_monthly_cost(request):
         return Response({"Error": errmsg}, status=status.HTTP_400_BAD_REQUEST)
 
     # Grab info needed for bigqury from source
-    account_info = AccountsAccessor().get_accounts(provider_uuid)[0]
-    project_id = account_info.get("credentials", {}).get("project_id")
-    dataset = account_info.get("data_source", {}).get("dataset")
-    table_id = account_info.get("data_source", {}).get("table_id")
+    with ProviderCollector() as collector:
+        all_providers = collector.get_provider_uuid_map()
+        provider = all_providers.get(str(provider_uuid))
+        if not provider:
+            errmsg = f"The provider_uuid {provider_uuid} does not exist."
+            return Response({"Error": errmsg}, status=status.HTTP_400_BAD_REQUEST)
+    credentials = provider.authentication.credentials
+    data_source = provider.billing_source.data_source
+
+    dataset = data_source.get("dataset")
+    table_id = data_source.get("table_id")
+    project_id = credentials.get("project_id")
+
     if None in [project_id, dataset, table_id]:
         errmsg = "Could not build gcp table name due to mising information."
         return Response({"Error": errmsg}, status=status.HTTP_400_BAD_REQUEST)
