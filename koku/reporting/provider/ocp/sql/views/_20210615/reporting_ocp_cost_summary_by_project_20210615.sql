3c7de28480daf57eadede7b02986b5b149fb925c
HAP <hproctor@redhat.com>
2021-08-18 10:41:52 -0400

    revert view def

--- koku/reporting/provider/ocp/sql/views/_20210615/reporting_ocp_cost_summary_by_project_20210615.sql
+++ koku/reporting/provider/ocp/sql/views/_20210615/reporting_ocp_cost_summary_by_project_20210615.sql
@@ -2,41 +2,42 @@ DROP INDEX IF EXISTS ocp_cost_summary_by_project;
 DROP MATERIALIZED VIEW IF EXISTS reporting_ocp_cost_summary_by_project;
 
 CREATE MATERIALIZED VIEW reporting_ocp_cost_summary_by_project AS(
- SELECT row_number() OVER (ORDER BY usage_start, cluster_id, cluster_alias, namespace) AS "id",
-    usage_start,
-    usage_start AS "usage_end",
-    cluster_id,
-    cluster_alias,
-    namespace,
-    json_build_object(
-        'cpu', sum(((supplementary_usage_cost ->> 'cpu'::text))::numeric),
-        'memory', sum(((supplementary_usage_cost ->> 'memory'::text))::numeric),
-        'storage', sum(((supplementary_usage_cost ->> 'storage'::text))::numeric)
-    ) AS "supplementary_usage_cost",
-    json_build_object(
-        'cpu', sum(((infrastructure_usage_cost ->> 'cpu'::text))::numeric),
-        'memory', sum(((infrastructure_usage_cost ->> 'memory'::text))::numeric),
-        'storage', sum(((infrastructure_usage_cost ->> 'storage'::text))::numeric)
-    ) AS "infrastructure_usage_cost",
-    sum(infrastructure_project_raw_cost) AS "infrastructure_project_raw_cost",
-    sum(infrastructure_project_markup_cost) AS "infrastructure_project_markup_cost",
-    json_build_object(
-        'cpu', sum(((COALESCE(supplementary_project_monthly_cost, '{"cpu": 0}'::jsonb) ->> 'cpu'::text))::numeric),
-        'memory', sum(((COALESCE(supplementary_project_monthly_cost, '{"memory": 0}'::jsonb) ->> 'memory'::text))::numeric),
-        'pvc', sum(((COALESCE(supplementary_project_monthly_cost, '{"pvc": 0}'::jsonb) ->> 'pvc'::text))::numeric)
-    ) AS "supplementary_project_monthly_cost",
-    sum(supplementary_monthly_cost) AS "supplementary_monthly_cost",
-    json_build_object(
-        'cpu', sum(((COALESCE(infrastructure_project_monthly_cost, '{"cpu": 0}'::jsonb) ->> 'cpu'::text))::numeric),
-        'memory', sum(((COALESCE(infrastructure_project_monthly_cost, '{"memory": 0}'::jsonb) ->> 'memory'::text))::numeric),
-        'pvc', sum(((COALESCE(infrastructure_project_monthly_cost, '{"pvc": 0}'::jsonb) ->> 'pvc'::text))::numeric)
-    ) AS "infrastructure_project_monthly_cost",
-    sum(infrastructure_monthly_cost) AS "infrastructure_monthly_cost",
-    source_uuid
-   FROM reporting_ocpusagelineitem_daily_summary
-  WHERE (usage_start >= ("date_trunc"('month'::text, (now() - '2 mons'::interval)))::date)
-  GROUP BY usage_start, cluster_id, cluster_alias, namespace, source_uuid
-
+    SELECT row_number() OVER(ORDER BY usage_start, cluster_id, cluster_alias, namespace) as id,
+        usage_start as usage_start,
+        usage_start as usage_end,
+        cluster_id,
+        cluster_alias,
+        namespace,
+        json_build_object(
+            'cpu', sum((supplementary_usage_cost->>'cpu')::decimal),
+            'memory', sum((supplementary_usage_cost->>'memory')::decimal),
+            'storage', sum((supplementary_usage_cost->>'storage')::decimal)
+        ) as supplementary_usage_cost,
+        json_build_object(
+            'cpu', sum((infrastructure_usage_cost->>'cpu')::decimal),
+            'memory', sum((infrastructure_usage_cost->>'memory')::decimal),
+            'storage', sum((infrastructure_usage_cost->>'storage')::decimal)
+        ) as infrastructure_usage_cost,
+        sum(infrastructure_project_raw_cost) as infrastructure_project_raw_cost,
+        sum(infrastructure_project_markup_cost) as infrastructure_project_markup_cost,
+        json_build_object(
+            'cpu', sum(((coalesce(supplementary_project_monthly_cost, '{"cpu": 0}'::jsonb))->>'cpu')::decimal),
+            'memory', sum(((coalesce(supplementary_project_monthly_cost, '{"memory": 0}'::jsonb))->>'memory')::decimal),
+            'pvc', sum(((coalesce(supplementary_project_monthly_cost, '{"pvc": 0}'::jsonb))->>'pvc')::decimal)
+        ) as supplementary_project_monthly_cost,
+        sum(supplementary_monthly_cost) as supplementary_monthly_cost,
+        json_build_object(
+            'cpu', sum(((coalesce(infrastructure_project_monthly_cost, '{"cpu": 0}'::jsonb))->>'cpu')::decimal),
+            'memory', sum(((coalesce(infrastructure_project_monthly_cost, '{"memory": 0}'::jsonb))->>'memory')::decimal),
+            'pvc', sum(((coalesce(infrastructure_project_monthly_cost, '{"pvc": 0}'::jsonb))->>'pvc')::decimal)
+        ) as infrastructure_project_monthly_cost,
+        sum(infrastructure_monthly_cost) as infrastructure_monthly_cost,
+        source_uuid
+    FROM reporting_ocpusagelineitem_daily_summary
+    -- Get data for this month or last month
+    WHERE usage_start >= DATE_TRUNC('month', NOW() - '2 month'::interval)::date
+    GROUP BY usage_start, cluster_id, cluster_alias, namespace, source_uuid
+)
 WITH DATA
 ;
 
