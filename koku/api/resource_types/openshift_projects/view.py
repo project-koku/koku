2728fa5da307e9bf4bf563fc4ecbe1c6f70fbdae
Aaron <afosmire@redhat.com>
2021-09-01 15:19:13 -0400

    changed variable names

--- koku/api/resource_types/openshift_projects/view.py
+++ koku/api/resource_types/openshift_projects/view.py
@@ -49,6 +49,7 @@ class OCPProjectsView(generics.ListAPIView):
         elif request.user.access:
             if request.user.access.get("openshift.cluster", {}).get("read", []):
                 user_access = request.user.access.get("openshift.cluster", {}).get("read", [])
+                # We hold a copy of the filtered queryset just incase the user has a wildcard for user access
                 query_holder = self.queryset.filter(cluster_id__in=user_access)
             elif request.user.access.get("openshift.project", {}).get("read", []):
                 user_access = request.user.access.get("openshift.project", {}).get("read", [])
