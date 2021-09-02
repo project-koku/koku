90f9cffd78e4d7dc3043a5b887c673b923dccb61
Aaron <afosmire@redhat.com>
2021-09-02 14:24:52 -0400

    update endpoints

--- koku/api/resource_types/openshift_projects/view.py
+++ koku/api/resource_types/openshift_projects/view.py
@@ -38,7 +38,7 @@ class OCPProjectsView(generics.ListAPIView):
         supported_query_params = ["search", "limit"]
         user_access = []
         error_message = {}
-        query_holder = False
+        query_holder = None
         # Test for only supported query_params
         if self.request.query_params:
             for key in self.request.query_params:
@@ -47,14 +47,15 @@ class OCPProjectsView(generics.ListAPIView):
                     return Response(error_message, status=status.HTTP_400_BAD_REQUEST)
         if request.user.admin:
             return super().list(request)
-        elif request.user.access:
+        if request.user.access:
+            query_holder = self.queryset
+            if request.user.access.get("openshift.project", {}).get("read", []):
+                user_access = request.user.access.get("openshift.project", {}).get("read", [])
+                query_holder = query_holder.filter(namespace__in=user_access)
             if request.user.access.get("openshift.cluster", {}).get("read", []):
                 user_access = request.user.access.get("openshift.cluster", {}).get("read", [])
                 # We hold a copy of the filtered queryset just incase the user has a wildcard for user access
-                query_holder = self.queryset.filter(cluster_id__in=user_access)
-            elif request.user.access.get("openshift.project", {}).get("read", []):
-                user_access = request.user.access.get("openshift.project", {}).get("read", [])
-                query_holder = self.queryset.filter(namespace__in=user_access)
+                query_holder = query_holder.filter(cluster_id__in=user_access)
         if user_access and user_access[0] == "*":
             return super().list(request)
         # if query_holder does not exist we return an empty queryset
