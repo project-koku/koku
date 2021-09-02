5218660ca842796e1ad94b3a28a8fe2509899c46
Doug Curtis <docurtis@redhat.com>
2021-09-02 09:24:31 -0400

    currency model additions

---
+++ koku/currency/common.py
@@ -0,0 +1,25 @@
+#
+# Copyright 2021 Red Hat Inc.
+# SPDX-License-Identifier: Apache-2.0
+#
+"""Common utilities and helpers for Currency."""
+import json
+import os
+
+from api.iam.models import Customer
+from currency.models import CurrencyOptions
+from currency.models import CurrencySettings as Currency
+from koku.settings import KOKU_DEFAULT_CURRENCY
+
+CURRENCY_FILE_NAME = f"{os.path.dirname(os.path.realpath(__file__))}/specs/currencies.json"
+__CURRENCY_CHOICES = None
+
+
+def load_currencies_from_file():
+	with open(CURRENCY_FILE_NAME) as api_file:
+		data = json.load(api_file)
+	return data
+
+
+def load_currency_choices():
+	return tuple([(currency.get("code"), currency.get("code")) for currency in load_currencies_from_file()])
---
+++ koku/currency/models.py
@@ -0,0 +1,35 @@
+#
+# Copyright 2021 Red Hat Inc.
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from django.db import models
+
+from api.iam.models import Customer
+from koku.settings import KOKU_DEFAULT_CURRENCY
+
+
+class CurrencyOptions(models.Model):
+	"""A collection of supported currencies for OCP cost models."""
+
+	class Meta:
+		"""Meta for Currency."""
+
+		db_table = "currency_options"
+		ordering = ["code"]
+
+	code = models.CharField(max_length=5)
+	name = models.CharField(max_length=150)
+	symbol = models.CharField(max_length=150)
+	description = models.TextField()
+
+
+class CurrencySettings(models.Model):
+	"""A table that maps between account and currency: currency has many accounts."""
+
+	class Meta:
+		db_table = "currency_settings"
+		ordering = ["currency_code"]
+
+	currency_code = models.ForeignKey(CurrencyOptions, on_delete=models.SET(KOKU_DEFAULT_CURRENCY))
+	customer = models.OneToOneField(Customer, on_delete=models.CASCADE, primary_key=True)
