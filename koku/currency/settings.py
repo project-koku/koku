724a5ba0b2d7945b6f2c2d1219aff4c42062c499
Doug Curtis <docurtis@redhat.com>
2021-09-03 14:22:44 -0400

    lint

--- koku/currency/settings.py
+++ koku/currency/settings.py
@@ -19,16 +19,16 @@ LOG = logging.getLogger(__name__)
 
 
 class CurrencySettings:
-	"""Class for generating currency settings."""
+    """Class for generating currency settings."""
 
-	def __init__(self, request):
-		"""Initialize settings object with incoming request."""
-		self.request = request
-		self.factory = RequestFactory()
-		self.schema = request.user.customer.schema_name
+    def __init__(self, request):
+        """Initialize settings object with incoming request."""
+        self.request = request
+        self.factory = RequestFactory()
+        self.schema = request.user.customer.schema_name
 
-	def handle_settings(self, settings):
-		"""
+    def handle_settings(self, settings):
+        """
 		Handle setting results
 
 		Args:
@@ -37,45 +37,45 @@ class CurrencySettings:
 		Returns:
 			(Bool) - True, if a setting had an effect, False otherwise
 		"""
-		currency = settings.get("api", {}).get("settings", {}).get("currency", None)
-		try:
-			stored_currency = get_selected_currency_or_setup(self.schema)
-		except Exception as exp:
-			LOG.warn(f"Failed to retrieve currency for schema {self.schema}. Reason: {exp}")
-			return False
-		if currency is None or stored_currency == currency:
-			return False
+        currency = settings.get("api", {}).get("settings", {}).get("currency", None)
+        try:
+            stored_currency = get_selected_currency_or_setup(self.schema)
+        except Exception as exp:
+            LOG.warn(f"Failed to retrieve currency for schema {self.schema}. Reason: {exp}")
+            return False
+        if currency is None or stored_currency == currency:
+            return False
 
-		try:
-			set_currency(self.schema, currency)
-		except Exception as exp:
-			LOG.warn(f"Failed to store new currency settings for schema {self.schema}. Reason: {exp}")
-			return False
-		invalidate_view_cache_for_tenant_and_source_type(self.schema, Provider.PROVIDER_OCP)
-		return True
+        try:
+            set_currency(self.schema, currency)
+        except Exception as exp:
+            LOG.warn(f"Failed to store new currency settings for schema {self.schema}. Reason: {exp}")
+            return False
+        invalidate_view_cache_for_tenant_and_source_type(self.schema, Provider.PROVIDER_OCP)
+        return True
 
-	def build_settings(self):
-		"""
+    def build_settings(self):
+        """
 		Generate currency settings
 
 		Returns:
 			(List) - List of setting items
 		"""
-		settings = {
-			"title": "Currency",
-			"fields": [
-				{
-					"label": "Select the prefered currency to view Cost Information in",
-					"name": "api.settings.openshift.title",
-					"component": "plain-text",
-				},
-				{
-					"component": "select",
-					"name": f"{SETTINGS_PREFIX}.currency",
-					"label": "Currency",
-					"options": get_currency_options(),
-					"initialValue": get_selected_currency_or_setup(self.schema),
-				},
-			],
-		}
-		return [settings]
+        settings = {
+            "title": "Currency",
+            "fields": [
+                {
+                    "label": "Select the prefered currency to view Cost Information in",
+                    "name": "api.settings.openshift.title",
+                    "component": "plain-text",
+                },
+                {
+                    "component": "select",
+                    "name": f"{SETTINGS_PREFIX}.currency",
+                    "label": "Currency",
+                    "options": get_currency_options(),
+                    "initialValue": get_selected_currency_or_setup(self.schema),
+                },
+            ],
+        }
+        return [settings]
--- koku/currency/test/tests_common.py
+++ koku/currency/test/tests_common.py
@@ -3,44 +3,41 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 """Test the currency common."""
-import json
-
 from tenant_schemas.utils import schema_context
-from koku.settings import KOKU_DEFAULT_CURRENCY
 
-from currency.common import CURRENCY_FILE_NAME
-from currency.common import load_currencies_from_file
-from currency.common import get_selected_currency_or_setup
-from currency.common import set_currency
 from currency.common import get_currency_options
-from reporting.currency.models import CurrencySettings
+from currency.common import get_selected_currency_or_setup
+from currency.common import load_currencies_from_file
+from currency.common import set_currency
+from koku.settings import KOKU_DEFAULT_CURRENCY
 from masu.test import MasuTestCase
+from reporting.currency.models import CurrencySettings
 
 
 class TestCurrencyCommon(MasuTestCase):
-	"""Test cases for currency common."""
+    """Test cases for currency common."""
 
-	def setUp(self):
-		"""Set up test suite."""
-		with schema_context(self.schema):
-			CurrencySettings.objects.all().delete()
+    def setUp(self):
+        """Set up test suite."""
+        with schema_context(self.schema):
+            CurrencySettings.objects.all().delete()
 
-	def test_load_currencies_from_file(self):
-		"""Test to load currencies from json file."""
-		currency_data = load_currencies_from_file()
-		self.assertIsNotNone(currency_data)
+    def test_load_currencies_from_file(self):
+        """Test to load currencies from json file."""
+        currency_data = load_currencies_from_file()
+        self.assertIsNotNone(currency_data)
 
-	def test_get_selected_currency_or_setup(self):
-		"""Test currency initialization."""
-		currency = get_selected_currency_or_setup(self.schema)
-		self.assertEqual(currency, KOKU_DEFAULT_CURRENCY)
-		
-		new_currency = 'JPY'
-		set_currency(self.schema, currency_code=new_currency)
-		currency = get_selected_currency_or_setup(self.schema)
-		self.assertEqual(currency, new_currency)
+    def test_get_selected_currency_or_setup(self):
+        """Test currency initialization."""
+        currency = get_selected_currency_or_setup(self.schema)
+        self.assertEqual(currency, KOKU_DEFAULT_CURRENCY)
 
-	def test_get_currency_options(self):
-		"""Test get_currency_options."""
-		options = get_currency_options()
-		self.assertTrue(len(options) != 0)
+        new_currency = "JPY"
+        set_currency(self.schema, currency_code=new_currency)
+        currency = get_selected_currency_or_setup(self.schema)
+        self.assertEqual(currency, new_currency)
+
+    def test_get_currency_options(self):
+        """Test get_currency_options."""
+        options = get_currency_options()
+        self.assertTrue(len(options) != 0)
