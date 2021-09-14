599b3275a9c2bc277b140de045af30702e4e5a78
Aaron <afosmire@redhat.com>
2021-09-14 15:28:16 -0400

    update wildcard check

--- koku/api/resource_types/gcp_services/view.py
+++ koku/api/resource_types/gcp_services/view.py
@@ -54,10 +54,7 @@ class GCPServiceView(generics.ListAPIView):
                 query_holder = query_holder.filter(project_id__in=gcp_project_access)
             if gcp_account_access:
                 query_holder = query_holder.filter(account_id__in=gcp_account_access)
-            if gcp_account_access == "*" or gcp_project_access == "*":
+            if gcp_account_access[0] == "*" or gcp_project_access[0] == "*":
                 return super().list(request)
             self.queryset = query_holder
-        else:
-            error_message = [{"insufficient access rights"}]
-            return Response(error_message, status=status.HTTP_403_FORBIDDEN)
         return super().list(request)
