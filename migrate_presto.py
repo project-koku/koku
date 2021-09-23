bbc7b168f99e20ad9e4dd5d0c9b200a7ddaed439
Ashley Aiken <aaiken@redhat.com>
2021-09-23 08:58:36 -0400

    fix lint

--- migrate_presto.py
+++ migrate_presto.py
@@ -1,7 +1,9 @@
 import logging
+
 import prestodb
 
 LOG = logging.getLogger(__name__)
+
 
 def check_schema(schema, presto_cursor):
     schema_check_sql = f"SHOW SCHEMAS LIKE '{schema}'"
@@ -11,12 +13,19 @@ def check_schema(schema, presto_cursor):
         return True
     return False
 
+
 def get_schemas(presto_cursor):
     schemas_sql = "SELECT schema_name FROM information_schema.schemata"
     presto_cursor.execute(schemas_sql)
     schemas = presto_cursor.fetchall()
-    schemas = [schema for listed_schema in schemas for schema in listed_schema if schema not in ['default', 'information_schema']]
+    schemas = [
+        schema
+        for listed_schema in schemas
+        for schema in listed_schema
+        if schema not in ["default", "information_schema"]
+    ]
     return schemas
+
 
 table_names = ["aws_line_items", "aws_line_items_daily", "aws_openshift_daily"]
 presto_conn = False
@@ -25,16 +34,19 @@ try:
     presto_cur = presto_conn.cursor()
     schemas = get_schemas(presto_cur)
     presto_conn.close()
-    for schema in schemas: 
+    for schema in schemas:
         presto_conn = prestodb.dbapi.connect(host="localhost", port=8080, user="admin", catalog="hive", schema=schema)
         presto_cur = presto_conn.cursor()
         if check_schema(schema, presto_cur):
             for table_name in table_names:
                 try:
                     print(f"Dropping table {table_name} from {schema}.")
-                    presto_cur.execute(f"""
+                    presto_cur.execute(
+                        f"""
                         DROP TABLE IF EXISTS {table_name}
-                    """, None)
+                    """,
+                        None,
+                    )
                     print("Success")
                     result = presto_cur.fetchall()
                     print(result)
