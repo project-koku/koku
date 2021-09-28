efe61c5e7a323675b8bf4f758b35a85528ae33d6
Michael Skarbek <mskarbek@redhat.com>
2021-09-28 10:24:53 -0400

    convert data to stringData in koku-aws secret so that we do not need to b64 encode the values

--- .github/scripts/check_clowdapp.sh
+++ .github/scripts/check_clowdapp.sh
@@ -9,7 +9,7 @@ curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack
 changed=`git diff --name-only HEAD`
 
 if [[ $changed == *"clowdapp"* ]]; then
-  echo "clowdapp.yaml needs to be updated through kustomize."
+  echo "clowdapp.yaml cannot be directly modified. Update the base.yaml or the patch files and run `make clowdapp`."
   exit_code=1
 else
   echo "clowdapp.yaml is up to date."
