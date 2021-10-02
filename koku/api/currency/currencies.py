c6ef0292fb2780439fb9c738c1db8dfa855b15a4
Boaz Shuster <boaz.shuster.github@gmail.com>
2021-10-02 23:05:32 +0300

    Update currencies symbols to ICU symbols
    This is required for showing the correct symbols on the UI with react-intl
    
    Signed-off-by: Boaz Shuster <boaz.shuster.github@gmail.com>

--- koku/api/currency/currencies.py
+++ koku/api/currency/currencies.py
@@ -9,92 +9,92 @@ CURRENCIES = [
     {
         "code": "AUD",
         "name": "Australian Dollar",
-        "symbol": "AU$",
-        "description": "AUD (AU$) - Australian Dollar"
+        "symbol": "A$",
+        "description": "AUD (A$) - Australian Dollar",
     },
     {
         "code": "CAD",
         "name": "Canadian Dollar",
-        "symbol": "C$",
-        "description": "CAD (C$) - Canadian Dollar"
+        "symbol": "CA$",
+        "description": "CAD (CA$) - Canadian Dollar",
     },
     {
         "code": "CHF",
         "name": "Swiss Franc",
         "symbol": "CHF",
-        "description": "CHF (CHF) - Swiss Franc"
+        "description": "CHF (CHF) - Swiss Franc",
     },
     {
         "code": "CNY",
         "name": "Chinese Yuan",
         "symbol": "CN\u00a5",
-        "description": "CNY (CN\u00a5) - Chinese Yuan"
+        "description": "CNY (CN\u00a5) - Chinese Yuan",
     },
     {
         "code": "DKK",
         "name": "Danish Krone",
-        "symbol": "Dkr",
-        "description": "DKK (Dkr) - Danish Krone"
+        "symbol": "DKK",
+        "description": "DKK (DKK) - Danish Krone",
     },
     {
         "code": "EUR",
         "name": "Euro",
         "symbol": "\u20ac",
-        "description": "EUR (\u20ac) - Euro"
+        "description": "EUR (\u20ac) - Euro",
     },
     {
         "code": "GBP",
         "name": "British Pound",
         "symbol": "\u00a3",
-        "description": "GBP (\u00a3) - British Pound"
+        "description": "GBP (\u00a3) - British Pound",
     },
     {
         "code": "HKD",
         "name": "Hong Kong Dollar",
         "symbol": "HK$",
-        "description": "HKD (HK$) - Hong Kong Dollar"
+        "description": "HKD (HK$) - Hong Kong Dollar",
     },
     {
         "code": "JPY",
         "name": "Japanese Yen",
         "symbol": "\u00a5",
-        "description": "JPY (\u00a5) - Japanese Yen"
+        "description": "JPY (\u00a5) - Japanese Yen",
     },
     {
         "code": "NOK",
         "name": "Norwegian Krone",
-        "symbol": "Nkr",
-        "description": "NOK (Nkr) - Norwegian Krone"
+        "symbol": "NOK",
+        "description": "NOK (NOK) - Norwegian Krone",
     },
     {
         "code": "NZD",
         "name": "New Zealand Dollar",
         "symbol": "NZ$",
-        "description": "NZD (NZ$) - New Zealand Dollar"
+        "description": "NZD (NZ$) - New Zealand Dollar",
     },
     {
         "code": "SEK",
         "name": "Swedish Krona",
-        "symbol": "Skr",
-        "description": "SEK (Skr) - Swedish Krona"
+        "symbol": "SEK",
+        "description": "SEK (SEK) - Swedish Krona",
     },
     {
         "code": "SGD",
         "name": "Singapore Dollar",
-        "symbol": "S$",
-        "description": "SGD (S$) - Singapore Dollar"
+        "symbol": "SGD",
+        "description": "SGD (SGD) - Singapore Dollar",
     },
     {
         "code": "USD",
         "name": "United States Dollar",
         "symbol": "$",
-        "description": "USD ($) - United States Dollar"
+        "description": "USD ($) - United States Dollar",
     },
     {
         "code": "ZAR",
         "name": "South African Rand",
-        "symbol": "R",
-        "description": "ZAR (R) - South African Rand"
+        "symbol": "ZAR",
+        "description": "ZAR (ZAR) - South African Rand",
     },
 ]
 # fmt: on
