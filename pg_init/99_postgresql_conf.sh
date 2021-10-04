456228ca6bf9a7cef26e6187bd946fac6db479cf
Michael Skarbek <mskarbek@redhat.com>
2021-10-04 14:32:45 -0400

    [COST-1847] unleash client for trino enablement (#3163)
    * add unleash client
    
    * add the `cost-trino-processor` flag check in `enable_trino_processing`
    
    * add UNLEASH_LOG_LEVEL

--- pg_init/99_postgresql_conf.sh
+++ pg_init/99_postgresql_conf.sh
@@ -5,3 +5,24 @@ psql -d $POSTGRES_DB -U $POSTGRES_USER -c "create extension if not exists pg_sta
 
 echo "Creating dbmonitor role..."
 psql -d $POSTGRES_DB -U $POSTGRES_USER -c "create role dbmonitor with inherit login encrypted password 'dbmonitor' superuser in role $POSTGRES_USER;"
+
+
+
+# from https://github.com/mrts/docker-postgresql-multiple-databases
+function create_user_and_database() {
+	local database=$1
+	echo "  Creating user and database '$database'"
+	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+	    CREATE USER $database;
+	    CREATE DATABASE $database;
+	    GRANT ALL PRIVILEGES ON DATABASE $database TO $database;
+EOSQL
+}
+
+if [ -n "$POSTGRES_MULTIPLE_DATABASES" ]; then
+	echo "Multiple database creation requested: $POSTGRES_MULTIPLE_DATABASES"
+	for db in $(echo $POSTGRES_MULTIPLE_DATABASES | tr ',' ' '); do
+		create_user_and_database $db
+	done
+	echo "Multiple databases created"
+fi
