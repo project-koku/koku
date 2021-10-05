2f90cfa9edbfe7cbbfb65779293543b15cbf3b6c
Michael Skarbek <mskarbek@redhat.com>
2021-10-05 16:50:34 -0400

    base prefix on port number

--- koku/koku/settings.py
+++ koku/koku/settings.py
@@ -137,10 +137,10 @@ if DEVELOPMENT:
     MIDDLEWARE.insert(5, "koku.dev_middleware.DevelopmentIdentityHeaderMiddleware")
 
 ### Feature Flags
-UNLEASH_PREFIX = ENVIRONMENT.get_value("UNLEASH_PREFIX", default="https://")
 UNLEASH_HOST = CONFIGURATOR.get_feature_flag_host()
 UNLEASH_PORT = CONFIGURATOR.get_feature_flag_port()
-UNLEASH_URL = f"{UNLEASH_PREFIX}{UNLEASH_HOST}:{UNLEASH_PORT}/api"
+UNLEASH_PREFIX = "https" if UNLEASH_PORT == 443 else "http"
+UNLEASH_URL = f"{UNLEASH_PREFIX}://{UNLEASH_HOST}:{UNLEASH_PORT}/api"
 UNLEASH_TOKEN = CONFIGURATOR.get_feature_flag_token()
 UNLEASH_CACHE_DIR = ENVIRONMENT.get_value("UNLEASH_CACHE_DIR", default=os.path.join(BASE_DIR, "..", ".unleash"))
 
