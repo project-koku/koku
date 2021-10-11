30865acecc372c293aa042513a306259bc7cc14a
HAP <hproctor@redhat.com>
2021-10-11 17:49:55 -0400

    Stop processing on unknown provider_uuid

--- koku/masu/processor/report_summary_updater.py
+++ koku/masu/processor/report_summary_updater.py
@@ -34,8 +34,14 @@ class ReportSummaryUpdaterError(Exception):
     pass
 
 
-class ReportSummaryUpdaterCloudError(Exception):
+class ReportSummaryUpdaterCloudError(ReportSummaryUpdaterError):
     """Report Summary Updater Cloud Error."""
+
+    pass
+
+
+class ReportSummaryUpdaterProviderNotFoundError(ReportSummaryUpdaterError):
+    """Provider not found error"""
 
     pass
 
@@ -63,8 +69,12 @@ class ReportSummaryUpdater:
         with ProviderDBAccessor(self._provider_uuid) as provider_accessor:
             self._provider = provider_accessor.get_provider()
 
+        LOG.warning(f"self._provider_uuid = {self._provider_uuid}")
+        LOG.warning(f"bool(self._provider) = {bool(self._provider)}")
         if not self._provider:
-            raise ReportSummaryUpdaterError("Provider not found.")
+            raise ReportSummaryUpdaterProviderNotFoundError(
+                f"Provider data for uuid '{self._provider_uuid}' not found."
+            )
 
         try:
             self._updater, self._ocp_cloud_updater = self._set_updater()
