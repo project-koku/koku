818ec1764c7b4f058d07d5919c9202b34267e135
Boaz Shuster <ripcurld.github@gmail.com>
2021-10-13 01:05:25 +0300

    Update koku/cost_models/serializers.py
    Co-authored-by: HAP <59652772+Red-HAP@users.noreply.github.com>

--- koku/cost_models/serializers.py
+++ koku/cost_models/serializers.py
@@ -446,11 +446,12 @@ class CostModelSerializer(serializers.Serializer):
     def validate(self, data):
         """Validate that the source type is acceptable."""
         # The cost model has markup, no rates, and is for a valid non-OpenShift source type
-        if self.instance and data.get("currency") and self.instance.currency != data.get("currency"):
+        data_currency = data.get("currency")
+        if self.instance and data_currency and self.instance.currency != data_currency:
             raise serializers.ValidationError(
-                f"{self.instance.currency} currency can not be updated to {data.get('currency')}."
+                f"{self.instance.currency} currency can not be updated to {data_currency}."
             )
-        if data.get("currency") is None and self.instance is None:
+        if data_currency is None and self.instance is None:
             data["currency"] = self.context["default_currency"]
         source_type = data.get("source_type")
         if source_type and Provider.PROVIDER_CASE_MAPPING.get(source_type.lower()):
