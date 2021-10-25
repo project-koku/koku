6e24e6779cc7d34e71b7a6dcea047d9e6f1073ee
shannon-donahue <49072851+shannon-donahue@users.noreply.github.com>
2021-10-25 14:23:37 -0400

    [Cost -1861] - adding a cost_type parameter to AWS report api  (#3180)
    * table added for user settings beginning work to lay out provider map to include cost_type
    
    * work with the api report pages need to pull in updated db
    
    * fix default, figuring out where provider map used to add passing cost_type
    
    * fixed forecast page to inccorporate aws cost_type, need to fix more for unit test failures
    
    * [COST-1802]-[COST-1806] AWS savings plan (#3130)
    
    * pick commits from COST-1802
    
    * update requirements
    
    * script for aws savings plan migration adding column to tables
    
    * fixing the views to be in version files under version date folder
    
    * add mat views migrations
    
    * fix for error in logs Column 'savingsplan_effective_cost' cannot be resolved
    
    * fix pre-commit
    
    * changes for unit test error
    
    * respond to changes in main
    
    * fixing some migration conflicts
    
    * first attempt at presto migrations
    
    * update the schema name away from hack
    
    * update to check if schema exists
    
    * update migration to contain helpful log
    
    * delete migration add script
    
    * fix lint
    
    * pull from env vars
    
    * first pass at a cji
    
    * fix lint
    
    * job updates
    
    * move script for job
    
    * update bash script
    
    * update clowdapp
    
    * fix logger
    
    * updates to job name:
    
    * update base.yaml
    
    * fix clowdapp
    
    * respond to PR comments
    
    * switch to trino
    
    * switch back to altering the table
    
    * revert back to drop table
    
    * test moving cji to template
    
    * update template
    
    Co-authored-by: Ashley Aiken <aaiken@redhat.com>
    Co-authored-by: Michael Skarbek <mskarbek@redhat.com>
    
    * fixing some merge errors
    
    * unit tests for cost_type in query param serializer
    
    * update the script (#3175)
    
    * log the result and update CJI name (#3179)
    
    * fix lint and rebase
    
    * updating a few files at a time to make sure they are in date with main
    
    * should fix differences in clowdapp.yaml
    
    * fix Koku-cji to be up to date
    
    * more file fixes to update
    
    * more file fixes to update
    
    * hopefully finale file fixes to update
    
    * minor fix
    
    * Allow blended cost (#3188)
    
    * undoing file changes from earlier because now git is being confusing
    
    * hopefully the last commit for git acting strange
    
    * add cost_type to aws tags calling aws provider map
    
    * fixed unit tests for aws test_views and tests_queries
    
    * commented out code to be added later for default cost_type in report
    
    * add cost_type param to aws report endpoint param choices for openapi spec
    
    * added default from user settings table for api cost report
    
    * fixing a few files that were changed for testing
    
    * last minor detail fix
    
    * fixed most unit tests, still one more to fix
    
    * fixing a few more tests, need to fix 'Mock' not iterable issue
    
    * add in param tests
    
    * fix serializer tests
    
    * test invalid cost type serializer
    
    * fixing missed doc for function
    
    * fixing miss-type
    
    * more unit tests for cost_type
    
    * change tests to call validate for code cov
    
    * reverting settings url to original form
    
    Co-authored-by: Ashley Aiken <aaiken@redhat.com>
    Co-authored-by: Michael Skarbek <mskarbek@redhat.com>
    Co-authored-by: Andrew Berglund <aberglun@redhat.com>

--- koku/reporting/provider/aws/sql/views/_20210910/reporting_aws_cost_summary_by_region_20210910.sql
+++ koku/reporting/provider/aws/sql/views/_20210910/reporting_aws_cost_summary_by_region_20210910.sql
@@ -11,6 +11,7 @@ CREATE MATERIALIZED VIEW reporting_aws_cost_summary_by_region AS(
         region,
         availability_zone,
         sum(unblended_cost) as unblended_cost,
+        sum(blended_cost) as blended_cost,
         sum(savingsplan_effective_cost) as savingsplan_effective_cost,
         sum(markup_cost) as markup_cost,
         max(currency_code) as currency_code,
