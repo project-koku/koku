8afa453598ead0f3fba020bd13530442deb91737
Michael Skarbek <mskarbek@redhat.com>
2021-10-26 19:18:02 -0400

    remove variable from pr-check

--- pr_check.sh
+++ pr_check.sh
@@ -17,7 +17,6 @@ export IQE_PLUGINS="cost_management"
 export IQE_MARKER_EXPRESSION="cost_smoke"
 export IQE_FILTER_EXPRESSION="test_api"
 export IQE_CJI_TIMEOUT="90m"
-export GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 export GIT_COMMIT=$(git rev-parse HEAD)
 
 set -ex
