d682b7d999e93f7fd4086e5289e39014fb92b03f
Michael Skarbek <mskarbek@redhat.com>
2021-11-03 20:54:45 -0400

    update subset of ui tables

--- koku/masu/processor/ocp/ocp_cloud_parquet_summary_updater.py
+++ koku/masu/processor/ocp/ocp_cloud_parquet_summary_updater.py
@@ -22,6 +22,7 @@ from masu.util.ocp.common import get_cluster_alias_from_cluster_id
 from masu.util.ocp.common import get_cluster_id_from_provider
 from reporting.provider.aws.openshift.models import OCPAWSCostLineItemProjectDailySummary
 from reporting.provider.azure.openshift.models import OCPAzureCostLineItemProjectDailySummary
+from reporting.provider.ocp.models import UI_SUMMARY_TABLES_SUBSET
 
 LOG = logging.getLogger(__name__)
 
@@ -111,7 +112,7 @@ class OCPCloudParquetReportSummaryUpdater(OCPCloudReportSummaryUpdater):
                 ocp_accessor.populate_ocp_on_all_project_daily_summary("aws", sql_params)
                 ocp_accessor.populate_ocp_on_all_daily_summary("aws", sql_params)
 
-                ocp_accessor.populate_ui_summary_tables(start, end, openshift_provider_uuid)
+                ocp_accessor.populate_ui_summary_tables(start, end, openshift_provider_uuid, UI_SUMMARY_TABLES_SUBSET)
 
     def update_azure_summary_tables(self, openshift_provider_uuid, azure_provider_uuid, start_date, end_date):
         """Update operations specifically for OpenShift on Azure."""
@@ -195,4 +196,4 @@ class OCPCloudParquetReportSummaryUpdater(OCPCloudReportSummaryUpdater):
                 ocp_accessor.populate_ocp_on_all_project_daily_summary("azure", sql_params)
                 ocp_accessor.populate_ocp_on_all_daily_summary("azure", sql_params)
 
-                ocp_accessor.populate_ui_summary_tables(start, end, openshift_provider_uuid)
+                ocp_accessor.populate_ui_summary_tables(start, end, openshift_provider_uuid, UI_SUMMARY_TABLES_SUBSET)
