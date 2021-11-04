2f4fe06b30a637f94edbfa1f8f1823de8d433a08
Michael Skarbek <mskarbek@redhat.com>
2021-11-04 12:19:09 -0400

    CJI to copy OCP mat view data into new partables

--- scripts/copy_static_perspective_data.py
+++ scripts/copy_static_perspective_data.py
@@ -18,7 +18,7 @@ logging.basicConfig(
     datefmt="%m/%d/%Y %I:%M:%S %p",
     level=getattr(logging, os.environ.get("KOKU_LOG_LEVEL", "INFO")),
 )
-LOG = logging.getLogger(os.path.basename(sys.argv[0] or "copy_aws_matview_data_console"))
+LOG = logging.getLogger(os.path.basename(sys.argv[0] or "copy_ocp_matview_data_console"))
 
 
 def connect():
@@ -43,7 +43,7 @@ def _execute(conn, sql, params=None):
     return cur
 
 
-def get_aws_matviews(conn):
+def get_ocp_matviews(conn):
     sql = """
 with matview_info as (
 select m.relname::text as "matview_name",
@@ -54,7 +54,7 @@ select m.relname::text as "matview_name",
    and mc.attnum > 0
  where m.relkind = 'm'
    and m.relnamespace = 'template0'::regnamespace
-   and m.relname ~ '^reporting_aws'
+   and m.relname ~ '^reporting_ocp'
  group
     by m.relname
 ),
@@ -67,7 +67,7 @@ select t.relname::text as "partable_name",
    and tc.attnum > 0
  where t.relkind = 'p'
    and t.relnamespace = 'template0'::regnamespace
-   and t.relname ~ '^reporting_aws.*_p$'
+   and t.relname ~ '^reporting_ocp.*_p$'
  group
     by t.relname
 )
@@ -91,7 +91,7 @@ select t.schema_name
   join public.api_customer c
     on c.schema_name = t.schema_name
  where t.schema_name ~ '^acct'
-   and exists (select 1 from public.api_provider p where p.customer_id = c.id and p.type ~ '^AWS');
+   and exists (select 1 from public.api_provider p where p.customer_id = c.id and p.type ~ '^OCP');
 """
     LOG.info("Getting all customer schemata...")
     return [r["schema_name"] for r in _execute(conn, sql).fetchall()]
@@ -225,7 +225,7 @@ def data_exists(conn, schema_name, partable_name):
     return res["data_exists"]
 
 
-def process_aws_matviews(conn, schemata, matviews):  # noqa
+def process_ocp_matviews(conn, schemata, matviews):  # noqa
     LOG.info("This script is part of Jira ticket COST-1976 https://issues.redhat.com/browse/COST-1976")
     LOG.info("This script is speficically for sub-task COST-1979 https://issues.redhat.com/browse/COST-1979")
 
@@ -297,11 +297,11 @@ def process_aws_matviews(conn, schemata, matviews):  # noqa
 
 def main():
     with connect() as conn:
-        matviews = get_aws_matviews(conn)
+        matviews = get_ocp_matviews(conn)
         schemata = get_customer_schemata(conn)
         conn.rollback()  # close any open tx from selects
 
-        process_aws_matviews(conn, schemata, matviews)
+        process_ocp_matviews(conn, schemata, matviews)
 
 
 if __name__ == "__main__":
