41bcd1823b2c3193f8044f85a50a52684a64d739
Ashley Aiken <aaiken@redhat.com>
2021-11-11 10:50:44 -0500

    make cost_type unsupported param for ocp aws

--- koku/api/report/aws/openshift/serializers.py
+++ koku/api/report/aws/openshift/serializers.py
@@ -104,3 +104,11 @@ class OCPAWSQueryParamSerializer(awsser.QueryParamSerializer):
             error = {"delta": f'"{value}" is not a valid choice.'}
             raise serializers.ValidationError(error)
         return value
+
+    def validate(self, data):
+        """Override the super validate to not allow cost_type."""
+        if data.get("cost_type"):
+            error = {"cost_type": ["Unsupported parameter or invalid value"]}
+            raise serializers.ValidationError(error)
+        super().validate(data)
+        return data
