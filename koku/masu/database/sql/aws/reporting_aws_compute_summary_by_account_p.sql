f9a71064e4453972fea9b513fdd882dcd9637143
Ashley Aiken <aaiken@redhat.com>
2021-11-11 09:01:12 -0500

    update sql

--- koku/masu/database/sql/aws/reporting_aws_compute_summary_by_account_p.sql
+++ koku/masu/database/sql/aws/reporting_aws_compute_summary_by_account_p.sql
@@ -40,6 +40,8 @@ INSERT INTO {{schema | sqlsafe}}.reporting_aws_compute_summary_by_account_p (
         c.blended_cost,
         c.savingsplan_effective_cost,
         c.markup_cost,
+        c.markup_cost_blended,
+        c.markup_cost_savingsplan,
         c.currency_code,
         c.source_uuid
     FROM (
