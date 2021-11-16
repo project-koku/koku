4e2fc7bab1dddae9c7fde807ef5cd7d4634fb802
HAP <hproctor@redhat.com>
2021-11-16 11:12:38 -0500

    Tune up migration job script

--- scripts/run_django_migrations.sh
+++ scripts/run_django_migrations.sh
@@ -3,16 +3,21 @@
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
 # The "scripts" dir should be a sibling to the "koku" dir that actually contains the app code
-cd ../koku
+cd $(dirname ${SCRIPT_DIR})/koku
 
 # Leave _APP and _MIGRATION unset to execute any migrations for all apps
-# Put a registered app name here to constrain migrations to that app only
-_APP=
-# Put a migration number (or full name minus the '.py')
-# to execute up to (and including) the specified migration
+# Use a registered app name as arg 1 to constrain migrations to that app only
+_APP="$1"
+# Use a migration number (or full name minus the '.py')
+# as arg 2 to execute up to (and including) the specified migration
 # or to execute in reverse down to (but excluding) the specified migration
-_MIGRATION=
+_MIGRATION="$2"
 
-DJANGO_READ_DOT_ENV_FILE=True python3 ./manage.py migrate_schemas $(_APP) $(_MIGRATION)
+
+if [[ "${DEVELOPMENT}" == "True" ]]; then
+    export DJANGO_READ_DOT_ENV_FILE=True
+fi
+
+python3 ./manage.py migrate_schemas ${_APP} ${_MIGRATION}
 
 exit $?
