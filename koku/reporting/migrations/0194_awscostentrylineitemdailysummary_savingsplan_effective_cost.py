5c3fb4509ec72371288b56300d7b8cadd734851a
Michael Skarbek <mskarbek@redhat.com>
2021-12-08 10:25:17 -0500

    fix migration

--- koku/reporting/migrations/0194_awscostentrylineitemdailysummary_savingsplan_effective_cost.py
+++ koku/reporting/migrations/0194_awscostentrylineitemdailysummary_savingsplan_effective_cost.py
@@ -6,22 +6,8 @@ from django.db import migrations
 from django.db import models
 
 
-def add_aws_views(apps, schema_editor):
-    """Create the AWS Materialized views from files."""
-    version = "_20210910"
-    views = {
-        f"sql/views/{version}/reporting_aws_compute_summary": ["", "_by_account", "_by_region", "_by_service"],
-        f"sql/views/{version}/reporting_aws_cost_summary": ["", "_by_account", "_by_region", "_by_service"],
-        f"sql/views/{version}/reporting_aws_storage_summary": ["", "_by_account", "_by_region", "_by_service"],
-        f"sql/views/{version}/reporting_aws_database_summary": [""],
-        f"sql/views/{version}/reporting_aws_network_summary": [""],
-    }
-    for base_path, view_tuple in views.items():
-        for view in view_tuple:
-            view_sql = pkgutil.get_data("reporting.provider.aws", f"{base_path}{view}{version}.sql")
-            view_sql = view_sql.decode("utf-8")
-            with connection.cursor() as cursor:
-                cursor.execute(view_sql)
+def no_op(apps, schema_editor):
+    pass
 
 
 class Migration(migrations.Migration):
@@ -34,5 +20,5 @@ class Migration(migrations.Migration):
             name="savingsplan_effective_cost",
             field=models.DecimalField(decimal_places=9, max_digits=24, null=True),
         ),
-        migrations.RunPython(add_aws_views),
+        migrations.RunPython(no_op),
     ]
