8fe280f0d2270e5817c42d600c05787544f33a5b
Corey Goodfred <cgoodfre@redhat.com>
2021-12-15 08:12:52 -0500

    uptick migration that will be in separate PR when done

--- koku/reporting/migrations/0213_unitusage_ocpgcp.py
+++ koku/reporting/migrations/0213_unitusage_ocpgcp.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class Migration(migrations.Migration):
 
-    dependencies = [("reporting", "0201_ocp_partables")]
+    dependencies = [("reporting", "0212_auto_20211203_1640")]
 
     operations = [
         migrations.AddField(
