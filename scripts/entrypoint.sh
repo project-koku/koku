26c456fb681ad1a6440a40eb1a077f3814b3c09c
Michael Skarbek <mskarbek@redhat.com>
2021-12-23 16:40:15 -0500

    minimal dockerfiles

---
+++ scripts/entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+# enable container user without requiring nss_wrapper
+if [ `id -u` -ge 10000 ]; then
+    cat /etc/passwd | sed -e "s/^koku:/builder:/" > /tmp/passwd
+    echo "koku:x:`id -u`:`id -g`:Koku User:${HOME}:/bin/bash" >> /tmp/passwd
+    cat /tmp/passwd > /etc/passwd
+    rm /tmp/passwd
+fi
+
+if [[ -z "${ACG_CONFIG}" ]]; then
+    CLOWDER_PORT=8000
+else
+    CLOWDER_PORT=`python -c 'import app_common_python; print(app_common_python.LoadedConfig.publicPort)'`
+fi
+
+cd $APP_HOME
+gunicorn koku.wsgi --bind=0.0.0.0:$CLOWDER_PORT --access-logfile=- --config gunicorn_conf.py --preload
