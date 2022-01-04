b1f3c7ec6195a1489016d29e10bf1c4f753abe7b
Michael Skarbek <mskarbek@redhat.com>
2022-01-04 11:51:44 -0500

    make GIT_COMMIT a build arg

--- koku/koku/settings.py
+++ koku/koku/settings.py
@@ -34,6 +34,8 @@ from .env import ENVIRONMENT
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Version info
+GIT_COMMIT = ENVIRONMENT.get_value("GIT_COMMIT", default="local-dev")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
