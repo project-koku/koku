6b3cc2c3620072f6a07955186aaeac7e7e059bdd
Doug Donahue <ddonahue@redhat.com>
2022-01-05 12:01:09 -0500

    update unit test to use 2 dates

--- koku/masu/test/api/test_hcs_report_data.py
+++ koku/masu/test/api/test_hcs_report_data.py
@@ -3,6 +3,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 """Test the hcs_report_data endpoint view."""
+from datetime import timedelta
 from unittest.mock import call
 from unittest.mock import patch
 
@@ -27,17 +28,19 @@ class HCSDataTests(TestCase):
         """Test the GET report_data endpoint."""
         provider_type = Provider.PROVIDER_AWS
         mock_accessor.return_value.__enter__.return_value.get_type.return_value = provider_type
-        start_date = DateHelper().today.date().strftime("%Y-%m-%d")
+        end_date = DateHelper().today
+        start_date = end_date - timedelta(days=1)
         params = {
             "schema": "acct10001",
-            "start_date": start_date,
+            "start_date": start_date.date().strftime("%Y-%m-%d"),
+            "end_date": end_date.date().strftime("%Y-%m-%d"),
             "provider_uuid": "6e212746-484a-40cd-bba0-09a19d132d64",
         }
         expected_key = "HCS Report Data Task ID"
 
         response = self.client.get(reverse(self.ENDPOINT), params)
         body = response.json()
-        expected_calls = [call(params["start_date"], params["start_date"])]
+        expected_calls = [call(params["start_date"], params["end_date"])]
         self.assertEqual(response.status_code, 200)
         self.assertIn(expected_key, body)
         mock_celery.s.assert_has_calls(expected_calls, any_order=True)
