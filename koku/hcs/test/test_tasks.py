d84727536243ed0d449003ca7409c85399f0b709
Doug Donahue <57504257+ddonahue007@users.noreply.github.com>
2022-01-05 14:38:37 -0500

    [Cost 2134] -  Masu API to kick off HCS report  (#3349)
    Masu endpoint(s) for HCS integration

--- koku/hcs/test/test_tasks.py
+++ koku/hcs/test/test_tasks.py
@@ -14,7 +14,7 @@ LOG = logging.getLogger(__name__)
 
 
 class TestHCSTasks(HCSTestCase):
-    """Test cases for Processor Celery tasks."""
+    """Test cases for HCS Celery tasks."""
 
     @classmethod
     def setUpClass(cls):
@@ -24,7 +24,7 @@ class TestHCSTasks(HCSTestCase):
         cls.yesterday = cls.today - timedelta(days=1)
 
     def test_get_report_dates(self):
-        """Test raising download warning is handled."""
+        """Test with start and end dates provided"""
         with self.assertLogs("hcs.tasks", "INFO") as _logs:
             start_date = self.yesterday
             end_date = self.today
@@ -32,8 +32,16 @@ class TestHCSTasks(HCSTestCase):
 
             self.assertIn(f"OUTPUT FROM HCS TASK, Start-date: {start_date}, End-date: {end_date}", _logs.output[0])
 
+    def test_get_report_no_start_date(self):
+        """Test to with no start or end dates provided"""
+        with self.assertLogs("hcs.tasks", "INFO") as _logs:
+            start_date = self.today
+            collect_hcs_report_data()
+
+            self.assertIn(f"OUTPUT FROM HCS TASK, Start-date: {start_date}", _logs.output[0])
+
     def test_get_report_no_end_date(self):
-        """Test raising download warning is handled."""
+        """TTest to with no start end provided"""
         with self.assertLogs("hcs.tasks", "INFO") as _logs:
             start_date = self.yesterday
             collect_hcs_report_data(start_date)
--- koku/masu/api/views.py
+++ koku/masu/api/views.py
@@ -9,6 +9,7 @@ from masu.api.download import download_report
 from masu.api.enabled_tags import enabled_tags
 from masu.api.expired_data import expired_data
 from masu.api.gcp_invoice_monthly_cost import gcp_invoice_monthly_cost
+from masu.api.hcs_report_data import hcs_report_data
 from masu.api.manifest.views import ManifestView
 from masu.api.report_data import report_data
 from masu.api.running_celery_tasks import celery_queue_lengths
