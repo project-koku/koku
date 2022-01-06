1073b0a9cbf5578ab55d334096b474bb7f1cd6de
Andrew Berglund <aberglun@redhat.com>
2022-01-06 13:14:17 -0500

    Use lpad for much victory

--- koku/masu/database/presto_sql/reporting_ocpusagelineitem_daily_summary.sql
+++ koku/masu/database/presto_sql/reporting_ocpusagelineitem_daily_summary.sql
@@ -454,6 +454,6 @@ SELECT cast(uuid as UUID),
 FROM hive.{{schema | sqlsafe}}.reporting_ocpusagelineitem_daily_summary AS lids
 WHERE lids.source = {{source}}
     AND lids.year = {{year}}
-    AND (lids.month = replace(ltrim(replace({{month}}, '0', ' ')),' ', '0') OR lids.month = {{month}})
+    AND lpad(lids.month, 2, '0') = {{month}} -- Zero pad the month when fewer than 2 characters
     AND lids.day IN ({{days}})
 ;
