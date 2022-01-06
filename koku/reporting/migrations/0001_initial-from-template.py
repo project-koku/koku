34079f3503b6a3ad4945df483bea4c1ecddef22b
Michael Skarbek <mskarbek@redhat.com>
2022-01-06 11:57:01 -0800

    [COST-2146,COST-2050,COST-2051] - remove OCP-on-X mat view imports (#3370)

--- koku/reporting/migrations/0001_initial-from-template.py
+++ koku/reporting/migrations/0001_initial-from-template.py
@@ -17,8 +17,6 @@ import reporting.partition.models
 from koku import migration_sql_helpers as msh
 from koku.database import set_partitioned_schema_editor
 from koku.database import unset_partitioned_schema_editor
-from reporting.provider.aws.openshift.models import VIEWS as OCP_ON_AWS_MATERIALIZED_VIEWS
-from reporting.provider.azure.openshift.models import VIEWS as OCP_ON_AZURE_MATERIALIZED_VIEWS
 
 
 LOG = logging.getLogger(__name__)
@@ -52,21 +50,7 @@ def unset_partitioned_mode(apps, schema_editor):
 
 
 def apply_views(apps, schema_editor):
-    conn = schema_editor.connection
-
-    for view in OCP_ON_AWS_MATERIALIZED_VIEWS:
-        LOG.info(f"Applying materialized view {view}")
-        LOG.info(f"Path: reporting/provider/aws/openshift/sql/views/{view}.sql")
-        view_sql = pkgutil.get_data("reporting.provider.aws.openshift", f"sql/views/{view}.sql").decode("utf-8")
-        with conn.cursor() as cur:
-            cur.execute(view_sql)
-
-    for view in OCP_ON_AZURE_MATERIALIZED_VIEWS:
-        LOG.info(f"Applying materialized view {view}")
-        LOG.info(f"Path: reporting/provider/azure/openshift/sql/views/{view}.sql")
-        view_sql = pkgutil.get_data("reporting.provider.azure.openshift", f"sql/views/{view}.sql").decode("utf-8")
-        with conn.cursor() as cur:
-            cur.execute(view_sql)
+    pass
 
 
 class Migration(migrations.Migration):
