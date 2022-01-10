14e0cccc2de0545454e58be6e3cafad379783a7c
Andrew Berglund <aberglun@redhat.com>
2022-01-10 14:47:18 -0500

    For deletes use direct reference to month column

--- koku/masu/database/aws_report_db_accessor.py
+++ koku/masu/database/aws_report_db_accessor.py
@@ -387,7 +387,7 @@ class AWSReportDBAccessor(SQLScriptAtomicExecutorMixin, ReportDBAccessorBase):
                     WHERE aws_source = '{aws_source}'
                     AND ocp_source = '{ocp_source}'
                     AND year = '{year}'
-                    AND lpad(month, 2, '0') = '{month}'
+                    AND (month = replace(ltrim(replace('{{month}}', '0', ' ')),' ', '0') OR month = '{{month}}')
                     AND day = '{day}';"""
                 final_sql_list.append(sql)
             final_sql = "".join(final_sql_list)
