4640204079b1c341c9f56f9f49e0f6fafd4dc878
Andrew Berglund <aberglun@redhat.com>
2022-01-10 15:23:15 -0500

    For deletes use direct reference to month column (#3386)
    * For deletes of Hive partitions, use direct reference to month column

--- koku/masu/database/ocp_report_db_accessor.py
+++ koku/masu/database/ocp_report_db_accessor.py
@@ -653,7 +653,7 @@ class OCPReportDBAccessor(SQLScriptAtomicExecutorMixin, ReportDBAccessorBase):
                 DELETE FROM hive.{self.schema}.{table}
                 WHERE source = '{source}'
                 AND year = '{year}'
-                AND lpad(month, 2, '0') = '{month}'
+                AND (month = replace(ltrim(replace('{month}', '0', ' ')),' ', '0') OR month = '{month}')
                 AND day = '{day}';
                 """
                 final_sql_list.append(sql)
