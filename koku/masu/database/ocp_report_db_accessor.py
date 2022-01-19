6035f9643656aa8b7a29c3b48a4834ea5f534ce9
Luke Couzens <lcouzens@redhat.com>
2022-01-19 09:53:59 +0000

    Adding support for effective cost in cost updater

--- koku/masu/database/ocp_report_db_accessor.py
+++ koku/masu/database/ocp_report_db_accessor.py
@@ -23,6 +23,7 @@ from django.db.models import F
 from django.db.models import Sum
 from django.db.models import Value
 from django.db.models.functions import Coalesce
+from django.db.models.functions import Greatest
 from jinjasql import JinjaSql
 from tenant_schemas.utils import schema_context
 from trino.exceptions import TrinoExternalError
@@ -2031,7 +2032,13 @@ class OCPReportDBAccessor(SQLScriptAtomicExecutorMixin, ReportDBAccessorBase):
                     Value(infrastructure_rates.get("cpu_core_usage_per_hour", 0), output_field=DecimalField())
                     * Coalesce(F("pod_usage_cpu_core_hours"), Value(0), output_field=DecimalField())
                     + Value(infrastructure_rates.get("cpu_core_request_per_hour", 0), output_field=DecimalField())
-                    * Coalesce(F("pod_request_cpu_core_hours"), Value(0), output_field=DecimalField()),
+                    * Coalesce(F("pod_request_cpu_core_hours"), Value(0), output_field=DecimalField())
+                    + Value(infrastructure_rates.get("cpu_core_effective_per_hour", 0), output_field=DecimalField())
+                    * Coalesce(
+                        Greatest(F("pod_usage_cpu_core_hours"), F("pod_request_cpu_core_hours")),
+                        Value(0),
+                        output_field=DecimalField(),
+                    ),
                     0,
                     output_field=DecimalField(),
                 ),
@@ -2040,7 +2047,13 @@ class OCPReportDBAccessor(SQLScriptAtomicExecutorMixin, ReportDBAccessorBase):
                     Value(infrastructure_rates.get("memory_gb_usage_per_hour", 0), output_field=DecimalField())
                     * Coalesce(F("pod_usage_memory_gigabyte_hours"), Value(0), output_field=DecimalField())
                     + Value(infrastructure_rates.get("memory_gb_request_per_hour", 0), output_field=DecimalField())
-                    * Coalesce(F("pod_request_memory_gigabyte_hours"), Value(0), output_field=DecimalField()),
+                    * Coalesce(F("pod_request_memory_gigabyte_hours"), Value(0), output_field=DecimalField())
+                    + Value(infrastructure_rates.get("memory_gb_effective_per_hour", 0), output_field=DecimalField())
+                    * Coalesce(
+                        Greatest(F("pod_usage_memory_gigabyte_hours"), F("pod_request_memory_gigabyte_hours")),
+                        Value(0),
+                        output_field=DecimalField(),
+                    ),
                     0,
                     output_field=DecimalField(),
                 ),
@@ -2060,7 +2073,13 @@ class OCPReportDBAccessor(SQLScriptAtomicExecutorMixin, ReportDBAccessorBase):
                     Value(supplementary_rates.get("cpu_core_usage_per_hour", 0), output_field=DecimalField())
                     * Coalesce(F("pod_usage_cpu_core_hours"), Value(0), output_field=DecimalField())
                     + Value(supplementary_rates.get("cpu_core_request_per_hour", 0), output_field=DecimalField())
-                    * Coalesce(F("pod_request_cpu_core_hours"), Value(0), output_field=DecimalField()),
+                    * Coalesce(F("pod_request_cpu_core_hours"), Value(0), output_field=DecimalField())
+                    + Value(supplementary_rates.get("cpu_core_effective_per_hour", 0), output_field=DecimalField())
+                    * Coalesce(
+                        Greatest(F("pod_usage_cpu_core_hours"), F("pod_request_cpu_core_hours")),
+                        Value(0),
+                        output_field=DecimalField(),
+                    ),
                     0,
                     output_field=DecimalField(),
                 ),
@@ -2069,7 +2088,13 @@ class OCPReportDBAccessor(SQLScriptAtomicExecutorMixin, ReportDBAccessorBase):
                     Value(supplementary_rates.get("memory_gb_usage_per_hour", 0), output_field=DecimalField())
                     * Coalesce(F("pod_usage_memory_gigabyte_hours"), Value(0), output_field=DecimalField())
                     + Value(supplementary_rates.get("memory_gb_request_per_hour", 0), output_field=DecimalField())
-                    * Coalesce(F("pod_request_memory_gigabyte_hours"), Value(0), output_field=DecimalField()),
+                    * Coalesce(F("pod_request_memory_gigabyte_hours"), Value(0), output_field=DecimalField())
+                    + Value(supplementary_rates.get("memory_gb_effective_per_hour", 0), output_field=DecimalField())
+                    * Coalesce(
+                        Greatest(F("pod_usage_memory_gigabyte_hours"), F("pod_request_memory_gigabyte_hours")),
+                        Value(0),
+                        output_field=DecimalField(),
+                    ),
                     0,
                     output_field=DecimalField(),
                 ),
@@ -2108,8 +2133,10 @@ class OCPReportDBAccessor(SQLScriptAtomicExecutorMixin, ReportDBAccessorBase):
         metric_usage_type_map = {
             "cpu_core_usage_per_hour": "cpu",
             "cpu_core_request_per_hour": "cpu",
+            "cpu_core_effective_per_hour": "cpu",
             "memory_gb_usage_per_hour": "memory",
             "memory_gb_request_per_hour": "memory",
+            "memory_gb_effective_per_hour": "memory",
             "storage_gb_usage_per_month": "storage",
             "storage_gb_request_per_month": "storage",
         }
@@ -2189,8 +2216,10 @@ class OCPReportDBAccessor(SQLScriptAtomicExecutorMixin, ReportDBAccessorBase):
         metric_usage_type_map = {
             "cpu_core_usage_per_hour": "cpu",
             "cpu_core_request_per_hour": "cpu",
+            "cpu_core_effective_per_hour": "cpu",
             "memory_gb_usage_per_hour": "memory",
             "memory_gb_request_per_hour": "memory",
+            "memory_gb_effective_per_hour": "memory",
             "storage_gb_usage_per_month": "storage",
             "storage_gb_request_per_month": "storage",
         }
