cc2586945dbe8a9ce44f6f69788ab41340484324
Corey Goodfred <cgoodfre@redhat.com>
2022-01-26 10:34:25 -0500

    parse these labels again

--- koku/masu/database/presto_sql/gcp/openshift/reporting_ocpgcpcostlineitem_daily_summary.sql
+++ koku/masu/database/presto_sql/gcp/openshift/reporting_ocpgcpcostlineitem_daily_summary.sql
@@ -186,7 +186,7 @@ SELECT gcp.uuid as gcp_uuid,
     max(ocp.cluster_capacity_cpu_core_hours) as cluster_capacity_cpu_core_hours,
     max(ocp.cluster_capacity_memory_gigabyte_hours) as cluster_capacity_memory_gigabyte_hours,
     max(ocp.volume_labels) as volume_labels,
-    max(gcp.labels) as tags,
+    max(json_format(json_parse(gcp.labels))) as tags,
     row_number() OVER (partition by gcp.uuid) as project_rank,
     1 as data_source_rank
 FROM hive.{{schema | sqlsafe}}.gcp_openshift_daily as gcp
@@ -288,7 +288,7 @@ SELECT gcp.uuid as gcp_uuid,
     max(ocp.cluster_capacity_cpu_core_hours) as cluster_capacity_cpu_core_hours,
     max(ocp.cluster_capacity_memory_gigabyte_hours) as cluster_capacity_memory_gigabyte_hours,
     max(ocp.volume_labels) as volume_labels,
-    max(gcp.labels) as tags,
+    max(json_format(json_parse(gcp.labels))) as tags,
     row_number() OVER (partition by gcp.uuid, ocp.namespace) as project_rank,
     row_number() OVER (partition by gcp.uuid, ocp.namespace) as data_source_rank
 FROM hive.{{schema | sqlsafe}}.gcp_openshift_daily as gcp
