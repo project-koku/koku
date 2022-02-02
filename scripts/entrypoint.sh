587a44a43f172c0bff56b4e6cd00d4e2079acb00
Michael Skarbek <mskarbek@redhat.com>
2022-02-02 17:27:49 -0500

    move logic into the gunicorn config

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
 set -e
-
-if [[ -z "${ACG_CONFIG}" ]]; then
-    CLOWDER_PORT=8000
-else
-    if [ "$MASU" = true ] || [ "$SOURCES" = true ]; then
-        CLOWDER_PORT=$(python -c 'import app_common_python; print(app_common_python.LoadedConfig.privatePort)')
-    else
-        CLOWDER_PORT=$(python -c 'import app_common_python; print(app_common_python.LoadedConfig.publicPort)')
-    fi
-fi
-
 cd $APP_HOME
-gunicorn koku.wsgi --bind=0.0.0.0:$CLOWDER_PORT --access-logfile=- --config gunicorn_conf.py --preload
+gunicorn koku.wsgi --access-logfile=- --config gunicorn_conf.py --preload
