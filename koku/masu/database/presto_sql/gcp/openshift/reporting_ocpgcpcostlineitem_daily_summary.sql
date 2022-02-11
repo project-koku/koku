2d1bec707c7792df19103a877611d15fe86f8e81
Corey Goodfred <53098963+cgoodfred@users.noreply.github.com>
2022-02-11 11:44:37 -0500

    missing union, cluster alias checking as well, fix cluster alias in table (#3463)

--- koku/masu/database/presto_sql/gcp/openshift/reporting_ocpgcpcostlineitem_daily_summary.sql
+++ koku/masu/database/presto_sql/gcp/openshift/reporting_ocpgcpcostlineitem_daily_summary.sql
@@ -198,7 +198,8 @@ SELECT gcp.uuid as gcp_uuid,
 FROM hive.{{schema | sqlsafe}}.gcp_openshift_daily as gcp
 JOIN hive.{{ schema | sqlsafe}}.reporting_ocpusagelineitem_daily_summary as ocp
     ON date(gcp.usage_start_time) = ocp.usage_start
-        AND strpos(gcp.labels, 'kubernetes-io-cluster-{{cluster_id | sqlsafe}}') != 0 -- THIS IS THE SPECIFIC TO OCP ON GCP TAG MATCH
+        AND (strpos(gcp.labels, 'kubernetes-io-cluster-{{cluster_id | sqlsafe}}') != 0 -- THIS IS THE SPECIFIC TO OCP ON GCP TAG MATCH
+            OR strpos(gcp.labels, 'kubernetes-io-cluster-{{cluster_alias | sqlsafe}}') != 0)
 WHERE gcp.source = '{{gcp_source_uuid | sqlsafe}}'
     AND gcp.year = '{{year | sqlsafe}}'
     AND gcp.month = '{{month | sqlsafe}}'
