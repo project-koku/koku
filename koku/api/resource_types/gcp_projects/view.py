9d05077f97a94c16612c086cdf6e1d63f36ba53b
Luke Couzens <lcouzens@redhat.com>
2022-02-16 02:34:03 +0000

    Fixing openshift query set for ocp/gcp resource-type (#3468)

--- koku/api/resource_types/gcp_projects/view.py
+++ koku/api/resource_types/gcp_projects/view.py
@@ -4,6 +4,7 @@
 #
 """View for GCP Projects."""
 from django.db.models import F
+from django.db.models.functions import Coalesce
 from django.utils.decorators import method_decorator
 from django.views.decorators.vary import vary_on_headers
 from rest_framework import filters
@@ -16,7 +17,7 @@ from api.common.permissions.gcp_access import GcpAccessPermission
 from api.common.permissions.gcp_access import GcpProjectPermission
 from api.resource_types.serializers import ResourceTypeSerializer
 from reporting.provider.gcp.models import GCPTopology
-from reporting.provider.gcp.openshift.models import OCPGCPCostLineItemProjectDailySummaryP
+from reporting.provider.gcp.openshift.models import OCPGCPCostSummaryByGCPProjectP
 
 
 class GCPProjectsView(generics.ListAPIView):
@@ -32,43 +33,6 @@ class GCPProjectsView(generics.ListAPIView):
     @method_decorator(vary_on_headers(CACHE_RH_IDENTITY_HEADER))
     def list(self, request):
         # Reads the users values for GCP project id and displays values related to what the user has access to
-        supported_query_params = ["search", "limit"]
-        error_message = {}
-        query_holder = None
-        # Test for only supported query_params
-        if self.request.query_params:
-            for key in self.request.query_params:
-                if key not in supported_query_params:
-                    error_message[key] = [{"Unsupported parameter"}]
-                    return Response(error_message, status=status.HTTP_400_BAD_REQUEST)
-        if request.user.admin:
-            return super().list(request)
-        if request.user.access:
-            gcp_account_access = request.user.access.get("gcp.account", {}).get("read", [])
-            gcp_project_access = request.user.access.get("gcp.project", {}).get("read", [])
-            # Checks if the access exists, and the user has wildcard access
-            query_holder = self.queryset
-            if gcp_project_access and gcp_project_access[0] != "*":
-                query_holder = query_holder.filter(project_id__in=gcp_project_access)
-            if gcp_account_access and gcp_account_access[0] != "*":
-                query_holder = query_holder.filter(account_id__in=gcp_account_access)
-        self.queryset = query_holder
-        return super().list(request)
-
-
-class OCPGCPNamespaceView(generics.ListAPIView):
-    queryset = (
-        OCPGCPCostLineItemProjectDailySummaryP.objects.annotate(**{"value": F("namespace")}).values("value").distinct()
-    )
-    serializer_class = ResourceTypeSerializer
-    permission_classes = [GcpProjectPermission | GcpAccessPermission]
-    filter_backends = [filters.OrderingFilter, filters.SearchFilter]
-    ordering = ["value"]
-    search_fields = ["$value"]
-
-    @method_decorator(vary_on_headers(CACHE_RH_IDENTITY_HEADER))
-    def list(self, request):
-        # Reads the users values for GCP project id and displays values related to what the user has access to
         supported_query_params = ["search", "limit", "openshift"]
         error_message = {}
         query_holder = None
@@ -78,6 +42,16 @@ class OCPGCPNamespaceView(generics.ListAPIView):
                 if key not in supported_query_params:
                     error_message[key] = [{"Unsupported parameter"}]
                     return Response(error_message, status=status.HTTP_400_BAD_REQUEST)
+                elif key == "openshift":
+                    openshift = self.request.query_params.get("openshift")
+                    if openshift == "true":
+                        self.queryset = (
+                            OCPGCPCostSummaryByGCPProjectP.objects.annotate(
+                                **{"value": F("project_id"), "project": Coalesce(F("project_name"), "project_id")}
+                            )
+                            .values("value", "project")
+                            .distinct()
+                        )
         if request.user.admin:
             return super().list(request)
         if request.user.access:
