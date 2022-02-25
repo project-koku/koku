6d339e41696516fc43f17e3be845b33a4f394828
HAP <hproctor@redhat.com>
2022-02-25 11:22:08 -0500

    Remove unneeded function

--- scripts/truncate_old_tables.py
+++ scripts/truncate_old_tables.py
@@ -106,22 +106,6 @@ def trino_enabled_unleash(source_uuid, source_type, account):  # noqa
     return bool(UNLEASH_CLIENT.is_enabled("cost-trino-processor", context))
 
 
-def enable_trino_processing(source_uuid, source_type, account):  # noqa
-    """Helper to determine if source is enabled for Trino."""
-    if account and not account.startswith("acct"):
-        account = f"acct{account}"
-
-    context = {"schema": account, "source-type": source_type, "source-uuid": source_uuid}
-    LOG.info(f"enable_trino_processing context: {context}")
-    return bool(
-        settings.ENABLE_PARQUET_PROCESSING
-        or source_uuid in settings.ENABLE_TRINO_SOURCES
-        or source_type in settings.ENABLE_TRINO_SOURCE_TYPE
-        or account in settings.ENABLE_TRINO_ACCOUNTS
-        or UNLEASH_CLIENT.is_enabled("cost-trino-processor", context)
-    )
-
-
 def connect():
     engine = "postgresql"
     app = os.path.basename(sys.argv[0])
