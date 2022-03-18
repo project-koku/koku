925c03f867e435b20dc0a901a61a0b64c679ad12
HAP <hproctor@redhat.com>
2022-03-18 12:28:54 -0400

    New pod/storage label processing

---
+++ koku/masu/database/sql/_reporting_ocpstoragevolumelabel_summary.sql
@@ -0,0 +1,96 @@
+WITH cte_tag_value(key, value, report_period_id, namespace) AS (
+    SELECT key,
+        value,
+        li.report_period_id,
+        li.namespace,
+        li.node
+    FROM {{schema | sqlsafe}}.reporting_ocpusagelineitem_daily_summary AS li,
+        jsonb_each_text(li.volume_labels) labels
+    WHERE li.data_source = 'Storage'
+    {% if report_periods %}
+        AND li.report_period_id IN (
+        {%- for report_period_id in report_period_ids -%}
+        {{report_period_id}}{% if not loop.last %},{% endif %}
+        {%- endfor -%}
+    )
+    {% endif %}
+        AND li.usage_start >= {{start_date}}
+        AND li.usage_start <= {{end_date}}
+        AND value IS NOT NULL
+    GROUP BY key, value, li.report_period_id, li.namespace, li.node
+),
+cte_values_agg AS (
+    SELECT key,
+        array_agg(DISTINCT value) as "values",
+        report_period_id,
+        namespace,
+        node
+    FROM cte_tag_value
+    GROUP BY key, report_period_id, namespace, node
+),
+cte_distinct_values_agg AS (
+    SELECT v.key,
+        array_agg(DISTINCT v."values") as "values",
+        v.report_period_id,
+        v.namespace,
+        v.node
+    FROM (
+        SELECT va.key,
+            unnest(va."values" || coalesce(ls."values", '{}'::text[])) as "values",
+            va.report_period_id,
+            va.namespace,
+            va.node
+        FROM cte_values_agg AS va
+        LEFT JOIN {{schema | sqlsafe}}.reporting_ocpstoragevolumelabel_summary AS ls
+            ON va.key = ls.key
+                AND va.report_period_id = ls.report_period_id
+                AND va.namespace = ls.namespace
+                AND va.node = ls.node
+    ) as v
+    GROUP BY key, report_period_id, namespace, node
+),
+ins1 AS (
+    INSERT INTO {{schema | sqlsafe}}.reporting_ocpstoragevolumelabel_summary (uuid, key, report_period_id, namespace, node, values)
+    SELECT uuid_generate_v4() as uuid,
+        key,
+        report_period_id,
+        namespace,
+        node,
+        "values"
+    FROM cte_distinct_values_agg
+    ON CONFLICT (key, report_period_id, namespace, node) DO UPDATE SET values=EXCLUDED."values"
+    )
+INSERT INTO {{schema | sqlsafe}}.reporting_ocptags_values (uuid, key, value, cluster_ids, cluster_aliases, namespaces, nodes)
+SELECT uuid_generate_v4() as uuid,
+    tv.key,
+    tv.value,
+    array_agg(DISTINCT rp.cluster_id) as cluster_ids,
+    array_agg(DISTINCT rp.cluster_alias) as cluster_aliases,
+    array_agg(DISTINCT tv.namespace) as namespaces,
+    array_agg(DISTINCT tv.node) as nodes
+FROM cte_tag_value AS tv
+JOIN {{schema | sqlsafe}}.reporting_ocpusagereportperiod AS rp
+    ON tv.report_period_id = rp.id
+GROUP BY tv.key, tv.value
+ON CONFLICT (key, value) DO UPDATE SET namespaces=EXCLUDED.namespaces, nodes=EXCLUDED.nodes, cluster_ids=EXCLUDED.cluster_ids, cluster_aliases=EXCLUDED.cluster_aliases
+;
+
+-- We run this SQL in the volume label summary SQL as it is run after
+-- the pod summary SQL and we want to make sure we consider both
+-- source tables before deleting from the values table.
+WITH cte_expired_tag_keys AS (
+    SELECT DISTINCT tv.key
+    FROM {{schema | sqlsafe}}.reporting_ocptags_values AS tv
+    LEFT JOIN {{schema | sqlsafe}}.reporting_ocpusagepodlabel_summary AS pls
+        ON tv.key = pls.key
+    LEFT JOIN {{schema | sqlsafe}}.reporting_ocpstoragevolumelabel_summary vls
+        ON tv.key = vls.key
+    WHERE pls.key IS NULL
+        AND vls.key IS NULL
+
+
+)
+DELETE FROM {{schema | sqlsafe}}.reporting_ocptags_values tv
+    USING cte_expired_tag_keys etk
+    WHERE tv.key = etk.key
+;
--- koku/masu/database/sql/reporting_ocpusagepodlabel_summary.sql
+++ koku/masu/database/sql/reporting_ocpusagepodlabel_summary.sql
@@ -1,76 +1,192 @@
-WITH cte_tag_value(key, value, report_period_id, namespace) AS (
-    SELECT key,
-        value,
-        li.report_period_id,
-        li.namespace,
-        li.node
-    FROM {{schema | sqlsafe}}.reporting_ocpusagelineitem_daily_summary AS li,
-        jsonb_each_text(li.pod_labels) labels
-    WHERE li.data_source = 'Pod'
-    {% if report_periods %}
-        AND li.report_period_id IN (
-        {%- for report_period_id in report_period_ids -%}
-        {{report_period_id}}{% if not loop.last %},{% endif %}
-        {%- endfor -%}
-        )
-    {% endif %}
-        AND li.usage_start >= {{start_date}}
-        AND li.usage_start <= {{end_date}}
-        AND value IS NOT NULL
-    GROUP BY key, value, li.report_period_id, li.namespace, li.node
-),
-cte_values_agg AS (
-    SELECT key,
-        array_agg(DISTINCT value) as "values",
-        report_period_id,
-        namespace,
-        node
-    FROM cte_tag_value
-    GROUP BY key, report_period_id, namespace, node
-),
-cte_distinct_values_agg AS (
-    SELECT v.key,
-        array_agg(DISTINCT v."values") as "values",
-        v.report_period_id,
-        v.namespace,
-        v.node
-    FROM (
-        SELECT va.key,
-            unnest(va."values" || coalesce(ls."values", '{}'::text[])) as "values",
-            va.report_period_id,
-            va.namespace,
-            va.node
-        FROM cte_values_agg AS va
-        LEFT JOIN {{schema | sqlsafe}}.reporting_ocpusagepodlabel_summary AS ls
-            ON va.key = ls.key
-                AND va.report_period_id = ls.report_period_id
-                AND va.namespace = ls.namespace
-                AND va.node = ls.node
-    ) as v
-    GROUP BY key, report_period_id, namespace, node
-),
-ins1 AS (
-    INSERT INTO {{schema | sqlsafe}}.reporting_ocpusagepodlabel_summary (uuid, key, report_period_id, namespace, node, values)
-    SELECT uuid_generate_v4() as uuid,
-        key,
-        report_period_id,
-        namespace,
-        node,
-        "values"
-    FROM cte_distinct_values_agg
-    ON CONFLICT (key, report_period_id, namespace, node) DO UPDATE SET values=EXCLUDED."values"
-    )
-INSERT INTO {{schema | sqlsafe}}.reporting_ocptags_values (uuid, key, value, cluster_ids, cluster_aliases, namespaces, nodes)
-SELECT uuid_generate_v4() as uuid,
-    tv.key,
-    tv.value,
-    array_agg(DISTINCT rp.cluster_id) as cluster_ids,
-    array_agg(DISTINCT rp.cluster_alias) as cluster_aliases,
-    array_agg(DISTINCT tv.namespace) as namespaces,
-    array_agg(DISTINCT tv.node) as nodes
-FROM cte_tag_value AS tv
-JOIN {{schema | sqlsafe}}.reporting_ocpusagereportperiod AS rp
-    ON tv.report_period_id = rp.id
-GROUP BY tv.key, tv.value
-ON CONFLICT (key, value) DO UPDATE SET namespaces=EXCLUDED.namespaces, nodes=EXCLUDED.nodes, cluster_ids=EXCLUDED.cluster_ids, cluster_aliases=EXCLUDED.cluster_aliases
+--
+-- Copyright 2021 Red Hat Inc.
+-- SPDX-License-Identifier: Apache-2.0
+--
+-- ===================================================
+-- setup normal tables that will be truncated, dropped after this processing.
+-- ===================================================
+
+-- Make sure that the tables do not already exist
+-- PostgreSQL has no TRUNCATE TABLE IF EXISTS form, so we have to do this.
+do $$
+declare
+    processing_tables text[] := ARRAY['{{schema | sqlsafe}}._report_period_tag_values_{{uuid | sqlsafe}}',
+                                      '{{schema | sqlsafe}}._expanded_tag_values_{{uuid | sqlsafe}}',
+                                      '{{schema | sqlsafe}}._process_ocptagvalues_{{uuid | sqlsafe}}']::text[];
+    table_name text;
+begin
+    foreach table_name in array processing_tables
+    loop
+        if (to_regclass(table_name) is not NULL)
+        then
+            raise info 'Truncating table %%', table_name;
+            execute format('truncate table %%s;', table_name);
+            raise info 'Dropping table %%', table_name;
+            execute format('drop table %%s;', table_name);
+        end if;
+    end loop;
+end;
+$$ language plpgsql;
+
+
+create table {{schema | sqlsafe}}._expanded_tag_values_{{uuid | sqlsafe}} as
+SELECT distinct
+       key,
+       value,
+       li.report_period_id,
+       li.namespace,
+       li.node
+  FROM {{schema | sqlsafe}}.reporting_ocpusagelineitem_daily_summary AS li,
+       jsonb_each_text(li.pod_labels) labels
+ WHERE li.data_source = 'Pod'
+{% if report_periods %}
+   AND li.report_period_id IN (
+    {%- for report_period_id in report_period_ids -%}
+       {{report_period_id}}{% if not loop.last %},{% endif %}
+    {%- endfor -%}
+       )
+{% endif %}
+   AND li.usage_start >= {{start_date}}::date
+   AND li.usage_start <= {{end_date}}::date
+   AND value IS NOT NULL
 ;
+
+create index ix_expanded_tag_values__{{uuid | sqlsafe}} on {{schema | sqlsafe}}._expanded_tag_values_{{uuid | sqlsafe}} (key, report_period_id, namespace, node);
+
+
+create table {{schema | sqlsafe}}._report_period_tag_values_{{uuid | sqlsafe}} as
+select key,
+       array_agg(distinct value) as "values",
+       report_period_id,
+       namespace,
+       node
+  from (
+            -- get the unnested tag values
+           select r.key,
+                  r.value,
+                  r.report_period_id,
+                  r.namespace,
+                  r.node
+             from {{schema | sqlsafe}}._expanded_tag_values_{{uuid | sqlsafe}} as r
+            union
+           select e.key,
+                  unnest(e."values") as "value",
+                  e.report_period_id,
+                  e.namespace,
+                  e.node
+             from {{schema | sqlsafe}}.reporting_ocpusagepodlabel_summary as e
+             join {{schema | sqlsafe}}._expanded_tag_values_{{uuid | sqlsafe}} r1
+               on r1.key = e.key
+              and r1.report_period_id = e.report_period_id
+              and r1.namespace = e.namespace
+              and r1.node = e.node
+       ) as x
+ group
+    by key,
+       report_period_id,
+       namespace,
+       node
+;
+
+create index ix_report_period_tag_values_{{uuid | sqlsafe}} on {{schema | sqlsafe}}._report_period_tag_values_{{uuid | sqlsafe}} (key, report_period_id, namespace, node);
+
+
+create table {{schema | sqlsafe}}._process_ocptagvalues_{{uuid | sqlsafe}} as
+select uuid_generate_v4() as "uuid",
+       tv.key,
+       tv.value,
+       array_agg(distinct rp.cluster_id) as cluster_ids,
+       array_agg(distinct rp.cluster_alias) as cluster_aliases,
+       array_agg(distinct tv.namespace) as namespaces,
+       array_agg(distinct tv.node) as nodes
+  from {{schema | sqlsafe}}._expanded_tag_values_{{uuid | sqlsafe}} tv
+  join {{schema | sqlsafe}}.reporting_ocpusagereportperiod AS rp
+    on tv.report_period_id = rp.id
+ group
+    by tv.key,
+       tv.value
+;
+
+create index ix__process_ocptagvalues_{{uuid | sqlsafe}} on {{schema | sqlsafe}}._process_ocptagvalues_{{uuid | sqlsafe}} (key, value);
+
+
+-- ===================================================
+-- Handle reporting_ocpusagepodlabel_summary
+-- ===================================================
+update {{schema | sqlsafe}}.reporting_ocpusagepodlabel_summary as pl_summ
+   set "values" = pls."values"
+  from {{schema | sqlsafe}}._report_period_tag_values_{{uuid | sqlsafe}} pls
+ where pls.key = pl_summ.key
+   and pls.report_period_id = pl_summ.report_period_id
+   and pls.namespace = pl_summ.namespace
+   and pls.node = pl_summ.node
+;
+
+
+insert
+  into {{schema | sqlsafe}}.reporting_ocpusagepodlabel_summary (
+           "uuid",
+           key,
+           "values",
+           report_period_id,
+           namespace,
+           node
+       )
+select uuid_generate_v4(),
+       key,
+       "values",
+       report_period_id,
+       namespace,
+       node
+  from {{schema | sqlsafe}}._report_period_tag_values_{{uuid | sqlsafe}} atvi
+ where not exists (
+                      select 1
+                        from {{schema | sqlsafe}}.reporting_ocpusagepodlabel_summary ins
+                       where atvi.key = ins.key
+                         and atvi.report_period_id = ins.report_period_id
+                         and atvi.namespace = ins.namespace
+                         and atvi.node = ins.node
+                  )
+;
+
+
+-- ===================================================
+-- Handle reporting_ocptags_values
+-- ===================================================
+update {{schema | sqlsafe}}.reporting_ocptags_values as otv
+   set cluster_ids = rp.cluster_ids,
+       cluster_aliases = rp.cluster_aliases,
+       namespaces = rp.namespaces,
+       nodes = rp.nodes
+  from {{schema | sqlsafe}}._process_ocptagvalues_{{uuid | sqlsafe}} rp
+ where otv.key = rp.key
+   and otv.value = rp.value
+;
+
+
+insert
+  into {{schema | sqlsafe}}.reporting_ocptags_values
+select uuid_generate_v4(),
+       rp.key,
+       rp.value,
+       rp.cluster_ids,
+       rp.cluster_aliases,
+       rp.namespaces,
+       rp.nodes
+  from {{schema | sqlsafe}}._process_ocptagvalues_{{uuid | sqlsafe}} rp
+ where not exists (
+                      select 1
+                        from {{schema | sqlsafe}}.reporting_ocptags_values otv
+                       where otv.key = rp.key
+                         and otv.value = rp.value
+                  )
+;
+
+
+-- Cleanup
+truncate table {{schema | sqlsafe}}._process_ocptagvalues_{{uuid | sqlsafe}};
+truncate table {{schema | sqlsafe}}._report_period_tag_values_{{uuid | sqlsafe}};
+truncate table {{schema | sqlsafe}}._expanded_tag_values_{{uuid | sqlsafe}};
+drop table {{schema | sqlsafe}}._process_ocptagvalues_{{uuid | sqlsafe}};
+drop table {{schema | sqlsafe}}._report_period_tag_values_{{uuid | sqlsafe}};
+drop table {{schema | sqlsafe}}._expanded_tag_values_{{uuid | sqlsafe}};
