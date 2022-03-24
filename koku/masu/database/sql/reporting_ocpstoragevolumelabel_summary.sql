f760af27f12cf2e38863587078711801eaf47171
HAP <hproctor@redhat.com>
2022-03-24 07:01:09 -0400

    Backed out the radical changes

--- koku/masu/database/sql/reporting_ocpstoragevolumelabel_summary.sql
+++ koku/masu/database/sql/reporting_ocpstoragevolumelabel_summary.sql
@@ -1,18 +1,9 @@
---
--- Copyright 2021 Red Hat Inc.
--- SPDX-License-Identifier: Apache-2.0
---
--- ===================================================
--- setup normal tables that will be truncated, dropped after this processing.
--- ===================================================
-
--- Make sure that the tables do not already exist
--- PostgreSQL has no TRUNCATE TABLE IF EXISTS form, so we have to do this.
 do $$
 declare
-    processing_tables text[] := ARRAY['{{schema | sqlsafe}}._cte_tag_value_{{uuid | sqlsafe}}',
-                                      '{{schema | sqlsafe}}._cte_distinct_values_agg_{{uuid | sqlsafe}}',
-                                      '{{schema | sqlsafe}}._process_ocptagvalues_{{uuid | sqlsafe}}']::text[];
+    processing_tables text[] := ARRAY['{{schema | sqlsafe}}.cte_tag_value_{{uuid | sqlsafe}}',
+                                      '{{schema | sqlsafe}}.cte_values_agg_{{uuid | sqlsafe}}',
+                                      '{{schema | sqlsafe}}.cte_distinct_values_agg_{{uuid | sqlsafe}}',
+                                      '{{schema | sqlsafe}}.cte_kv_cluster_agg_{{uuid | sqlsafe}}']::text[];
     table_name text;
 begin
     foreach table_name in array processing_tables
@@ -29,186 +20,190 @@ end;
 $$ language plpgsql;
 
 
--- Create a "temp" table to hold unpacked label key, value data
-create table {{schema | sqlsafe}}._cte_tag_value_{{uuid | sqlsafe}} as
-select distinct
-       key,
-       value,
-       li.report_period_id,
-       li.namespace,
-       li.node
-  from {{schema | sqlsafe}}.reporting_ocpusagelineitem_daily_summary as li,
-       jsonb_each_text(li.volume_labels) labels
- where li.data_source = 'storage'
-{% if report_periods %}
-   and li.report_period_id in (
-    {%- for report_period_id in report_period_ids -%}
-       {{report_period_id}}{% if not loop.last %},{% endif %}
-    {%- endfor -%}
-       )
-{% endif %}
-   and li.usage_start >= {{start_date}}::date
-   and li.usage_start <= {{end_date}}::date
-   and value is not null
+create table {{schema | sqlsafe}}.cte_tag_value_{{uuid | sqlsafe}} as
+    SELECT key,
+        value,
+        li.report_period_id,
+        li.namespace,
+        li.node
+    FROM {{schema | sqlsafe}}.reporting_ocpusagelineitem_daily_summary AS li,
+        jsonb_each_text(li.volume_labels) labels
+    WHERE li.data_source = 'Storage'
+    {% if report_periods %}
+        AND li.report_period_id IN (
+        {%- for report_period_id in report_period_ids -%}
+        {{report_period_id}}{% if not loop.last %},{% endif %}
+        {%- endfor -%}
+    )
+    {% endif %}
+        AND li.usage_start >= {{start_date}}
+        AND li.usage_start <= {{end_date}}
+        AND value IS NOT NULL
+    GROUP BY key, value, li.report_period_id, li.namespace, li.node
+;
+
+create index ix_cte_tag_value_{{uuid | sqlsafe}}
+    on {{schema | sqlsafe}}.cte_tag_value_{{uuid | sqlsafe}}
+       (key, report_period_id, namespace, node)
 ;
 
 
--- Create a "temp" table to hold key and aggregated values
-create table {{schema | sqlsafe}}._cte_distinct_values_agg_{{uuid | sqlsafe}} as
-with cte_distinct_agg as (
-select key,
-       array_agg("value") as "values",
-       report_period_id,
-       namespace,
-       node
-  from {{schema | sqlsafe}}._cte_tag_value_{{uuid | sqlsafe}}
- group
-    by key,
-       report_period_id,
-       namespace,
-       node
-)
-select v.key,
-       array_agg(distinct v."values") as "values",
-       v.report_period_id,
-       v.namespace,
-       v.node
-  from (
-         select va.key,
-                unnest(va."values" || coalesce(ls."values", '{}'::text[])) as "values",
-                va.report_period_id,
-                va.namespace,
-                va.node
-           from cte_distinct_agg as va
-           left
-           join {{schema | sqlsafe}}.reporting_ocpstoragevolumelabel_summary as ls
-             on va.key = ls.key
-            and va.report_period_id = ls.report_period_id
-            and va.namespace = ls.namespace
-            and va.node = ls.node
-       ) as v
- group
-    by key,
-       report_period_id,
-       namespace,
-       node
+create table {{schema | sqlsafe}}.cte_values_agg_{{uuid | sqlsafe}} as
+    SELECT key,
+        array_agg(DISTINCT value) as "values",
+        report_period_id,
+        namespace,
+        node
+    FROM {{schema | sqlsafe}}.cte_tag_value_{{uuid | sqlsafe}}
+    GROUP BY key, report_period_id, namespace, node
 ;
 
-create index ix_cte_distinct_values_agg_{{uuid | sqlsafe}} on {{schema | sqlsafe}}._cte_distinct_values_agg_{{uuid | sqlsafe}} (key, report_period_id, namespace, node);
-
-
--- Create a "temp" table to hold key, value and aggregated cluster_id, cluster_ailas, namespace, node
-create table {{schema | sqlsafe}}._process_ocptagvalues_{{uuid | sqlsafe}} as
-select uuid_generate_v4() as "uuid",
-       tv.key,
-       tv.value,
-       array_agg(distinct rp.cluster_id) as cluster_ids,
-       array_agg(distinct rp.cluster_alias) as cluster_aliases,
-       array_agg(distinct tv.namespace) as namespaces,
-       array_agg(distinct tv.node) as nodes
-  from {{schema | sqlsafe}}._cte_tag_value_{{uuid | sqlsafe}} tv
-  join {{schema | sqlsafe}}.reporting_ocpusagereportperiod AS rp
-    on tv.report_period_id = rp.id
- group
-    by tv.key,
-       tv.value
-;
-
-create index ix__process_ocptagvalues_{{uuid | sqlsafe}} on {{schema | sqlsafe}}._process_ocptagvalues_{{uuid | sqlsafe}} (key, value);
-
-
--- ===================================================
--- Handle reporting_ocpstoragevolumelabel_summary
--- ===================================================
--- simple join here because we want matching here
-update {{schema | sqlsafe}}.reporting_ocpstoragevolumelabel_summary as pl_summ
-   set "values" = pls."values"
-  from {{schema | sqlsafe}}._cte_distinct_values_agg_{{uuid | sqlsafe}} pls
- where pls.key = pl_summ.key
-   and pls.report_period_id = pl_summ.report_period_id
-   and pls.namespace = pl_summ.namespace
-   and pls.node = pl_summ.node
+create unique index ix_cte_values_agg_{{uuid | sqlsafe}}
+    on {{schema | sqlsafe}}.cte_values_agg_{{uuid | sqlsafe}}
+       (key, report_period_id, namespace, node)
 ;
 
 
--- I have seen instances of better performance and accuracy using this pattern vs left join where col is null.
--- we'll see if this continues to bear out.
-insert
-  into {{schema | sqlsafe}}.reporting_ocpstoragevolumelabel_summary (
-           "uuid",
-           key,
-           "values",
-           report_period_id,
-           namespace,
-           node
-       )
-select uuid_generate_v4(),
-       key,
-       "values",
-       report_period_id,
-       namespace,
-       node
-  from {{schema | sqlsafe}}._cte_distinct_values_agg_{{uuid | sqlsafe}} atvi
- where not exists (
-                      select 1
-                        from {{schema | sqlsafe}}.reporting_ocpstoragevolumelabel_summary ins
-                       where atvi.key = ins.key
-                         and atvi.report_period_id = ins.report_period_id
-                         and atvi.namespace = ins.namespace
-                         and atvi.node = ins.node
-                  )
+create table {{schema | sqlsafe}}.cte_distinct_values_agg_{{uuid | sqlsafe}} as
+    SELECT v.key,
+        array_agg(DISTINCT v."values") as "values",
+        v.report_period_id,
+        v.namespace,
+        v.node
+    FROM (
+        SELECT va.key,
+            unnest(va."values" || coalesce(ls."values", '{}'::text[])) as "values",
+            va.report_period_id,
+            va.namespace,
+            va.node
+        FROM {{schema | sqlsafe}}.cte_values_agg_{{uuid | sqlsafe}} AS va
+        LEFT JOIN {{schema | sqlsafe}}.reporting_ocpstoragevolumelabel_summary AS ls
+            ON va.key = ls.key
+                AND va.report_period_id = ls.report_period_id
+                AND va.namespace = ls.namespace
+                AND va.node = ls.node
+    ) as v
+    GROUP BY key, report_period_id, namespace, node
+;
+
+create unique index ix_cte_distinct_values_agg_{{uuid | sqlsafe}}
+    on {{schema | sqlsafe}}.cte_distinct_values_agg_{{uuid | sqlsafe}}
+       (key, report_period_id, namespace, node)
 ;
 
 
--- ===================================================
--- Handle reporting_ocptags_values
--- ===================================================
--- simple join here because we want matching here
-update {{schema | sqlsafe}}.reporting_ocptags_values as otv
-   set cluster_ids = rp.cluster_ids,
-       cluster_aliases = rp.cluster_aliases,
-       namespaces = rp.namespaces,
-       nodes = rp.nodes
-  from {{schema | sqlsafe}}._process_ocptagvalues_{{uuid | sqlsafe}} rp
- where otv.key = rp.key
-   and otv.value = rp.value
+create table {{schema | sqlsafe}}.cte_kv_cluster_agg_{{uuid | sqlsafe}} as
+SELECT uuid_generate_v4() as uuid,
+    tv.key,
+    tv.value,
+    array_agg(DISTINCT rp.cluster_id) as cluster_ids,
+    array_agg(DISTINCT rp.cluster_alias) as cluster_aliases,
+    array_agg(DISTINCT tv.namespace) as namespaces,
+    array_agg(DISTINCT tv.node) as nodes
+FROM {{schema | sqlsafe}}.cte_tag_value_{{uuid | sqlsafe}} AS tv
+JOIN {{schema | sqlsafe}}.reporting_ocpusagereportperiod AS rp
+    ON tv.report_period_id = rp.id
+GROUP BY tv.key, tv.value
+;
+
+create unique index ix_cte_kv_cluster_agg_{{uuid | sqlsafe}}
+    on {{schema | sqlsafe}}.cte_kv_cluster_agg_{{uuid | sqlsafe}}
+       (key, value)
 ;
 
 
--- I have seen instances of better performance and accuracy using this pattern vs left join where col is null.
--- we'll see if this continues to bear out.
-insert
-  into {{schema | sqlsafe}}.reporting_ocptags_values
-select uuid_generate_v4(),
-       rp.key,
-       rp.value,
-       rp.cluster_ids,
-       rp.cluster_aliases,
-       rp.namespaces,
-       rp.nodes
-  from {{schema | sqlsafe}}._process_ocptagvalues_{{uuid | sqlsafe}} rp
- where not exists (
-                      select 1
-                        from {{schema | sqlsafe}}.reporting_ocptags_values otv
-                       where otv.key = rp.key
-                         and otv.value = rp.value
-                  )
+UPDATE {{schema | sqlsafe}}.reporting_ocpstoragevolumelabel_summary x
+   SET "values" = y."values"
+  FROM {{schema | sqlsafe}}.cte_distinct_values_agg_{{uuid | sqlsafe}} y
+ WHERE y.key = x.key
+   AND y.report_period_id = x.report_period_id
+   AND y.namespace = x.namespace
+   AND y.node = x.node
+;
+
+
+INSERT INTO {{schema | sqlsafe}}.reporting_ocpstoragevolumelabel_summary (uuid, key, report_period_id, namespace, node, values)
+SELECT uuid_generate_v4() as uuid,
+    key,
+    report_period_id,
+    namespace,
+    node,
+    "values"
+FROM {{schema | sqlsafe}}.cte_distinct_values_agg_{{uuid | sqlsafe}} x
+WHERE NOT EXISTS (
+          SELECT 1
+            FROM {{schema | sqlsafe}}.reporting_ocpstoragevolumelabel_summary y
+           WHERE y.key = x.key
+             AND y.report_period_id = x.report_period_id
+             AND y.namespace = x.namespace
+             AND y.node = x.node
+      )
+;
+
+
+UPDATE {{schema | sqlsafe}}.reporting_ocptags_values ov
+   SET cluster_ids = ca.cluster_ids,
+       cluster_aliases = ca.cluster_aliases,
+       namespaces = ca.namespaces,
+       nodes = ca.nodes
+  FROM {{schema | sqlsafe}}.cte_kv_cluster_agg_{{uuid | sqlsafe}} ca
+ WHERE ca.key = ov.key
+   AND ca.value = ov.value
+;
+
+
+INSERT INTO {{schema | sqlsafe}}.reporting_ocptags_values (uuid, key, value, cluster_ids, cluster_aliases, namespaces, nodes)
+SELECT uuid_generate_v4() as uuid,
+    tv.key,
+    tv.value,
+    tv.cluster_ids,
+    tv.cluster_aliases,
+    tv.namespaces,
+    tv.nodes
+FROM {{schema | sqlsafe}}.cte_kv_cluster_agg_{{uuid | sqlsafe}} AS tv
+WHERE not exists (
+          SELECT 1
+            FROM {{schema | sqlsafe}}.reporting_ocptags_values AS rp
+           WHERE rp.key = tv.key
+             AND rp.value = tv.value
+      )
 ;
 
 
 -- We run this SQL in the volume label summary SQL as it is run after
 -- the pod summary SQL and we want to make sure we consider both
 -- source tables before deleting from the values table.
-delete from {{schema | sqlsafe}}.reporting_ocptags_values tv
- where not exists ( select 1 from {{schema | sqlsafe}}.reporting_ocpusagepodlabel_summary AS pls pls.key = tv.key )
-   and not exists ( select 1 from {{schema | sqlsafe}}.reporting_ocpstoragevolumelabel_summary AS vls vls.key = tv.key )
+-- WITH cte_expired_tag_keys AS (
+--     SELECT DISTINCT tv.key
+--     FROM {{schema | sqlsafe}}.reporting_ocptags_values AS tv
+--     LEFT JOIN {{schema | sqlsafe}}.reporting_ocpusagepodlabel_summary AS pls
+--         ON tv.key = pls.key
+--     LEFT JOIN {{schema | sqlsafe}}.reporting_ocpstoragevolumelabel_summary vls
+--         ON tv.key = vls.key
+--     WHERE pls.key IS NULL
+--         AND vls.key IS NULL
+
+
+-- )
+DELETE FROM {{schema | sqlsafe}}.reporting_ocptags_values tv
+WHERE NOT EXISTS (
+          SELECT 1
+            FROM {{schema | sqlsafe}}.reporting_ocpusagepodlabel_summary AS pls
+           WHERE pls.key = tv.key
+      )
+  AND NOT EXISTS (
+          SELECT 1
+            FROM {{schema | sqlsafe}}.reporting_ocpstoragevolumelabel_summary AS vls
+           WHERE vls.key = tv.key
+      )
 ;
 
 
--- Cleanup
-truncate table {{schema | sqlsafe}}._process_ocptagvalues_{{uuid | sqlsafe}};
-truncate table {{schema | sqlsafe}}._cte_distinct_values_agg_{{uuid | sqlsafe}};
-truncate table {{schema | sqlsafe}}._cte_tag_value_{{uuid | sqlsafe}};
-drop table {{schema | sqlsafe}}._process_ocptagvalues_{{uuid | sqlsafe}};
-drop table {{schema | sqlsafe}}._cte_distinct_values_agg_{{uuid | sqlsafe}};
-drop table {{schema | sqlsafe}}._cte_tag_value_{{uuid | sqlsafe}};
+TRUNCATE TABLE {{schema | sqlsafe}}.cte_tag_value_{{uuid | sqlsafe}};
+DROP TABLE {{schema | sqlsafe}}.cte_tag_value_{{uuid | sqlsafe}};
+TRUNCATE TABLE {{schema | sqlsafe}}.cte_values_agg_{{uuid | sqlsafe}};
+DROP TABLE {{schema | sqlsafe}}.cte_values_agg_{{uuid | sqlsafe}};
+TRUNCATE TABLE {{schema | sqlsafe}}.cte_distinct_values_agg_{{uuid | sqlsafe}};
+DROP TABLE {{schema | sqlsafe}}.cte_distinct_values_agg_{{uuid | sqlsafe}};
+TRUNCATE TABLE {{schema | sqlsafe}}.cte_kv_cluster_agg_{{uuid | sqlsafe}};
+DROP TABLE {{schema | sqlsafe}}.cte_kv_cluster_agg_{{uuid | sqlsafe}};
