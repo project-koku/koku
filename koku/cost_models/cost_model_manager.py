25aa2521709123c05002799bb085f344b2c519b0
Shannon Donahue <sdonahue@redhat.com>
2022-03-25 10:41:16 -0400

    removing comment

--- koku/cost_models/cost_model_manager.py
+++ koku/cost_models/cost_model_manager.py
@@ -31,7 +31,6 @@ class CostModelManager:
 
     def __init__(self, cost_model_uuid=None):
         """Initialize properties for CostModelManager."""
-        # This comment is so I can try and kick off github actions will be removed shortly
         self._model = None
         self._cost_model_uuid = None
 
