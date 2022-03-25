01139401b7385e1e8fc46b7139bfd3d786d7aedb
Corey Goodfred <53098963+cgoodfred@users.noreply.github.com>
2022-03-25 07:50:28 -0400

    make it a bit more provider agnostic (#3544)

--- koku/hcs/database/hcs_report_db_accessor.py
+++ koku/hcs/database/hcs_report_db_accessor.py
@@ -1,5 +1,5 @@
 #
-# Copyright 2021 Red Hat Inc.
+# Copyright 2022 Red Hat Inc.
 # SPDX-License-Identifier: Apache-2.0
 #
 """Database accessor for report data."""
@@ -9,17 +9,26 @@ import pkgutil
 from jinjasql import JinjaSql
 
 from api.common import log_json
+from api.provider.models import Provider
 from hcs.csv_file_handler import CSVFileHandler
 from masu.config import Config
-from masu.database import AWS_CUR_TABLE_MAP
 from masu.database.report_db_accessor_base import ReportDBAccessorBase
 from masu.external.date_accessor import DateAccessor
-from reporting.provider.aws.models import PRESTO_LINE_ITEM_TABLE
+from reporting.provider.aws.models import PRESTO_LINE_ITEM_TABLE as AWS_PRESTO_LINE_ITEM_TABLE
+from reporting.provider.azure.models import PRESTO_LINE_ITEM_TABLE as AZURE_PRESTO_LINE_ITEM_TABLE
+
 
 LOG = logging.getLogger(__name__)
 
+PROVIDER_HCS_TABLE_MAP = {
+    Provider.PROVIDER_AWS: AWS_PRESTO_LINE_ITEM_TABLE,
+    Provider.PROVIDER_AWS_LOCAL: AWS_PRESTO_LINE_ITEM_TABLE,
+    Provider.PROVIDER_AZURE: AZURE_PRESTO_LINE_ITEM_TABLE,
+    Provider.PROVIDER_AZURE_LOCAL: AZURE_PRESTO_LINE_ITEM_TABLE,
+}
 
-class HCSAWSReportDBAccessor(ReportDBAccessorBase):
+
+class HCSReportDBAccessor(ReportDBAccessorBase):
     """Class to interact with customer reporting tables."""
 
     def __init__(self, schema):
@@ -31,7 +40,6 @@ class HCSAWSReportDBAccessor(ReportDBAccessorBase):
         self._datetime_format = Config.AWS_DATETIME_STR_FORMAT
         self.date_accessor = DateAccessor()
         self.jinja_sql = JinjaSql()
-        self._table_map = AWS_CUR_TABLE_MAP
 
     def get_hcs_daily_summary(self, date, provider, provider_uuid, sql_summary_file, tracing_id):
         """Build HCS daily report.
@@ -43,13 +51,14 @@ class HCSAWSReportDBAccessor(ReportDBAccessorBase):
 
         :returns (None)
         """
-        LOG.info(log_json(tracing_id, "acquiring marketplace data..."))
+        LOG.info(log_json(tracing_id, f"acquiring {provider} marketplace data..."))
         LOG.info(log_json(tracing_id, f"schema: {self.schema},  provider: {provider}, date: {date}"))
 
         sql = pkgutil.get_data("hcs.database", sql_summary_file)
         sql = sql.decode("utf-8")
 
-        sql_params = {"date": date, "schema": self.schema, "table": PRESTO_LINE_ITEM_TABLE}
+        table = PROVIDER_HCS_TABLE_MAP.get(provider)
+        sql_params = {"date": date, "schema": self.schema, "provider_uuid": provider_uuid, "table": table}
 
         sql, sql_params = self.jinja_sql.prepare_query(sql, sql_params)
         data = self._execute_presto_raw_sql_query(self.schema, sql, bind_params=sql_params)
