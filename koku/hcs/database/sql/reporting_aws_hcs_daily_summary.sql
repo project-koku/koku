316f81bf870dd26f017b07aaec3d5a4d74fe0175
Doug Donahue <ddonahue@redhat.com>
2022-03-25 08:10:51 -0400

    removed commented out sql statement

--- koku/hcs/database/sql/reporting_aws_hcs_daily_summary.sql
+++ koku/hcs/database/sql/reporting_aws_hcs_daily_summary.sql
@@ -1,7 +1,3 @@
--- SELECT *, '10001' as ebs_account_id
--- FROM hive.{{schema | sqlsafe}}.{{table | sqlsafe}}
--- WHERE bill_billingentity = 'AWS Marketplace'
---     AND product_productname like '%Red Hat%'
 SELECT *, '{{schema | sqlsafe}}' as ebs_account_id
 FROM hive.{{schema | sqlsafe}}.{{table | sqlsafe}}
 WHERE bill_billingentity = 'AWS Marketplace'
