c476a8fc87a0e74c83a592a60da20a1131d7becf
Shannon Donahue <sdonahue@redhat.com>
2022-03-30 09:21:28 -0400

    linting

--- koku/api/report/serializers.py
+++ koku/api/report/serializers.py
@@ -14,7 +14,7 @@ from api.utils import DateHelper
 from api.utils import get_currency
 from api.utils import materialized_view_month_start
 
-CURRENCY_CHOICES = tuple([(currency.get("code"), currency.get("code")) for currency in CURRENCIES])
+CURRENCY_CHOICES = tuple((currency.get("code"), currency.get("code")) for currency in CURRENCIES)
 
 
 def handle_invalid_fields(this, data):
