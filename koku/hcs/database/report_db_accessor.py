a6df0b743060414952396d24715cdbe2ae96e492
Doug Donahue <ddonahue@redhat.com>
2022-03-31 15:11:36 -0400

    updated SQL table added exception handling for friendlier error message

--- koku/hcs/database/report_db_accessor.py
+++ koku/hcs/database/report_db_accessor.py
@@ -12,7 +12,7 @@ from api.common import log_json
 from hcs.csv_file_handler import CSVFileHandler
 from masu.database.report_db_accessor_base import ReportDBAccessorBase
 from masu.external.date_accessor import DateAccessor
-from reporting.provider.aws.models import PRESTO_LINE_ITEM_TABLE
+from reporting.provider.aws.models import PRESTO_LINE_ITEM_DAILY_TABLE
 
 LOG = logging.getLogger(__name__)
 
@@ -42,16 +42,23 @@ class HCSReportDBAccessor(ReportDBAccessorBase):
         LOG.info(log_json(tracing_id, "acquiring marketplace data..."))
         LOG.info(log_json(tracing_id, f"schema: {self.schema}, provider: {provider}, date: {date}"))
 
-        sql = pkgutil.get_data("hcs.database", sql_summary_file)
-        sql = sql.decode("utf-8")
+        try:
+            sql = pkgutil.get_data("hcs.database", sql_summary_file)
+            sql = sql.decode("utf-8")
 
-        sql_params = {"date": date, "schema": self.schema, "table": PRESTO_LINE_ITEM_TABLE}
-        sql, sql_params = self.jinja_sql.prepare_query(sql, sql_params)
-        data = self._execute_presto_raw_sql_query(self.schema, sql, bind_params=sql_params)
+            sql_params = {"date": date, "schema": self.schema, "table": PRESTO_LINE_ITEM_DAILY_TABLE}
+            sql, sql_params = self.jinja_sql.prepare_query(sql, sql_params)
+            data = self._execute_presto_raw_sql_query(self.schema, sql, bind_params=sql_params)
 
-        if len(data) > 0:
-            LOG.info(log_json(tracing_id, f"data found for date: {date}"))
-            csv_handler = CSVFileHandler(self.schema, provider, provider_uuid)
-            csv_handler.write_csv_to_s3(date, data, tracing_id)
-        else:
-            LOG.info(log_json(tracing_id, f"data not found for date: {date}"))
+            if len(data) > 0:
+                LOG.info(log_json(tracing_id, f"data found for date: {date}"))
+                csv_handler = CSVFileHandler(self.schema, provider, provider_uuid)
+                csv_handler.write_csv_to_s3(date, data, tracing_id)
+            else:
+                LOG.info(log_json(tracing_id, f"no data found for date: {date}"))
+
+        except FileNotFoundError:
+            LOG.error(log_json(tracing_id, f"unable to locate SQL file: {sql_summary_file}"))
+
+        except Exception as err:
+            LOG.error(log_json(tracing_id, err))
