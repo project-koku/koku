9a0c3674bfb279e4b6aed89782f64b1536e983f0
Michael Skarbek <mskarbek@redhat.com>
2022-04-12 13:10:40 -0400

    use specific header for probe, and fake header for tests

--- koku/sources/config.py
+++ koku/sources/config.py
@@ -22,9 +22,17 @@ class Config:
     SOURCES_API_URL = f"http://{SOURCES_API_HOST}:{SOURCES_API_PORT}"
     SOURCES_API_PREFIX = ENVIRONMENT.get_value("SOURCES_API_PREFIX", default="/api/v1.0")
     SOURCES_INTERNAL_API_PREFIX = ENVIRONMENT.get_value("SOURCES_INTERNAL_API_PREFIX", default="/internal/v1.0")
+    SOURCES_PROBE_HEADER = ENVIRONMENT.get_value(
+        "SOURCES_PROBE_HEADER",
+        default="eyJpZGVudGl0eSI6IHsiYWNjb3VudF9udW1iZXIiOiAiMTIzNDUiLCAib3JnX2lkIjogIjEyMzQ1In19Cg==",
+    )
     SOURCES_FAKE_HEADER = ENVIRONMENT.get_value(
         "SOURCES_FAKE_HEADER",
-        default=("eyJpZGVudGl0eSI6IHsiYWNjb3VudF9udW1iZXIiOiAiMTIzNDUiLCAib3JnX2lkIjogIjEyMzQ1In19Cg=="),
+        default=(
+            "eyJpZGVudGl0eSI6IHsiYWNjb3VudF9udW1iZXIiOiAiMTIzNDUiLCAidXNlciI6IHsiaXNfb3J"
+            "nX2FkbWluIjogImZhbHNlIiwgInVzZXJuYW1lIjogInNvdXJjZXMiLCAiZW1haWwiOiAic291cm"
+            "Nlc0Bzb3VyY2VzLmlvIn0sICJpbnRlcm5hbCI6IHsib3JnX2lkIjogIjU0MzIxIn19fQ=="
+        ),
     )
     SOURCES_FAKE_CLUSTER_HEADER = ENVIRONMENT.get_value(
         "SOURCES_FAKE_CLUSTER_HEADER",
