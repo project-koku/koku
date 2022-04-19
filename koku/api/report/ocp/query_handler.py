fffd41135def14753b05997b34a456d3a1a192df
myersCody <codymyers63@gmail.com>
2022-04-19 14:24:07 -0400

    Fix daily deltas for ocp during currency roll ups.

--- koku/api/report/ocp/query_handler.py
+++ koku/api/report/ocp/query_handler.py
@@ -194,7 +194,7 @@ class OCPReportQueryHandler(ReportQueryHandler):
                     currencys[currency] = data
                 else:
                     base_values = currencys.get(currency)
-                    remove_keys = ["source_uuid_id"]
+                    remove_keys = ["source_uuid_id", "delta_percent"]
                     data_keys = data.keys()
                     # remove source_uuid_id from data keys
                     keys = list(filter(lambda w: w not in remove_keys, data_keys))
@@ -204,9 +204,9 @@ class OCPReportQueryHandler(ReportQueryHandler):
                                 orig_value = base_values.get(key).get(each).get("value")
                                 new_value = Decimal(data.get(key).get(each).get("value")) * Decimal(exchange_rate)
                                 base_values[key][each]["value"] = Decimal(new_value) + Decimal(orig_value)
-                        elif key in ["delta_value", "delta_percent"]:
-                            if data.get(key):
-                                base_values[key] = base_values.get(key, 0) + data.get(key)
+                        # Use date to see if we are out of the for loop
+                        elif key == "delta_value" and data.get(key):
+                            base_values[key] = base_values.get(key, 0) + data.get(key)
                         else:
                             base_val = base_values.get(key)
                             new_val = data.get(key)
@@ -250,6 +250,7 @@ class OCPReportQueryHandler(ReportQueryHandler):
                 "total": {"value": 0, "units": self.currency},
             },
         }
+        overall_previous = 0
         for currency_entry in currency_codes:
             values = currency_entry.get("values")
             for data in values:
@@ -266,9 +267,18 @@ class OCPReportQueryHandler(ReportQueryHandler):
                             orig_value = total_query.get(key).get(each).get("value")
                             new_value = Decimal(data.get(key).get(each).get("value")) * Decimal(exchange_rate)
                             total_query[key][each]["value"] = Decimal(new_value) + Decimal(orig_value)
-                    elif key in ["delta_value", "delta_percent"]:
-                        if data.get(key):
-                            total_query[key] = total_query.get(key, 0) + data.get(key)
+                    elif key == "delta_value" and data.get(key):
+                        # We can just add all of the delta_values together to get the aggregated deltas
+                        total_query[key] = total_query.get(key, 0) + data.get(key)
+                    elif key == "delta_percent":
+                        current_delta = data.get("delta_value", 0)
+                        percentage = data.get("delta_percent", None)
+                        # To calculate the overall delta percentage we need the overall previous_total.
+                        # percentage = (delta / previous_toal) * 100
+                        if percentage:
+                            percent_ratio = percentage / 100
+                            previous_total = current_delta / percent_ratio
+                            overall_previous += previous_total
                     else:
                         base_val = total_query.get(key)
                         new_val = data.get(key)
@@ -280,7 +290,8 @@ class OCPReportQueryHandler(ReportQueryHandler):
                         if base_val and not isinstance(base_val, str):
                             new_val = base_val + new_val
                         total_query[key] = new_val
-
+        if total_query.get("delta_value") and overall_previous:
+            total_query["delta_percentage"] = (total_query.get("delta_value") / overall_previous) * 100
         return total_query, new_codes
 
     def execute_query(self):  # noqa: C901
