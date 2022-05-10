00a96248bcc1382e14e6a0915cc8620f8fac2efa
Luke Couzens <lcouzens@redhat.com>
2022-05-10 16:37:47 +0100

    fixing unit tests

--- koku/providers/test/oci/test_oci_provider.py
+++ koku/providers/test/oci/test_oci_provider.py
@@ -3,28 +3,37 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 """Tests the OCIProvider implementation for the Koku interface."""
-from unittest.mock import MagicMock
 from unittest.mock import patch
 
 from django.test import TestCase
 from django.utils.translation import ugettext as _
 from faker import Faker
+from oci.exceptions import ClientError
+from oci.exceptions import ServiceError
 from rest_framework.exceptions import ValidationError
 
 from providers.oci.provider import error_obj
 from providers.oci.provider import OCIProvider
 
-# from oci.exceptions import ClientError
-# from oci.exceptions import ServiceError
-
 FAKE = Faker()
 
 
-def mock_oci_exception(**kwargs):
-    """Raise oci exception for testing."""
-    # msg = "OCI authentication error"
-    # headers = {"opc-request-id": "1"}
-    raise Exception("Boom")
+class mock_OCI_exception:
+    def __init__(self, **kwargs):
+        pass
+
+    def list_objects(self, namespace=None, bucket=None, prefix=None):
+        msg = "OCI authentication error"
+        headers = {"opc-request-id": "1"}
+        raise ServiceError(code="code", headers=headers, message=msg, status="400")
+
+
+class mock_OCI_exception_client:
+    def __init__(self, **kwargs):
+        pass
+
+    def list_objects(self, namespace=None, bucket=None, prefix=None):
+        raise ClientError
 
 
 class OCIProviderTestCase(TestCase):
@@ -55,14 +64,19 @@ class OCIProviderTestCase(TestCase):
     @patch("providers.oci.provider.storage_client.ObjectStorageClient")
     def test_check_cost_report_access_service_error(self, mock_storage_client):
         """Test_check_cost_report_access error."""
-        oci_client = MagicMock()
-        oci_client.list_objects.side_effect = mock_oci_exception
-        mock_storage_client.return_value = oci_client
-        # mock_storage_client.list_objects.side_effect = mock_oci_exception
+        mock_storage_client.return_value = mock_OCI_exception
         provider_interface = OCIProvider()
         data_source = {"bucket": "bucket", "bucket_namespace": "namespace", "bucket_region": "region"}
         with self.assertRaises(ValidationError):
-            # mock_oci_exception()
+            provider_interface.cost_usage_source_is_reachable(FAKE.md5(), data_source)
+
+    @patch("providers.oci.provider.storage_client.ObjectStorageClient")
+    def test_check_cost_report_access_client_error(self, mock_storage_client):
+        """Test_check_cost_report_access error."""
+        mock_storage_client.return_value = mock_OCI_exception_client
+        provider_interface = OCIProvider()
+        data_source = {"bucket": "bucket", "bucket_namespace": "namespace", "bucket_region": "region"}
+        with self.assertRaises(ValidationError):
             provider_interface.cost_usage_source_is_reachable(FAKE.md5(), data_source)
 
     @patch(
