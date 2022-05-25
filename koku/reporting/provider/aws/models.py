b82eb6da8e4a2c60b44d8ea7ea5ebe18e3756f11
HAP <hproctor@redhat.com>
2022-05-25 10:00:48 -0400

    Revert "remove lintitem, lineitem_daily tables"
    This reverts commit 2962459b63253d9068ba4af5cea2557b62e9b88b.

--- koku/reporting/provider/aws/models.py
+++ koku/reporting/provider/aws/models.py
@@ -103,6 +103,112 @@ class AWSCostEntry(models.Model):
     bill = models.ForeignKey("AWSCostEntryBill", on_delete=models.CASCADE)
 
 
+class AWSCostEntryLineItem(models.Model):
+    """A line item in a cost entry.
+
+    This identifies specific costs and usage of AWS resources.
+
+    """
+
+    id = models.BigAutoField(primary_key=True)
+
+    cost_entry = models.ForeignKey("AWSCostEntry", on_delete=models.CASCADE, db_constraint=False)
+    cost_entry_bill = models.ForeignKey("AWSCostEntryBill", on_delete=models.CASCADE, db_constraint=False)
+    cost_entry_product = models.ForeignKey(
+        "AWSCostEntryProduct", on_delete=models.SET_NULL, null=True, db_constraint=False
+    )
+    cost_entry_pricing = models.ForeignKey(
+        "AWSCostEntryPricing", on_delete=models.SET_NULL, null=True, db_constraint=False
+    )
+    cost_entry_reservation = models.ForeignKey(
+        "AWSCostEntryReservation", on_delete=models.SET_NULL, null=True, db_constraint=False
+    )
+
+    tags = JSONField(null=True)
+
+    # Invoice ID is null until the bill is finalized
+    invoice_id = models.CharField(max_length=63, null=True)
+    line_item_type = models.CharField(max_length=50, null=False)
+    usage_account_id = models.CharField(max_length=50, null=False)
+    usage_start = models.DateTimeField(null=False)
+    usage_end = models.DateTimeField(null=False)
+    product_code = models.CharField(max_length=50, null=False)
+    usage_type = models.CharField(max_length=50, null=True)
+    operation = models.CharField(max_length=50, null=True)
+    availability_zone = models.CharField(max_length=50, null=True)
+    resource_id = models.CharField(max_length=256, null=True)
+    usage_amount = models.DecimalField(max_digits=24, decimal_places=9, null=True)
+    normalization_factor = models.FloatField(null=True)
+    normalized_usage_amount = models.DecimalField(max_digits=24, decimal_places=9, null=True)
+    currency_code = models.CharField(max_length=10)
+    unblended_rate = models.DecimalField(max_digits=24, decimal_places=9, null=True)
+    unblended_cost = models.DecimalField(max_digits=24, decimal_places=9, null=True)
+    blended_rate = models.DecimalField(max_digits=24, decimal_places=9, null=True)
+    blended_cost = models.DecimalField(max_digits=24, decimal_places=9, null=True)
+    savingsplan_effective_cost = models.DecimalField(max_digits=24, decimal_places=9, null=True)
+    public_on_demand_cost = models.DecimalField(max_digits=24, decimal_places=9, null=True)
+    public_on_demand_rate = models.DecimalField(max_digits=24, decimal_places=9, null=True)
+    reservation_amortized_upfront_fee = models.DecimalField(max_digits=24, decimal_places=9, null=True)
+    reservation_amortized_upfront_cost_for_usage = models.DecimalField(max_digits=24, decimal_places=9, null=True)
+    reservation_recurring_fee_for_usage = models.DecimalField(max_digits=24, decimal_places=9, null=True)
+    # Unused reservation fields more useful for later predictions.
+    reservation_unused_quantity = models.DecimalField(max_digits=24, decimal_places=9, null=True)
+    reservation_unused_recurring_fee = models.DecimalField(max_digits=24, decimal_places=9, null=True)
+    tax_type = models.TextField(null=True)
+
+
+class AWSCostEntryLineItemDaily(models.Model):
+    """A daily aggregation of line items.
+
+    This table is aggregated by AWS resource.
+
+    """
+
+    class Meta:
+        """Meta for AWSCostEntryLineItemDaily."""
+
+        db_table = "reporting_awscostentrylineitem_daily"
+
+        indexes = [
+            models.Index(fields=["usage_start"], name="usage_start_idx"),
+            models.Index(fields=["product_code"], name="product_code_idx"),
+            models.Index(fields=["usage_account_id"], name="usage_account_id_idx"),
+            models.Index(fields=["resource_id"], name="resource_id_idx"),
+            GinIndex(fields=["tags"], name="aws_cost_entry"),
+            GinIndex(fields=["product_code"], name="aws_cost_pcode_like", opclasses=["gin_trgm_ops"]),
+        ]
+
+    id = models.BigAutoField(primary_key=True)
+
+    cost_entry_bill = models.ForeignKey("AWSCostEntryBill", on_delete=models.CASCADE, null=True)
+    cost_entry_product = models.ForeignKey("AWSCostEntryProduct", on_delete=models.SET_NULL, null=True)
+    cost_entry_pricing = models.ForeignKey("AWSCostEntryPricing", on_delete=models.SET_NULL, null=True)
+    cost_entry_reservation = models.ForeignKey("AWSCostEntryReservation", on_delete=models.SET_NULL, null=True)
+
+    line_item_type = models.CharField(max_length=50, null=False)
+    usage_account_id = models.CharField(max_length=50, null=False)
+    usage_start = models.DateField(null=False)
+    usage_end = models.DateField(null=True)
+    product_code = models.CharField(max_length=50, null=False)
+    usage_type = models.CharField(max_length=50, null=True)
+    operation = models.CharField(max_length=50, null=True)
+    availability_zone = models.CharField(max_length=50, null=True)
+    resource_id = models.CharField(max_length=256, null=True)
+    usage_amount = models.DecimalField(max_digits=24, decimal_places=9, null=True)
+    normalization_factor = models.FloatField(null=True)
+    normalized_usage_amount = models.FloatField(null=True)
+    currency_code = models.CharField(max_length=10)
+    unblended_rate = models.DecimalField(max_digits=24, decimal_places=9, null=True)
+    unblended_cost = models.DecimalField(max_digits=24, decimal_places=9, null=True)
+    blended_rate = models.DecimalField(max_digits=24, decimal_places=9, null=True)
+    blended_cost = models.DecimalField(max_digits=24, decimal_places=9, null=True)
+    savingsplan_effective_cost = models.DecimalField(max_digits=24, decimal_places=9, null=True)
+    public_on_demand_cost = models.DecimalField(max_digits=24, decimal_places=9, null=True)
+    public_on_demand_rate = models.DecimalField(max_digits=24, decimal_places=9, null=True)
+    tax_type = models.TextField(null=True)
+    tags = JSONField(null=True)
+
+
 class AWSCostEntryLineItemDailySummary(models.Model):
     """A daily aggregation of line items.
 
