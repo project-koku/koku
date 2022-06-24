b90ad6c3cad8e964e6c1fc78c87d94a633be7088
Luke Couzens <lcouzens@redhat.com>
2022-06-24 19:55:25 +0100

    Adding unit tests

--- koku/koku/notifications.py
+++ koku/koku/notifications.py
@@ -15,54 +15,75 @@ from masu.prometheus_stats import KAFKA_CONNECTION_ERRORS_COUNTER
 LOG = logging.getLogger(__name__)
 
 
-def cost_model_notification(account):
-    """Send a notification to notifications service via kafka"""
-    event_type = "missing-cost-model"
-    send_notification(account, event_type)
+class NotificationService:
+    """A class to handle interactions with the Notifications service."""
 
+    def __init__(self):
+        """Initialize Notifications class"""
+        self.msg_uuid = str(uuid.uuid4())
+        self.timestamp = str(DateAccessor().today())
+        # TODO Protecting your Kafka messages against duplicate processing
+        # add header RecordHeaders().add("rh-message-id", messageId)
+        # encoded_id = msg_uuid.encode()
+        # message_id = bytearray(encoded_id)
 
-@KAFKA_CONNECTION_ERRORS_COUNTER.count_exceptions()
-def send_notification(account, event_type):
-    """
-    Send kafka notification message to Insights Notifications Service.
-    Args:
-        account (Object): account for notifications being sent.
-        notification (String): Notification string
-        uuid (UUID): uuid of object in question
-    Returns:
-        None
-    """
-    msg_uuid = str(uuid.uuid4())
-    # TODO Protecting your Kafka messages against duplicate processing
-    # encoded_id = msg_uuid.encode()
-    # message_id = bytearray(encoded_id)
-    producer = get_producer()
-    notification_json = {
-        "id": msg_uuid,
-        "bundle": "openshift",
-        "application": "cost-management",
-        "event_type": event_type,
-        "timestamp": str(DateAccessor().today()),
-        "account_id": account.get("schema_name"),
-        "org_id": account.get("org_id"),
-        "context": {
-            "source_id": str(account.get("provider_uuid")),
-            "source_name": account.get("name"),
-            "host_url": f"https://console.redhat.com/settings/sources/detail/{str(account.get('provider_uuid'))}",
-        },
-        "events": [
-            {
-                "metadata": {},
-                "payload": {
-                    "description": "Openshift source has no cost model assigned, add one via the following link.",
-                    "host_url": "https://console.redhat.com/openshift/cost-management/cost-models",
-                },
-            }
-        ],
-    }
-    msg = bytes(json.dumps(notification_json), "utf-8")
-    producer.produce(Config.NOTIFICATION_TOPIC, value=msg)
-    # Wait up to 1 second for events. Callbacks will be invoked during
-    # this method call if the message is acknowledged.
-    # `flush` makes this process synchronous compared to async with `poll`
-    producer.flush(1)
+    def build_notification_json(self, account, event_type, host_url, description):
+        """
+        Build json message for sending to notifications service
+        Args:
+            account (Object): account object for notifications being sent
+            event_type (String): notification event type
+            host_url (String): URL for notification event
+            description (String): Description of event being sent
+        Returns:
+            J
+        """
+        notification_json = {
+            "id": self.msg_uuid,
+            "bundle": "openshift",
+            "application": "cost-management",
+            "event_type": event_type,
+            "timestamp": self.timestamp,
+            "account_id": account.get("schema_name"),
+            "org_id": account.get("org_id"),
+            "context": {
+                "source_id": str(account.get("provider_uuid")),
+                "source_name": account.get("name"),
+                "host_url": f"https://console.redhat.com/settings/sources/detail/{str(account.get('provider_uuid'))}",
+            },
+            "events": [
+                {
+                    "metadata": {},
+                    "payload": {
+                        "description": description,
+                        "host_url": host_url,
+                    },
+                }
+            ],
+        }
+        msg = bytes(json.dumps(notification_json), "utf-8")
+        return msg
+
+    @KAFKA_CONNECTION_ERRORS_COUNTER.count_exceptions()
+    def send_notification(self, msg):
+        """
+        Send kafka notification message to Insights Notifications Service.
+        Args:
+            msg (string): message for kafka notification.
+        Returns:
+            None
+        """
+        producer = get_producer()
+        producer.produce(Config.NOTIFICATION_TOPIC, value=msg)
+        # Wait up to 1 second for events. Callbacks will be invoked during
+        # this method call if the message is acknowledged.
+        # `flush` makes this process synchronous compared to async with `poll`
+        producer.flush(1)
+
+    def cost_model_notification(self, account):
+        """Send a cost-model to notification service via kafka"""
+        event_type = "missing-cost-model"
+        host_url = "https://console.redhat.com/openshift/cost-management/cost-models"
+        description = "Openshift source has no cost model assigned, add one via the following link."
+        msg = self.build_notification_json(account, event_type, host_url, description)
+        self.send_notification(msg)
--- koku/koku/test_notifications.py
+++ koku/koku/test_notifications.py
@@ -3,3 +3,41 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 """Test the NOTIFICATION Service interaction."""
+from unittest.mock import patch
+
+from django.test import TestCase
+
+from koku.notifications import NotificationService
+from masu.external.accounts_accessor import AccountsAccessor
+
+
+class NotificationsTest(TestCase):
+    """Test Notifications object."""
+
+    @patch("koku.notifications.NotificationService.send_notification", return_result=True)
+    def test_cost_model_notification(self, mock_send_notification):
+        """Test triggering a cost model notification."""
+        polling_accounts = AccountsAccessor().get_accounts()
+        notification = NotificationService()
+        notification.cost_model_notification(polling_accounts[0])
+        mock_send_notification.assert_called()
+
+    @patch("koku.notifications.get_producer")
+    def test_send_notification(self, mock_producer):
+        """Test sending notification payload."""
+        msg = "notification-test-message"
+        notification = NotificationService()
+        notification.send_notification(msg)
+        mock_producer.assert_called()
+
+    def test_building_notification_json(self):
+        """Test sending notification payload."""
+        account = AccountsAccessor().get_accounts()[0]
+        event_type = "testing-event"
+        host_url = "test-url"
+        description = "test notification description"
+        notification = NotificationService()
+        msg = notification.build_notification_json(account, event_type, host_url, description)
+        assert bytes(event_type, "utf-8") in msg
+        assert bytes(host_url, "utf-8") in msg
+        assert bytes(description, "utf-8") in msg
