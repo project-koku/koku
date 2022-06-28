ec7c9ada54da22dab882c78b4cfd712f65988a64
Doug Donahue <57504257+ddonahue007@users.noreply.github.com>
2022-06-28 17:55:47 -0400

    [Cost-2137] HCS code hardening (#3724)
    * refactor hcs finalize
    harden code
    more unit tests

--- koku/hcs/test/test_daily_report.py
+++ koku/hcs/test/test_daily_report.py
@@ -4,8 +4,8 @@
 #
 """Test HCS csv_file_handler."""
 import logging
+from datetime import timedelta
 
-from api.models import Provider
 from api.utils import DateHelper
 from hcs.daily_report import ReportHCS
 from hcs.test import HCSTestCase
@@ -21,14 +21,13 @@ class TestReportHCS(HCSTestCase):
         """Set up the class."""
         super().setUpClass()
         cls.today = DateHelper().today
-        cls.provider = Provider.PROVIDER_AWS
-        cls.provider_uuid = "cabfdddb-4ed5-421e-a041-311b75daf235"
+        cls.yesterday = cls.today - timedelta(days=1)
         cls.tracing_id = "12345-12345-12345"
 
     def test_init(self):
         """Test the initializer."""
-        dr = ReportHCS(self.schema, self.provider, self.provider_uuid, "12345-12345-12345")
-        self.assertEqual(dr._schema_name, "acct10001")
-        self.assertEqual(dr._provider, "AWS")
-        self.assertEqual(dr._provider_uuid, "cabfdddb-4ed5-421e-a041-311b75daf235")
-        self.assertEqual(self.tracing_id, "12345-12345-12345")
+        dr = ReportHCS(self.schema, self.aws_provider_type, self.aws_provider_uuid, self.tracing_id)
+        self.assertEqual(dr._schema_name, self.schema)
+        self.assertEqual(dr._provider, self.aws_provider_type)
+        self.assertEqual(dr._provider_uuid, self.aws_provider_uuid)
+        self.assertEqual(dr._tracing_id, self.tracing_id)
