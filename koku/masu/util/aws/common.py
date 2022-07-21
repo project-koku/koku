2520cc232a16e1c76783bd9217acf07079ae5491
Doston Toirov <31990136+Dostonbek1@users.noreply.github.com>
2022-07-21 16:53:01 -0400

    gcp bulk delete in groups of 250 (#3756)
    * change bulk delete to 250
    
    Co-authored-by: myersCody <cmyers@redhat.com>

--- koku/masu/util/aws/common.py
+++ koku/masu/util/aws/common.py
@@ -447,12 +447,18 @@ def gcp_self_healing_remove_files_for_manifest_from_s3_bucket(request_id, s3_pat
                         bulk_delete_objects.append({"Key": key})
                         removed.append(key)
             bucket = s3_resource.Bucket(settings.S3_BUCKET_NAME)
-            bucket.delete_objects(Delete={"Objects": bulk_delete_objects})
-            if removed:
-                msg = f"Removed files from s3 bucket {settings.S3_BUCKET_NAME}: {','.join(removed)}."
-                LOG.info(log_json(request_id, msg, context))
+            # split the bulk delete objects into x number of objects to delete at a time
+            num_files_delete = 250
+            for i in range(0, len(bulk_delete_objects), num_files_delete):
+                sublist = bulk_delete_objects[i : i + num_files_delete]  # noqa E203
+                bucket.delete_objects(Delete={"Objects": sublist})
+                sublist_values = [val for obj in sublist for val in obj.values()]
+                removed.append(sublist_values)
+                if removed:
+                    msg = f"BULK: Removed files from s3 bucket {settings.S3_BUCKET_NAME}: {','.join(sublist_values)}."
+                    LOG.info(log_json(request_id, msg, context))
         except (EndpointConnectionError, ClientError) as err:
-            msg = f"Unable to remove data in bucket {settings.S3_BUCKET_NAME}.  Reason: {str(err)}"
+            msg = f"Unable to remove data in bucket {settings.S3_BUCKET_NAME}.  Reason:{str(err)}"
             LOG.info(log_json(request_id, msg, context))
     return removed
 
