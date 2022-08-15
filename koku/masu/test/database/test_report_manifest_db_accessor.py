9f09c4f43316123b4e01994558a22920d3eb8260
myersCody <cmyers@redhat.com>
2022-08-15 19:37:20 -0400

    Improve test coverage.

--- koku/masu/test/database/test_report_manifest_db_accessor.py
+++ koku/masu/test/database/test_report_manifest_db_accessor.py
@@ -209,3 +209,24 @@ class ReportManifestDBAccessorTest(IamTestCase):
 
             status = self.manifest_accessor.get_s3_parquet_cleared(manifest)
             self.assertTrue(status)
+
+    def test_bulk_delete_manifests(self):
+        """Test bulk delete of manifests."""
+        with schema_context(self.schema):
+            manifest_list = []
+            for fake_assembly_id in ["1234", "12345", "123456"]:
+                self.manifest_dict["assembly_id"] = fake_assembly_id
+                manifest = self.manifest_accessor.add(**self.manifest_dict)
+                manifest_list.append(manifest.id)
+            self.manifest_accessor.bulk_delete_manifests(self.provider_uuid, manifest_list)
+            current_manifests = self.manifest_accessor.get_manifest_list_for_provider_and_bill_date(self.provider_uuid, self.billing_start)
+            current_manifests = [manifest.id for manifest in current_manifests]
+        for deleted_manifest in manifest_list:
+            self.assertNotIn(deleted_manifest, current_manifests)
+
+    def test_bulk_delete_manifests_empty_list(self):
+        """Test bulk delete with an empty manifest list."""
+        with schema_context(self.schema):
+            manifest_list = []
+            value = self.manifest_accessor.bulk_delete_manifests(self.provider_uuid, manifest_list)
+            self.assertIsNone(value)
