33907179489c62102d527d2ccd0de66223962a04
myersCody <cmyers@redhat.com>
2022-10-13 15:28:22 -0400

    Uptick migrations.

--- koku/reporting/migrations/0261_standard_numeric_on_calculated_feilds.py
+++ koku/reporting/migrations/0261_standard_numeric_on_calculated_feilds.py
@@ -6,7 +6,7 @@ from django.db import models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("reporting", "0259_ocpnode_node_role"),
+        ("reporting", "0260_productcode_textfields"),
     ]
 
     operations = [
