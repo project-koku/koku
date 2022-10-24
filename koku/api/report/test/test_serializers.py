b8aff3b3d721ea657de75861347a22dce68baac2
maskarb <mskarbek@redhat.com>
2022-10-24 11:53:32 -0400

    remove test

--- koku/api/report/test/test_serializers.py
+++ koku/api/report/test/test_serializers.py
@@ -792,27 +792,3 @@ class ReportQueryParamSerializerTest(IamTestCase):
         with self.assertRaises(ValidationError):
             self.assertFalse(serializer.is_valid())
             serializer.is_valid(raise_exception=True)
-
-    def test_validate_units(self):
-        """Test `units` validation."""
-        params_list = [
-            {"valid": False, "params": {"units": "%2F%2F8028544394501274830.owasp.org"}},
-            {"valid": False, "params": {"units": "%253CscrIpt%253Ealert%25281%2529%253B%253C%252FscRipt%253E"}},
-            {"valid": False, "params": {"units": "units%3Bget-help"}},
-            {"valid": False, "params": {"units": "units%27%7Ctimeout+%2FT+15"}},
-            {"valid": False, "params": {"units": "%5D%5D>"}},
-            {"valid": True, "params": {"units": "Hrs"}},
-        ]
-        path = "/api/cost-management/v1/"
-        ctx = self._create_request_context(
-            self.customer_data, self._create_user_data(), create_customer=False, create_user=True, path=path
-        )
-        for param in params_list:
-            with self.subTest(param=param):
-                serializer = ReportQueryParamSerializer(data=param["params"], context=ctx)
-                if param["valid"]:
-                    self.assertTrue(serializer.is_valid(raise_exception=True))
-                else:
-                    with self.assertRaises(ValidationError):
-                        self.assertFalse(serializer.is_valid())
-                        serializer.is_valid(raise_exception=True)
