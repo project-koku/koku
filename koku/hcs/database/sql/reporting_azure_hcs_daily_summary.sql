ff1ba7d40f03ef2d61dae01b53fc19226bd8e331
Corey Goodfred <cgoodfre@redhat.com>
2022-10-31 12:01:40 -0400

    update azure queries to include CCSP data

--- koku/hcs/database/sql/reporting_azure_hcs_daily_summary.sql
+++ koku/hcs/database/sql/reporting_azure_hcs_daily_summary.sql
@@ -4,6 +4,8 @@ WHERE source = '{{provider_uuid | sqlsafe}}'
     AND year = '{{year | sqlsafe}}'
     AND month = '{{month | sqlsafe}}'
     AND publishertype = 'Marketplace'
-    AND publishername like '%Red Hat%'
+    AND (publishername like '%Red Hat%'
+        OR metersubcategory like '%Red Hat%'
+        OR serviceinfo2 like '%Red Hat%')
     AND coalesce(date, usagedatetime) >= TIMESTAMP '{{date | sqlsafe}}'
     AND coalesce(date, usagedatetime) < date_add('day', 1, TIMESTAMP '{{date | sqlsafe}}')
