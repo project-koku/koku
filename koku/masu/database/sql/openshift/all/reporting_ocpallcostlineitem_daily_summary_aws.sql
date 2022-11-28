204eba3d7267e17b69d561ab1e063c59b25806e1
maskarb <mskarbek@redhat.com>
2022-11-28 13:12:01 -0500

    fix table reference

--- koku/masu/database/sql/openshift/all/reporting_ocpallcostlineitem_daily_summary_aws.sql
+++ koku/masu/database/sql/openshift/all/reporting_ocpallcostlineitem_daily_summary_aws.sql
@@ -60,7 +60,7 @@ SELECT 'AWS'::text AS source_type,
        max(cost_category_id) as cost_category_id,
        max(aws.shared_projects),
        {{source_uuid}}::uuid as source_uuid
-  FROM {{schema_name | sqlsafe}}.reporting_ocpawscostlineitem_project_daily_summary AS aws
+  FROM {{schema_name | sqlsafe}}.reporting_ocpawscostlineitem_project_daily_summary_p AS aws
  WHERE aws.usage_start >= {{start_date}}::date
    AND aws.usage_start <= {{end_date}}::date
    AND aws.cluster_id = {{cluster_id}}
