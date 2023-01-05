5c111867c38f4e55898b00c1789e7dc39db42ea0
David N <dnakabaa@redhat.com>
2023-01-05 17:15:51 -0500

    invalidate cache for oci

--- koku/koku/cache.py
+++ koku/koku/cache.py
@@ -94,6 +94,8 @@ def invalidate_view_cache_for_tenant_and_source_type(schema_name, source_type):
         cache_key_prefixes = (AZURE_CACHE_PREFIX, OPENSHIFT_AZURE_CACHE_PREFIX, OPENSHIFT_ALL_CACHE_PREFIX)
     elif source_type in (Provider.PROVIDER_GCP, Provider.PROVIDER_GCP_LOCAL):
         cache_key_prefixes = (GCP_CACHE_PREFIX, OPENSHIFT_GCP_CACHE_PREFIX, OPENSHIFT_ALL_CACHE_PREFIX)
+    elif source_type in (Provider.PROVIDER_OCI, Provider.PROVIDER_OCI_LOCAL):
+        cache_key_prefixes = (OCI_CACHE_PREFIX,)
 
     for cache_key_prefix in cache_key_prefixes:
         invalidate_view_cache_for_tenant_and_cache_key(schema_name, cache_key_prefix)
