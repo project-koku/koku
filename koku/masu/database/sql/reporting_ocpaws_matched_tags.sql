aba020bf2fa346ee2f44deac7f522f7fe717838a
Andrew Berglund <aberglun@redhat.com>
2023-01-16 11:10:49 -0500

    Check for matching keys first

--- koku/masu/database/sql/reporting_ocpaws_matched_tags.sql
+++ koku/masu/database/sql/reporting_ocpaws_matched_tags.sql
@@ -1,9 +1,29 @@
-WITH cte_unnested_aws_tags AS (
+WITH cte_aws_tag_keys AS (
+    SELECT DISTINCT ts.key
+    FROM {{schema | sqlsafe}}.reporting_awstags_summary AS ts
+    JOIN {{schema | sqlsafe}}.reporting_awsenabledtagkeys as enabled_tags
+        ON lower(enabled_tags.key) = lower(ts.key)
+),
+cte_ocp_tag_keys AS (
+    SELECT DISTINCT ts.key
+    FROM {{schema | sqlsafe}}.reporting_ocptags_values AS ts
+    JOIN {{schema | sqlsafe}}.reporting_ocpenabledtagkeys as enabled_tags
+        ON lower(enabled_tags.key) = lower(ts.key)
+),
+cte_matched_tag_keys AS (
+    SELECT DISTINCT aws.key as aws_key,
+        ocp.key as ocp_key
+    FROM cte_aws_tag_keys AS aws
+    JOIN cte_ocp_tag_keys AS ocp
+        ON lower(aws.key) = lower(ocp.key)
+),
+cte_unnested_aws_tags AS (
     SELECT DISTINCT ts.key,
         ts.value
     FROM {{schema | sqlsafe}}.reporting_awstags_values AS ts
     JOIN {{schema | sqlsafe}}.reporting_awsenabledtagkeys as enabled_tags
         ON lower(enabled_tags.key) = lower(ts.key)
+    WHERE ts.key IN (SELECT aws_key FROM cte_matched_tag_keys)
 ),
 cte_unnested_ocp_tags AS (
     SELECT DISTINCT ts.key,
@@ -11,6 +31,7 @@ cte_unnested_ocp_tags AS (
     FROM {{schema | sqlsafe}}.reporting_ocptags_values AS ts
     JOIN {{schema | sqlsafe}}.reporting_ocpenabledtagkeys as enabled_tags
         ON lower(enabled_tags.key) = lower(ts.key)
+    WHERE ts.key IN (SELECT ocp_key FROM cte_matched_tag_keys)
 )
 SELECT jsonb_build_object(key, value) as tag
 FROM (
