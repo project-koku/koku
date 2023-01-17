e51a9330ccdafc30bdee680546a1e66655e8cc1e
Andrew Berglund <aberglun@redhat.com>
2023-01-17 14:36:59 -0500

    Split out SQL into separate files

---
+++ koku/masu/database/presto_sql/aws/reporting_ocpinfrastructure_provider_map.sql
@@ -0,0 +1,36 @@
+
+WITH cte_aws_resource_ids AS (
+    SELECT DISTINCT lineitem_resourceid,
+        aws.source
+    FROM hive.{{schema | sqlsafe}}.aws_line_items_daily AS aws
+    WHERE aws.lineitem_usagestartdate >= TIMESTAMP '{{start_date | sqlsafe}}'
+        AND aws.lineitem_usagestartdate < date_add('day', 1, TIMESTAMP '{{end_date | sqlsafe}}')
+        AND aws.lineitem_resourceid IS NOT NULL
+        AND aws.lineitem_resourceid != ''
+        {% if aws_provider_uuid %}
+        AND aws.source = '{{aws_provider_uuid | sqlsafe}}'
+        {% endif %}
+        AND aws.year = '{{year | sqlsafe}}'
+        AND aws.month = '{{month | sqlsafe}}'
+),
+cte_ocp_resource_ids AS (
+    SELECT DISTINCT resource_id,
+        ocp.source
+    FROM hive.{{schema | sqlsafe}}.openshift_pod_usage_line_items_daily AS ocp
+    WHERE ocp.interval_start >= TIMESTAMP '{{start_date | sqlsafe}}'
+    AND ocp.interval_start < date_add('day', 1, TIMESTAMP '{{end_date | sqlsafe}}')
+    AND ocp.resource_id IS NOT NULL
+    AND ocp.resource_id != ''
+    {% if ocp_provider_uuid %}
+    AND ocp.source = '{{ocp_provider_uuid | sqlsafe}}'
+    {% endif %}
+    AND ocp.year = '{{year | sqlsafe}}'
+    AND ocp.month = '{{month | sqlsafe}}'
+)
+SELECT DISTINCT ocp.source as ocp_uuid,
+    aws.source as infra_uuid,
+    'AWS' as type
+FROM cte_aws_resource_ids AS aws
+JOIN cte_ocp_resource_ids AS ocp
+    ON strpos(aws.lineitem_resourceid, ocp.resource_id) != 0
+;
---
+++ koku/masu/database/presto_sql/azure/reporting_ocpinfrastructure_provider_map.sql
@@ -0,0 +1,34 @@
+
+WITH cte_azure_instances AS (
+    SELECT DISTINCT split_part(coalesce(azure.resourceid, azure.instanceid), '/', 9) as instance,
+        azure.source
+    FROM hive.{{schema | sqlsafe}}.azure_line_items AS azure
+    WHERE coalesce(azure.date, azure.usagedatetime) >= TIMESTAMP '{{start_date | sqlsafe}}'
+        AND coalesce(azure.date, azure.usagedatetime) < date_add('day', 1, TIMESTAMP '{{end_date | sqlsafe}}')
+        {% if azure_provider_uuid %}
+        AND azure.source = '{{azure_provider_uuid | sqlsafe}}'
+        {% endif %}
+        AND azure.year = '{{year | sqlsafe}}'
+        AND azure.month = '{{month | sqlsafe}}'
+),
+cte_ocp_nodes AS (
+    SELECT DISTINCT ocp.node,
+        ocp.source
+    FROM hive.{{schema | sqlsafe}}.openshift_pod_usage_line_items_daily AS ocp
+    WHERE ocp.interval_start >= TIMESTAMP '{{start_date | sqlsafe}}'
+        AND ocp.interval_start < date_add('day', 1, TIMESTAMP '{{end_date | sqlsafe}}')
+        AND ocp.node IS NOT NULL
+        AND ocp.node != ''
+        {% if ocp_provider_uuid %}
+        AND ocp.source = '{{ocp_provider_uuid | sqlsafe}}'
+        {% endif %}
+        AND ocp.year = '{{year | sqlsafe}}'
+        AND ocp.month = '{{month | sqlsafe}}'
+)
+SELECT DISTINCT ocp.source as ocp_uuid,
+    azure.source as infra_uuid,
+    'Azure' as type
+FROM cte_azure_instances AS azure
+JOIN cte_ocp_nodes AS ocp
+    ON ocp.node = azure.instance
+;
