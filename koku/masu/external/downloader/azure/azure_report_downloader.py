4db2197a7dcff0aff150cebe9373809f341b18cc
Luke Couzens <lcouzens@redhat.com>
2023-02-22 15:48:11 +0000

    Update koku/masu/external/downloader/azure/azure_report_downloader.py
    Co-authored-by: Sam Doran <sdoran@redhat.com>

--- koku/masu/external/downloader/azure/azure_report_downloader.py
+++ koku/masu/external/downloader/azure/azure_report_downloader.py
@@ -206,7 +206,7 @@ class AzureReportDownloader(ReportDownloaderBase, DownloaderInterface):
                 try:
                     blob = self._azure_client.get_latest_cost_export_for_path(report_path, self.container_name)
                 except AzureCostReportNotFound as ex:
-                    msg = f"Unable to find manifest. Error: {str(ex)}"
+                    msg = f"Unable to find manifest. Error: {ex}"
                     LOG.info(log_json(self.tracing_id, msg, self.context))
                     return manifest, None
                 report_name = blob.name
