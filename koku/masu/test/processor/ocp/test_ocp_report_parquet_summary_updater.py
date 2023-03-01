2aa3a4adc4ea3c59b74efc8b2438a563390099dd
Luke Couzens <lcouzens@redhat.com>
2023-03-01 16:30:33 +0000

    date disabled

--- koku/masu/test/processor/ocp/test_ocp_report_parquet_summary_updater.py
+++ koku/masu/test/processor/ocp/test_ocp_report_parquet_summary_updater.py
@@ -7,8 +7,8 @@ import logging
 from datetime import timedelta
 from unittest.mock import patch
 
-from api.utils import DateHelper
 from masu.database.report_manifest_db_accessor import ReportManifestDBAccessor
+from masu.external.date_accessor import DateAccessor
 from masu.processor.ocp.ocp_report_parquet_summary_updater import OCPReportParquetSummaryUpdater
 from masu.test import MasuTestCase
 
@@ -22,12 +22,12 @@ class OCPParquetReportSummaryUpdaterTest(MasuTestCase):
     def setUpClass(cls):
         """Set up the test class with required objects."""
         super().setUpClass()
-        cls.dh = DateHelper()
+        cls._date_accessor = DateAccessor()
 
     def setUp(self):
         """Set up tests."""
         super().setUp()
-        self.today = self.dh.today
+        self.today = self._date_accessor.today
 
     @patch(
         "masu.processor.ocp.ocp_report_parquet_summary_updater.determine_if_full_summary_update_needed",
@@ -39,19 +39,19 @@ class OCPParquetReportSummaryUpdaterTest(MasuTestCase):
         with ReportManifestDBAccessor() as manifest_accessor:
             manifest = manifest_accessor.get_manifest_by_id(1)
         updater = OCPReportParquetSummaryUpdater(self.schema_name, self.ocp_provider, manifest)
-        start_str = (self.dh.last_month_end - timedelta(days=3)).isoformat()
-        end_str = self.dh.last_month_end.isoformat()
+        start_str = (self._date_accessor.last_month_end - timedelta(days=3)).isoformat()
+        end_str = self._date_accessor.last_month_end.isoformat()
         start, end = updater._get_sql_inputs(start_str, end_str)
-        self.assertEqual(start, self.dh.last_month_start.date())
-        self.assertEqual(end, self.dh.last_month_end.date())
+        self.assertEqual(start, self._date_accessor.last_month_start.date())
+        self.assertEqual(end, self._date_accessor.last_month_end.date())
 
         # Current month
         with ReportManifestDBAccessor() as manifest_accessor:
             manifest = manifest_accessor.get_manifest_by_id(2)
         updater = OCPReportParquetSummaryUpdater(self.schema_name, self.ocp_provider, manifest)
-        start_str = self.dh.this_month_start.isoformat()
-        end_str = self.dh.this_month_end.isoformat()
+        start_str = self._date_accessor.this_month_start.isoformat()
+        end_str = self._date_accessor.this_month_end.isoformat()
         start, end = updater._get_sql_inputs(start_str, end_str)
-        self.assertEqual(start, self.dh.this_month_start.date())
-        expected_end = self.dh.this_month_end.replace(day=self.today.day)
+        self.assertEqual(start, self._date_accessor.this_month_start.date())
+        expected_end = self._date_accessor.this_month_end.replace(day=self.today.day)
         self.assertEqual(end, expected_end.date())
