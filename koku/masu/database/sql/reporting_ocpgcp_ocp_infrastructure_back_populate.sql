704bf6024539101a155240d1bc2c9caaa00e71d7
maskarb <mskarbek@redhat.com>
2023-03-10 22:30:09 -0500

    remove "postgres" from sql

--- koku/masu/database/sql/reporting_ocpgcp_ocp_infrastructure_back_populate.sql
+++ koku/masu/database/sql/reporting_ocpgcp_ocp_infrastructure_back_populate.sql
@@ -1,5 +1,5 @@
 
-INSERT INTO postgres.{{schema | sqlsafe}}.reporting_ocpusagelineitem_daily_summary (
+INSERT INTO {{schema | sqlsafe}}.reporting_ocpusagelineitem_daily_summary (
     uuid,
     report_period_id,
     usage_start,
@@ -83,8 +83,8 @@ INSERT INTO postgres.{{schema | sqlsafe}}.reporting_ocpusagelineitem_daily_summa
         0 as volume_request_storage_gigabyte_months,
         0 as persistentvolumeclaim_usage_gigabyte_months,
         max(ocp_gcp.currency) as raw_currency
-    FROM postgres.{{schema | sqlsafe}}.reporting_ocpgcpcostlineitem_project_daily_summary_p AS ocp_gcp
-    JOIN postgres.{{schema | sqlsafe}}.reporting_ocpusagereportperiod AS rp
+    FROM {{schema | sqlsafe}}.reporting_ocpgcpcostlineitem_project_daily_summary_p AS ocp_gcp
+    JOIN {{schema | sqlsafe}}.reporting_ocpusagereportperiod AS rp
         ON ocp_gcp.cluster_id = rp.cluster_id
             AND DATE_TRUNC('month', ocp_gcp.usage_start)::date = date(rp.report_period_start)
     WHERE ocp_gcp.usage_start >= {{start_date}}::date
--- koku/masu/test/database/test_gcp_report_db_accessor.py
+++ koku/masu/test/database/test_gcp_report_db_accessor.py
@@ -407,8 +407,8 @@ class GCPReportDBAccessorTest(MasuTestCase):
         value = self.accessor.check_for_matching_enabled_keys()
         self.assertTrue(value)
 
-    @patch("masu.database.gcp_report_db_accessor.GCPReportDBAccessor._execute_presto_multipart_sql_query")
-    def test_back_populate_ocp_infrastructure_costs(self, mock_presto):
+    @patch("masu.database.gcp_report_db_accessor.GCPReportDBAccessor._execute_raw_sql_query")
+    def test_back_populate_ocp_infrastructure_costs(self, mock_sql_execute):
         """Test that ocp on gcp back populate runs"""
         dh = DateHelper()
         start_date = dh.this_month_start.date()
@@ -416,7 +416,7 @@ class GCPReportDBAccessorTest(MasuTestCase):
         report_period_id = 4
         self.accessor.back_populate_ocp_infrastructure_costs(start_date, end_date, report_period_id)
 
-        mock_presto.assert_called()
+        mock_sql_execute.assert_called()
 
     @patch("masu.database.gcp_report_db_accessor.GCPReportDBAccessor.get_gcp_topology_trino")
     def test_populate_gcp_topology_information_tables(self, mock_get_topo):
