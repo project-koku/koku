0bd6f9e1234a3e3bc69d5159da860a3e95c9e2fb
David N <dnakabaa@redhat.com>
2023-03-14 12:47:03 -0400

    add more patttern matching for hcs aws sql

--- koku/hcs/database/sql/reporting_aws_hcs_daily_summary.sql
+++ koku/hcs/database/sql/reporting_aws_hcs_daily_summary.sql
@@ -13,6 +13,18 @@ WHERE
       bill_billingentity = 'AWS Marketplace'
       AND lineitem_legalentity like '%Red Hat%'
     )
+    OR(
+      bill_billingentity = 'AWS Marketplace'
+      AND lineitem_legalentity like '%RHEL%'
+    )
+    OR(
+      bill_billingentity = 'Amazon Web Services Marketplace'
+      AND lineitem_legalentity like '%Red Hat%'
+    )
+    OR(
+      bill_billingentity = 'Amazon Web Services Marketplace'
+      AND lineitem_legalentity like '%RHEL%'
+    )
     OR (
       lineitem_legalentity like '%Amazon Web Services%'
       AND lineitem_lineitemdescription like '%Red Hat%'
