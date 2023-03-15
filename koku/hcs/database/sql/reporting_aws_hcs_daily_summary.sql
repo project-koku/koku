9d6f5ed279bad4e65bb54c83a708861b38f1d36a
David N <dnakabaa@redhat.com>
2023-03-15 12:22:20 -0400

    clean up sql

--- koku/hcs/database/sql/reporting_aws_hcs_daily_summary.sql
+++ koku/hcs/database/sql/reporting_aws_hcs_daily_summary.sql
@@ -3,46 +3,24 @@ SELECT
   '{{ebs_acct_num | sqlsafe}}' as ebs_account_id,
   '{{org_id | sqlsafe}}' as org_id
 FROM
-  hive.{{schema | sqlsafe}}.{{table | sqlsafe}}
+  hive.{ { schema | sqlsafe } }.{ { table | sqlsafe } }
 WHERE
   source = '{{provider_uuid | sqlsafe}}'
   AND year = '{{year | sqlsafe}}'
   AND month = '{{month | sqlsafe}}'
   AND (
-    (
-      bill_billingentity = 'AWS Marketplace'
-      AND lineitem_legalentity like '%Red Hat%'
+    regexp_like(
+      bill_billingentity,
+      'AWS Marketplace|Amazon Web Services Marketplace'
     )
-    OR(
-      bill_billingentity = 'AWS Marketplace'
-      AND lineitem_legalentity like '%RHEL%'
+    AND regexp_like(lineitem_legalentity, 'RHEL|Red Hat')
+  )
+  OR (
+    regexp_like(
+      lineitem_legalentity,
+      'AWS|Amazon Web Services'
     )
-    OR(
-      bill_billingentity = 'Amazon Web Services Marketplace'
-      AND lineitem_legalentity like '%Red Hat%'
-    )
-    OR(
-      bill_billingentity = 'Amazon Web Services Marketplace'
-      AND lineitem_legalentity like '%RHEL%'
-    )
-    OR (
-      lineitem_legalentity like '%Amazon Web Services%'
-      AND lineitem_lineitemdescription like '%Red Hat%'
-    )
-    OR (
-      lineitem_legalentity like '%Amazon Web Services%'
-      AND lineitem_lineitemdescription like '%RHEL%'
-    )
-    OR (
-      lineitem_legalentity like '%AWS%'
-      AND lineitem_lineitemdescription like '%Red Hat%'
-    )
-    OR (
-      lineitem_legalentity like '%AWS%'
-      AND lineitem_lineitemdescription like '%RHEL%'
-    )
+    AND regexp_like(lineitem_lineitemdescription, 'RHEL|Red Hat')
   )
   AND lineitem_usagestartdate >= TIMESTAMP '{{date | sqlsafe}}'
-  AND lineitem_usagestartdate < date_add(
-    'day', 1, TIMESTAMP '{{date | sqlsafe}}'
-  )
+  AND lineitem_usagestartdate < date_add('day', 1, TIMESTAMP '{{date | sqlsafe}}')
