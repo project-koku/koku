c83f5ce28451c98d0206a69cc6ee82cb4bd62cf4
David N <dnakabaa@redhat.com>
2023-03-23 18:50:53 -0400

    clean up wording

--- koku/api/report/ocp/provider_map.py
+++ koku/api/report/ocp/provider_map.py
@@ -216,7 +216,7 @@ class OCPProviderMap(ProviderMap):
                             "cost_total": self.cloud_infrastructure_cost_by_project
                             + self.markup_cost_by_project
                             + self.cost_model_cost,
-                            "cost_distributed": self.cloud_infrastructure_cost_by_project
+                            "cost_total_distributed": self.cloud_infrastructure_cost_by_project
                             + self.markup_cost_by_project
                             + self.cost_model_cost
                             + self.cost_model_distributed_cost_by_project,
@@ -239,7 +239,7 @@ class OCPProviderMap(ProviderMap):
                             "cost_total": self.cloud_infrastructure_cost_by_project
                             + self.markup_cost_by_project
                             + self.cost_model_cost,
-                            "cost_distributed": self.cloud_infrastructure_cost_by_project
+                            "cost_total_distributed": self.cloud_infrastructure_cost_by_project
                             + self.markup_cost_by_project
                             + self.cost_model_cost
                             + self.cost_model_distributed_cost_by_project,
