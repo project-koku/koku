1e3478f938aa1b8f240b639f8da6bca99afb4d3c
myersCody <cmyers@redhat.com>
2023-03-24 17:10:29 -0400

    update test.

--- koku/masu/test/database/test_ocp_report_db_accessor.py
+++ koku/masu/test/database/test_ocp_report_db_accessor.py
@@ -22,7 +22,6 @@ from django.db.models.query import QuerySet
 from tenant_schemas.utils import schema_context
 from trino.exceptions import TrinoExternalError
 
-from api.common import log_json
 from api.iam.test.iam_test_case import FakeTrinoConn
 from api.utils import DateHelper
 from koku import trino_database as trino_db
@@ -1039,15 +1038,10 @@ select * from eek where val1 in {{report_period_id}} ;
         """Test that updating monthly costs without a matching report period no longer throws an error"""
         start_date = "2000-01-01"
         end_date = "2000-02-01"
-        msg = "No report period for OCP provider, skipping platform_and_worker_distributed_cost_sql update."
-        context = {"provider_uuid": self.provider_uuid, "start_date": {start_date}}
-        expected = f"INFO:masu.database.ocp_report_db_accessor:{log_json(self.provider_uuid, msg, context)}"
-
-        with self.assertLogs("masu.database.ocp_report_db_accessor", level="INFO") as logger:
-            self.accessor.populate_platform_and_worker_distributed_cost_sql(
-                start_date, end_date, self.provider_uuid, {"platform_cost": True}
-            )
-            self.assertIn(expected, logger.output)
+        result = self.accessor.populate_platform_and_worker_distributed_cost_sql(
+            start_date, end_date, self.provider_uuid, {"platform_cost": True}
+        )
+        self.assertIsNone(result)
 
     @patch("masu.database.ocp_report_db_accessor.pkgutil.get_data")
     @patch("masu.database.ocp_report_db_accessor.OCPReportDBAccessor._execute_raw_sql_query")
