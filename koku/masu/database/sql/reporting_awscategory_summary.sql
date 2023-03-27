5cb7fc760b5ed81be49d8aedf9e27fb7c296ea29
myersCody <cmyers@redhat.com>
2023-03-27 10:45:37 -0400

    [COST-3649] Populate aws cost category summary table for type ahead.

---
+++ koku/masu/database/sql/reporting_awscategory_summary.sql
@@ -0,0 +1,59 @@
+WITH cte_tag_value AS (
+    SELECT key,
+        value,
+        li.cost_entry_bill_id,
+        li.usage_account_id
+    FROM {{schema | sqlsafe}}.reporting_awscostentrylineitem_daily_summary AS li,
+        jsonb_each_text(li.cost_category) labels
+    WHERE li.usage_start >= {{start_date}}
+        AND li.usage_start <= {{end_date}}
+    {% if bill_ids %}
+        AND li.cost_entry_bill_id IN (
+        {%- for bill_id in bill_ids -%}
+        {{bill_id}}{% if not loop.last %},{% endif %}
+        {%- endfor -%}
+    )
+    {% endif %}
+    GROUP BY key, value, li.cost_entry_bill_id, li.usage_account_id
+),
+cte_values_agg AS (
+    SELECT tv.key,
+        array_agg(DISTINCT value) as "values",
+        cost_entry_bill_id,
+        usage_account_id,
+        aa.id as account_alias_id
+    FROM cte_tag_value AS tv
+    LEFT JOIN {{schema | sqlsafe}}.reporting_awsaccountalias AS aa
+        ON tv.usage_account_id = aa.account_id
+    GROUP BY tv.key, cost_entry_bill_id, usage_account_id, aa.id
+),
+cte_distinct_values_agg AS (
+    SELECT v.key,
+        array_agg(DISTINCT v."values") as "values",
+        v.cost_entry_bill_id,
+        v.usage_account_id,
+        v.account_alias_id
+    FROM (
+        SELECT va.key,
+            unnest(va."values" || coalesce(ls."values", '{}'::text[])) as "values",
+            va.cost_entry_bill_id,
+            va.usage_account_id,
+            va.account_alias_id
+        FROM cte_values_agg AS va
+        LEFT JOIN {{schema | sqlsafe}}.reporting_awscategory_summary AS ls
+            ON va.key = ls.key
+                AND va.cost_entry_bill_id = ls.cost_entry_bill_id
+                AND va.usage_account_id = ls.usage_account_id
+                AND va.account_alias_id = ls.account_alias_id
+    ) as v
+    GROUP BY key, cost_entry_bill_id, usage_account_id, account_alias_id
+)
+INSERT INTO {{schema | sqlsafe}}.reporting_awscategory_summary (uuid, key, cost_entry_bill_id, usage_account_id, account_alias_id, values)
+SELECT uuid_generate_v4() as uuid,
+    key,
+    cost_entry_bill_id,
+    usage_account_id,
+    account_alias_id,
+    "values"
+FROM cte_distinct_values_agg
+ON CONFLICT (key, cost_entry_bill_id, usage_account_id) DO UPDATE SET values=EXCLUDED."values"
