a5846032afe5256d7adbdd40f8156b712d403303
myersCody <cmyers@redhat.com>
2023-03-28 13:39:23 -0400

    COST-3614: Support type ahead for aws category.

---
+++ koku/api/resource_types/aws_category/view.py
@@ -0,0 +1,53 @@
+#
+# Copyright 2021 Red Hat Inc.
+# SPDX-License-Identifier: Apache-2.0
+#
+"""View for AWS organizational units."""
+from django.conf import settings
+from django.db.models import F
+from django.utils.decorators import method_decorator
+from django.views.decorators.vary import vary_on_headers
+from rest_framework import filters
+from rest_framework import generics
+from rest_framework import status
+from rest_framework.response import Response
+
+from api.common import CACHE_RH_IDENTITY_HEADER
+from api.common.pagination import ResourceTypeViewPaginator
+from api.common.permissions.aws_access import AWSOUAccessPermission
+from api.resource_types.serializers import ResourceTypeSerializer
+from reporting.provider.aws.models import AWSCategorySummary
+
+
+class AWSCategoryView(generics.ListAPIView):
+    """API GET list view for AWS organizational units."""
+
+    queryset = AWSCategorySummary.objects.annotate(**({"value": F("key")})).values("value").distinct()
+    serializer_class = ResourceTypeSerializer
+    permission_classes = [AWSOUAccessPermission]
+    filter_backends = [filters.OrderingFilter, filters.SearchFilter]
+    ordering = ["value"]
+    search_fields = ["value"]
+    pagination_class = ResourceTypeViewPaginator
+
+    @method_decorator(vary_on_headers(CACHE_RH_IDENTITY_HEADER))
+    def list(self, request):
+        # Reads the users values for org unit id and displays values related to what the user has access to
+        supported_query_params = ["search", "limit"]
+        user_access = []
+        error_message = {}
+
+        # Test for only supported query_params
+        if self.request.query_params:
+            for key in self.request.query_params:
+                if key not in supported_query_params:
+                    error_message[key] = [{"Unsupported parameter"}]
+                    return Response(error_message, status=status.HTTP_400_BAD_REQUEST)
+        if settings.ENHANCED_ORG_ADMIN and request.user.admin:
+            return super().list(request)
+        elif request.user.access:
+            user_access = request.user.access.get("aws.account", {}).get("read", [])
+        if user_access and user_access[0] == "*":
+            return super().list(request)
+        self.queryset = self.queryset.filter(usage_account_id__in=user_access)
+        return super().list(request)
