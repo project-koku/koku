3e615902789da9a2f9746a68f36a51c41243f0db
Andrew Berglund <aberglun@redhat.com>
2023-03-30 12:23:14 -0400

    Test update

--- koku/masu/test/processor/gcp/test_gcp_report_parquet_summary_updater.py
+++ koku/masu/test/processor/gcp/test_gcp_report_parquet_summary_updater.py
@@ -121,7 +121,7 @@ class GCPReportParquetSummaryUpdaterTest(MasuTestCase):
             expected_start, expected_end, self.gcp_provider.uuid, current_bill_id, markup_value, start
         )
         mock_tag_update.assert_called_with(bill_ids, start, end)
-        mock_summary_update.assert_called_with(start, end, bill_ids)
+        mock_summary_update.assert_not_called()
 
         self.assertEqual(start_return, start)
         self.assertEqual(end_return, end)
