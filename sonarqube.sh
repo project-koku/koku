ee33a16cb3d7ec2b56a548bedcfa6ca2a2c9ef04
Michael Skarbek <mskarbek@redhat.com>
2023-04-11 12:21:46 -0400

    sonarqube: compare gitBranch to origin/main (#4286)

--- sonarqube.sh
+++ sonarqube.sh
@@ -3,14 +3,14 @@
 set -ex
 
 COMMIT_SHORT=$(git rev-parse --short=7 HEAD)
-gitBranch=${GIT_BRANCH:-main}
+gitBranch=${GIT_BRANCH:-origin/main}
 
 # When doing a PR check, send sonarqube results to a separate branch.
 # Otherwise, send it to the default 'master' branch.
 # Both ${GIT_BRANCH}  and ${ghprbPullId} are provided by App-Interface's Jenkins.
 # SonarQube parameters can be found below:
 #   https://sonarqube.corp.redhat.com/documentation/analysis/pull-request/
-if [[ "${gitBranch}" != "main" ]]; then
+if [[ "${gitBranch}" != "origin/main" ]]; then
     export PR_CHECK_OPTS="-Dsonar.pullrequest.branch=${GIT_BRANCH} -Dsonar.pullrequest.key=${ghprbPullId} -Dsonar.pullrequest.base=main";
 fi
 
