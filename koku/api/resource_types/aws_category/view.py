161bd93ef6641519d160063181a39b14f65aaf5d
myersCody <cmyers@redhat.com>
2023-04-13 12:13:33 -0400

    [COST-3614] Add filter options to aws category resource types endpoint.

--- koku/api/resource_types/aws_category/view.py
+++ koku/api/resource_types/aws_category/view.py
@@ -3,7 +3,10 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 """View for AWS category resource types."""
+import logging
+
 from django.conf import settings
+from django.core.exceptions import PermissionDenied
 from django.db.models import Exists
 from django.db.models import F
 from django.db.models import Func
@@ -17,9 +20,13 @@ from rest_framework.response import Response
 from api.common import CACHE_RH_IDENTITY_HEADER
 from api.common.pagination import ResourceTypeViewPaginator
 from api.common.permissions import AwsAccessPermission
+from api.query_filter import QueryFilter
+from api.query_filter import QueryFilterCollection
 from api.resource_types.aws_category.serializers import AWSCategorySerializer
 from reporting.provider.aws.models import AWSCategorySummary
 from reporting.provider.aws.models import AWSEnabledCategoryKeys
+
+LOG = logging.getLogger(__name__)
 
 
 class AWSCategoryView(generics.ListAPIView):
@@ -36,29 +43,77 @@ class AWSCategoryView(generics.ListAPIView):
         "enabled": Exists(AWSEnabledCategoryKeys.objects.filter(key=OuterRef("key")).filter(enabled=True)),
     }
     queryset = AWSCategorySummary.objects.values("key").annotate(**annotations).distinct()
-    SUPPORTED_FILTERS = ["search", "limit"]
     pagination_class = ResourceTypeViewPaginator
     serializer_class = AWSCategorySerializer
 
+    FILTER_MAP = {
+        "key": {"field": "key", "operation": "icontains", "composition_key": "key_filter"},
+        "value": {"field": "values", "operation": "icontains", "composition_key": "value_filter"},
+        "account": [
+            {
+                "field": "account_alias__account_alias",
+                "operation": "icontains",
+                "composition_key": "account_filter",
+            },
+            {"field": "usage_account_id", "operation": "icontains", "composition_key": "account_filter"},
+        ],
+    }
+    SUPPORTED_FILTERS = ["limit"] + list(FILTER_MAP.keys())
+
+    def check_user_access(self):
+        """
+        Checks to see if we need to filter the account access.
+        """
+        account_filter = self.request.query_params.get("account")
+        if not isinstance(account_filter, list):
+            account_filter = [account_filter]
+        if settings.ENHANCED_ORG_ADMIN and self.request.user.admin:
+            return account_filter
+        elif self.request.user.access:
+            user_access = self.request.user.access.get("aws.account", {}).get("read", [])
+
+        if user_access and user_access[0] == "*":
+            return account_filter
+        if user_access and account_filter:
+            for account in account_filter:
+                if account not in user_access:
+                    LOG.warning(
+                        "User does not have permissions for the requested params: %s. Current access: %s.",
+                        account,
+                        user_access,
+                    )
+                    raise PermissionDenied()
+        return user_access
+
+    def build_filters(self):
+        """Builds the query filters"""
+        filters = QueryFilterCollection()
+        accounts = self.check_user_access()
+        if accounts:
+            for filter in self.FILTER_MAP.get("account"):
+                account_filter = QueryFilter(parameter=accounts, **filter)
+                filters.add(account_filter)
+        for key, value in self.request.query_params.items():
+            if key in ["account", "limit"]:
+                # we set account separately
+                continue
+            filter_kwargs = self.FILTER_MAP.get(key)
+            query_filter = QueryFilter(parameter=value, **filter_kwargs)
+            filters.add(query_filter)
+        return filters.compose()
+
     @method_decorator(vary_on_headers(CACHE_RH_IDENTITY_HEADER))
     def list(self, request):
-        supported_query_params = ["search", "limit"]
-        user_access = []
         error_message = {}
-
         # # Test for only supported query_params
         if self.request.query_params:
             for key in self.request.query_params:
-                if key not in supported_query_params:
+                if key not in self.SUPPORTED_FILTERS:
                     error_message[key] = [{"Unsupported parameter"}]
                     return Response(error_message, status=status.HTTP_400_BAD_REQUEST)
-        if settings.ENHANCED_ORG_ADMIN and request.user.admin:
-            return super().list(request)
-        elif request.user.access:
-            user_access = request.user.access.get("aws.account", {}).get("read", [])
-        if user_access and user_access[0] == "*":
-            return super().list(request)
-        self.queryset = self.queryset.filter(usage_account_id__in=user_access)
+        filters = self.build_filters()
+        if filters:
+            self.queryset = self.queryset.filter(filters)
         return super().list(request)
 
     # Overwrite generics.ListAPIView paginate_queryset to
--- koku/api/resource_types/test/aws_category/test_views.py
+++ koku/api/resource_types/test/aws_category/test_views.py
@@ -8,8 +8,10 @@ from rest_framework import status
 from tenant_schemas.utils import schema_context
 
 from api.iam.test.iam_test_case import RbacPermissions
+from api.report.test.util.constants import AWS_CONSTANTS
 from api.utils import DateHelper
 from masu.test import MasuTestCase
+from reporting.provider.aws.models import AWSCategorySummary
 from reporting.provider.aws.models import AWSEnabledCategoryKeys
 
 
@@ -27,6 +29,7 @@ class ResourceTypesViewTestAWSCategory(MasuTestCase):
         self.dh = DateHelper()
         self.start_date = self.dh.this_month_start
         self.end_date = self.dh.this_month_end
+        self.aws_category_tuple = AWS_CONSTANTS["cost_category"]
 
     @RbacPermissions({"aws.account": {"read": ["*"]}})
     def test_aws_categories_view_with_wildcard(self):
@@ -59,3 +62,65 @@ class ResourceTypesViewTestAWSCategory(MasuTestCase):
         self.assertIsNotNone(json_result.get("data"))
         self.assertIsInstance(json_result.get("data"), list)
         self.assertEqual(len(json_result.get("data")), 0)
+
+    @RbacPermissions({"aws.account": {"read": ["123456"]}})
+    def test_aws_categories_filter_unauthorized_account(self):
+        """Test aws categories return."""
+        url = reverse("aws-categories") + "?account=9999999999999"
+        response = self.client.get(url, **self.headers)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    @RbacPermissions({"aws.account": {"read": ["*"]}})
+    def test_aws_categories_limit_filter(self):
+        """Test aws categories return."""
+        url = reverse("aws-categories") + "?limit=1"
+        response = self.client.get(url, **self.headers)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        json_result = response.json()
+        self.assertIsNotNone(json_result.get("data"))
+        self.assertIsInstance(json_result.get("data"), list)
+        self.assertEqual(len(json_result.get("data")), 1)
+
+    @RbacPermissions({"aws.account": {"read": ["*"]}})
+    def test_aws_categories_key_filter(self):
+        """Test aws categories return."""
+        aws_cat_dict = self.aws_category_tuple[0]
+        aws_cat_key = list(aws_cat_dict.keys())[0]
+        url = reverse("aws-categories") + f"?key={aws_cat_key}"
+        response = self.client.get(url, **self.headers)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        json_result = response.json()
+        self.assertIsNotNone(json_result.get("data"))
+        self.assertIsInstance(json_result.get("data"), list)
+        self.assertEqual(len(json_result.get("data")), 1)
+        for item in json_result.get("data"):
+            self.assertEqual(item.get("key"), aws_cat_key)
+
+    def test_aws_categories_value_filter(self):
+        """Test aws categories return."""
+        aws_cat_dict = self.aws_category_tuple[0]
+        aws_cat_value = list(aws_cat_dict.values())[0]
+        url = reverse("aws-categories") + f"?key={aws_cat_value}"
+        response = self.client.get(url, **self.headers)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        json_result = response.json()
+        self.assertIsNotNone(json_result.get("data"))
+        self.assertIsInstance(json_result.get("data"), list)
+        self.assertEqual(len(json_result.get("data")), 1)
+        for item in json_result.get("data"):
+            self.assertIn(aws_cat_value, item.get("values"))
+
+    def test_aws_categories_account_filter(self):
+        """Test aws categories return."""
+        with schema_context(self.schema):
+            row = AWSCategorySummary.objects.filter(account_alias__isnull=False).first()
+            accounts = [row.usage_account_id, row.account_alias.account_alias]
+        for account in accounts:
+            with self.subTest(account=account):
+                url = reverse("aws-categories") + f"?account={account}"
+                response = self.client.get(url, **self.headers)
+                self.assertEqual(response.status_code, status.HTTP_200_OK)
+                json_result = response.json()
+                self.assertIsNotNone(json_result.get("data"))
+                self.assertIsInstance(json_result.get("data"), list)
+                self.assertNotEqual(len(json_result.get("data")), 0)
