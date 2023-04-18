40adc258251163b7abbab0ce2100a7026bcc0076
Luke Couzens <lcouzens@redhat.com>
2023-04-18 11:03:37 +0100

    fixing sentry substring not found error

--- koku/api/report/queries.py
+++ koku/api/report/queries.py
@@ -16,7 +16,7 @@ from decimal import InvalidOperation
 from functools import cached_property
 from itertools import groupby
 from json import dumps as json_dumps
-from urllib.parse import quote_plus
+from urllib.parse import quote_from_bytes
 
 import ciso8601
 import numpy as np
@@ -661,7 +661,7 @@ class ReportQueryHandler(QueryHandler):
             tag_db_name = self._mapper.tag_column + "__" + strip_prefix(tag, TAG_PREFIX)
             group_data = self.parameters.get_group_by(tag)
             if group_data:
-                tag = quote_plus(tag, safe=URL_ENCODED_SAFE)
+                tag = quote_from_bytes(tag, safe=URL_ENCODED_SAFE)
                 group_pos = self.parameters.url_data.index(tag)
                 group_by.append((tag_db_name, group_pos))
         return group_by
@@ -675,7 +675,7 @@ class ReportQueryHandler(QueryHandler):
                 db_name = aws_category_column + "__" + strip_prefix(aws_category, AWS_CATEGORY_PREFIX)
                 group_data = self.parameters.get_group_by(aws_category)
                 if group_data:
-                    aws_category = quote_plus(aws_category, safe=URL_ENCODED_SAFE)
+                    aws_category = quote_from_bytes(aws_category, safe=URL_ENCODED_SAFE)
                     group_pos = self.parameters.url_data.index(aws_category)
                     group_by.append((db_name, group_pos))
         return group_by
