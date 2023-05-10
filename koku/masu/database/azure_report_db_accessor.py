b0980324916ea092bdcd859951080371862daba3
Andrew Berglund <aberglun@redhat.com>
2023-05-10 13:59:35 -0400

    Add additional schema checks

--- koku/masu/database/azure_report_db_accessor.py
+++ koku/masu/database/azure_report_db_accessor.py
@@ -232,7 +232,7 @@ class AzureReportDBAccessor(SQLScriptAtomicExecutorMixin, ReportDBAccessorBase):
         """Deletes partitions individually for each day in days list."""
         table = "reporting_ocpazurecostlineitem_project_daily_summary"
         retries = settings.HIVE_PARTITION_DELETE_RETRIES
-        if self.table_exists_trino(table):
+        if self.schema_exists_trino() and self.table_exists_trino(table):
             LOG.info(
                 "Deleting Hive partitions for the following: \n\tSchema: %s "
                 "\n\tOCP Source: %s \n\tAzure Source: %s \n\tTable: %s \n\tYear-Month: %s-%s \n\tDays: %s",
