97a3b12fe8df02e7abc49ea38152e7c42d73846c
Sam Doran <sdoran@redhat.com>
2023-05-11 13:06:16 -0400

    Simpler fix using duck typing
    Do not worry about sorting bug related to groupby for now

--- koku/api/report/queries.py
+++ koku/api/report/queries.py
@@ -733,6 +733,7 @@ class ReportQueryHandler(QueryHandler):
 
         Args:
             group_by_list (List): list of strings to group data by
+            group_index (Int): Position in group_by_list that contains group to group by
             data    (List): list of query results
         Returns:
             (Dict): dictionary of grouped query results or the original data
@@ -748,20 +749,33 @@ class ReportQueryHandler(QueryHandler):
         for key, group in groupby(data, lambda by: by.get(curr_group)):
             grouped = list(group)
             grouped = ReportQueryHandler._group_data_by_list(group_by_list, (group_index + 1), grouped)
-            datapoint = out_data.get(key)
-            if datapoint and isinstance(datapoint, dict):
-                if isinstance(grouped, OrderedDict) and isinstance(datapoint, OrderedDict):
-                    datapoint_keys = list(datapoint.keys())
-                    grouped_keys = list(grouped.keys())
-                    intersect_keys = list(set(datapoint_keys).intersection(grouped_keys))
-                    if intersect_keys != []:
-                        for inter_key in intersect_keys:
-                            grouped[inter_key].update(datapoint[inter_key])
-                out_data[key].update(grouped)
-            elif datapoint and isinstance(datapoint, list):
+
+            # Default to empty list for use in the set() constructor later on
+            datapoint = out_data.get(key, [])
+
+            # Start by using the grouped data
+            out_data[key] = grouped
+            try:
+                # If datapoint is a list, combine it with grouped
                 out_data[key] = grouped + datapoint
-            else:
-                out_data[key] = grouped
+            except TypeError:
+                # datapoint is a dictionary
+                #
+                # Update the data if any keys in the grouped dictionary exist in the datapoint.
+                for inter_key in set(datapoint).intersection(grouped):
+                    data_to_update = grouped[inter_key]
+                    try:
+                        data_to_update.update(datapoint[inter_key])
+                    except AttributeError:
+                        # data_to_update is a list of dicts
+                        #
+                        # Since datapoint[inter_key] is the new data, use insert
+                        # so new data appears before existing data in the final list.
+                        # data_to_update.insert(0, *datapoint[inter_key])
+                        data_to_update.extend(datapoint[inter_key])
+
+                out_data[key].update(grouped)
+
         return out_data
 
     def _clean_prefix_grouping_labels(self, group, all_pack_keys=[]):
