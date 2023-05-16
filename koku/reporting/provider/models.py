254ee6ae81e2c43cd6aa28e11cdbe4efe2fbc19b
Andrew Berglund <aberglun@redhat.com>
2023-05-16 13:26:46 -0400

    Model for tenant specific provider table

---
+++ koku/reporting/provider/models.py
@@ -0,0 +1,23 @@
+#
+# Copyright 2023 Red Hat Inc.
+# SPDX-License-Identifier: Apache-2.0
+#
+"""Models for provider management."""
+from uuid import uuid4
+
+from django.db import models
+
+from api.provider.models import Provider
+
+
+class Provider(models.Model):
+    """A tenant specific provider model"""
+
+    class Meta:
+        """Meta for Provider."""
+
+        db_table = "provider"
+
+    uuid = models.UUIDField(default=uuid4, primary_key=True)
+    name = models.TextField(null=False)
+    type = models.TextField(null=False, choices=Provider.PROVIDER_CHOICES)
