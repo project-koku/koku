5115f076a929e3aba36f1d9e4156fb073d09343b
Sam Doran <sdoran@redhat.com>
2023-05-16 17:53:52 -0400

    Do not try maniuplating data if it is not a mapping
    Fall through to calling the parent method, which will raise a validation error
    for non-mapping types.

--- koku/api/report/ocp/serializers.py
+++ koku/api/report/ocp/serializers.py
@@ -3,7 +3,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 """OCP Report Serializers."""
-import logging
+from collections.abc import Mapping
 
 from django.utils.translation import ugettext as _
 from rest_framework import serializers
@@ -17,8 +17,6 @@ from api.report.serializers import ReportQueryParamSerializer
 from api.report.serializers import StringOrListField
 
 DISTRIBUTED_COST_INTERNAL = {"distributed_cost": "cost_total_distributed"}
-
-LOG = logging.getLogger(__name__)
 
 
 class OCPGroupBySerializer(GroupSerializer):
@@ -44,13 +42,9 @@ class OCPOrderBySerializer(OrderSerializer):
 
     def to_internal_value(self, data):
         """Send to internal value."""
-        try:
-            keys = data.keys()
-        except AttributeError as exc:
-            LOG.error(f"Unexpected data encountered during serialization: {exc}")
-        else:
+        if isinstance(data, Mapping):
             for serializer_key, internal_key in DISTRIBUTED_COST_INTERNAL.items():
-                if serializer_key in keys:
+                if serializer_key in data.keys():
                     data[internal_key] = data.pop(serializer_key)
 
         return super().to_internal_value(data)
