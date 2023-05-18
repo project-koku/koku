ca1006ecd80aa5126d0edf65533277af11bfde82
Andrew Berglund <aberglun@redhat.com>
2023-05-18 15:10:51 -0400

    Make masu sources PATCH api for external id

--- koku/masu/api/sources/serializers.py
+++ koku/masu/api/sources/serializers.py
@@ -7,8 +7,10 @@ from rest_framework import serializers
 
 from api.iam.models import Customer
 from api.provider.models import Provider
+from api.provider.models import ProviderAuthentication
 from api.provider.models import ProviderInfrastructureMap
 from api.provider.models import Sources
+from sources.api.view import SourcesException
 
 
 class CustomerSerializer(serializers.Serializer):
@@ -32,6 +34,19 @@ class ProviderInfrastructureSerializer(serializers.Serializer):
     infrastructure_provider_id = serializers.UUIDField()
 
 
+class ProviderAuthenticationSerializer(serializers.ModelSerializer):
+    """Serializer for the Provider Authentication model."""
+
+    uuid = serializers.UUIDField(read_only=True)
+    credentials = serializers.JSONField(allow_null=False, required=True)
+
+    class Meta:
+        """Metadata for the serializer."""
+
+        model = ProviderAuthentication
+        fields = ("uuid", "credentials")
+
+
 class ProviderSerializer(serializers.Serializer):
     """Serializer for Provider."""
 
@@ -45,6 +60,7 @@ class ProviderSerializer(serializers.Serializer):
     active = serializers.BooleanField()
     paused = serializers.BooleanField()
     customer = CustomerSerializer()
+    authentication = ProviderAuthenticationSerializer(required=False)
     infrastructure = ProviderInfrastructureSerializer(required=False)
 
 
@@ -60,6 +76,7 @@ class SourceSerializer(serializers.Serializer):
     auth_header = serializers.CharField()
     offset = serializers.IntegerField()
     account_id = serializers.CharField()
+    org_id = serializers.CharField()
     source_type = serializers.CharField()
     authentication = serializers.JSONField()
     billing_source = serializers.JSONField()
@@ -71,3 +88,21 @@ class SourceSerializer(serializers.Serializer):
     paused = serializers.BooleanField()
     provider = ProviderSerializer()
     additional_context = serializers.JSONField(required=False)
+
+    def update(self, instance, validated_data):
+        """Update authentication for PUT method."""
+        allowed_keys = {"authentication"}
+        if allowed_keys.difference(validated_data.keys()):
+            err = f"Can only PATCH with keys in: {allowed_keys}"
+            raise SourcesException(err)
+
+        if instance.source_type != Provider.PROVIDER_AWS:
+            err = f"Can currently only update credentials for {Provider.PROVIDER_AWS}"
+            raise SourcesException(err)
+
+        authentication = validated_data.get("authentication", {})
+        instance.authentication = authentication
+        instance.provider.authentication.credentials = authentication.get("credentials", {})
+        instance.save()
+        instance.provider.authentication.save()
+        return instance
