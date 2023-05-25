7e9108374c60aadfb98e39d916512c5418c23956
Corey Goodfred <cgoodfre@redhat.com>
2023-05-25 18:06:59 -0400

    update AWSEnabledCategorykeys model to have a UUID

--- koku/reporting/provider/aws/models.py
+++ koku/reporting/provider/aws/models.py
@@ -282,6 +282,7 @@ class AWSEnabledCategoryKeys(models.Model):
         indexes = [models.Index(fields=["key", "enabled"], name="aws_enabled_category_key_index")]
         db_table = "reporting_awsenabledcategorykeys"
 
+    uuid = models.UUIDField(default=uuid4, editable=False, unique=True, null=False)
     key = models.CharField(max_length=253, primary_key=True)
     enabled = models.BooleanField(default=True)
 
