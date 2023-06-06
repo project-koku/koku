1f7aa116ce04725c3781e647aa5bb303cc607488
Luke Couzens <lcouzens@redhat.com>
2023-06-06 17:14:35 +0100

    Drop gcp_openshift_daily

--- scripts/cji_scripts/migrate_trino.py
+++ scripts/cji_scripts/migrate_trino.py
@@ -120,9 +120,7 @@ def main():
     logging.info(schemas)
 
     tables_to_drop = [
-        "aws_openshift_daily_resource_matched_temp",
-        "aws_openshift_daily_tag_matched_temp",
-        "reporting_ocpawscostlineitem_project_daily_summary_temp",
+        "gcp_openshift_daily",
     ]
     # columns_to_drop = ["ocp_matched"]
     # columns_to_add = {
