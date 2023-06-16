587c5a5d95c52102bed918ceb1dc45208062ed5b
Corey Goodfred <cgoodfre@redhat.com>
2023-06-16 05:34:48 -0400

    update Azure HCS query to include additional CCSP filter

--- koku/hcs/database/sql/reporting_azure_hcs_daily_summary.sql
+++ koku/hcs/database/sql/reporting_azure_hcs_daily_summary.sql
@@ -1,12 +1,33 @@
 SELECT *, '{{ebs_acct_num | sqlsafe}}' as ebs_account_id, '{{org_id | sqlsafe}}' as org_id
 FROM hive.{{schema | sqlsafe}}.{{table | sqlsafe}}
-WHERE source = '{{provider_uuid | sqlsafe}}'
-    AND year = '{{year | sqlsafe}}'
-    AND month = '{{month | sqlsafe}}'
-    AND publishertype = 'Marketplace'
-    AND (publishername like '%Red Hat%'
-        OR ((publishername = 'Microsoft' OR publishername = 'Azure') AND (
-           metersubcategory like '%Red Hat%'
-           OR serviceinfo2 like '%Red Hat%')))
-    AND coalesce(date, usagedatetime) >= TIMESTAMP '{{date | sqlsafe}}'
-    AND coalesce(date, usagedatetime) < date_add('day', 1, TIMESTAMP '{{date | sqlsafe}}')
+WHERE
+  source = '{{provider_uuid | sqlsafe}}'
+  AND year = '{{year | sqlsafe}}'
+  AND month = '{{month | sqlsafe}}'
+  AND (
+    -- CCSP
+    publishertype = 'Azure'
+    AND (
+      strpos(metersubcategory, 'Red Hat') > 0
+      OR strpos(serviceinfo2, 'Red Hat') > 0
+    )
+  )
+  OR (
+    publishertype = 'Marketplace'
+    AND (
+      strpos(publishername, 'Red Hat') > 0
+      OR (
+        -- Alternate CCSP
+        (
+          publishername = 'Microsoft'
+          OR publishername = 'Azure'
+        )
+        AND (
+          strpos(metersubcategory, 'Red Hat') > 0
+          OR strpos(serviceinfo2, 'Red Hat') > 0
+        )
+      )
+    )
+  )
+  AND coalesce(date, usagedatetime) >= TIMESTAMP '{{date | sqlsafe}}'
+  AND coalesce(date, usagedatetime) < date_add('day', 1, TIMESTAMP '{{date | sqlsafe}}')
