da23fc995c3ec15ea6bf683d529ffe1b9f5214dd
David N <dnakabaa@redhat.com>
2023-06-29 13:40:51 -0400

    clean up

--- koku/masu/processor/azure/azure_report_parquet_summary_updater.py
+++ koku/masu/processor/azure/azure_report_parquet_summary_updater.py
@@ -45,7 +45,7 @@ class AzureReportParquetSummaryUpdater(PartitionHandlerMixin):
                     last_day_of_month = calendar.monthrange(bill_date.year, bill_date.month)[1]
                     start_date = bill_date
                     end_date = bill_date.replace(day=last_day_of_month)
-                    LOG.info(log_json(msg="overriding start and end date to process full month."))
+                    LOG.info(log_json(msg="overriding start and end date to process full month"))
 
         if isinstance(start_date, str):
             start_date = ciso8601.parse_datetime(start_date).date()
