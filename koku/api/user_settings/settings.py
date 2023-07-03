2622b174272ab3726ed14acc733a3ad47b57d12c
myersCody <cmyers@redhat.com>
2023-07-03 13:01:04 -0400

    Fix copy & paste var.

--- koku/api/user_settings/settings.py
+++ koku/api/user_settings/settings.py
@@ -24,7 +24,7 @@ COST_TYPES = [
     },
 ]
 VALID_COST_TYPES = [cost_type["code"] for cost_type in COST_TYPES]
-COST_TYPE_CHOICES = tuple((currency, currency) for currency in VALID_COST_TYPES)
+COST_TYPE_CHOICES = tuple((cost_type, cost_type) for cost_type in VALID_COST_TYPES)
 
 """Default users settings"""
 # fmt: off
