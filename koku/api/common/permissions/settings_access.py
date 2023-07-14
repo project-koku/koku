76a0aab283883af62709a8cc5245815275f66517
myersCody <cmyers@redhat.com>
2023-07-14 13:46:35 -0400

    Fix settings rbac role deprecation logic.

--- koku/api/common/permissions/settings_access.py
+++ koku/api/common/permissions/settings_access.py
@@ -21,28 +21,11 @@ class SettingsAccessPermission(permissions.BasePermission):
             return False
 
         if request.method in permissions.SAFE_METHODS:
-            if request.user.access.get(self.resource_type, {}).get("read", []):
-                return True
+            # TODO: Uncomment this during deprecated setting removal
+            # if request.user.access.get(self.resource_type, {}).get("read", []):
+            return True
         else:
             setting_write = request.user.access.get(self.resource_type, {}).get("write", [])
             if "*" in setting_write:
                 return True
-        return False
-
-
-# TODO: Remove after settings switch over
-# Deprecated check:
-class DeprecatedSettingsAccessPermission(permissions.BasePermission):
-    """Determines if a user can update Settings data."""
-
-    resource_type = "settings"
-
-    def has_permission(self, request, view):
-        """Check permission to view and update settings data."""
-        if request.user.admin:
-            return True
-
-        if request.method in permissions.SAFE_METHODS:
-            return True
-
         return False
--- koku/api/common/permissions/test/test_settings_access.py
+++ koku/api/common/permissions/test/test_settings_access.py
@@ -7,7 +7,7 @@ from unittest.mock import Mock
 
 from django.test import TestCase
 
-from api.common.permissions.settings_access import DeprecatedSettingsAccessPermission
+from api.common.permissions.settings_access import SettingsAccessPermission
 from api.iam.models import User
 
 
@@ -18,7 +18,7 @@ class SettingsAccessPermissionTest(TestCase):
         """Test that an admin user can execute."""
         user = Mock(spec=User, admin=True)
         req = Mock(user=user)
-        accessPerm = DeprecatedSettingsAccessPermission()
+        accessPerm = SettingsAccessPermission()
         result = accessPerm.has_permission(request=req, view=None)
         self.assertTrue(result)
 
@@ -26,7 +26,7 @@ class SettingsAccessPermissionTest(TestCase):
         """Test that a user read."""
         user = Mock(spec=User, admin=False)
         req = Mock(user=user, method="GET")
-        accessPerm = DeprecatedSettingsAccessPermission()
+        accessPerm = SettingsAccessPermission()
         result = accessPerm.has_permission(request=req, view=None)
         self.assertTrue(result)
 
@@ -34,6 +34,6 @@ class SettingsAccessPermissionTest(TestCase):
         """Test that a user cannot execute POST."""
         user = Mock(spec=User, admin=False)
         req = Mock(user=user, method="POST", META={"PATH_INFO": "http://localhost/api/v1/settings/"})
-        accessPerm = DeprecatedSettingsAccessPermission()
+        accessPerm = SettingsAccessPermission()
         result = accessPerm.has_permission(request=req, view=None)
         self.assertFalse(result)
