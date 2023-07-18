3f8107f10e127650f1c42aeac2f352d0d1c61238
Sam Doran <sdoran@redhat.com>
2023-07-18 10:57:32 -0400

    [COST-3984] Do not update summary tables if there are no AWS bills (#4552)
    * Do not process AWS data if no billing data exists
    * Add test
        - Test that no processing is done if bill info is missing
        - Use a non-naive datetime for the datehelper
    
    * Update AWS get_bills_from_privider to return a list instead of a QuerySet
        An unevaluated QuerySet needs to be evaluated in the schema context, which is
        not obvious. This will always be a small number of items (~<=30) so it storing
        it in memory should not be expensive.
    
    * Keep bill IDs as int
        I see no benefit to casting them to str. They eventually get passed through a
        Jinja template. The type in the model is int.
    
    * Simplify and fix test
        Set attributes on Mock instantiation. Remove the patching of __iter__ and
        first() since the code uses __getitem__ now.
    
    * Update get_bills_from_provider() functions to return a list of objects
        Instead of returning an unevaluated QuerySet, evaluate and return the QuerySet.
        This does cost memory to store the result, but it should be small. The benefit
        is not having to use the schema context on the result in order to avoid errors.
    
    * Remove unnecessary ternary test
        There is now a test for aws_bills before this line is hit.
    
    * Fix test
        - Mock out external calls.
        - Explicitly fail if exeuction gets farther than expected.

--- koku/masu/processor/ocp/ocp_cloud_parquet_summary_updater.py
+++ koku/masu/processor/ocp/ocp_cloud_parquet_summary_updater.py
@@ -224,6 +224,20 @@ class OCPCloudParquetReportSummaryUpdater(PartitionHandlerMixin, OCPCloudUpdater
                 openshift_provider_uuid, report_period.id, start_date, end_date
             )
         aws_bills = aws_get_bills_from_provider(aws_provider_uuid, self._schema, start_date, end_date)
+        if not aws_bills:
+            # Without bill data, we cannot populate the summary table
+            LOG.info(
+                log_json(
+                    msg="no AWS bill data found - skipping AWS summary table update",
+                    schema_name=self._schema,
+                    start_date=start_date,
+                    end_date=end_date,
+                    source_uuid=aws_provider_uuid,
+                    cluster_id=cluster_id,
+                )
+            )
+            return
+
         with schema_context(self._schema):
             self._handle_partitions(
                 self._schema,
@@ -247,8 +261,8 @@ class OCPCloudParquetReportSummaryUpdater(PartitionHandlerMixin, OCPCloudUpdater
                 end_date,
             )
 
-            aws_bill_ids = [str(bill.id) for bill in aws_bills]
-            current_aws_bill_id = aws_bills.first().id if aws_bills else None
+            aws_bill_ids = [bill.id for bill in aws_bills]
+            current_aws_bill_id = aws_bill_ids[0]
             current_ocp_report_period_id = report_period.id
 
         with CostModelDBAccessor(self._schema, aws_provider_uuid) as cost_model_accessor:
