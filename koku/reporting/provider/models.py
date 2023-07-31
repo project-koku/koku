ed61da9ef20573a091391739d4819378add438d9
Corey Goodfred <cgoodfre@redhat.com>
2023-07-31 09:36:53 -0400

    [COST-3890/3892] Data Extraction and Kafka Messaging for SUBS (#4556)
    * add subs data extraction
    
    * add subs message transmission
    
    * adjust kafka message and gate messaging on unleash
    
    * update subs last processed time to be stored in postgres
    
    * handle product_ids for subs processing and case insensitive match for required tag
    
    ---------
    
    Co-authored-by: David Nakabaale <dnakabaa@redhat.com>

--- koku/reporting/provider/models.py
+++ koku/reporting/provider/models.py
@@ -20,3 +20,20 @@ class TenantAPIProvider(models.Model):
     name = models.TextField(null=False)
     type = models.TextField(null=False)
     provider = models.ForeignKey("api.Provider", on_delete=models.CASCADE, null=True)
+
+
+class SubsLastProcessed(models.Model):
+    """A model for storing last processed time for a subs provider"""
+
+    class Meta:
+        """Meta for subs last processed"""
+
+        db_table = "reporting_subs_last_processed_time"
+        unique_together = ("source_uuid", "year", "month")
+
+    source_uuid = models.ForeignKey(
+        "reporting.TenantAPIProvider", on_delete=models.CASCADE, unique=False, null=False, db_column="source_uuid"
+    )
+    year = models.CharField(null=False, max_length=4)
+    month = models.CharField(null=False, max_length=2)
+    latest_processed_time = models.DateTimeField(null=True)
