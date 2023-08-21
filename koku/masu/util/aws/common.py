d317485b9b4934f05d2c96a1b27de1c717636681
Luke Couzens <lcouzens@redhat.com>
2023-08-21 13:40:31 +0100

    Update common.py

--- koku/masu/util/aws/common.py
+++ koku/masu/util/aws/common.py
@@ -790,7 +790,7 @@ def match_openshift_resources_and_labels(data_frame, cluster_topologies, matched
     tags = data_frame["resourcetags"]
     if not tags.isna().values.all():
         tags = tags.str.lower()
-
+        LOG.info("Matching OpenShift on AWS by tags.")
         special_case_tag_matched = tags.str.contains(
             "|".join(["openshift_cluster", "openshift_project", "openshift_node"])
         )
