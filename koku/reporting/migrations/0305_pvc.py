f9667133b65a19f8343860242c50ba69d5a4f77f
myersCody <cmyers@redhat.com>
2023-09-11 11:01:31 -0400

    Fix bad uptick.

--- koku/reporting/migrations/0305_pvc.py
+++ koku/reporting/migrations/0305_pvc.py
@@ -6,7 +6,7 @@ from django.db import models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("reporting", "0305_pvc"),
+        ("reporting", "0304_subslastprocessed"),
     ]
 
     operations = [
