83706e75c977b85ec43ce4a6ca57baf10b60e961
maskarb <mskarbek@redhat.com>
2023-09-14 12:48:34 -0400

    use the filename-stem instead of the report-type to track clearing s3

--- koku/masu/database/report_manifest_db_accessor.py
+++ koku/masu/database/report_manifest_db_accessor.py
@@ -246,20 +246,20 @@ class ReportManifestDBAccessor(KokuDBAccess):
         )
         return result
 
-    def get_s3_parquet_cleared(self, manifest: CostUsageReportManifest, report_type: str = None) -> bool:
+    def get_s3_parquet_cleared(self, manifest: CostUsageReportManifest, file_prefix: str = None) -> bool:
         """Return whether we have cleared CSV files from S3 for this manifest."""
         if not manifest:
             return False
-        if manifest.cluster_id and report_type:
-            return manifest.s3_parquet_cleared_tracker.get(report_type)
+        if manifest.cluster_id and file_prefix:
+            return manifest.s3_parquet_cleared_tracker.get(file_prefix)
         return manifest.s3_parquet_cleared
 
-    def mark_s3_parquet_cleared(self, manifest: CostUsageReportManifest, report_type: str = None) -> None:
+    def mark_s3_parquet_cleared(self, manifest: CostUsageReportManifest, file_prefix: str = None) -> None:
         """Mark Parquet files have been cleared from S3 for this manifest."""
         if not manifest:
             return
-        if manifest.cluster_id and report_type:
-            manifest.s3_parquet_cleared_tracker[report_type] = True
+        if manifest.cluster_id and file_prefix:
+            manifest.s3_parquet_cleared_tracker[file_prefix] = True
             update_fields = ["s3_parquet_cleared_tracker"]
         else:
             manifest.s3_parquet_cleared = True
