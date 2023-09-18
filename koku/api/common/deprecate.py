429cf67bafcdc1ef26d2291c61685d67120a35f6
myersCody <cmyers@redhat.com>
2023-09-18 10:28:51 -0400

    Deprecate wrapper updates.

--- koku/api/common/deprecate.py
+++ koku/api/common/deprecate.py
@@ -1,10 +1,13 @@
-import functools
+from functools import wraps
 
 
-def deprecate_url(func):
-    @functools.wraps(func)
-    def wrapper_deprecate(*args, **kwargs):
-        # TODO: Grab header and add deprecation
-        pass
+def deprecate_view(**kwargs):
+    def _deprecate_controller(viewfunc):
+        @wraps(viewfunc)
+        def _deprecate_controlled(request, *args, **kw):
+            response = viewfunc(request, *args, **kw)
+            return response
 
-    return wrapper_deprecate
+        return _deprecate_controlled
+
+    return _deprecate_controller
