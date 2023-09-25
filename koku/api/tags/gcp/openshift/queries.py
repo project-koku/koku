bc9e923d3f241a6701315a4d9b968aa7dfbaf0d8
maskarb <mskarbek@redhat.com>
2023-09-25 11:33:47 -0400

    add schema name to provider maps

--- koku/api/tags/gcp/openshift/queries.py
+++ koku/api/tags/gcp/openshift/queries.py
@@ -53,7 +53,9 @@ class OCPGCPTagQueryHandler(GCPTagQueryHandler, OCPTagQueryHandler):
 
         """
         self._parameters = parameters
-        self._mapper = OCPGCPProviderMap(provider=self.provider, report_type=parameters.report_type)
+        self._mapper = OCPGCPProviderMap(
+            provider=self.provider, report_type=parameters.report_type, schema_name=parameters.tenant.schema_name
+        )
         if parameters.get_filter("enabled") is None:
             parameters.set_filter(**{"enabled": True})
         # super() needs to be called after _mapper is set
