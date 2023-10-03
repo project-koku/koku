056cc8a074a3be80d8fc867f216a391053315b84
myersCody <cmyers@redhat.com>
2023-10-03 15:16:02 -0400

    Address comments.

--- koku/api/common/deprecate_view.py
+++ koku/api/common/deprecate_view.py
@@ -22,29 +22,27 @@ from api.utils import DateHelper
 LOG = logging.getLogger(__name__)
 HTTP_DATE_FORMAT = "%a, %d %b %Y %H:%M:%S"
 
-# Developer Note:
-# In order to deprecate an endpoint you will need to add the
-# deprecation_datetime & sunset_datetime to the view you are
-# deprecating.
-
-# Example:
-# class SettingsView(APIView):
-#     """
-#     View to interact with settings for a customer.
-#     """
-#     deprecation_datetime = datetime(2023, 9, 22)
-#     sunset_datetime = datetime(2023, 1, 10)
-#     link = "https://github.com/project-koku/koku/pull/4670"
-
-# Then import deprecate_view wrapper and it to the view in the
-# urls.py
-
-# Example:
-# path("settings/", deprecate_view(SettingsView.as_view()), name="settings"),
-
 
 @dataclass
 class DeprecateEndpoint:
+    """
+    In order to deprecate an endpoint you will need to add the
+    deprecation_datetime & sunset_datetime to the view you are
+    deprecating.
+
+    Example:
+    class SettingsView(APIView):
+        deprecation_datetime = datetime(2023, 9, 22)
+        sunset_datetime = datetime(2023, 1, 10)
+        link = "https://github.com/project-koku/koku/pull/4670"
+
+    Then import deprecate_view wrapper and add it to the view in the
+    urls.py
+
+    Example:
+        path("settings/", deprecate_view(SettingsView.as_view()), name="settings"),
+    """
+
     viewclass: t.Callable
     sunset_endpoint: bool = field(init=False, default=False)
 
@@ -73,7 +71,7 @@ def deprecate_view(viewfunc):
     def _wrapped_view_func(request, *args, **kw):
         deprecate = DeprecateEndpoint(viewfunc.view_class)
         response = viewfunc(request, *args, **kw)
-        deprecate.extract_data_from_view(response)
+        deprecate.update_response_headers(response)
         if deprecate.sunset_endpoint:
             return SunsetView(request, *args, **kw)
         return response
