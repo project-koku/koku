c38d5c148a9137c6b621ca3ce91e8e9a177e2661
Sam Doran <sdoran@redhat.com>
2023-10-26 23:01:16 -0400

    Add filtering and ordering

--- koku/api/settings/platform/view.py
+++ koku/api/settings/platform/view.py
@@ -4,15 +4,69 @@
 #
 from django.utils.decorators import method_decorator
 from django.views.decorators.cache import never_cache
+from querystring_parser import parser
 from rest_framework.views import APIView
 
 from api.common.pagination import ListPaginator
 from api.common.permissions.settings_access import SettingsAccessPermission
 from api.deprecated_settings.settings import Settings
+from api.report.constants import URL_ENCODED_SAFE
 from api.settings.serializers import NonEmptyListSerializer
 from reporting.provider.ocp.models import OpenshiftCostCategory
 
 SETTINGS_GENERATORS = {"settings": Settings}
+
+
+class PlatformFilter:
+    valid_fields = {"name", "kind", "project"}
+    _default_platform_projects = ("kube-%", "openshift-%", "Platform unallocated")
+
+    def __init__(self, request):
+        self.request = request
+        self._order_by = None
+        self._filter_params = None
+        self._query_params = None
+
+    @property
+    def query_params(self):
+        if self._query_params is None:
+            self._query_params = parser.parse(self.request.query_params.urlencode(safe=URL_ENCODED_SAFE))
+
+        return self._query_params
+
+    @property
+    def order_by(self):
+        if self._order_by is None:
+            self._order_by = self.query_params.get("order_by", {"name": "asc"})
+
+        return self._order_by
+
+    @property
+    def filter_params(self):
+        if self._filter_params is None:
+            self._filter_params = self.query_params.get("filter", {})
+
+        return self._filter_params
+
+    def filter_data(self, data):
+        result = []
+        for item in data:
+            group = "default" if item in self._default_platform_projects else "platform"
+            result.append({"group": group, "name": item})
+
+        for key, value in self.filter_params.items():
+            # FIXME: This needs to do a logical OR for multiple filters of the same field
+            result = [item for item in result if value.lower() in item.get(key, "")]
+
+        field, order = next(iter(self.order_by.items()))
+        reverse = order.lower().startswith("desc")
+
+        def _sort_key(value):
+            return value.get(field, "name")
+
+        result.sort(key=_sort_key, reverse=reverse)
+
+        return result
 
 
 class PlatformCategoriesView(APIView):
@@ -23,7 +77,7 @@ class PlatformCategoriesView(APIView):
     """
 
     permission_classes = (SettingsAccessPermission,)
-    _default_platform_projects = ("kube-%", "openshift-%", "Platform unallocated")
+    filter_class = PlatformFilter
 
     @property
     def _platform_record(self):
@@ -31,7 +85,9 @@ class PlatformCategoriesView(APIView):
 
     @method_decorator(never_cache)
     def get(self, request):
-        paginator = ListPaginator(self._platform_record.namespace, request)
+        filter_class = self.filter_class(request)
+        data = filter_class.filter_data(self._platform_record.namespace)
+        paginator = ListPaginator(data, request)
 
         return paginator.paginated_response
 
@@ -39,12 +95,14 @@ class PlatformCategoriesView(APIView):
         serializer = NonEmptyListSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
 
+        filter_class = self.filter_class(request)
+
         projects = serializer.validated_data["projects"]
         platform_record = self._platform_record
         platform_record.namespace = list(set(platform_record.namespace).union(projects))
         platform_record.save()
 
-        paginator = ListPaginator(self._platform_record.namespace, request)
+        paginator = ListPaginator(filter_class.filter_data(platform_record.namespace), request)
 
         return paginator.paginated_response
 
@@ -52,12 +110,14 @@ class PlatformCategoriesView(APIView):
         serializer = NonEmptyListSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
 
+        filter_class = self.filter_class(request)
+
         projects = serializer.validated_data["projects"]
-        request_without_defaults = set(projects).difference(self._default_platform_projects)
+        request_without_defaults = set(projects).difference(self.filter_class._default_platform_projects)
         platform_record = self._platform_record
         platform_record.namespace = list(set(platform_record.namespace).difference(request_without_defaults))
         platform_record.save()
 
-        paginator = ListPaginator(self._platform_record.namespace, request)
+        paginator = ListPaginator(filter_class.filter_data(platform_record.namespace), request)
 
         return paginator.paginated_response
