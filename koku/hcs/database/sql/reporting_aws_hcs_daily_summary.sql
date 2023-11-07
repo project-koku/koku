cbf0be02750f6f1a1c014a48230ff04ea10c100d
Corey Goodfred <cgoodfre@redhat.com>
2023-11-07 14:54:21 -0500

    [COST-4366] Remove CCSP data filter from AWS HCS temporarily(#4751)

--- koku/hcs/database/sql/reporting_aws_hcs_daily_summary.sql
+++ koku/hcs/database/sql/reporting_aws_hcs_daily_summary.sql
@@ -13,22 +13,22 @@ WHERE
       bill_billingentity = 'AWS Marketplace'
       AND lineitem_legalentity like '%Red Hat%'
     )
-    OR (
-      lineitem_legalentity like '%Amazon Web Services%'
-      AND lineitem_lineitemdescription like '%Red Hat%'
-    )
-    OR (
-      lineitem_legalentity like '%Amazon Web Services%'
-      AND lineitem_lineitemdescription like '%RHEL%'
-    )
-    OR (
-      lineitem_legalentity like '%AWS%'
-      AND lineitem_lineitemdescription like '%Red Hat%'
-    )
-    OR (
-      lineitem_legalentity like '%AWS%'
-      AND lineitem_lineitemdescription like '%RHEL%'
-    )
+    -- OR (
+    --   lineitem_legalentity like '%Amazon Web Services%'
+    --   AND lineitem_lineitemdescription like '%Red Hat%'
+    -- )
+    -- OR (
+    --   lineitem_legalentity like '%Amazon Web Services%'
+    --   AND lineitem_lineitemdescription like '%RHEL%'
+    -- )
+    -- OR (
+    --   lineitem_legalentity like '%AWS%'
+    --   AND lineitem_lineitemdescription like '%Red Hat%'
+    -- )
+    -- OR (
+    --   lineitem_legalentity like '%AWS%'
+    --   AND lineitem_lineitemdescription like '%RHEL%'
+    -- )
   )
   AND lineitem_usagestartdate >= {{date}}
   AND lineitem_usagestartdate < date_add(
