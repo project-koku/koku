fb3f74aa6a43b0cc078e816193db144e479dde17
Corey Goodfred <cgoodfre@redhat.com>
2023-12-14 12:29:13 -0500

    consolidate to one statement without the UNION to avoid any duplicate risk

--- koku/hcs/database/sql/reporting_aws_hcs_daily_summary.sql
+++ koku/hcs/database/sql/reporting_aws_hcs_daily_summary.sql
@@ -1,4 +1,3 @@
--- Data with no estimated cost
 SELECT
   lineitem_resourceid,
   lineitem_usagestartdate,
@@ -21,7 +20,19 @@ SELECT
   lineitem_normalizedusageamount,
   lineitem_currencycode,
   lineitem_unblendedrate,
-  lineitem_unblendedcost,
+  CASE
+    WHEN
+      (
+        try_cast(product_vcpu AS INT) <= 4
+        AND (lineitem_lineitemdescription LIKE '%Red Hat%' OR lineitem_lineitemdescription LIKE '%RHEL%')
+      ) THEN 0.06 * lineitem_usageamount
+    WHEN
+      (
+        try_cast(product_vcpu AS INT) > 4
+        AND (lineitem_lineitemdescription LIKE '%Red Hat%' OR lineitem_lineitemdescription LIKE '%RHEL%')
+      ) THEN 0.13 * lineitem_usageamount
+    ELSE lineitem_unblendedcost
+  END AS lineitem_unblendedcost,
   lineitem_blendedrate,
   lineitem_blendedcost,
   pricing_publicondemandcost,
@@ -56,59 +67,8 @@ WHERE
       lineitem_legalentity LIKE '%AWS%'
       AND product_productname LIKE '%Red Hat%'
     )
-  )
-  AND lineitem_usagestartdate >= {{date}}
-  AND lineitem_usagestartdate < date_add('day', 1, {{date}})
-UNION
-  -- CCSP data with an estimated cost
-SELECT
-  lineitem_resourceid,
-  lineitem_usagestartdate,
-  bill_payeraccountid,
-  lineitem_usageaccountid,
-  lineitem_legalentity,
-  lineitem_lineitemdescription,
-  bill_billingentity,
-  lineitem_productcode,
-  lineitem_availabilityzone,
-  lineitem_lineitemtype,
-  product_productfamily,
-  product_instancetype,
-  product_region,
-  pricing_unit,
-  resourcetags,
-  costcategory,
-  lineitem_usageamount,
-  lineitem_normalizationfactor,
-  lineitem_normalizedusageamount,
-  lineitem_currencycode,
-  lineitem_unblendedrate,
-  CASE
-    WHEN try_cast(product_vcpu AS INT) <= 4 THEN 0.06 * lineitem_usageamount
-    WHEN try_cast(product_vcpu AS INT) > 4 THEN 0.13 * lineitem_usageamount
-    ELSE lineitem_unblendedcost
-  END AS lineitem_unblendedcost,
-  lineitem_blendedrate,
-  lineitem_blendedcost,
-  pricing_publicondemandcost,
-  pricing_publicondemandrate,
-  savingsplan_savingsplaneffectivecost,
-  product_productname,
-  bill_invoiceid,
-  product_vcpu,
-  source,
-  year,
-  month,
-  {{ebs_acct_num}} as ebs_account_id,
-  {{org_id}} as org_id
-FROM
-  hive.{{schema | sqlsafe}}.{{table | sqlsafe}}
-WHERE
-  source = {{provider_uuid}}
-  AND year = {{year}}
-  AND month = {{month}}
-  AND (
-    (
+    -- CCSP with estimated costs
+    OR (
       lineitem_legalentity LIKE '%Amazon Web Services%'
       AND lineitem_lineitemdescription LIKE '%Red Hat%'
     )
