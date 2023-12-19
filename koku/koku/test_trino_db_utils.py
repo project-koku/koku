3e9d4b6ae124976253a96143191628d7ac8117b6
Sam Doran <sdoran@redhat.com>
2023-12-19 17:14:49 -0500

    Dedent raw SQL statements in tests for better legibility

--- koku/koku/test_trino_db_utils.py
+++ koku/koku/test_trino_db_utils.py
@@ -1,3 +1,4 @@
+import textwrap
 import uuid
 
 from jinjasql import JinjaSql
@@ -24,24 +25,24 @@ class TestTrinoDatabaseUtils(IamTestCase):
         Test execution of a buffer containing multiple statements
         """
         sqlscript = """
-drop table if exists hive.{{schema | sqlsafe}}.__test_{{uuid | sqlsafe}};
-create table hive.{{schema | sqlsafe}}.__test_{{uuid | sqlsafe}}
-(
-    id varchar,
-    i_data integer,
-    t_data varchar
-);
+            drop table if exists hive.{{schema | sqlsafe}}.__test_{{uuid | sqlsafe}};
+            create table hive.{{schema | sqlsafe}}.__test_{{uuid | sqlsafe}}
+            (
+                id varchar,
+                i_data integer,
+                t_data varchar
+            );
 
-insert into hive.{{schema | sqlsafe}}.__test_{{uuid | sqlsafe}} (id, i_data, t_data)
-values (cast(uuid() as varchar), 10, 'default');
+            insert into hive.{{schema | sqlsafe}}.__test_{{uuid | sqlsafe}} (id, i_data, t_data)
+            values (cast(uuid() as varchar), 10, 'default');
 
-insert into hive.{{schema | sqlsafe}}.__test_{{uuid | sqlsafe}} (id, i_data, t_data)
-values (cast(uuid() as varchar), {{int_data}}, {{txt_data}});
+            insert into hive.{{schema | sqlsafe}}.__test_{{uuid | sqlsafe}} (id, i_data, t_data)
+            values (cast(uuid() as varchar), {{int_data}}, {{txt_data}});
 
-select t_data from hive.{{schema | sqlsafe}}.__test_{{uuid | sqlsafe}} where i_data = {{int_data}};
+            select t_data from hive.{{schema | sqlsafe}}.__test_{{uuid | sqlsafe}} where i_data = {{int_data}};
 
-drop table if exists hive.{{schema | sqlsafe}}.__test_{{uuid | sqlsafe}};
-"""
+            drop table if exists hive.{{schema | sqlsafe}}.__test_{{uuid | sqlsafe}};
+        """
         conn = FakeTrinoConn()
         params = {
             "uuid": str(uuid.uuid4()).replace("-", "_"),
@@ -49,7 +50,9 @@ drop table if exists hive.{{schema | sqlsafe}}.__test_{{uuid | sqlsafe}};
             "int_data": 255,
             "txt_data": "This is a test",
         }
-        results = trino_db.executescript(conn, sqlscript, params=params, preprocessor=JinjaSql().prepare_query)
+        results = trino_db.executescript(
+            conn, textwrap.dedent(sqlscript), params=params, preprocessor=JinjaSql().prepare_query
+        )
         self.assertEqual(results, [["eek"], ["eek"], ["eek"], ["eek"], ["eek"], ["eek"]])
 
     def test_executescript_err(self):
@@ -58,22 +61,24 @@ drop table if exists hive.{{schema | sqlsafe}}.__test_{{uuid | sqlsafe}};
         """
         conn = FakeTrinoConn()
         sqlscript = """
-select * from eek where val1 in {{val_list}};
-"""
+            select * from eek where val1 in {{val_list}};
+        """
         params = {"val_list": (1, 2, 3, 4, 5)}
         with self.assertRaises(trino_db.PreprocessStatementError):
-            trino_db.executescript(conn, sqlscript, params=params, preprocessor=JinjaSql().prepare_query)
+            trino_db.executescript(
+                conn, textwrap.dedent(sqlscript), params=params, preprocessor=JinjaSql().prepare_query
+            )
 
     def test_executescript_no_preprocess(self):
         """
         Test executescript will raise a preprocessor error
         """
         sqlscript = """
-select x from y;
-select a from b;
-"""
+            select x from y;
+            select a from b;
+        """
         conn = FakeTrinoConn()
-        res = trino_db.executescript(conn, sqlscript)
+        res = trino_db.executescript(conn, textwrap.dedent(sqlscript))
         self.assertEqual(res, [["eek"], ["eek"]])
 
     def test_preprocessor_err(self):
@@ -81,13 +86,13 @@ select a from b;
             raise TypeError("This is a test")
 
         sqlscript = """
-select x from y;
-select a from b;
-"""
+            select x from y;
+            select a from b;
+        """
         params = {"eek": 1}
         conn = FakeTrinoConn()
         with self.assertRaises(trino_db.PreprocessStatementError):
-            trino_db.executescript(conn, sqlscript, params=params, preprocessor=t_preprocessor)
+            trino_db.executescript(conn, textwrap.dedent(sqlscript), params=params, preprocessor=t_preprocessor)
 
     def test_executescript_error(self):
         def t_exec_error(*args, **kwargs):
@@ -102,9 +107,9 @@ select a from b;
                 return FakerFakeTrinoCur()
 
         sqlscript = """
-select x from y;
-select a from b;
-"""
-        with self.assertRaises(trino_db.TrinoStatementExecError):
+            select x from y;
+            select a from b;
+        """
+        with self.assertRaises(ValueError):
             conn = FakerFakeTrinoConn()
             trino_db.executescript(conn, sqlscript)
