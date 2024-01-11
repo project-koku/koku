d7cdbae20f447ed983206ef17d2b88246265ef43
Sam Doran <sdoran@redhat.com>
2024-01-11 14:48:44 -0500

    Update deprecated ‘warn’ method

--- koku/masu/processor/parquet/parquet_report_processor.py
+++ koku/masu/processor/parquet/parquet_report_processor.py
@@ -441,7 +441,7 @@ class ParquetReportProcessor:
             file_list = self.split_file_list
 
         if not file_list:
-            LOG.warn(
+            LOG.warning(
                 log_json(
                     self.tracing_id,
                     msg="no split files to convert to parquet",
@@ -453,7 +453,7 @@ class ParquetReportProcessor:
         for csv_filename in file_list:
             self.prepare_parquet_s3(Path(csv_filename))
             if self.provider_type == Provider.PROVIDER_OCP and self.report_type is None:
-                LOG.warn(
+                LOG.warning(
                     log_json(
                         self.tracing_id,
                         msg="could not establish report type",
@@ -474,7 +474,7 @@ class ParquetReportProcessor:
                 failed_conversion.append(csv_filename)
 
         if failed_conversion:
-            LOG.warn(
+            LOG.warning(
                 log_json(
                     self.tracing_id,
                     msg="failed to convert files to parquet",
@@ -563,7 +563,7 @@ class ParquetReportProcessor:
                 self.create_parquet_table(parquet_filepath)
 
         except Exception as err:
-            LOG.warn(
+            LOG.warning(
                 log_json(
                     self.tracing_id,
                     msg="could not write parquet to temp file",
@@ -657,7 +657,7 @@ class ParquetReportProcessor:
                 )
         except Exception as err:
             s3_key = f"{self.parquet_path_s3}/{file_path}"
-            LOG.warn(
+            LOG.warning(
                 log_json(
                     self.tracing_id,
                     msg="file could not be written to s3",
