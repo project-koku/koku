7148d61a0a59dd913cf17d8a8712f03ec11f5732
Corey Goodfred <cgoodfre@redhat.com>
2024-01-24 16:53:26 -0500

    update logic to delete all months from now until october

--- koku/masu/management/commands/aws_null_bill_cleanup.py
+++ koku/masu/management/commands/aws_null_bill_cleanup.py
@@ -1,17 +1,21 @@
 from __future__ import annotations
 
 import argparse
+import logging
 from typing import Any
 
 from dateutil import parser
-from django.conf import settings
+from dateutil.relativedelta import relativedelta
 from django.core.management.base import BaseCommand
-from django.db import DEFAULT_DB_ALIAS
 from django_tenants.utils import schema_context
 
+from api.common import log_json
 from api.provider.models import Provider
 from koku.database import cascade_delete
 from reporting.models import AWSCostEntryBill
+
+
+LOG = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
@@ -19,66 +23,63 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser: argparse.ArgumentParser) -> None:
         parser.add_argument(
-            "--database",
-            default=DEFAULT_DB_ALIAS,
-            help='Which database to update. Defaults to the "default" database.',
-        )
-        parser.add_argument(
             "--delete",
             action="store_true",
             default=False,
             help="Actually delete the cost entry bills.",
         )
-        parser.add_argument(
-            "--bill-date",
-            required=True,
-            help="The date to cleanup bills for.",
-        )
 
-    def handle(self, *args: Any, database: str, delete: bool, bill_date: str, **kwargs: Any) -> None:
-        def output(text: str) -> None:
-            self.stdout.write(text)
-            self.stdout.flush()
-
+    def handle(self, *args: Any, delete: bool, **kwargs: Any) -> None:
         if not delete:
-            output("In dry run mode (--delete not passed)")
+            LOG.info(log_json(msg="In dry run mode (--delete not passed)"))
+        else:
+            LOG.info(log_json(msg="DELETING BILLS (--delete passed)"))
+        cleanup_aws_bills(delete)
 
-        cleanup_aws_bills(delete, bill_date)
 
-
-def cleanup_aws_bills(delete, bill_date):
-    formatted_date = parser.parse(bill_date).replace(tzinfo=settings.UTC)
-    print(f"attempting to cleanup bills for a billing_period_start of {formatted_date}")
+def cleanup_aws_bills(delete):
+    """Deletes AWS Bills with a null payer account ID for January 2024 back through October 2023."""
+    SUMMARY_URL_TEMPLATE = "api/cost-management/v1/report_data/?schema={}&provider_uuid={}&start_date={}&end_date={}"
+    DATE_FORMAT = "%Y-%m-%d"
+    initial_date = parser.parse("2024-01-01T00:00:00+00:00")
     total_cleaned_bills = 0
     total_cleaned_providers = 0
     providers = Provider.objects.filter(type__in=[Provider.PROVIDER_AWS, Provider.PROVIDER_AWS_LOCAL])
     for prov in providers:
         schema = prov.customer.schema_name
         provider_uuid = prov.uuid
+        total_cleaned_providers += 1
+        start_date = initial_date
+        # adding a relative delta of day=31 will not cross a month boundary whereas days=31 would
+        end_date = start_date + relativedelta(day=31)
         with schema_context(schema):
-            bills = AWSCostEntryBill.objects.filter(
-                provider_id=provider_uuid, payer_account_id=None, billing_period_start=formatted_date
-            )
-            if not bills:
-                print(f"no bills found with empty payer account for provider: {provider_uuid} on schema: {schema}")
-                continue
-            num_bills = len(bills)
-            total_cleaned_providers += 1
-            total_cleaned_bills += num_bills
-            print(
-                f"found {num_bills} bills with empty payer account for provider: {provider_uuid} on schema: {schema}"
-            )
-            if delete:
-                print("beginning cascade deletes")
-                cascade_delete(bills.query.model, bills)
-                print("cascade delete finished")
-            else:
-                bill_ids = [bill.id for bill in bills]
-                print(f"bills {bill_ids} would be deleted for provider: {provider_uuid}")
+            while start_date >= parser.parse("2023-10-01T00:00:00+00:00"):
+                bills = AWSCostEntryBill.objects.filter(
+                    provider_id=provider_uuid, payer_account_id=None, billing_period_start=start_date
+                )
+                summary_url = SUMMARY_URL_TEMPLATE.format(
+                    schema, provider_uuid, start_date.strftime(DATE_FORMAT), end_date.strftime(DATE_FORMAT)
+                )
+                start_date = start_date - relativedelta(months=1)
+                end_date = start_date + relativedelta(day=31)
+                if not bills:
+                    continue
+                num_bills = len(bills)
+                total_cleaned_bills += num_bills
+                if delete:
+                    cascade_delete(bills.query.model, bills)
+                    LOG.info(log_json(msg="Deletes completed and ready for summary", summary_url=summary_url))
+
+                else:
+                    bill_ids = [bill.id for bill in bills]
+                    LOG.info(log_json(msg=f"bills {bill_ids} would be deleted for provider: {provider_uuid}"))
+
     if delete:
-        print(f"{total_cleaned_bills} bills deleted across {total_cleaned_providers} providers.")
+        LOG.info(log_json(msg=f"{total_cleaned_bills} bills deleted across {total_cleaned_providers} providers."))
 
     else:
-        print(
-            f"{total_cleaned_bills} bills would be deleted across {total_cleaned_providers} providers on a real run."
+        LOG.info(
+            log_json(
+                msg=f"DRY RUN: {total_cleaned_bills} bills would be deleted across {total_cleaned_providers} providers."
+            )
         )
