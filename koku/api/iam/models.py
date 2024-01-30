ea13b05a263ed6e851dbdfa3a3ba93efe6fc3afa
Sam Doran <sdoran@redhat.com>
2024-01-30 12:50:29 -0500

    Correct docstring and class attribute name

--- koku/api/iam/models.py
+++ koku/api/iam/models.py
@@ -85,14 +85,15 @@ class Tenant(TenantMixin):
     _TEMPLATE_SCHEMA = os.environ.get("TEMPLATE_SCHEMA", "template0")
     _CLONE_SCHEMA_FUNC_FILENAME = os.path.join(find_db_functions_dir(), "clone_schema.sql")
     _CLONE_SCHEMA_FUNC_SCHEMA = "public"
-    _CLONE_SHEMA_FUNC_NAME = "clone_schema"
+    _CLONE_SCHEMA_FUNC_NAME = "clone_schema"
     _CLONE_SCHEMA_FUNC_SIG = (
-        f"{_CLONE_SCHEMA_FUNC_SCHEMA}.{_CLONE_SHEMA_FUNC_NAME}("
+        f"{_CLONE_SCHEMA_FUNC_SCHEMA}.{_CLONE_SCHEMA_FUNC_NAME}("
         "source_schema text, dest_schema text, "
         "copy_data boolean DEFAULT false, "
         "_verbose boolean DEFAULT false"
         ")"
     )
+    _CLONE_SCHEMA_FUNC_SHA256 = "ca31203705f4485213f11f0fd6686d492d29857a5240026f6f992737ce1823cf"
 
     # Override the mixin domain url to make it nullable, non-unique
     domain_url = None
@@ -102,16 +103,16 @@ class Tenant(TenantMixin):
     auto_create_schema = False
 
     def _check_clone_func(self):
-        LOG.info(f'Verify that clone function "{self._CLONE_SCHEMA_FUNC_SIG}" exists')
+        LOG.info(f'Verify that clone function "{self._CLONE_SCHEMA_FUNC_NAME}" exists')
         res = dbfunc_exists(
-            conn, self._CLONE_SCHEMA_FUNC_SCHEMA, self._CLONE_SHEMA_FUNC_NAME, self._CLONE_SCHEMA_FUNC_SIG
+            conn, self._CLONE_SCHEMA_FUNC_SCHEMA, self._CLONE_SCHEMA_FUNC_NAME, self._CLONE_SCHEMA_FUNC_SIG
         )
         if not res:
-            LOG.warning(f'Clone function "{self._CLONE_SCHEMA_FUNC_SIG}" does not exist')
-            LOG.info(f'Creating clone function "{self._CLONE_SCHEMA_FUNC_SIG}"')
+            LOG.warning(f'Clone function "{self._CLONE_SCHEMA_FUNC_NAME}" does not exist')
+            LOG.info(f'Creating clone function "{self._CLONE_SCHEMA_FUNC_NAME}"')
             apply_sql_file(conn.schema_editor(), self._CLONE_SCHEMA_FUNC_FILENAME, literal_placeholder=True)
             res = dbfunc_exists(
-                conn, self._CLONE_SCHEMA_FUNC_SCHEMA, self._CLONE_SHEMA_FUNC_NAME, self._CLONE_SCHEMA_FUNC_SIG
+                conn, self._CLONE_SCHEMA_FUNC_SCHEMA, self._CLONE_SCHEMA_FUNC_NAME, self._CLONE_SCHEMA_FUNC_SIG
             )
         else:
             LOG.info("Clone function exists")
@@ -120,7 +121,7 @@ class Tenant(TenantMixin):
 
     def _verify_template(self, verbosity=1):
         LOG.info(f'Verify that template schema "{self._TEMPLATE_SCHEMA}" exists')
-        # This is using the teanant table data as the source of truth which can be dangerous.
+        # This is using the tenant table data as the source of truth which can be dangerous.
         # If this becomes unreliable, then the database itself should be the source of truth
         # and extra code must be written to handle the sync of the table data to the state of
         # the database.
