ce335d2fcca096d6f9bb983f55198f269ce80ec8
Sam Doran <sdoran@redhat.com>
2024-01-31 17:47:17 -0500

    Improvements to serial to identity management command
    - Use SQL composition rather than format strings to protect against SQL injection
      attacks. The risk is quite low, but it’s best to just do the right thing.
    - Reorganize the code to avoid a nested function definiton and an unnecessary
      global variable that only needed to be local.

--- koku/masu/management/commands/migrate_serial_to_identity_columns.py
+++ koku/masu/management/commands/migrate_serial_to_identity_columns.py
@@ -1,12 +1,13 @@
-# Copyright 2022 Adam Johnson
+# Copyright (c) 2022 Adam Johnson
+# Copyright (c) 2024 Red Hat, Inc.
 #
 # SPDX-License-Identifier: MIT
 #
 # "Migrate PostgreSQL IDs from serial to identity after upgrading to Django 4.1"
 # https://adamj.eu/tech/2022/10/21/migrate-postgresql-ids-serial-identity-django-4.1/
-from __future__ import annotations
 
 import argparse
+import textwrap
 from collections.abc import Callable
 from typing import Any
 
@@ -15,6 +16,7 @@ from django.db import connections
 from django.db import DEFAULT_DB_ALIAS
 from django.db.backends.utils import CursorWrapper
 from django.db.transaction import atomic
+from psycopg2 import sql
 
 
 class Command(BaseCommand):
@@ -44,55 +46,53 @@ class Command(BaseCommand):
         )
 
     def handle(self, *args: Any, database: str, write: bool, like: str, schema: str, **kwargs: Any) -> None:
-        def output(text: str) -> None:
-            self.stdout.write(text)
-            self.stdout.flush()
+        # Adapted from: https://dba.stackexchange.com/a/90567
+        find_serial_columns = """\
+            SELECT
+                 a.attrelid::regclass::text AS table_name,
+                 a.attname AS column_name
+            FROM pg_attribute a
+                 JOIN pg_class c ON c.oid = a.attrelid
+            WHERE
+                a.attrelid::regclass::text LIKE %s
+                AND c.relkind IN ('r', 'p')  /* regular and partitioned tables */
+                AND a.attnum > 0
+                AND NOT a.attisdropped
+                AND a.atttypid = ANY ('{int,int8,int2}'::regtype[])
+                AND EXISTS (
+                    SELECT FROM pg_attrdef ad
+                    WHERE
+                        ad.adrelid = a.attrelid
+                        AND ad.adnum = a.attnum
+                        AND (
+                            pg_get_expr(ad.adbin, ad.adrelid)
+                            =
+                            'nextval('''
+                            || (
+                                pg_get_serial_sequence(a.attrelid::regclass::text, a.attname)
+                            )::regclass
+                            || '''::regclass)'
+                        )
+                )
+            ORDER BY a.attnum
+        """
 
         if not write:
-            output("In dry run mode (--write not passed)")
+            self._output("In dry run mode (--write not passed)")
 
         with connections[database].cursor() as cursor:
             attrelid_filter = f"{schema}.%" if schema else like
-            cursor.execute(find_serial_columns, (attrelid_filter,))
+            cursor.execute(textwrap.dedent(find_serial_columns), (attrelid_filter,))
             column_specs = cursor.fetchall()
-            output(f"Found {len(column_specs)} columns to update")
+            self._output(f"Found {len(column_specs)} columns to update")
 
             cursor.execute("SET statement_timeout='3s'")
-
             for table_name, column_name in column_specs:
-                migrate_serial_to_identity(database, write, output, cursor, table_name, column_name)
+                migrate_serial_to_identity(database, write, self._output, cursor, table_name, column_name)
 
-
-# Adapted from: https://dba.stackexchange.com/a/90567
-find_serial_columns = """\
-SELECT
-     a.attrelid::regclass::text AS table_name,
-     a.attname AS column_name
-FROM pg_attribute a
-     JOIN pg_class c ON c.oid = a.attrelid
-WHERE
-    a.attrelid::regclass::text LIKE %s
-    AND c.relkind IN ('r', 'p')  /* regular and partitioned tables */
-    AND a.attnum > 0
-    AND NOT a.attisdropped
-    AND a.atttypid = ANY ('{int,int8,int2}'::regtype[])
-    AND EXISTS (
-        SELECT FROM pg_attrdef ad
-        WHERE
-            ad.adrelid = a.attrelid
-            AND ad.adnum = a.attnum
-            AND (
-                pg_get_expr(ad.adbin, ad.adrelid)
-                =
-                'nextval('''
-                || (
-                    pg_get_serial_sequence(a.attrelid::regclass::text, a.attname)
-                )::regclass
-                || '''::regclass)'
-            )
-    )
-ORDER BY a.attnum
-"""
+    def _output(self, text: str) -> None:
+        self.stdout.write(text)
+        self.stdout.flush()
 
 
 def migrate_serial_to_identity(
@@ -107,13 +107,13 @@ def migrate_serial_to_identity(
     # answer on Stack Overflow:
     # https://stackoverflow.com/a/59233169
 
-    qn = cursor.db.ops.quote_name
     tbl = table_name.split(".")
     if len(tbl) > 1:
         schema = tbl[0]
         table = tbl[1]
-        table_name = f"{qn(schema)}.{qn(table)}"
-    print(f"{qn(table_name)}.{qn(column_name)}", flush=True)
+        composed_table_name = sql.SQL(".").join([sql.Identifier(schema), sql.Identifier(table)])
+
+    print(f"{table_name}.{column_name}", flush=True)
 
     # Get sequence name
     cursor.execute(
@@ -126,9 +126,11 @@ def migrate_serial_to_identity(
     with atomic(using=database):
         # Prevent concurrent inserts so we know the sequence is fixed
         if write:
-            cursor.execute(
-                f"LOCK TABLE {qn(table_name)} IN ACCESS EXCLUSIVE MODE",
+            # breakpoint()
+            query = sql.SQL("LOCK TABLE {table} IN ACCESS EXCLUSIVE MODE").format(
+                table=composed_table_name,
             )
+            cursor.execute(query)
 
         # Get next sequence value
         cursor.execute("SELECT nextval(%s)", (sequence_name,))
@@ -138,22 +140,30 @@ def migrate_serial_to_identity(
 
         if write:
             # Drop default, sequence
-            cursor.execute(
-                f"""\
-                ALTER TABLE {qn(table_name)}
-                    ALTER COLUMN {qn(column_name)} DROP DEFAULT
+            query = sql.SQL(
+                """\
+                ALTER TABLE {table}
+                    ALTER COLUMN {column_name} DROP DEFAULT
                 """
+            ).format(
+                table=composed_table_name,
+                column_name=sql.Identifier(column_name),
             )
+            cursor.execute(query)
+
             cursor.execute(f"DROP SEQUENCE {sequence_name}")
 
             # Change column to identity
-            cursor.execute(
-                f"""\
-                ALTER TABLE {qn(table_name)}
-                    ALTER {qn(column_name)}
+            query = sql.SQL(
+                """\
+                ALTER TABLE {table}
+                    ALTER {column_name}
                         ADD GENERATED BY DEFAULT AS IDENTITY (RESTART %s)
-                """,
-                (next_value,),
+                """
+            ).format(
+                table=composed_table_name,
+                column_name=sql.Identifier(column_name),
             )
+            cursor.execute(query, [next_value])
 
             print("    Updated.", flush=True)
