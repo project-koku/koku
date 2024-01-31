4d0a102de18115fac2b7dee07baabc7d0fff79ff
Luke Couzens <lcouzens@redhat.com>
2024-01-31 09:57:50 +0000

    add initial data migraiton

--- koku/reporting_common/migrations/0036_auto_20240129_1604.py
+++ koku/reporting_common/migrations/0036_auto_20240129_1604.py
@@ -4,6 +4,31 @@ from django.db import migrations
 from django.db import models
 
 
+def populate_manifest_fields(apps, schema_editor):
+    CostUsageReportManifest = apps.get_model("reporting_common", "costusagereportmanifest")
+
+    CostUsageReportManifest.objects.filter(completed_datetime__isnull=True).update(
+        completed_datetime=models.F("manifest_completed_datetime")
+    )
+    CostUsageReportManifest.objects.filter(creation_datetime__isnull=False).update(
+        creation_datetime=models.F("manifest_creation_datetime")
+    )
+    CostUsageReportManifest.objects.filter(export_datetime__isnull=True).update(
+        export_datetime=models.F("manifest_modified_datetime")
+    )
+
+
+def populate_report_status_fields(apps, schema_editor):
+    CostUsageReportManifest = apps.get_model("reporting_common", "costusagereportstatus")
+
+    CostUsageReportManifest.objects.filter(completed_datetime__isnull=True).update(
+        completed_datetime=models.F("last_completed_datetime")
+    )
+    CostUsageReportManifest.objects.filter(started_datetime__isnull=True).update(
+        started_datetime=models.F("last_started_datetime")
+    )
+
+
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -11,11 +36,6 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.AddField(
-            model_name="costusagereportmanifest",
-            name="completed_datetime",
-            field=models.DateTimeField(null=True),
-        ),
         migrations.AddField(
             model_name="costusagereportmanifest",
             name="creation_datetime",
@@ -28,17 +48,24 @@ class Migration(migrations.Migration):
         ),
         migrations.AddField(
             model_name="costusagereportmanifest",
-            name="state",
-            field=models.JSONField(default=dict, null=True),
-        ),
-        migrations.AddField(
-            model_name="costusagereportstatus",
             name="completed_datetime",
             field=models.DateTimeField(null=True),
+        ),
+        migrations.AddField(
+            model_name="costusagereportmanifest",
+            name="state",
+            field=models.JSONField(default=dict, null=True),
         ),
         migrations.AddField(
             model_name="costusagereportstatus",
             name="started_datetime",
             field=models.DateTimeField(null=True),
         ),
+        migrations.AddField(
+            model_name="costusagereportstatus",
+            name="completed_datetime",
+            field=models.DateTimeField(null=True),
+        ),
+        migrations.RunPython(populate_manifest_fields),
+        migrations.RunPython(populate_report_status_fields),
     ]
