4f4a042d862605140a479cf1c7d891c00d514f61
David N <dnakabaa@redhat.com>
2024-02-08 13:12:04 -0500

    temp: move rank annotation after applying aggregate

--- koku/api/report/queries.py
+++ koku/api/report/queries.py
@@ -1120,13 +1120,15 @@ class ReportQueryHandler(QueryHandler):
             query.annotate(**self.annotations)
             .values(*group_by_value)
             .annotate(**rank_annotations)
-            .annotate(rank=rank_by_total)
+            # .annotate(rank=rank_by_total)
             .annotate(source_uuid=ArrayAgg(F("source_uuid"), filter=Q(source_uuid__isnull=False), distinct=True))
         )
         if self.is_aws and "account" in self.parameters.url_data:
             ranks = ranks.annotate(**{"account_alias": F("account_alias__account_alias")})
         if self.is_openshift:
             ranks = ranks.annotate(clusters=ArrayAgg(Coalesce("cluster_alias", "cluster_id"), distinct=True))
+
+        ranks = ranks.annotate(rank=rank_by_total)
 
         rankings = []
         distinct_ranks = []
