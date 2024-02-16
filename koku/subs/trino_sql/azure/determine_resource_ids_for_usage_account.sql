a7d38edda0b9a03d967019939ad7630c95781f4d
Corey Goodfred <cgoodfre@redhat.com>
2024-02-16 08:45:47 -0500

    [COST-4589] Correctly get end of month data for Azure Metering (#4897)
    * move SQL to files per provider, case statement for Azure to handle EoM scenario

---
+++ koku/subs/trino_sql/azure/determine_resource_ids_for_usage_account.sql
@@ -0,0 +1,25 @@
+SELECT
+    resource_id,
+    CASE
+        WHEN max_date < date_add('day', -2, current_date) THEN max_date
+        ELSE date_add('day', -1, max_date)
+    END
+FROM (
+    SELECT
+     COALESCE(NULLIF(resourceid, ''), instanceid) resource_id,
+     max(COALESCE(date, usagedatetime)) max_date
+   FROM
+     hive.{{schema | sqlsafe}}.azure_line_items
+   WHERE
+    source={{source_uuid}}
+    AND year={{year}}
+    AND month={{month}}
+    AND metercategory = 'Virtual Machines'
+    AND json_extract_scalar(lower(additionalinfo), '$.vcpus') IS NOT NULL
+    AND json_extract_scalar(lower(tags), '$.com_redhat_rhel') IS NOT NULL
+    AND (subscriptionid = {{usage_account}} or subscriptionguid = {{usage_account}})
+    {% if excluded_ids %}
+        and coalesce(NULLIF(resourceid, ''), instanceid) NOT IN {{excluded_ids | inclause}}
+    {% endif %}
+   GROUP BY resourceid, instanceid
+)
