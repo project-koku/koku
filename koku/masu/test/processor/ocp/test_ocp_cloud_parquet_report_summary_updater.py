520e27d5e271e289fa66fc961ae0ddb1472f74ae
Luke Couzens <lcouzens@redhat.com>
2024-02-20 13:21:21 +0000

    units!

--- koku/masu/test/processor/ocp/test_ocp_cloud_parquet_report_summary_updater.py
+++ koku/masu/test/processor/ocp/test_ocp_cloud_parquet_report_summary_updater.py
@@ -513,8 +513,8 @@ class OCPCloudParquetReportSummaryUpdaterTest(MasuTestCase):
         """Test set_provider_infra_map."""
         infra_uuid = self.aws_provider_uuid
         infra_type = self.aws_provider.type
-        infra_account = "88888888"
-        infra_region = "East"
+        infra_account = "account"
+        infra_region = "east"
         p: Provider = self.baker.make("Provider", type=Provider.PROVIDER_OCP)
 
         infra_map = {str(p.uuid): (infra_uuid, infra_type, infra_account, infra_region)}
@@ -542,8 +542,8 @@ class OCPCloudParquetReportSummaryUpdaterTest(MasuTestCase):
         """Test set_provider_infra_map does not raise exception when providermap is not found."""
         infra_uuid = self.aws_provider_uuid
         infra_type = self.aws_provider.type
-        infra_account = "88888888"
-        infra_region = "East"
+        infra_account = "account"
+        infra_region = "east"
         p: Provider = self.baker.make("Provider", type=Provider.PROVIDER_OCP)
 
         mock_map.get_or_create.side_effect = IntegrityError()
@@ -561,8 +561,8 @@ class OCPCloudParquetReportSummaryUpdaterTest(MasuTestCase):
         """Test set_provider_infra_map during race condition creating map."""
         infra_uuid = self.aws_provider_uuid
         infra_type = self.aws_provider.type
-        infra_account = "88888888"
-        infra_region = "East"
+        infra_account = "account"
+        infra_region = "east"
         p: Provider = self.baker.make("Provider", type=Provider.PROVIDER_OCP)
         expected_infra = ProviderInfrastructureMap.objects.get(infrastructure_provider_id=infra_uuid)
 
