f79cbca5b7b7b569232cc96d35d3c7c596dad973
Luke Couzens <lcouzens@redhat.com>
2024-02-28 14:25:58 +0000

    lint

--- scripts/cji_scripts/migrate_trino.py
+++ scripts/cji_scripts/migrate_trino.py
@@ -171,16 +171,18 @@ def main():
     columns_to_add = {
         "subscription_name": "varchar",
     }
-
     for schema in schemas:
         CONNECT_PARAMS["schema"] = schema
         logging.info(f"*** Adding column to tables for schema {schema} ***")
         add_columns_to_table(columns_to_add, "reporting_ocpazurecostlineitem_project_daily_summary", CONNECT_PARAMS)
         add_columns_to_table(columns_to_add, "azure_openshift_daily_resource_matched_temp", CONNECT_PARAMS)
         add_columns_to_table(columns_to_add, "azure_openshift_daily_tag_matched_temp", CONNECT_PARAMS)
-        add_columns_to_table(columns_to_add, "reporting_ocpazurecostlineitem_project_daily_summary_temp", CONNECT_PARAMS)
+        add_columns_to_table(
+            columns_to_add, "reporting_ocpazurecostlineitem_project_daily_summary_temp", CONNECT_PARAMS
+        )
         # logging.info(f"*** Dropping tables {tables_to_drop} for schema {schema} ***")
         # drop_tables(tables_to_drop, CONNECT_PARAMS)
+
 
 if __name__ == "__main__":
     main()
