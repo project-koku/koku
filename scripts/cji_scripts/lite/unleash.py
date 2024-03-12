371b952600671a1178d63be7b4f6dfb81604c49c
Luke Couzens <lcouzens@redhat.com>
2024-03-12 10:35:55 +0000

    [COST-4757] - Remove custom Unleash strategies

--- scripts/cji_scripts/lite/unleash.py
+++ scripts/cji_scripts/lite/unleash.py
@@ -3,7 +3,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 from UnleashClient import UnleashClient
-from UnleashClient.strategies import Strategy
 
 from .env import ENVIRONMENT
 
@@ -19,22 +18,7 @@ class LiteUnleashClient(UnleashClient):
         self.scheduler.shutdown()
 
 
-class LiteSchemaStrategy(Strategy):
-    def load_provisioning(self) -> list:
-        return self.parameters["schema-name"]
-
-    def apply(self, context):
-        default_value = False
-        if "schema" in context and context["schema"] is not None:
-            default_value = context["schema"] in self.parsed_provisioning
-        return default_value
-
-
 def new_unleash_client(settings):
-    strategies = {
-        # All new strategies should be added here.
-        "schema-strategy": LiteSchemaStrategy
-    }
     headers = {}
     if settings.UNLEASH_TOKEN:
         headers["Authorization"] = f"Bearer {settings.UNLEASH_TOKEN}"
@@ -45,7 +29,6 @@ def new_unleash_client(settings):
         environment=ENVIRONMENT.get_value("KOKU_SENTRY_ENVIRONMENT", default="development"),
         instance_id=ENVIRONMENT.get_value("APP_POD_NAME", default="unleash-client-python"),
         custom_headers=headers,
-        custom_strategies=strategies,
         cache_directory=settings.UNLEASH_CACHE_DIR,
         verbose_log_level=settings.LOGGING_LEVEL,
     )
