92259bd8f4a59589d36d9cca131797b7b65a6b33
Sam Doran <sdoran@redhat.com>
2024-03-29 11:28:14 -0400

    Add option for forcing a download

--- dev/scripts/snyk-scan.py
+++ dev/scripts/snyk-scan.py
@@ -34,9 +34,9 @@ class SnykDownloader:
     def url(self) -> str:
         return getattr(self, f"{self._system}_{self._machine}".lower(), getattr(self, f"{self._system.lower()}", None))
 
-    def download(self) -> Path:
+    def download(self, force_download=False) -> Path:
         snyk_binary_path = Path(self.destination).expanduser().joinpath("snyk")
-        if not snyk_binary_path.exists():
+        if force_download or not snyk_binary_path.exists():
             if self.url is None:
                 sys.exit(f"No snyk executable available for '{self._system} {self._machine}'.")
 
@@ -60,6 +60,7 @@ def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
     parser.add_argument("--download-dest", "-d", default="~/.cache/snyk", type=Path, help="Directory to download snyk")
     parser.add_argument("--severity-threshold", default="medium")
+    parser.add_argument("--force-download", action="store_true")
     parser.add_argument("path", nargs="?", default="", help="Path to scan")
 
     if HAS_ARGCOMPLETE:
@@ -84,7 +85,7 @@ def main() -> None:
     args = parse_args()
 
     if not (snyk_binary := shutil.which("snyk")):
-        snyk_binary = SnykDownloader(args.download_dest).download()
+        snyk_binary = SnykDownloader(args.download_dest).download(args.force_download)
 
     check_auth(snyk_binary)
 
