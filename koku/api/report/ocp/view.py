85945f69a11a280e90510c9d06bd290f2065d685
Sam Doran <sdoran@redhat.com>
2024-04-17 15:17:00 -0400

    Create API for node network costs returning example data
    This is just for UI dev purposes while the backend work is still
    in progress.

--- koku/api/report/ocp/view.py
+++ koku/api/report/ocp/view.py
@@ -3,6 +3,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 """View for OpenShift Usage Reports."""
+from api.common.pagination import ReportPagination
 from api.common.permissions.openshift_access import OpenShiftAccessPermission
 from api.models import Provider
 from api.report.ocp.query_handler import OCPReportQueryHandler
@@ -44,3 +45,70 @@ class OCPVolumeView(OCPView):
     """Get OpenShift volume usage data."""
 
     report = "volume"
+
+
+class OCPNetworkView(OCPView):
+    """OpenShift node network usage"""
+
+    report = "network"
+
+    def get(self, request, **kwargs):
+        response_fixture = {
+            "total": {
+                "usage": {
+                    "value": 3.6801746441,
+                    "units": "GB",
+                },
+                "data_transfer_in": {
+                    "value": 2.789192834,
+                    "units": "GB",
+                },
+                "data_transfer_out": {
+                    "value": 0.8909818101,
+                    "units": "GB",
+                },
+                "infrastructure": {
+                    "raw": {"value": 1945.51192915, "units": "USD"},
+                    "markup": {"value": 0.0, "units": "USD"},
+                    "usage": {"value": 0.0, "units": "USD"},
+                    "total": {"value": 1945.51192915, "units": "USD"},
+                },
+                "supplementary": {
+                    "raw": {"value": 0.0, "units": "USD"},
+                    "markup": {"value": 0.0, "units": "USD"},
+                    "usage": {"value": 0.0, "units": "USD"},
+                    "total": {"value": 0.0, "units": "USD"},
+                },
+                "cost": {
+                    "raw": {"value": 1945.51192915, "units": "USD"},
+                    "markup": {"value": 0.0, "units": "USD"},
+                    "usage": {"value": 0.0, "units": "USD"},
+                    "total": {"value": 1945.51192915, "units": "USD"},
+                },
+            },
+            "data": [
+                {
+                    "date": "2024-03-06",
+                    "values": [
+                        {
+                            "date": "2024-03-06",
+                            "data_transfer_in": {
+                                "value": 3.6801746441,
+                                "units": "GB",
+                            },
+                            "data_transfer_out": {
+                                "value": 2.6866746441,
+                                "units": "GB",
+                            },
+                            "resource_id": "i-727f8dc9c567f1552",
+                            "clusters": ["Test OCP on AWS"],
+                            "source_uuid": "6795d5e6-951c-4382-a8d3-390515d7ec3d",
+                            "region": "us-east-1",
+                        }
+                    ],
+                }
+            ],
+        }
+        paginator = ReportPagination()
+        paginated_result = paginator.paginate_queryset(response_fixture, request)
+        return paginator.get_paginated_response(paginated_result)
