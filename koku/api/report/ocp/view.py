943f5a2951d638c68952de0a3c4927d3aa30d043
Sam Doran <sdoran@redhat.com>
2024-05-03 11:18:36 -0400

    Generate example API data for several days

--- koku/api/report/ocp/view.py
+++ koku/api/report/ocp/view.py
@@ -3,12 +3,15 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 """View for OpenShift Usage Reports."""
+import dataclasses
+
 from rest_framework.pagination import Response
 from rest_framework.views import status
 
 from api.common.pagination import ReportPagination
 from api.common.permissions.openshift_access import OpenShiftAccessPermission
 from api.models import Provider
+from api.report.ocp.network.example import ExampleResponseBody
 from api.report.ocp.query_handler import OCPReportQueryHandler
 from api.report.ocp.serializers import OCPCostQueryParamSerializer
 from api.report.ocp.serializers import OCPInventoryQueryParamSerializer
@@ -63,118 +66,7 @@ class OCPNetworkView(OCPView):
         ):
             return Response(data="Under development", status=status.HTTP_400_BAD_REQUEST)
 
-        response_fixture = {
-            "total": {
-                "usage": {
-                    "value": 3.6801746441,
-                    "units": "GB",
-                },
-                "data_transfer_in": {
-                    "value": 12.82510775,
-                    "units": "GB",
-                },
-                "data_transfer_out": {
-                    "value": 8.080656137,
-                    "units": "GB",
-                },
-                "infrastructure": {
-                    "raw": {"value": 1945.51192915, "units": "USD"},
-                    "markup": {"value": 0.0, "units": "USD"},
-                    "usage": {"value": 0.0, "units": "USD"},
-                    "total": {"value": 1945.51192915, "units": "USD"},
-                },
-                "supplementary": {
-                    "raw": {"value": 0.0, "units": "USD"},
-                    "markup": {"value": 0.0, "units": "USD"},
-                    "usage": {"value": 0.0, "units": "USD"},
-                    "total": {"value": 0.0, "units": "USD"},
-                },
-                "cost": {
-                    "raw": {"value": 4863.77982288, "units": "USD"},
-                    "markup": {"value": 0.0, "units": "USD"},
-                    "usage": {"value": 0.0, "units": "USD"},
-                    "total": {"value": 4863.77982288, "units": "USD"},
-                },
-            },
-            "data": [
-                {
-                    "date": "2024-03-31",
-                    "values": [
-                        {
-                            "date": "2024-03-31",
-                            "data_transfer_in": {
-                                "value": 3.6801746441,
-                                "units": "GB",
-                            },
-                            "data_transfer_out": {
-                                "value": 2.6866746441,
-                                "units": "GB",
-                            },
-                            "resource_id": "i-727f8dc9c567f1552",
-                            "clusters": ["Test OCP on AWS"],
-                            "source_uuid": "6795d5e6-951c-4382-a8d3-390515d7ec3d",
-                            "region": "us-east-1",
-                            "infrastructure": {
-                                "raw": {"value": 1945.51192915, "units": "USD"},
-                                "markup": {"value": 0.0, "units": "USD"},
-                                "usage": {"value": 0.0, "units": "USD"},
-                                "total": {"value": 1945.5119291598, "units": "USD"},
-                            },
-                            "supplementary": {
-                                "raw": {"value": 0.0, "units": "USD"},
-                                "markup": {"value": 0.0, "units": "USD"},
-                                "usage": {"value": 0.0, "units": "USD"},
-                                "total": {"value": 0.0, "units": "USD"},
-                            },
-                            "cost": {
-                                "raw": {"value": 1945.51192915, "units": "USD"},
-                                "markup": {"value": 0.0, "units": "USD"},
-                                "usage": {"value": 0.0, "units": "USD"},
-                                "total": {"value": 1945.51192915, "units": "USD"},
-                            },
-                        },
-                    ],
-                },
-                {
-                    "date": "2024-04-01",
-                    "values": [
-                        {
-                            "date": "2024-04-01",
-                            "data_transfer_in": {
-                                "value": 9.1449331092,
-                                "units": "GB",
-                            },
-                            "data_transfer_out": {
-                                "value": 5.3939814929,
-                                "units": "GB",
-                            },
-                            "resource_id": "i-727f8dc9c567f1552",
-                            "clusters": ["Test OCP on AWS"],
-                            "source_uuid": "6795d5e6-951c-4382-a8d3-390515d7ec3d",
-                            "region": "us-east-1",
-                            "infrastructure": {
-                                "raw": {"value": 2918.26789372, "units": "USD"},
-                                "markup": {"value": 0.0, "units": "USD"},
-                                "usage": {"value": 0.0, "units": "USD"},
-                                "total": {"value": 2918.26789372, "units": "USD"},
-                            },
-                            "supplementary": {
-                                "raw": {"value": 0.0, "units": "USD"},
-                                "markup": {"value": 0.0, "units": "USD"},
-                                "usage": {"value": 0.0, "units": "USD"},
-                                "total": {"value": 0.0, "units": "USD"},
-                            },
-                            "cost": {
-                                "raw": {"value": 2918.267893725, "units": "USD"},
-                                "markup": {"value": 0.0, "units": "USD"},
-                                "usage": {"value": 0.0, "units": "USD"},
-                                "total": {"value": 2918.267893725, "units": "USD"},
-                            },
-                        }
-                    ],
-                },
-            ],
-        }
+        data = ExampleResponseBody.generate()
         paginator = ReportPagination()
-        paginated_result = paginator.paginate_queryset(response_fixture, request)
+        paginated_result = paginator.paginate_queryset(dataclasses.asdict(data), request)
         return paginator.get_paginated_response(paginated_result)
