d11c8226779ed87ead507efe56d45bbdcc4815a2
Luke Couzens <lcouzens@redhat.com>
2024-06-06 12:10:51 +0100

    Override start date for cost model updates

--- koku/masu/processor/tasks.py
+++ koku/masu/processor/tasks.py
@@ -873,6 +873,9 @@ def update_cost_model_costs(
         None
 
     """
+    # Override cost model start date to calculate costs for full month
+    LOG.info(f"overriding cost model start date to process full month")
+    start_date = DateHelper().month_start(start_date)
     task_name = "masu.processor.tasks.update_cost_model_costs"
     cache_args = [schema_name, provider_uuid, start_date, end_date]
     if not synchronous:
