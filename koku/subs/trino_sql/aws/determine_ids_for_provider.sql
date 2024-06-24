8cbfd222ad49600002b4bd0c7db0d9716c975b8d
Corey Goodfred <cgoodfre@redhat.com>
2024-06-24 07:53:00 -0400

    Filter accounts by matching criteria during subs processing to prevent unnecessary SQL from running (#5184)

--- koku/subs/trino_sql/aws/determine_ids_for_provider.sql
+++ koku/subs/trino_sql/aws/determine_ids_for_provider.sql
@@ -3,6 +3,8 @@ FROM hive.{{schema | sqlsafe}}.aws_line_items
 WHERE source={{source_uuid}}
   AND year={{year}}
   AND month={{month}}
+  AND lineitem_productcode = 'AmazonEC2'
+  AND strpos(lower(resourcetags), 'com_redhat_rhel') > 0
   {% if excluded_ids %}
     AND lineitem_usageaccountid NOT IN {{excluded_ids | inclause}}
   {% endif %}
