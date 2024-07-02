dc9b19b836845470c1fe8f1c7be343ecd72b2c19
David N <dnakabaa@redhat.com>
2024-07-02 11:41:24 -0400

    additional log clean up

--- koku/masu/processor/_tasks/remove_expired.py
+++ koku/masu/processor/_tasks/remove_expired.py
@@ -5,6 +5,7 @@
 """Remove expired data asynchronous tasks."""
 import logging
 
+from api.common import log_json
 from masu.processor.expired_data_remover import ExpiredDataRemover
 
 LOG = logging.getLogger(__name__)
@@ -23,17 +24,11 @@ def _remove_expired_data(schema_name, provider, simulate, provider_uuid=None):
         None
 
     """
-    log_statement = (
-        f"Remove expired data:\n"
-        f" schema_name: {schema_name}\n"
-        f" provider: {provider}\n"
-        f" simulate: {simulate}\n"
-    )
-    LOG.info(log_statement)
+
+    LOG.info(log_json(msg="Remove expired data", schema=schema_name, provider=provider, simulate=simulate))
 
     remover = ExpiredDataRemover(schema_name, provider)
     removed_data = remover.remove(simulate=simulate, provider_uuid=provider_uuid)
     if removed_data:
         status_msg = "Expired Data" if simulate else "Removed Data"
-        result_msg = f"{status_msg}:\n {str(removed_data)}"
-        LOG.info(result_msg)
+        LOG.info(log_json(msg=status_msg, removed_data=removed_data))
