b2849ef65f917138e05975ff93506224237720e4
David N <dnakabaa@redhat.com>
2024-07-26 14:23:04 -0400

    data is empty dict if no queryset_data

--- koku/api/common/pagination.py
+++ koku/api/common/pagination.py
@@ -323,7 +323,7 @@ class AWSEC2ComputePagination(ReportPagination):
         paginated_data = []
 
         data = (
-            queryset_data[0] if (queryset_data := queryset.get("data", [])) else []
+            queryset_data[0] if (queryset_data := queryset.get("data", [])) else {}
         )  # only single month data expected
         resource_ids = data.get("resource_ids", [])
         resource_count = len(resource_ids)
