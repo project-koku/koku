1a0ca020c6a6582d550d1c6b22a1db398f2bfd66
Lucas Bacciotti <lbaciotti@gmail.com>
2024-07-30 09:02:25 +0100

    Update koku/common/utils.py
    Co-authored-by: Sam Doran <sdoran@redhat.com>

--- koku/common/utils.py
+++ koku/common/utils.py
@@ -34,7 +34,7 @@ def retry(
     original_callable: t.Callable = None,
     *,
     retries: int = settings.HIVE_PARTITION_DELETE_RETRIES,
-    retry_on: tuple[type[Exception], ...] = (Exception,),
+    retry_on: type[Exception] | tuple[type[Exception], ...] = Exception,
     max_wait: int = 30,
     log_ref: str = "Trino query",
     log_message: t.Optional[str] = "Retrying...",
