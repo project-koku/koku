19088d9ec884be8f59e51b495d5940aabc372a7b
myersCody <cmyers@redhat.com>
2024-08-05 15:11:37 -0400

    [COST-4820] Remove all_label unleash flag

--- koku/api/report/test/ocp/view/test_views.py
+++ koku/api/report/test/ocp/view/test_views.py
@@ -1131,7 +1131,6 @@ class OCPReportViewTest(IamTestCase):
                 result = data_totals.get(key, {}).get("value")
             self.assertEqual(result, expected)
 
-    @patch("api.report.ocp.provider_map.is_feature_cost_3083_all_labels_enabled", return_value=True)
     def test_execute_costs_query_with_tag_filter(self, mock_unleash):
         """Test that data is filtered by tag key."""
         url = "?filter[type]=pod&filter[time_scope_value]=-10&filter[enabled]=true"
