c048ecf12aa952231260351fbec6947488714858
myersCody <cmyers@redhat.com>
2024-09-27 11:55:52 -0400

    Update schema var

--- koku/masu/database/trino_sql/verify/managed_ocp_on_aws_verification.sql
+++ koku/masu/database/trino_sql/verify/managed_ocp_on_aws_verification.sql
@@ -16,7 +16,7 @@ FROM
 ) t1,
 (
     SELECT sum(unblended_cost) as parquet_total_cost
-    FROM hive.org1234567.reporting_ocpawscostlineitem_project_daily_summary as parquet_table
+    FROM hive.{{schema | sqlsafe}}.reporting_ocpawscostlineitem_project_daily_summary as parquet_table
     WHERE parquet_table.aws_source = {{cloud_source_uuid}}
     AND parquet_table.year = {year}
     AND lpad(parquet_table.month, 2, '0') = {month}
