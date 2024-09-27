0df81914e35ec1c4e1dbb8d3760d885a567771b4
myersCody <cmyers@redhat.com>
2024-09-27 13:21:33 -0400

    pushed up unsaved changes

--- koku/masu/database/trino_sql/verify/managed_ocp_on_aws_verification.sql
+++ koku/masu/database/trino_sql/verify/managed_ocp_on_aws_verification.sql
@@ -6,7 +6,7 @@ SELECT
 FROM
 (
     SELECT sum(lineitem_unblendedcost) AS managed_total_cost
-    FROM hive.{{schema | sqlsafe}}.managed_gcp_openshift_daily as managed_ocpcloud
+    FROM hive.{{schema | sqlsafe}}.managed_aws_openshift_daily as managed_ocpcloud
     WHERE managed_ocpcloud.source = {{cloud_source_uuid}}
     AND managed_ocpcloud.year = {{year}}
     AND managed_ocpcloud.month = {{month}}
@@ -18,6 +18,6 @@ FROM
     SELECT sum(unblended_cost) as parquet_total_cost
     FROM hive.{{schema | sqlsafe}}.reporting_ocpawscostlineitem_project_daily_summary as parquet_table
     WHERE parquet_table.aws_source = {{cloud_source_uuid}}
-    AND parquet_table.year = {year}
-    AND lpad(parquet_table.month, 2, '0') = {month}
+    AND parquet_table.year = {{year}}
+    AND lpad(parquet_table.month, 2, '0') = {{month}}
 ) t2;
