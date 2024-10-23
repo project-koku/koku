b4e902f2f05724990a1717b8fb8af87813c661c8
Lucas Bacciotti <lbacciot@redhat.com>
2024-10-23 12:52:56 +0100

    Creating CTE and adapting query to get latest values.

--- koku/masu/database/trino_sql/reporting_awscostentrylineitem_summary_by_ec2_compute_p.sql
+++ koku/masu/database/trino_sql/reporting_awscostentrylineitem_summary_by_ec2_compute_p.sql
@@ -38,7 +38,28 @@ with cte_pg_enabled_keys as (
       from postgres.{{schema | sqlsafe}}.reporting_enabledtagkeys
      where enabled = true
      and provider_type = 'AWS'
+),
+cte_latest_values as (
+    SELECT
+            lineitem_resourceid as resource_id,
+            nullif(product_instancetype, '') as instance_type,
+            max(json_extract_scalar(json_parse(resourcetags), '$.Name')) AS instance_name,
+            ARRAY_AGG(resourcetags) as tags,
+            ARRAY_AGG(costcategory) as cost_category
+        FROM aws_line_items_daily as alid
+      WHERE source = '{{source_uuid | sqlsafe}}'
+        AND year = '{{year | sqlsafe}}'
+        AND month = '{{month | sqlsafe}}'
+            AND lineitem_productcode = 'AmazonEC2'
+            AND product_productfamily LIKE '%Compute Instance%'
+            AND lineitem_resourceid != ''
+            AND lineitem_usagestartdate = (
+                SELECT max(date(lv.lineitem_usagestartdate)) as usage_start from aws_line_items_daily as lv
+            )
+        GROUP BY lineitem_resourceid,
+            product_instancetype
 )
+
 SELECT uuid() as uuid,
     usage_start,
     usage_end,
@@ -52,13 +73,13 @@ SELECT uuid() as uuid,
     memory,
     cast(
         map_filter(
-            cast(json_parse(tags) as map(varchar, varchar)),
+            cast(json_parse(tags[1]) as map(varchar, varchar)),
             (k,v) -> contains(pek.keys, k)
         ) as json
-     ) as tags,
-    json_parse(costcategory) as cost_category,
+    ) as tags,
+    cast(json_parse(cost_category[1]) as json) as cost_category,
     unit,
-    cast(usage_amount AS decimal(24,9)),
+    cast(usage_amount as decimal(24,9)) as usage_amount,
     normalization_factor,
     normalized_usage_amount,
     cast(currency_code AS varchar(10)),
@@ -79,82 +100,65 @@ SELECT uuid() as uuid,
     aa.id as account_alias_id
 FROM (
     SELECT min(date(lineitem_usagestartdate)) as usage_start,
-        max(date(lineitem_usagestartdate)) as usage_end,
-        lineitem_usageaccountid as usage_account_id,
-        lineitem_resourceid as resource_id,
-        json_extract_scalar(json_parse(resourcetags), '$.Name') AS instance_name,
-        nullif(product_instancetype, '') as instance_type,
-        nullif(product_operatingsystem, '') as operating_system,
-        nullif(product_region, '') as region,
-        cast(nullif(product_vcpu, '') AS INTEGER) as vcpu,
-        nullif(product_memory, '') as memory,
-        resourcetags as tags,
-        costcategory,
-        nullif(pricing_unit, '') as unit,
-        -- SavingsPlanNegation needs to be negated to prevent duplicate usage COST-5369
-        sum(
-            CASE
-                WHEN lineitem_lineitemtype='SavingsPlanNegation'
-                THEN 0.0
-                ELSE lineitem_usageamount
-            END
-        ) as usage_amount,
-        max(lineitem_normalizationfactor) as normalization_factor,
-        sum(lineitem_normalizedusageamount) as normalized_usage_amount,
-        max(lineitem_currencycode) as currency_code,
-        max(lineitem_unblendedrate) as unblended_rate,
-        /* SavingsPlanCoveredUsage entries have corresponding SavingsPlanNegation line items
-           that offset that cost.
-           https://docs.aws.amazon.com/cur/latest/userguide/cur-sp.html
-        */
-        sum(
-            CASE
-                WHEN lineitem_lineitemtype='SavingsPlanCoveredUsage'
-                THEN 0.0
-                ELSE lineitem_unblendedcost
-            END
-        ) as unblended_cost,
-        max(lineitem_blendedrate) as blended_rate,
-        /* SavingsPlanCoveredUsage entries have corresponding SavingsPlanNegation line items
-           that offset that cost.
-           https://docs.aws.amazon.com/cur/latest/userguide/cur-sp.html
-        */
-        sum(
-            CASE
-                WHEN lineitem_lineitemtype='SavingsPlanCoveredUsage'
-                THEN 0.0
-                ELSE lineitem_blendedcost
-            END
-        ) as blended_cost,
-        sum(savingsplan_savingsplaneffectivecost) as savingsplan_effective_cost,
-        sum(
-            CASE
-                WHEN lineitem_lineitemtype='Tax'
-                OR   lineitem_lineitemtype='Usage'
-                THEN lineitem_unblendedcost
-                ELSE savingsplan_savingsplaneffectivecost
-            END
-        ) as calculated_amortized_cost,
-        sum(pricing_publicondemandcost) as public_on_demand_cost,
-        max(pricing_publicondemandrate) as public_on_demand_rate
-
-    FROM hive.{{schema | sqlsafe}}.aws_line_items_daily
+       max(date(lineitem_usagestartdate)) as usage_end,
+       max(lineitem_usageaccountid) as usage_account_id,
+       lineitem_resourceid as resource_id,
+       max(json_extract_scalar(json_parse(resourcetags), '$.Name')) AS instance_name,
+       max(cte_l.instance_type) as instance_type,
+       max(cte_l.tags) as tags,
+       max(nullif(product_operatingsystem, '')) as operating_system,
+       max(nullif(product_region, '')) as region,
+       max(cast(nullif(product_vcpu, '') AS INTEGER)) as vcpu,
+       max(nullif(product_memory, '')) as memory,
+       max(cte_l.cost_category) as cost_category,
+       max(nullif(pricing_unit, '')) as unit,
+       sum(
+           CASE
+               WHEN lineitem_lineitemtype='SavingsPlanNegation'
+               THEN 0.0
+               ELSE lineitem_usageamount
+           END
+       ) as usage_amount,
+       max(lineitem_normalizationfactor) as normalization_factor,
+       sum(lineitem_normalizedusageamount) as normalized_usage_amount,
+       max(lineitem_currencycode) as currency_code,
+       max(lineitem_unblendedrate) as unblended_rate,
+       sum(
+           CASE
+               WHEN lineitem_lineitemtype='SavingsPlanCoveredUsage'
+               THEN 0.0
+               ELSE lineitem_unblendedcost
+           END
+       ) as unblended_cost,
+       max(lineitem_blendedrate) as blended_rate,
+       sum(
+           CASE
+               WHEN lineitem_lineitemtype='SavingsPlanCoveredUsage'
+               THEN 0.0
+               ELSE lineitem_blendedcost
+           END
+       ) as blended_cost,
+       sum(savingsplan_savingsplaneffectivecost) as savingsplan_effective_cost,
+       sum(
+           CASE
+               WHEN lineitem_lineitemtype='Tax'
+               OR lineitem_lineitemtype='Usage'
+               THEN lineitem_unblendedcost
+               ELSE savingsplan_savingsplaneffectivecost
+           END
+       ) as calculated_amortized_cost,
+       sum(pricing_publicondemandcost) as public_on_demand_cost,
+       max(pricing_publicondemandrate) as public_on_demand_rate
+    FROM aws_line_items_daily as lid
+        JOIN cte_latest_values AS cte_l
+        ON lid.lineitem_resourceid = cte_l.resource_id
     WHERE source = '{{source_uuid | sqlsafe}}'
         AND year = '{{year | sqlsafe}}'
         AND month = '{{month | sqlsafe}}'
         AND lineitem_productcode = 'AmazonEC2'
         AND product_productfamily LIKE '%Compute Instance%'
         AND lineitem_resourceid != ''
-    GROUP BY lineitem_resourceid,
-        lineitem_usageaccountid,
-        product_instancetype,
-        product_operatingsystem,
-        product_region,
-        product_memory,
-        product_vcpu,
-        resourcetags,
-        costcategory,
-        pricing_unit
+    GROUP BY lineitem_resourceid
 ) AS ds
 CROSS JOIN cte_pg_enabled_keys AS pek
 LEFT JOIN postgres.{{schema | sqlsafe}}.reporting_awsaccountalias AS aa
