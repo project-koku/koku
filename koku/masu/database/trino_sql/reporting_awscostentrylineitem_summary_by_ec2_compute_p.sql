1383df4850c59b0c811fe80cfbb46da26d5e4cf1
Lucas Bacciotti <lbacciot@redhat.com>
2024-11-12 11:44:09 +0000

    Specifying the resourceid in the where clause to contemplate edge cases

--- koku/masu/database/trino_sql/reporting_awscostentrylineitem_summary_by_ec2_compute_p.sql
+++ koku/masu/database/trino_sql/reporting_awscostentrylineitem_summary_by_ec2_compute_p.sql
@@ -56,7 +56,8 @@ cte_latest_values as (
     AND lineitem_usagestartdate = (
       SELECT max(date(lv.lineitem_usagestartdate)) AS usage_start
       FROM hive.{{schema | sqlsafe}}.aws_line_items_daily AS lv
-      WHERE year = '{{year | sqlsafe}}'
+      WHERE lineitem_resourceid = alid.lineitem_resourceid
+      AND year = '{{year | sqlsafe}}'
       AND month = '{{month | sqlsafe}}'
       AND source = '{{source_uuid | sqlsafe}}'
     )
