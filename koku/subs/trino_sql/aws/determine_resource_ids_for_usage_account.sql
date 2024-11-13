5fe85ff5f465c79e5ea23579443aaa55ff9dae0c
maskarb <mskarbek@redhat.com>
2024-11-13 15:46:04 -0500

    fix sql

--- koku/subs/trino_sql/aws/determine_resource_ids_for_usage_account.sql
+++ koku/subs/trino_sql/aws/determine_resource_ids_for_usage_account.sql
@@ -1,18 +1,3 @@
-SELECT lineitem_resourceid, max(lineitem_usagestartdate)
-FROM hive.{{schema | sqlsafe}}.aws_line_items
-WHERE source={{source_uuid}}
-AND year={{year}}
-AND month={{month}}
-AND lineitem_productcode = 'AmazonEC2'
-AND strpos(lower(resourcetags), 'com_redhat_rhel') > 0
-AND lineitem_usageaccountid = {{usage_account}}
-    {% if excluded_ids %}
-        AND lineitem_resourceid NOT IN {{excluded_ids | inclause}}
-    {% endif %}
-GROUP BY lineitem_resourceid
-
-
-
 SELECT
     lineitem_resourceid,
     max(lineitem_usagestartdate)
