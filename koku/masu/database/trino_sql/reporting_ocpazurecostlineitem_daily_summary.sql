8c7b14cd479c26fb06feac238cd8c66f55db9d4f
Luke Couzens <lcouzens@redhat.com>
2025-01-13 15:03:23 +0000

    revert ocp where clause

--- koku/masu/database/trino_sql/reporting_ocpazurecostlineitem_daily_summary.sql
+++ koku/masu/database/trino_sql/reporting_ocpazurecostlineitem_daily_summary.sql
@@ -510,8 +510,7 @@ WITH cte_total_pv_capacity as (
             OR
                 (lower(ocp.csi_volume_handle) = lower(azure.resource_id))
             )
-        WHERE ocp.source = {{ocp_source_uuid}}
-            AND ocp.year = {{year}}
+        WHERE ocp.year = {{year}}
             AND lpad(ocp.month, 2, '0') = {{month}}
             AND ocp.usage_start >= {{start_date}}
             AND ocp.usage_start < date_add('day', 1, {{end_date}})
