2e11c09238fea17213721c395daf31092aff257a
maskarb <mskarbek@redhat.com>
2025-02-19 16:41:53 -0500

    calculate over the month, not last day

--- koku/masu/database/sql/openshift/reporting_ocp_vm_summary_p.sql
+++ koku/masu/database/sql/openshift/reporting_ocp_vm_summary_p.sql
@@ -95,17 +95,8 @@ WHERE usage_start >= {{start_date}}::date
 GROUP BY cluster_alias, cluster_id, namespace, latest.node_name, vm_name, cost_model_rate_type, latest.labels
 ;
 
-WITH second_to_last_day AS (
-    SELECT DISTINCT usage_start::date AS day
-    FROM {{schema | sqlsafe}}.reporting_ocpusagelineitem_daily_summary
-    WHERE usage_start >= {{start_date}}::date
-        AND usage_start <= {{end_date}}::date
-        AND source_uuid = {{source_uuid}}
-        AND pod_request_cpu_core_hours IS NOT NULL
-    ORDER BY day DESC
-    OFFSET 1 LIMIT 1  -- Get the second-to-last day
-),
-cte_distribution_type AS (
+
+WITH cte_distribution_type AS (
     -- get the distribution type from the cost model associated with this source
     SELECT
         {{source_uuid}}::uuid as source_uuid,
@@ -115,25 +106,41 @@ cte_distribution_type AS (
         ON cmm.cost_model_id = cm.uuid
     WHERE cmm.provider_uuid = {{source_uuid}}
 ),
+cte_node_usage AS (
+    SELECT
+        namespace,
+        node,
+        usage_start,
+        sum(pod_effective_usage_cpu_core_hours) as cpu_usage,
+        sum(pod_effective_usage_memory_gigabyte_hours) as mem_usage
+    FROM {{schema | sqlsafe}}.reporting_ocpusagelineitem_daily_summary
+    WHERE usage_start >= {{start_date}}::date
+        AND usage_start <= {{end_date}}::date
+        AND source_uuid = {{source_uuid}}
+    GROUP BY namespace, node, usage_start
+),
 cte_latest_resources AS (
-    SELECT DISTINCT ON (vm_name)
-        all_labels->>'vm_kubevirt_io_name' AS vm_name,
-        pod_request_cpu_core_hours AS cpu_request_hours,
-        pod_request_memory_gigabyte_hours AS memory_request_hours,
+    SELECT
+        pod_labels->>'vm_kubevirt_io_name' AS vm_name,
         CASE WHEN cte_distribution_type.dt = 'cpu'
-            THEN pod_effective_usage_cpu_core_hours / node_capacity_cpu_core_hours
-            ELSE pod_effective_usage_memory_gigabyte_hours / node_capacity_memory_gigabyte_hours
+            THEN sum(pod_effective_usage_cpu_core_hours) / sum(cte_node_usage.cpu_usage)
+            ELSE sum(pod_effective_usage_memory_gigabyte_hours) / sum(cte_node_usage.mem_usage)
         END as ratio,
-        node as node_name,
-        namespace as namespace_name,
-        pod_labels as labels
+        cte_node_usage.node as node_name,
+        cte_node_usage.namespace as namespace_name,
+        pod_labels as labels,
+        cte_node_usage.usage_start
     FROM {{schema | sqlsafe}}.reporting_ocpusagelineitem_daily_summary as ocp
     JOIN cte_distribution_type
         ON cte_distribution_type.source_uuid = ocp.source_uuid
-    WHERE usage_start::date = (SELECT day FROM second_to_last_day)
-        AND pod_request_cpu_core_hours IS NOT NULL
-        AND all_labels ? 'vm_kubevirt_io_name'
-    ORDER BY vm_name, usage_start DESC
+    JOIN cte_node_usage
+        ON cte_node_usage.namespace = ocp.namespace
+        AND cte_node_usage.node = ocp.node
+        AND cte_node_usage.usage_start = ocp.usage_start
+    WHERE pod_labels ? 'vm_kubevirt_io_name'
+        AND pod_effective_usage_cpu_core_hours != 0
+        AND pod_effective_usage_memory_gigabyte_hours != 0
+    GROUP BY vm_name, cte_distribution_type.dt, cte_node_usage.usage_start, cte_node_usage.node, cte_node_usage.namespace, pod_labels
 )
 INSERT INTO {{schema | sqlsafe}}.reporting_ocp_vm_summary_p (
     id,
@@ -156,22 +163,23 @@ SELECT
     cluster_alias,
     cluster_id,
     namespace,
-    latest.node_name as node,
-    latest.labels->>'vm_kubevirt_io_name' as vm_name,
+    node,
+    latest.vm_name as vm_name,
     cost_model_rate_type,
-    sum(distributed_cost) * latest.ratio as distributed_cost, -- the only cost inserted in this statement
-    latest.labels as pod_labels,
+    sum(distributed_cost * latest.ratio) as distributed_cost, -- the only cost inserted in this statement
+    pod_labels,
     max(raw_currency) as raw_currency,
     array_agg(DISTINCT resource_id) as resource_ids,
-    min(usage_start) as usage_start,
-    max(usage_start) as usage_end,
+    min(latest.usage_start) as usage_start,
+    max(latest.usage_start) as usage_end,
     {{source_uuid}}::uuid as source_uuid
-FROM org1234567_mskarbek.reporting_ocpusagelineitem_daily_summary as ocp
+FROM {{schema | sqlsafe}}.reporting_ocpusagelineitem_daily_summary as ocp
 JOIN cte_latest_resources as latest
-    ON latest.node_name = ocp.node AND latest.namespace_name = ocp.namespace
-WHERE usage_start >= {{start_date}}::date
-    AND usage_start <= {{end_date}}::date
-    AND source_uuid = {{source_uuid}}
+    ON latest.node_name = ocp.node
+    AND latest.namespace_name = ocp.namespace
+    AND latest.usage_start = ocp.usage_start
+WHERE source_uuid = {{source_uuid}}
     AND distributed_cost != 0
-GROUP BY cluster_alias, cluster_id, namespace, latest.node_name, latest.ratio, vm_name, latest.labels, cost_model_rate_type
+GROUP BY cluster_alias, cluster_id, namespace, node, vm_name, pod_labels, cost_model_rate_type
+
 ;
