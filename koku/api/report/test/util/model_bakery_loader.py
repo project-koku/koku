45492b2a403c75938cc319a5854e3a1e228cbd09
myersCody <cmyers@redhat.com>
2025-02-26 12:04:15 -0500

    Add more mocks to the bakery loader for race fix sql

--- koku/api/report/test/util/model_bakery_loader.py
+++ koku/api/report/test/util/model_bakery_loader.py
@@ -423,7 +423,12 @@ class ModelBakeryDataLoader(DataLoader):
                         )
 
         report_period_ids = [report_period.id for report_period in report_periods]
-        with patch("masu.database.ocp_report_db_accessor.OCPReportDBAccessor._execute_trino_multipart_sql_query"):
+        with patch(
+            "masu.database.ocp_report_db_accessor.OCPReportDBAccessor._execute_trino_multipart_sql_query"
+        ), patch(
+            "masu.database.ocp_report_db_accessor.OCPReportDBAccessor._execute_trino_raw_sql_query_with_description"
+        ) as mock_description_sql:
+            mock_description_sql.return_value = ([], [])
             with OCPReportDBAccessor(self.schema) as accessor:
                 accessor.populate_unit_test_tag_data(report_period_ids, self.first_start_date, self.last_end_date)
                 update_cost_category(self.schema)
