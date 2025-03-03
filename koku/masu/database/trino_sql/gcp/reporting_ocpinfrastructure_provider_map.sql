df5bb7d0502040b9246a8478e0004286330d95cd
Luke Couzens <lcouzens@redhat.com>
2025-03-03 12:48:24 +0000

    union already matched clusters

--- koku/masu/database/trino_sql/gcp/reporting_ocpinfrastructure_provider_map.sql
+++ koku/masu/database/trino_sql/gcp/reporting_ocpinfrastructure_provider_map.sql
@@ -44,6 +44,7 @@
             AND gcp.month = {{month}}
     ),
     cte_ocp_nodes AS (
+        {% if ocp_provider_uuid %}
         SELECT DISTINCT ocp.node,
             ocp.source
         FROM hive.{{schema | sqlsafe}}.openshift_pod_usage_line_items_daily AS ocp
@@ -51,11 +52,24 @@
             AND ocp.interval_start < date_add('day', 1, {{end_date}})
             AND ocp.node IS NOT NULL
             AND ocp.node != ''
-            {% if ocp_provider_uuid %}
             AND ocp.source = {{ocp_provider_uuid}}
-            {% endif %}
             AND ocp.year = {{year}}
             AND ocp.month = {{month}}
+        {% else %}
+        SELECT DISTINCT ocp.node,
+            ocp.source
+        FROM hive.{{schema | sqlsafe}}.openshift_pod_usage_line_items_daily AS ocp
+        INNER JOIN postgres.public.api_provider as provider
+            on ocp.source = cast(provider.uuid as varchar)
+        WHERE ocp.interval_start >= {{start_date}}
+        AND ocp.interval_start < date_add('day', 1, {{end_date}})
+        AND ocp.node IS NOT NULL
+            AND ocp.node != ''
+        AND ocp.year = {{year}}
+        AND ocp.month = {{month}}
+        AND provider.type = 'OCP'
+        and provider.infrastructure_id IS NULL
+        {% endif %}
     )
     SELECT DISTINCT ocp.source as ocp_uuid,
         gcp.source as infra_uuid,
@@ -65,4 +79,16 @@
         ON strpos(gcp.resource_name, ocp.node) != 0
     JOIN postgres.{{schema | sqlsafe}}.reporting_tenant_api_provider as api_provider
         ON gcp.source = cast(api_provider.uuid as varchar)
+
+    {% if gcp_provider_uuid %}
+    UNION
+
+    SELECT CAST(uuid AS varchar),
+        {{gcp_provider_uuid}},
+        infra_uuid.infrastructure_type
+    FROM postgres.public.api_provider AS provider_union
+    JOIN postgres.public.api_providerinfrastructuremap AS infra_uuid
+        ON provider_union.infrastructure_id = infra_uuid.id
+    WHERE CAST(infrastructure_provider_id AS varchar) = {{gcp_provider_uuid}}
+    {% endif %}
 {% endif %}
--- koku/masu/processor/ocp/ocp_cloud_updater_base.py
+++ koku/masu/processor/ocp/ocp_cloud_updater_base.py
@@ -157,19 +157,3 @@ class OCPCloudUpdaterBase:
         openshift_provider_uuids = list(infra_map)
         infra_provider_uuids = [value[0] for value in infra_map.values()]
         return openshift_provider_uuids, infra_provider_uuids
-
-    def get_ocp_provider_uuids_wo_infra_map(self):
-        """Fetch list of OCP providers for the current customer which have no infrastructure mapping
-
-        Returns:
-            unmapped_ocp_provider_uuids list[uuids]: A list of uuids without mappings
-                of the form [OpenShift Provider UUID,]
-
-        """
-        unmapped_ocp_provider_uuids = []
-        ps = Provider.objects.filter(
-            infrastructure=None, type=Provider.PROVIDER_OCP, customer_id=self._provider.customer_id
-        )
-        for provider in ps:
-            unmapped_ocp_provider_uuids.append(str(provider.uuid))
-        return unmapped_ocp_provider_uuids
