161049924621960ba4e9e74002e47d6ff817db09
Lucas Bacciotti <lbacciot@redhat.com>
2025-04-03 09:51:02 +0100

    fixing SQL to distribute cost over the cluster occurrences

--- koku/masu/database/trino_sql/openshift/cost_model/hourly_cost_cluster.sql
+++ koku/masu/database/trino_sql/openshift/cost_model/hourly_cost_cluster.sql
@@ -45,39 +45,66 @@ cte_cluster_usage_hours AS (
         ON pod_usage.namespace = cte_clusters.namespace
     WHERE source = {{source_uuid}}
     GROUP BY cte_clusters.cluster_id, cte_clusters.cluster_alias, DATE(interval_start)
+),
+
+-- Get the number of occurrences per cluster per day.
+cluster_usage_count AS (
+    SELECT
+        cluster_id,
+        usage_start,
+        COUNT(*) AS usage_line_count
+    FROM postgres.{{schema | sqlsafe}}.reporting_ocpusagelineitem_daily_summary
+    WHERE usage_start BETWEEN DATE({{start_date}}) AND DATE({{end_date}})
+        AND report_period_id = {{report_period_id}}
+        AND data_source = 'Pod'
+        AND monthly_cost_type IS NULL
+        AND pod_usage_cpu_core_hours IS NOT NULL
+        AND pod_request_cpu_core_hours IS NOT NULL
+        AND pod_request_cpu_core_hours != 0
+    GROUP BY cluster_id, usage_start
 )
 
 SELECT uuid(),
     max(report_period_id) AS report_period_id,
     lids.cluster_id,
     max(clusterhrs.cluster_alias) AS cluster_alias,
-    'Pod' AS data_source,
-    usage_start,
+    data_source,
+    lids.usage_start,
     max(usage_end) AS usage_end,
-    NULL AS namespace,
-    NULL AS node,
-    NULL AS resource_id,
-    NULL AS pod_labels,
-    NULL AS all_labels,
+    lids.namespace,
+    node,
+    max(resource_id) AS resource_id,
+    pod_labels,
+    all_labels,
     source_uuid,
     {{rate_type}} AS cost_model_rate_type,
-    max(clusterhrs.cluster_interval_hours) * CAST({{cluster_cost_per_hour}} AS DECIMAL(33, 15)) AS cost_model_cpu_cost,
+    (max(clusterhrs.cluster_interval_hours) * CAST({{cluster_cost_per_hour}} AS DECIMAL(33, 15))) / max(usage_count.usage_line_count) AS cost_model_cpu_cost,
     0 AS cost_model_memory_cost,
     0 AS cost_model_volume_cost,
-    NULL AS cost_category_id
+    cost_category_id
 FROM postgres.{{schema | sqlsafe}}.reporting_ocpusagelineitem_daily_summary AS lids
 JOIN cte_cluster_usage_hours AS clusterhrs
     ON lids.cluster_id = clusterhrs.cluster_id
     AND clusterhrs.interval_day = lids.usage_start
-WHERE usage_start BETWEEN DATE({{start_date}}) AND DATE({{end_date}})
+JOIN cluster_usage_count AS usage_count
+    ON lids.cluster_id = usage_count.cluster_id
+    AND lids.usage_start = usage_count.usage_start
+WHERE lids.usage_start BETWEEN DATE({{start_date}}) AND DATE({{end_date}})
     AND report_period_id = {{report_period_id}}
     AND data_source = 'Pod'
     AND lids.monthly_cost_type IS NULL
     AND pod_usage_cpu_core_hours IS NOT NULL
     AND pod_request_cpu_core_hours IS NOT NULL
     AND pod_request_cpu_core_hours != 0
-GROUP BY
-    lids.cluster_id,
-    usage_start,
-    source_uuid
+GROUP BY lids.usage_start,
+         usage_end,
+         source_uuid,
+         lids.cluster_id,
+         clusterhrs.cluster_alias,
+         node,
+         lids.namespace,
+         data_source,
+         cost_category_id,
+         pod_labels,
+         all_labels
 ;
