9b83d0f0b30f07f4950d882c70a32bee8842d5a5
Lucas Bacciotti <lbacciot@redhat.com>
2025-04-04 10:21:04 +0100

    Update SQL (simplification)

--- koku/masu/database/trino_sql/openshift/cost_model/hourly_cost_cluster.sql
+++ koku/masu/database/trino_sql/openshift/cost_model/hourly_cost_cluster.sql
@@ -19,55 +19,22 @@ INSERT INTO postgres.{{schema | sqlsafe}}.reporting_ocpusagelineitem_daily_summa
     cost_category_id
 )
 
--- Get clusters from daily summary
-WITH cte_clusters AS (
+WITH cte_cluster_usage_hours AS (
     SELECT
-        DISTINCT cluster_id,
-        cluster_alias,
-        namespace
-    FROM postgres.{{schema | sqlsafe}}.reporting_ocpusagelineitem_daily_summary AS ocp
-    WHERE usage_start >= DATE({{start_date}})
-        AND usage_start <= DATE({{end_date}})
-        AND source_uuid = CAST({{source_uuid}} AS uuid)
-        AND pod_request_cpu_core_hours IS NOT NULL
-        AND pod_request_cpu_core_hours != 0
-),
-
--- Get number of hours for each cluster
-cte_cluster_usage_hours AS (
-    SELECT
-        cte_clusters.cluster_id AS cluster_id,
-        cte_clusters.cluster_alias AS cluster_alias,
+        CAST(source AS VARCHAR) AS source,
         DATE(interval_start) AS interval_day,
-        COUNT(DISTINCT EXTRACT(hour FROM pod_usage.interval_start)) AS cluster_interval_hours
-    FROM hive.{{schema | sqlsafe}}.openshift_pod_usage_line_items pod_usage
-    INNER JOIN cte_clusters
-        ON pod_usage.namespace = cte_clusters.namespace
-    WHERE source = {{source_uuid}}
-    GROUP BY cte_clusters.cluster_id, cte_clusters.cluster_alias, DATE(interval_start)
-),
-
--- Get the number of occurrences per cluster per day.
-cluster_usage_count AS (
-    SELECT
-        cluster_id,
-        usage_start,
-        COUNT(*) AS usage_line_count
-    FROM postgres.{{schema | sqlsafe}}.reporting_ocpusagelineitem_daily_summary
-    WHERE usage_start BETWEEN DATE({{start_date}}) AND DATE({{end_date}})
-        AND report_period_id = {{report_period_id}}
-        AND data_source = 'Pod'
-        AND monthly_cost_type IS NULL
-        AND pod_usage_cpu_core_hours IS NOT NULL
-        AND pod_request_cpu_core_hours IS NOT NULL
-        AND pod_request_cpu_core_hours != 0
-    GROUP BY cluster_id, usage_start
+        COUNT(DISTINCT interval_start) AS cluster_hours
+    FROM hive.{{schema | sqlsafe}}.openshift_pod_usage_line_items
+    WHERE source = {{source_uuid}} AND
+          YEAR(cast(interval_start as date)) = YEAR(cast({{start_date}} as date)) AND
+          MONTH(cast(interval_start as date)) = MONTH(cast({{start_date}} as date))
+    GROUP BY source, DATE(interval_start)
 )
 
 SELECT uuid(),
     max(report_period_id) AS report_period_id,
     lids.cluster_id,
-    max(clusterhrs.cluster_alias) AS cluster_alias,
+    lids.cluster_alias,
     data_source,
     lids.usage_start,
     max(usage_end) AS usage_end,
@@ -78,17 +45,14 @@ SELECT uuid(),
     all_labels,
     source_uuid,
     {{rate_type}} AS cost_model_rate_type,
-    (max(clusterhrs.cluster_interval_hours) * CAST({{cluster_cost_per_hour}} AS DECIMAL(33, 15))) / max(usage_count.usage_line_count) AS cost_model_cpu_cost,
+    (max(clusterhrs.cluster_hours) * CAST({{cluster_cost_per_hour}} AS DECIMAL(33, 15))) AS cost_model_cpu_cost,
     0 AS cost_model_memory_cost,
     0 AS cost_model_volume_cost,
     cost_category_id
 FROM postgres.{{schema | sqlsafe}}.reporting_ocpusagelineitem_daily_summary AS lids
 JOIN cte_cluster_usage_hours AS clusterhrs
-    ON lids.cluster_id = clusterhrs.cluster_id
+    ON CAST(lids.source_uuid AS VARCHAR) = clusterhrs.source
     AND clusterhrs.interval_day = lids.usage_start
-JOIN cluster_usage_count AS usage_count
-    ON lids.cluster_id = usage_count.cluster_id
-    AND lids.usage_start = usage_count.usage_start
 WHERE lids.usage_start BETWEEN DATE({{start_date}}) AND DATE({{end_date}})
     AND report_period_id = {{report_period_id}}
     AND data_source = 'Pod'
@@ -100,7 +64,7 @@ GROUP BY lids.usage_start,
          usage_end,
          source_uuid,
          lids.cluster_id,
-         clusterhrs.cluster_alias,
+         lids.cluster_alias,
          node,
          lids.namespace,
          data_source,
