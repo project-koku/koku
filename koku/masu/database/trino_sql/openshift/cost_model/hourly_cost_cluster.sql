73451558f8c69fa726481836a00525f10649f253
Lucas Bacciotti <lbacciot@redhat.com>
2025-04-07 16:24:23 +0100

    Updating SQL piece.

--- koku/masu/database/trino_sql/openshift/cost_model/hourly_cost_cluster.sql
+++ koku/masu/database/trino_sql/openshift/cost_model/hourly_cost_cluster.sql
@@ -21,55 +21,53 @@ INSERT INTO postgres.{{schema | sqlsafe}}.reporting_ocpusagelineitem_daily_summa
 
 WITH cte_cluster_usage_hours AS (
     SELECT
-        CAST(source AS VARCHAR) AS source,
         DATE(interval_start) AS interval_day,
         COUNT(DISTINCT interval_start) AS cluster_hours
     FROM hive.{{schema | sqlsafe}}.openshift_pod_usage_line_items
-    WHERE source = {{source_uuid}} AND
-          YEAR(cast(interval_start as date)) = YEAR(cast({{start_date}} as date)) AND
-          MONTH(cast(interval_start as date)) = MONTH(cast({{start_date}} as date))
-    GROUP BY source, DATE(interval_start)
+    WHERE source = {{source_uuid}}
+        AND year = {{year}}
+        AND month = {{month}}
+    GROUP BY DATE(interval_start)
 )
 
 SELECT uuid(),
-    max(report_period_id) AS report_period_id,
+    {{report_period_id}} as report_period_id,
     lids.cluster_id,
     lids.cluster_alias,
-    data_source,
+    lids.data_source,
     lids.usage_start,
-    max(usage_end) AS usage_end,
+    max(lids.usage_end) AS usage_end,
     lids.namespace,
-    node,
-    max(resource_id) AS resource_id,
-    pod_labels,
-    all_labels,
-    source_uuid,
+    lids.node,
+    max(lids.resource_id) AS resource_id,
+    lids.pod_labels,
+    lids.all_labels,
+    lids.source_uuid,
     {{rate_type}} AS cost_model_rate_type,
     (max(clusterhrs.cluster_hours) * CAST({{cluster_cost_per_hour}} AS DECIMAL(33, 15))) AS cost_model_cpu_cost,
     0 AS cost_model_memory_cost,
     0 AS cost_model_volume_cost,
-    cost_category_id
+    lids.cost_category_id
 FROM postgres.{{schema | sqlsafe}}.reporting_ocpusagelineitem_daily_summary AS lids
 JOIN cte_cluster_usage_hours AS clusterhrs
-    ON CAST(lids.source_uuid AS VARCHAR) = clusterhrs.source
+    ON cast({{source_uuid}} as uuid) = lids.source_uuid
     AND clusterhrs.interval_day = lids.usage_start
-WHERE lids.usage_start BETWEEN DATE({{start_date}}) AND DATE({{end_date}})
-    AND report_period_id = {{report_period_id}}
-    AND data_source = 'Pod'
+WHERE lids.usage_start >= date({{start_date}}) AND lids.usage_start <= date({{end_date}})
+    AND lids.report_period_id = {{report_period_id}}
+    AND lids.data_source = 'Pod'
     AND lids.monthly_cost_type IS NULL
-    AND pod_usage_cpu_core_hours IS NOT NULL
-    AND pod_request_cpu_core_hours IS NOT NULL
-    AND pod_request_cpu_core_hours != 0
+    AND lids.pod_usage_cpu_core_hours IS NOT NULL
+    AND lids.pod_request_cpu_core_hours IS NOT NULL
+    AND lids.pod_request_cpu_core_hours != 0
 GROUP BY lids.usage_start,
-         usage_end,
-         source_uuid,
+         lids.usage_end,
+         lids.source_uuid,
          lids.cluster_id,
          lids.cluster_alias,
-         node,
+         lids.node,
          lids.namespace,
-         data_source,
-         cost_category_id,
-         pod_labels,
-         all_labels
- LIMIT 1
+         lids.data_source,
+         lids.cost_category_id,
+         lids.pod_labels,
+         lids.all_labels
 ;
