275595024271b05fe10cd40bd059a41141218b5a
Lucas Bacciotti <lbacciot@redhat.com>
2025-04-16 14:18:02 +0100

    feat: add pod_cost calculation and new cost model notebook

--- koku/masu/database/sql/openshift/all/reporting_ocpallcostlineitem_project_daily_summary_aws.sql
+++ koku/masu/database/sql/openshift/all/reporting_ocpallcostlineitem_project_daily_summary_aws.sql
@@ -32,6 +32,7 @@ INSERT
            unit,
            unblended_cost,
            project_markup_cost,
+           pod_cost,
            currency_code,
            cost_category_id,
            source_uuid
@@ -58,6 +59,7 @@ SELECT 'AWS' as source_type,
       --  OCP on ALL tables should use calculated_amortized_cost
        sum(calculated_amortized_cost) as unblended_cost,
        sum(markup_cost_amortized) as project_markup_cost,
+       sum(pod_cost) as pod_cost,
        max(currency_code) as currency_code,
        max(cost_category_id) as cost_category_id,
        {{source_uuid}}::uuid as source_uuid
