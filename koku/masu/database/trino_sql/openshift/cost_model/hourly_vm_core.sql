caca7fa92149b3af82499e417921bb6a25414089
myersCody <cmyers@redhat.com>
2025-06-12 12:16:37 -0400

    Combine labels during insert for vm hour metric

--- koku/masu/database/trino_sql/openshift/cost_model/hourly_vm_core.sql
+++ koku/masu/database/trino_sql/openshift/cost_model/hourly_vm_core.sql
@@ -23,7 +23,7 @@ WITH
             max(interval_start) AS max_interval_start
         FROM hive.{{schema | sqlsafe}}.openshift_vm_usage_line_items
         WHERE
-            source = {{source_uuid}}
+            source = {{source_uuid | string}}
             AND year = {{year}}
             AND month = {{month}}
         GROUP BY
@@ -39,7 +39,7 @@ WITH
             ON node_info.vm_name = vmi.vm_name
             AND node_info.interval_start = vmi.max_interval_start
         WHERE
-           node_info.source = {{source_uuid}}
+           node_info.source = {{source_uuid | string}}
             AND node_info.year = {{year}}
             AND node_info.month = {{month}}
     ),
@@ -56,6 +56,34 @@ WITH
             AND vm_map.month = {{month}}
         GROUP BY
             1, 2, vm_map.vm_name
+    ),
+    latest_vm_labels AS (
+        SELECT
+            json_extract_scalar(lids.pod_labels, '$.vm_kubevirt_io_name') AS vm_name,
+            CAST(
+                REDUCE(
+                    ARRAY_AGG(
+                        map_concat(
+                            COALESCE(CAST(lids.pod_labels AS map(varchar, varchar)), MAP()),
+                            COALESCE(CAST(lids.all_labels AS map(varchar, varchar)), MAP())
+                        )
+                    ),
+                    MAP(),
+                    (s, x) -> map_concat(s, x),
+                    s -> s
+                )
+            AS JSON) AS combined_labels
+        FROM
+            postgres.{{schema | sqlsafe}}.reporting_ocpusagelineitem_daily_summary AS lids
+        JOIN vm_max_interval as max
+            ON json_extract_scalar(lids.pod_labels, '$.vm_kubevirt_io_name') = max.vm_name
+            AND DATE(lids.usage_start) = DATE(max.max_interval_start)
+        WHERE lids.report_period_id = {{report_period_id}}
+            AND lids.data_source = 'Pod'
+            AND lids.pod_usage_cpu_core_hours IS NOT NULL
+            AND lids.pod_request_cpu_core_hours IS NOT NULL
+            AND lids.monthly_cost_type IS NULL
+        GROUP BY json_extract_scalar(lids.pod_labels, '$.vm_kubevirt_io_name')
     )
 SELECT
     uuid(),
@@ -68,8 +96,8 @@ SELECT
     lids.namespace,
     max(latest.node_name) AS node,
     max(latest.resource_id) AS resource_id,
-    lids.pod_labels,
-    lids.all_labels,
+    labels.combined_labels as pod_labels,
+    labels.combined_labels as all_labels,
     lids.source_uuid,
     {{rate_type}} AS cost_model_rate_type,
     max(vm_usage.vm_interval_hours) * max(vm_usage.vm_cpu_cores) * CAST({{hourly_rate}} as DECIMAL(33, 15)) AS cost_model_cpu_cost,
@@ -83,6 +111,8 @@ JOIN
 JOIN
     latest_vm_node_info AS latest
     ON json_extract_scalar(lids.pod_labels, '$.vm_kubevirt_io_name') = latest.name_of_vm
+JOIN latest_vm_labels as labels
+    ON json_extract_scalar(lids.pod_labels, '$.vm_kubevirt_io_name') = labels.vm_name
 WHERE usage_start >= DATE({{start_date}})
     AND usage_start <= DATE({{end_date}})
     AND report_period_id = {{report_period_id}}
@@ -99,6 +129,5 @@ GROUP BY
     lids.namespace,
     lids.data_source,
     lids.cost_category_id,
-    lids.pod_labels,
-    lids.all_labels
+    labels.combined_labels
 ;
