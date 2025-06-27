19f55862d971b997c9f19386ce74da68720d6203
Luke Couzens <lcouzens@redhat.com>
2025-06-27 17:36:42 +0100

    fix relatice delta

--- koku/masu/database/gcp_report_db_accessor.py
+++ koku/masu/database/gcp_report_db_accessor.py
@@ -655,8 +655,9 @@ class GCPReportDBAccessor(SQLScriptAtomicExecutorMixin, ReportDBAccessorBase):
         # GCP has crossover data meaning usage 2025-06-01 could be in 202505 partition
         # Our parquet table month partitions are based on invoice month not usage date
         # This means we need to account for crossover data during our matching logic
-        resource_matching_params["previous_month"] = DateHelper().bill_month_from_date(
-            sql_metadata.start_date.replace(month=sql_metadata.start_date.month - 1)
+        dh = DateHelper()
+        resource_matching_params["previous_month"] = dh.bill_month_from_date(
+            dh.relative_month_start(-1, sql_metadata.start_date)
         )
         LOG.info(log_json(msg="Resource matching for OCP on GCP flow", **resource_matching_params))
         self._execute_trino_multipart_sql_query(resource_matching_sql, bind_params=resource_matching_params)
