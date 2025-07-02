0d1129221518d9e8ce64235faa43242db0b7d4b1
maskarb <mskarbek@redhat.com>
2025-07-02 16:25:08 -0400

    revert file change

--- koku/masu/processor/__init__.py
+++ koku/masu/processor/__init__.py
@@ -106,16 +106,10 @@ def is_validation_enabled(account):  # pragma: no cover
 
 def is_managed_ocp_cloud_summary_enabled(account, provider_type):
     context = {"provider_type": provider_type}
-    if UNLEASH_CLIENT.is_enabled(
-        "cost-management.backend.feature-cost-5129-provider-type", context, fallback_function=fallback_development_true
-    ):
+    if UNLEASH_CLIENT.is_enabled("cost-management.backend.feature-cost-5129-provider-type", context):
         account = convert_account(account)
         context = {"schema": account}
-        result = UNLEASH_CLIENT.is_enabled(
-            "cost-management.backend.feature-cost-5129-ocp-cloud-summary",
-            context,
-            fallback_function=fallback_development_true,
-        )
+        result = UNLEASH_CLIENT.is_enabled("cost-management.backend.feature-cost-5129-ocp-cloud-summary", context)
         LOG.info(log_json(msg=f"managed table summary enabled: {result}", schema=account, provider_type=provider_type))
         return result
     return False
