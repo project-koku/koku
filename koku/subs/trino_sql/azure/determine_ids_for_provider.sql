8c935e6e728a8e0a14fb98443165d80ec7e08486
lcouzens <lcouzens@redhat.com>
2025-07-03 19:30:01 +0100

    fix jinja

--- koku/subs/trino_sql/azure/determine_ids_for_provider.sql
+++ koku/subs/trino_sql/azure/determine_ids_for_provider.sql
@@ -11,5 +11,5 @@ WHERE source={{source_uuid | string}}
     SELECT
       DISTINCT usage_id
     FROM postgres.{{schema | sqlsafe}}.reporting_subs_id_map
-    WHERE source_uuid!={{source_uuid | uuid}}
+    WHERE source_uuid!= cast({{source_uuid}} as uuid)
   )
