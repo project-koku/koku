43bad77dc0daf211331939756895fb2ea78450ae
Pedro Lopez <plopezpe@redhat.com>
2025-07-10 13:00:23 +0200

    Revert "export MR image with fix"
    This reverts commit 7b48cd187eaba3aae2692062c5fd8fff611f8146.

--- ci/functions.sh
+++ ci/functions.sh
@@ -122,8 +122,6 @@ function run_smoke_tests_stage() {
     echo "Running E2E tests with IQE:"
     echo "IQE_MARKER_EXPRESSION: '$IQE_MARKER_EXPRESSION'"
     echo "IQE_FILTER_EXPRESSION: '$IQE_FILTER_EXPRESSION'"
-    export IQE_IMAGE_TAG="cost-management-2388-b1ad2c10"
-
 
     source $CICD_ROOT/cji_smoke_test.sh
 }
