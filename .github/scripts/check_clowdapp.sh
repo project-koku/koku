8dbb1f44df0e3814e09fd2d55d7fdf663bb4dbd7
maskarb <mskarbek@redhat.com>
2025-07-21 09:04:02 -0400

    fix string

--- .github/scripts/check_clowdapp.sh
+++ .github/scripts/check_clowdapp.sh
@@ -7,7 +7,7 @@ kustomize build ./deploy/kustomize > ./deploy/clowdapp.yaml
 changed=`git diff --name-only HEAD`
 
 if [[ $changed == *"clowdapp"* ]]; then
-  echo "clowdapp.yaml cannot be directly modified. Update the base.yaml or the patch files and run `make clowdapp`."
+  echo "clowdapp.yaml cannot be directly modified. Update the base.yaml or the patch files and run 'make clowdapp'."
   exit_code=1
 else
   echo "clowdapp.yaml is up to date."
