662c9f3403bba1977ace0d624225d0ae09697059
maskarb <mskarbek@redhat.com>
2025-07-21 09:00:40 -0400

    drop installation of kustomize since it is already available

--- .github/scripts/check_clowdapp.sh
+++ .github/scripts/check_clowdapp.sh
@@ -2,9 +2,7 @@
 
 set -e
 
-curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh" | bash
-
-./kustomize build ./deploy/kustomize > ./deploy/clowdapp.yaml
+kustomize build ./deploy/kustomize > ./deploy/clowdapp.yaml
 
 changed=`git diff --name-only HEAD`
 
@@ -15,7 +13,5 @@ else
   echo "clowdapp.yaml is up to date."
   exit_code=0
 fi
-
-rm -rf ./kustomize
 
 exit $exit_code
