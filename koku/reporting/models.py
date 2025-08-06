118896f3c280ba1615c6665fbf4b8394e16ff030
maskarb <mskarbek@redhat.com>
2025-08-06 17:10:02 -0400

    [COST-6546] trino deletion prep

--- koku/reporting/models.py
+++ koku/reporting/models.py
@@ -151,18 +151,6 @@ OCP_ON_AZURE_PERSPECTIVES = (
 # These are cleaned during source delete
 TRINO_MANAGED_TABLES = {
     "reporting_ocpusagelineitem_daily_summary": "source",
-    "reporting_ocpawscostlineitem_project_daily_summary": "ocp_source",  # COST-5881
-    "reporting_ocpawscostlineitem_project_daily_summary_temp": "ocp_source",  # COST-5881
-    "aws_openshift_daily_resource_matched_temp": "ocp_source",  # COST-5881
-    "aws_openshift_daily_tag_matched_temp": "ocp_source",  # COST-5881
-    "azure_openshift_daily_resource_matched_temp": "ocp_source",  # COST-5881
-    "azure_openshift_daily_tag_matched_temp": "ocp_source",  # COST-5881
-    "reporting_ocpazurecostlineitem_project_daily_summary": "ocp_source",  # COST-5881
-    "reporting_ocpazurecostlineitem_project_daily_summary_temp": "ocp_source",  # COST-5881
-    "reporting_ocpgcpcostlineitem_project_daily_summary": "ocp_source",  # COST-5881
-    "reporting_ocpgcpcostlineitem_project_daily_summary_temp": "ocp_source",  # COST-5881
-    "gcp_openshift_daily_resource_matched_temp": "ocp_source",  # COST-5881
-    "gcp_openshift_daily_tag_matched_temp": "ocp_source",  # COST-5881
     "managed_gcp_openshift_daily_temp": "ocp_source",
     "managed_reporting_ocpgcpcostlineitem_project_daily_summary_temp": "ocp_source",
     "managed_reporting_ocpgcpcostlineitem_project_daily_summary": "ocp_source",
@@ -179,28 +167,28 @@ TRINO_MANAGED_TABLES = {
 # These are cleaned during expired_data flow
 EXPIRE_MANAGED_TABLES = {
     "reporting_ocpusagelineitem_daily_summary": "source",
-    "reporting_ocpawscostlineitem_project_daily_summary": "ocp_source",
-    "reporting_ocpgcpcostlineitem_project_daily_summary": "ocp_source",
-    "reporting_ocpazurecostlineitem_project_daily_summary": "ocp_source",
+    "reporting_ocpawscostlineitem_project_daily_summary": "ocp_source",  # COST-5881
+    "reporting_ocpgcpcostlineitem_project_daily_summary": "ocp_source",  # COST-5881
+    "reporting_ocpazurecostlineitem_project_daily_summary": "ocp_source",  # COST-5881
 }
 
 # TEMP tables are cleaned during day to day processing
 OCP_ON_AWS_TEMP_MANAGED_TABLES = {
     "reporting_ocpawscostlineitem_project_daily_summary_temp",
-    "aws_openshift_daily_resource_matched_temp",
-    "aws_openshift_daily_tag_matched_temp",
+    "aws_openshift_daily_resource_matched_temp",  # COST-5881
+    "aws_openshift_daily_tag_matched_temp",  # COST-5881
     "aws_openshift_disk_capacities_temp",
 }
 
 OCP_ON_AZURE_TEMP_MANAGED_TABLES = {
     "reporting_ocpazurecostlineitem_project_daily_summary_temp",
-    "azure_openshift_daily_resource_matched_temp",
-    "azure_openshift_daily_tag_matched_temp",
+    "azure_openshift_daily_resource_matched_temp",  # COST-5881
+    "azure_openshift_daily_tag_matched_temp",  # COST-5881
     "azure_openshift_disk_capacities_temp",
 }
 
 OCP_ON_GCP_TEMP_MANAGED_TABLES = {
     "reporting_ocpgcpcostlineitem_project_daily_summary_temp",
-    "gcp_openshift_daily_resource_matched_temp",
-    "gcp_openshift_daily_tag_matched_temp",
+    "gcp_openshift_daily_resource_matched_temp",  # COST-5881
+    "gcp_openshift_daily_tag_matched_temp",  # COST-5881
 }
