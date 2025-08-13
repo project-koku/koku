f9dd3d409cf662be34da909e5c35e82220445dc0
lcouzens <lcouzens@redhat.com>
2025-08-13 10:42:31 +0100

    sql improvements

--- koku/masu/database/trino_sql/gcp/get_invoice_months_and_dates.sql
+++ koku/masu/database/trino_sql/gcp/get_invoice_months_and_dates.sql
@@ -1,7 +1,15 @@
 SELECT
     invoice_month, min(usage_start_time), max(usage_start_time)
     FROM hive.{{schema | sqlsafe}}.gcp_line_items_daily
-    WHERE usage_start_time > {{start_date}}
+    WHERE usage_start_time >= {{start_date}}
     AND usage_start_time <= {{end_date}}
     AND source = {{source_uuid}}
+    AND (
+        month = LPAD(CAST(EXTRACT(MONTH FROM DATE({{start_date}})) AS VARCHAR), 2, '0')
+        OR month = LPAD(CAST(EXTRACT(MONTH FROM DATE({{end_date}}) - INTERVAL '1' MONTH) AS VARCHAR), 2, '0')
+    )
+    AND (
+        year = CAST(EXTRACT(YEAR FROM DATE({{start_date}})) AS VARCHAR)
+        OR YEAR = CAST(EXTRACT(YEAR FROM DATE({{end_date}})) AS VARCHAR)
+    )
     group by invoice_month
--- koku/masu/database/trino_sql/gcp/openshift/populate_daily_summary/1_resource_matching_by_cluster.sql
+++ koku/masu/database/trino_sql/gcp/openshift/populate_daily_summary/1_resource_matching_by_cluster.sql
@@ -47,6 +47,14 @@ WITH cte_usage_date_partitions as (
     where usage_start_time >= {{start_date}}
     AND usage_start_time <= {{end_date}}
     AND source = {{cloud_provider_uuid}}
+    AND (
+        month = LPAD(CAST(EXTRACT(MONTH FROM DATE({{start_date}})) AS VARCHAR), 2, '0')
+        OR month = LPAD(CAST(EXTRACT(MONTH FROM DATE({{end_date}}) - INTERVAL '1' MONTH) AS VARCHAR), 2, '0')
+    )
+    AND (
+        year = CAST(EXTRACT(YEAR FROM DATE({{start_date}})) AS VARCHAR)
+        OR YEAR = CAST(EXTRACT(YEAR FROM DATE({{end_date}})) AS VARCHAR)
+    )
     group by year, month
 ),
 cte_gcp_resource_names AS (
