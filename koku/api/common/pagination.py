34241ccf34dcefe1fee0015af4b2ccee03c150d6
maskarb <mskarbek@redhat.com>
2025-10-01 11:15:10 -0400

    create default_limit in init

--- koku/api/common/pagination.py
+++ koku/api/common/pagination.py
@@ -194,8 +194,6 @@ class ReportPagination(StandardResultsSetPagination):
 class ForecastListPaginator(ListPaginator):
     """A paginator that applies a default limit based on days in month."""
 
-    default_limit = DateHelper().this_month_end.day
-
     def __init__(self, data_set, request, cost_type=None):
         """Initialize the paginator."""
         self.cost_type = cost_type
@@ -204,6 +202,8 @@ class ForecastListPaginator(ListPaginator):
         self.count = len(data_set)
         self.limit = self.get_limit(self.request)
         self.offset = self.get_offset(self.request)
+
+        self.default_limit = DateHelper().this_month_end.day
 
     def get_paginated_response(self, data):
         """Override pagination output."""
