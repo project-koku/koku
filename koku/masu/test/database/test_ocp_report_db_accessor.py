5d16ee35aa29bbc623deb4cd233b39b11e4a3df3
Lucas Bacciotti <lbacciot@redhat.com>
2025-11-25 08:48:42 +0000

    fix: remove GPU cost preservation filter

--- koku/masu/test/database/test_ocp_report_db_accessor.py
+++ koku/masu/test/database/test_ocp_report_db_accessor.py
@@ -926,7 +926,6 @@ class OCPReportDBAccessorTest(MasuTestCase):
                     report_period_id=report_period_id,
                 )
                 .exclude(cost_model_rate_type="platform_distributed")
-                .exclude(data_source="GPU")
                 .count()
             )
             # the distributed cost is being added so the initial count is no longer zero.
@@ -939,14 +938,10 @@ class OCPReportDBAccessorTest(MasuTestCase):
                 provider_uuid, report_period_id, start_date, end_date
             )
 
-            new_non_raw_count = (
-                OCPUsageLineItemDailySummary.objects.filter(
-                    Q(infrastructure_raw_cost__isnull=True) | Q(infrastructure_raw_cost=0),
-                    report_period_id=report_period_id,
-                )
-                .exclude(data_source="GPU")
-                .count()
-            )
+            new_non_raw_count = OCPUsageLineItemDailySummary.objects.filter(
+                Q(infrastructure_raw_cost__isnull=True) | Q(infrastructure_raw_cost=0),
+                report_period_id=report_period_id,
+            ).count()
             new_raw_count = OCPUsageLineItemDailySummary.objects.filter(
                 Q(infrastructure_raw_cost__isnull=False) & ~Q(infrastructure_raw_cost=0),
                 report_period_id=report_period_id,
@@ -961,14 +956,10 @@ class OCPReportDBAccessorTest(MasuTestCase):
             report_period = acc.report_periods_for_provider_uuid(provider_uuid, start_date)
 
             report_period_id = report_period.id
-            initial_non_raw_count = (
-                OCPUsageLineItemDailySummary.objects.filter(
-                    Q(infrastructure_raw_cost__isnull=True) | Q(infrastructure_raw_cost=0),
-                    report_period_id=report_period_id,
-                )
-                .exclude(data_source="GPU")
-                .count()
-            )
+            initial_non_raw_count = OCPUsageLineItemDailySummary.objects.filter(
+                Q(infrastructure_raw_cost__isnull=True) | Q(infrastructure_raw_cost=0),
+                report_period_id=report_period_id,
+            ).count()
             initial_raw_count = OCPUsageLineItemDailySummary.objects.filter(
                 Q(infrastructure_raw_cost__isnull=False) & ~Q(infrastructure_raw_cost=0),
                 report_period_id=report_period_id,
@@ -978,14 +969,10 @@ class OCPReportDBAccessorTest(MasuTestCase):
                 provider_uuid, report_period_id, start_date, end_date
             )
 
-            new_non_raw_count = (
-                OCPUsageLineItemDailySummary.objects.filter(
-                    Q(infrastructure_raw_cost__isnull=True) | Q(infrastructure_raw_cost=0),
-                    report_period_id=report_period_id,
-                )
-                .exclude(data_source="GPU")
-                .count()
-            )
+            new_non_raw_count = OCPUsageLineItemDailySummary.objects.filter(
+                Q(infrastructure_raw_cost__isnull=True) | Q(infrastructure_raw_cost=0),
+                report_period_id=report_period_id,
+            ).count()
             new_raw_count = OCPUsageLineItemDailySummary.objects.filter(
                 Q(infrastructure_raw_cost__isnull=False) & ~Q(infrastructure_raw_cost=0),
                 report_period_id=report_period_id,
