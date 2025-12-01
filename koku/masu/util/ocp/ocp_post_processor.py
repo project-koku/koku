037a397e30f4bd9ffc515482521f7588abeec4d1
lcouzens <lcouzens@redhat.com>
2025-12-01 20:30:37 +0000

    lint

--- koku/masu/util/ocp/ocp_post_processor.py
+++ koku/masu/util/ocp/ocp_post_processor.py
@@ -198,9 +198,7 @@ class OCPPostProcessor:
                 label_key_set.update(json.loads(label).keys())
         gpu_column = "gpu_vendor_name"
         if gpu_column in data_frame.columns:
-            data_frame[gpu_column] = data_frame[gpu_column].replace(
-                "nvidia_com_gpu", "nvidia"
-            )
+            data_frame[gpu_column] = data_frame[gpu_column].replace("nvidia_com_gpu", "nvidia")
         self.enabled_tag_keys.update(label_key_set)
         return data_frame, self._generate_daily_data(data_frame)
 
