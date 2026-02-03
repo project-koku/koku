c14c4ed4a6c1745215aba7783058d32ffc29d501
dnakabaa <dnakabaa@redhat.com>
2026-02-03 12:52:37 -0500

    add logging

--- koku/api/common/permissions/ingress_access.py
+++ koku/api/common/permissions/ingress_access.py
@@ -3,18 +3,23 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 """Defines the Ingress Access Permissions class."""
+import logging
+
+from api.common import log_json
 from api.common.permissions.settings_access import SettingsAccessPermission
 from masu.processor import is_ingress_rbac_grace_period_enabled
+
+LOG = logging.getLogger(__name__)
 
 
 class IngressAccessPermission(SettingsAccessPermission):
     """Determines if a user can access ingress data."""
 
     def has_permission(self, request, view):
-        # bypasses RBAC checks if ingress RBAC grace period is enabled
+        """Check if the user has permission to access ingress data."""
         customer = getattr(request.user, "customer", None)
         if customer and is_ingress_rbac_grace_period_enabled(customer.schema_name):
+            LOG.info(log_json(msg="Ingress RBAC grace period is enabled for customer", schema=customer.schema_name))
             return True
 
-        # otherwise check settings access permissions
         return super().has_permission(request, view)
