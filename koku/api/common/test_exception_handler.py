8f3050a423528e5233644b0ca8ee637989169731
Eva Sebestova <esebesto@redhat.com>
2026-02-23 11:13:09 +0100

    DO NOT MERGE: testing pr

--- koku/api/common/test_exception_handler.py
+++ koku/api/common/test_exception_handler.py
@@ -15,6 +15,7 @@ class ExceptionHanlderTest(TestCase):
         """Test generating errors from dictionary errors."""
         kwargs = {"status_code": 400}
         response = {"non_field_errors": ["Cannot access AWS bucket with ARN", "ARN format is incorrect"]}
+        # fake line
         formatted_errors = _generate_errors_from_dict(response, **kwargs)
         expected = [
             {"detail": "Cannot access AWS bucket with ARN", "source": "non_field_errors", "status": 400},
