3ea9878e04cb6c4d653f1a01fcc093bad71d7170
myersCody <cmyers@redhat.com>
2026-03-17 13:55:08 -0400

    Update data processing to get mig slices from profile

--- koku/masu/util/ocp/common.py
+++ koku/masu/util/ocp/common.py
@@ -255,19 +255,16 @@ GPU_USAGE_COLUMNS = {
 }
 
 # MIG (Multi-Instance GPU) columns - optional, may not be present in older operator versions
-# Note: mig_memory_capacity_mib is derived in post-processor from mig_profile
-# so it's not expected from the operator
+# Note: mig_slice_count, mig_memory_capacity_mib, and parent_gpu_max_slices are derived
+# in post-processor from mig_profile and gpu_model_name, not from the operator
 GPU_USAGE_NEWV_COLUMNS_AND_TYPES = {
     "mig_instance_id": pd.StringDtype(storage="pyarrow"),
     "mig_profile": pd.StringDtype(storage="pyarrow"),
-    "mig_slice_count": pd.Int64Dtype(),
     "mig_strategy": pd.StringDtype(storage="pyarrow"),  # "single" or "mixed" or Null
-    # This is derived in post-processor, not from operator:
+    # These are derived in post-processor, not from operator:
+    "mig_slice_count": pd.Int64Dtype(),
     "mig_memory_capacity_mib": pd.Int64Dtype(),
     "gpu_max_slices": pd.Int64Dtype(),
-    # "gpu_max_slices" may be included in the operator gpu report,
-    # but we won't use it for now just statically calculated for now.
-    # To optionally include it may be a follow up.
 }
 
 GPU_GROUP_BY = ["node", "namespace", "pod", "gpu_uuid", "mig_instance_id"]
